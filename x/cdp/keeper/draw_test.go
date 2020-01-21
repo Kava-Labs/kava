c4c1627dc65744f86737dc2704d40e7df6d74011
Kevin Davis <kjydavis3@gmail.com>
2020-01-21 17:02:59 +0000

    test: remove payment > balance check

--- x/cdp/keeper/draw_test.go
+++ x/cdp/keeper/draw_test.go
@@ -88,8 +88,6 @@ func (suite *DrawTestSuite) TestAddRepayPrincipal() {
 	err = suite.keeper.AddPrincipal(suite.ctx, suite.addrs[0], "xrp", cs(c("usdx", 311000000)))
 	suite.Equal(types.CodeInvalidCollateralRatio, err.Result().Code)
 
-	err = suite.keeper.RepayPrincipal(suite.ctx, suite.addrs[0], "xrp", cs(c("usdx", 30000000)))
-	suite.Equal(types.CodePaymentExceedsDebt, err.Result().Code)
 	err = suite.keeper.RepayPrincipal(suite.ctx, suite.addrs[0], "xrp", cs(c("usdx", 10000000)))
 	suite.NoError(err)
 
