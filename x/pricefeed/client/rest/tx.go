5984a4c5886bb83c5c06f808784309d29599957a
denalimarsh <denalimarsh@gmail.com>
2020-01-31 15:51:56 -0800

    tx postprice implemented - /pricefeed/postprice/{MsgPostPrice}

--- x/pricefeed/client/rest/tx.go
+++ x/pricefeed/client/rest/tx.go
@@ -5,21 +5,24 @@ import (
 	"net/http"
 	"time"
 
-	"github.com/gorilla/mux"
-
 	"github.com/cosmos/cosmos-sdk/client/context"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
 	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
-	"github.com/kava-labs/kava/x/pricefeed/types"
+	"github.com/gorilla/mux"
+
+	// 	authclient "github.com/cosmos/cosmos-sdk/x/auth/client" TODO: use instead of utils?
 	tmtime "github.com/tendermint/tendermint/types/time"
+
+	"github.com/kava-labs/kava/x/pricefeed/types"
 )
 
 func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
-	r.HandleFunc(fmt.Sprintf("/%s/rawprices", types.ModuleName), postPriceHandler(cliCtx)).Methods("PUT")
+	r.HandleFunc(fmt.Sprintf("/%s/postprice", types.ModuleName), postPriceHandlerFn(cliCtx)).Methods("PUT")
+
 }
 
-func postPriceHandler(cliCtx context.CLIContext) http.HandlerFunc {
+func postPriceHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var req PostPriceReq
 
@@ -62,5 +65,4 @@ func postPriceHandler(cliCtx context.CLIContext) http.HandlerFunc {
 
 		utils.WriteGenerateStdTxResponse(w, cliCtx, baseReq, []sdk.Msg{msg})
 	}
-
 }
