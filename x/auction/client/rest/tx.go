5947bdb36dc73f47baa9b8c90110155f58d8aa35
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-02-02 16:16:45 +0000

    update rest tx endoint to use http body

--- x/auction/client/rest/tx.go
+++ x/auction/client/rest/tx.go
@@ -1,10 +1,8 @@
 package rest
 
 import (
-	"bytes"
 	"fmt"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 
@@ -16,65 +14,45 @@ import (
 	"github.com/kava-labs/kava/x/auction/types"
 )
 
-type placeBidReq struct {
-	BaseReq   rest.BaseReq `json:"base_req"`
-	AuctionID string       `json:"auction_id"`
-	Bidder    string       `json:"bidder"`
-	Bid       string       `json:"bid"`
-	Lot       string       `json:"lot"`
+func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
+	r.HandleFunc(fmt.Sprintf("/{%s}/auctions/{%s}/bids", types.ModuleName, restAuctionID), bidHandlerFn(cliCtx)).Methods("POST")
 }
 
-const (
-	restAuctionID = "auction_id"
-	restBidder    = "bidder"
-	restBid       = "bid"
-	restLot       = "lot"
-)
-
-func registerTxRoutes(cliCtx context.CLIContext, r *mux.Router) {
-	r.HandleFunc(
-		fmt.Sprintf("/auction/bid/{%s}/{%s}/{%s}", restAuctionID, restBidder, restBid), bidHandlerFn(cliCtx)).Methods("PUT")
+type placeBidReq struct {
+	BaseReq rest.BaseReq `json:"base_req"`
+	Amount  sdk.Coin     `json:"amount"`
 }
 
 func bidHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
+		// Get auction ID from url
+		auctionID, ok := rest.ParseUint64OrReturnBadRequest(w, mux.Vars(r)[restAuctionID])
+		if !ok {
+			return
+		}
+
+		// Get info from the http request body
 		var req placeBidReq
-		vars := mux.Vars(r)
-		strAuctionID := vars[restAuctionID]
-		bechBidder := vars[restBidder]
-		strBid := vars[restBid]
-
-		auctionID, err := strconv.ParseUint(strAuctionID, 10, 64)
+		if !rest.ReadRESTReq(w, r, cliCtx.Codec, &req) {
+			return
+		}
+		req.BaseReq = req.BaseReq.Sanitize()
+		if !req.BaseReq.ValidateBasic(w) {
+			return
+		}
+		bidderAddr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
 		if err != nil {
 			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
 
-		bidder, err := sdk.AccAddressFromBech32(bechBidder)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		bid, err := sdk.ParseCoin(strBid)
-		if err != nil {
-			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
-			return
-		}
-
-		msg := types.NewMsgPlaceBid(auctionID, bidder, bid)
+		// Create and return a StdTx
+		msg := types.NewMsgPlaceBid(auctionID, bidderAddr, req.Amount)
 		if err := msg.ValidateBasic(); err != nil {
 			rest.WriteErrorResponse(w, http.StatusBadRequest, err.Error())
 			return
 		}
-
-		fromAddr, err := sdk.AccAddressFromBech32(req.BaseReq.From)
-		if !bytes.Equal(fromAddr, bidder) {
-			rest.WriteErrorResponse(w, http.StatusUnauthorized, "must bid from own address")
-			return
-		}
 		utils.WriteGenerateStdTxResponse(w, cliCtx, req.BaseReq, []sdk.Msg{msg})
-
 	}
 }
