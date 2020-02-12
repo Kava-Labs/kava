975fda9b99c0082f7377ee741226ea4050da2e82
denalimarsh <denalimarsh@gmail.com>
2020-02-12 15:08:59 -0800

    msg codec registration

--- x/bep3/types/codec.go
+++ x/bep3/types/codec.go
@@ -10,17 +10,11 @@ func init() {
 	RegisterCodec(ModuleCdc)
 }
 
-// From https://github.com/binance-chain/go-sdk/blob/master/types/msg/wire.go:
-// cdc.RegisterConcrete(HTLTMsg{}, "tokens/HTLTMsg", nil)
-// cdc.RegisterConcrete(DepositHTLTMsg{}, "tokens/DepositHTLTMsg", nil)
-// cdc.RegisterConcrete(ClaimHTLTMsg{}, "tokens/ClaimHTLTMsg", nil)
-// cdc.RegisterConcrete(RefundHTLTMsg{}, "tokens/RefundHTLTMsg", nil)
-
 // RegisterCodec registers concrete types on the Amino code
 func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(HTLTMsg{}, "bep3/HTLTMsg", nil)
-
-	// cdc.RegisterConcrete(MsgDepositHTLT{}, fmt.Sprintf("bep3/%s", AliasMsgDepositHTLT), nil)
-	// cdc.RegisterConcrete(MsgClaimHTLT{}, fmt.Sprintf("bep3/%s", AliasMsgClaimHTLT), nil)
-	// cdc.RegisterConcrete(MsgRefundHTLT{}, fmt.Sprintf("bep3/%s", AliasMsgRefundHTLT), nil)
+	cdc.RegisterConcrete(MsgDepositHTLT{}, "bep3/MsgDepositHTLT", nil)
+	cdc.RegisterConcrete(MsgRefundHTLT{}, "bep3/MsgRefundHTLT", nil)
+	cdc.RegisterConcrete(MsgClaimHTLT{}, "bep3/MsgClaimHTLT", nil)
+	cdc.RegisterConcrete(MsgCalculateSwapID{}, "bep3/MsgCalculateSwapID", nil)
 }
