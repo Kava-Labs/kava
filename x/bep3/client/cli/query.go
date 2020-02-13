694f1e671da3710fa6e8e767828f456be2f5cc28
denalimarsh <denalimarsh@gmail.com>
2020-02-13 15:11:35 -0800

    CLI cmds:create htlt, query htlt

--- x/bep3/client/cli/query.go
+++ x/bep3/client/cli/query.go
@@ -1,14 +1,15 @@
 package cli
 
 import (
-	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/kava-labs/kava/x/bep3/types"
 )
 
@@ -21,6 +22,8 @@ func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	}
 
 	bep3QueryCmd.AddCommand(client.GetCommands(
+		QueryGetCalcSwapIDCmd(queryRoute, cdc),
+		QueryGetHtltCmd(queryRoute, cdc),
 		QueryGetHtltsCmd(queryRoute, cdc),
 		QueryParamsCmd(queryRoute, cdc),
 	)...)
@@ -28,11 +31,87 @@ func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	return bep3QueryCmd
 }
 
+// QueryGetCalcSwapIDCmd calculates the swapID for a random number hash, sender, and sender other chain
+func QueryGetCalcSwapIDCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:     "calc-id [randomNumberHash] [sender] [senderOtherChain]",
+		Short:   "calculate swap ID for the given random number hash, sender, and sender other chain",
+		Example: "calc-id 0677bd8a303dd981810f34d8e5cc6507f13b391899b84d3c1be6c6045a17d747 kava15qdefkmwswysgg4qxgcqpqr35k3m49pkx2jdfnw bnb1ud3q90r98l3mhd87kswv3h8cgrymzeljct8qn7",
+		Args:    cobra.MinimumNArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			// Parse query params
+			if len(strings.TrimSpace(args[0])) != types.RandomNumberHashLength {
+				return fmt.Errorf("random-number-hash should have length %d", types.RandomNumberHashLength)
+			}
+			randomNumberHash := args[0]
+
+			sender := sdk.AccAddress(args[1])
+			senderOtherChain := args[2]
+
+			// Calculate swap ID and convert to human-readable string
+			swapIDBytes, err := types.CalculateSwapID(randomNumberHash, sender, senderOtherChain)
+			if err != nil {
+				return err
+			}
+			swapID := types.BytesToHexEncodedString(swapIDBytes)
+
+			return cliCtx.PrintOutput(swapID)
+		},
+	}
+}
+
+// QueryGetHtltCmd queries an HTLT by swapID
+func QueryGetHtltCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	return &cobra.Command{
+		Use:     "htlt [swap-id]",
+		Short:   "get HTLT information",
+		Example: "bep3 htlt 6682c03cc3856879c8fb98c9733c6b0c30758299138166b6523fe94628b1d3af",
+		Args:    cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			// Parse query params
+			if len(strings.TrimSpace(args[0])) != types.SwapIDLength {
+				return fmt.Errorf("swap-id should have length %d", types.SwapIDLength)
+			}
+			hexEncodedSwapID := args[0]
+
+			// Decode swapID's hex encoded string to []byte
+			swapID, err := types.HexEncodedStringToBytes(hexEncodedSwapID)
+			if err != nil {
+				return err
+			}
+
+			// Prepare query params
+			bz, err := cdc.MarshalJSON(types.QuerySwapByID{
+				SwapID: swapID,
+			})
+			if err != nil {
+				return err
+			}
+
+			// Execute query
+			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/%s", queryRoute, types.QueryGetHTLT), bz)
+			if err != nil {
+				return err
+			}
+
+			// Decode and print results
+			var htlt types.HTLT
+			cdc.MustUnmarshalJSON(res, &htlt)
+			return cliCtx.PrintOutput(htlt)
+		},
+	}
+}
+
 // QueryGetHtltsCmd queries the htlts in the store
 func QueryGetHtltsCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
-		Use:   "htlts",
-		Short: "get a list of active htlts",
+		Use:     "htlts",
+		Short:   "get a list of active htlts",
+		Example: "bep3 htlts",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
 
@@ -45,7 +124,7 @@ func QueryGetHtltsCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 			cdc.MustUnmarshalJSON(res, &htlts)
 
 			if len(htlts) == 0 {
-				return errors.New("There are currently no htlts")
+				return fmt.Errorf("There are currently no htlts")
 			}
 
 			cliCtx = cliCtx.WithHeight(height)
@@ -54,13 +133,13 @@ func QueryGetHtltsCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	}
 }
 
-// QueryParamsCmd queries the auction module parameters
+// QueryParamsCmd queries the bep3 module parameters
 func QueryParamsCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
-		Use:   "params",
-		Short: "get the auction module parameters",
-		Long:  "Get the current global auction module parameters.",
-		Args:  cobra.NoArgs,
+		Use:     "params",
+		Short:   "get the bep3 module parameters",
+		Example: "bep3 params",
+		Args:    cobra.NoArgs,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
 
--- x/bep3/client/cli/tx.go
+++ x/bep3/client/cli/tx.go
@@ -1,6 +1,11 @@
 package cli
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
@@ -29,46 +34,62 @@ func GetTxCmd(cdc *codec.Codec) *cobra.Command {
 // GetCmdCreateHtlt cli command for creating htlts
 func GetCmdCreateHtlt(cdc *codec.Codec) *cobra.Command {
 	return &cobra.Command{
-		Use:   "create [to] [recipientOtherChain] [secretNumber] [timestamp] [coins] [expectedIncome] [heightSpan] [crosschain]",
-		Short: "create a new Hashed Time Locked Transaction (HTLT)",
-		Args:  cobra.MinimumNArgs(0),
+		Use:     "create [to] [recipientOtherChain] [hashedSecret] [timestamp] [coins] [expectedIncome] [heightSpan] [crosschain]",
+		Short:   "create a new Hashed Time Locked Transaction (HTLT)",
+		Example: "bep3 create kava15qdefkmwswysgg4qxgqpqr35k3m49pkx2jdfnw 0x9eB05a790e2De0a047a57a22199D8CccEA6d6D5A 0677bd8a303dd981810f34d8e5cc6507f13b391899b84d3c1be6c6045a17d747 9988776655 kava100 kava99 500000 true --from accA",
+		Args:    cobra.MinimumNArgs(8),
 		RunE: func(cmd *cobra.Command, args []string) error {
-
-			// TODO: User submits RandomNumberHash instead of [secretNumber], [timestamp]
-
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
 			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
 
-			// We need...
-			// 1. ReceiverAddr == KavaExecutor.DeputyAddress
-			//		- kava15qdefkmwswysgg4qxgqpqr35k3m49pkx2jdfnw
-			// 2. OtherChainAddr == OtherExecutor.DeputyAddress
-			//		- 0x9eB05a790e2De0a047a57a22199D8CccEA6d6D5A
-			// 3. txLog.OutCoin == deputy.Config.KavaConfig.Symbol (both are converted to uppercase)
-			//		- kava
-
-			// Acc A -> passed with '--from accA'
 			from := cliCtx.GetFromAddress()
-			// from := sdk.AccAddress("kava1xy7hrjy9r0algz9w3gzm8u6mrpq97kwta747gj")
 
-			// Acc B
+			// TODO: string -> sdk.AccAddress conversion
+			//	- sdk.AccAddressFromBech32(): 'decoding bech32 failed: checksum failed. Expected fefqtn, got 2jdfnw.'
+			//	- sdk.AccAddressFromHex(): encoding/hex: invalid byte: U+006B 'k'
+			//	- sdk.AccAddress(): creates HTLT with incorrect address length causing HTLT query to fail
+			to := from // same as KavaExecutor.DeputyAddress
 
-			to := sdk.AccAddress("kava15qdefkmwswysgg4qxgcqpqr35k3m49pkx2jdfnw")
-			recipientOtherChain := "0x9eB05a790e2De0a047a57a22199D8CccEA6d6D5A" //current depty eth address
+			if len(args[1]) == 0 {
+				return errors.New("recipient-other-chain cannot be empty")
+			}
+			recipientOtherChain := args[1] // same as OtherExecutor.DeputyAddress
+
+			// TODO: Add as optional arg
 			senderOtherChain := ""
 
-			randomNumberBytes := []byte{15}
-			timestampInt64 := int64(9988776655)
-			randomNumberHash := types.CalculateRandomHash(randomNumberBytes, timestampInt64)
+			if len(strings.TrimSpace(args[2])) != types.RandomNumberHashLength {
+				return fmt.Errorf("random-number-hash should have length %d", types.RandomNumberHashLength)
+			}
+			hashedSecret := args[2]
 
-			amount := sdk.Coins{sdk.Coin{Denom: "kava", Amount: sdk.NewInt(100)}}
-			expectedIncome := "kava100"
-			heightSpan := int64(500000)
-			crossChain := true
+			timeStamp, err := strconv.ParseInt(args[3], 10, 64)
+			if err != nil {
+				return err
+			}
 
-			msg := types.NewHTLTMsg(from, to, recipientOtherChain, senderOtherChain, randomNumberHash, timestampInt64, amount, expectedIncome, heightSpan, crossChain)
+			coins, err := sdk.ParseCoins(args[4])
+			if err != nil {
+				return err
+			}
+			expectedIncome := args[5]
 
-			err := msg.ValidateBasic()
+			heightSpan, err := strconv.ParseInt(args[6], 10, 64)
+			if err != nil {
+				return err
+			}
+
+			crossChain, err := strconv.ParseBool(args[7])
+			if err != nil {
+				return err
+			}
+
+			msg := types.NewHTLTMsg(
+				from, to, recipientOtherChain, senderOtherChain, hashedSecret,
+				timeStamp, coins, expectedIncome, heightSpan, crossChain,
+			)
+
+			err = msg.ValidateBasic()
 			if err != nil {
 				return err
 			}
