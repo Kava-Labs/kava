ce544faab358de38d59b15561cfe6f8062a720d6
John Maheswaran <john@kava.io>
2020-02-24 13:28:53 -0500

    adding query and rest updates

--- x/validator-vesting/client/rest/query.go
+++ x/validator-vesting/client/rest/query.go
@@ -6,15 +6,37 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/types/rest"
+	"github.com/gorilla/mux"
+	"github.com/kava-labs/kava/x/validator-vesting/internal/types"
 )
 
-func getCircuatingSupplyHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+// define routes that get registered by the main application
+func registerQueryRoutes(cliCtx context.CLIContext, r *mux.Router) {
+
+	r.HandleFunc("/vesting/circulatingsupply", getCirculatingSupplyHandlerFn(cliCtx)).Methods("GET")
+
+}
+
+func getCirculatingSupplyHandlerFn(cliCtx context.CLIContext) http.HandlerFunc {
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		res, _, err := cliCtx.QueryWithData(fmt.Sprintf("validator-vesting/circulatingsupply"), nil)
+		// Parse the query height
+		cliCtx, ok := rest.ParseQueryHeightOrReturnBadRequest(w, cliCtx, r)
+		if !ok {
+			return
+		}
+
+		res, height, err := cliCtx.Query(fmt.Sprintf("vesting/circulatingsupply", types.QueryCirculatingSupply))
 		if err != nil {
 			rest.WriteErrorResponse(w, http.StatusNotFound, err.Error())
 			return
 		}
-		rest.PostProcessResponse(w, cliCtx, res)
+
+		cliCtx = cliCtx.WithHeight(height)
+		// do not write as json, write direct output
+		// rest.PostProcessResponse(w, cliCtx, res)
+
+		w.Write(res) // write the result directly
 	}
+
 }
