0de7ce4d6f9b8fdaac390df3f95a5444a2a09821
John Maheswaran <john@kava.io>
2020-02-23 19:47:48 -0500

    adding register routes function

---
+++ x/validator-vesting/client/rest/rest.go
@@ -0,0 +1,13 @@
+package rest
+
+import (
+	"github.com/cosmos/cosmos-sdk/client/context"
+
+	"github.com/gorilla/mux"
+)
+
+// RegisterRoutes - Central function to define routes that get registered by the main application
+func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
+	r.HandleFunc("/validator-vesting/circulatingsypply", getCircuatingSupplyHandlerFn(cliCtx)).Methods("GET")
+
+}
