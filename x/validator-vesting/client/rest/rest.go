6d6ea6877bfcb1fd2202fec59bd449a03932056e
John Maheswaran <john@kava.io>
2020-02-24 16:02:08 -0500

    fix routes

--- x/validator-vesting/client/rest/rest.go
+++ x/validator-vesting/client/rest/rest.go
@@ -8,6 +8,6 @@ import (
 
 // RegisterRoutes - Central function to define routes that get registered by the main application
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
-	r.HandleFunc("/validator-vesting/circulatingsupply", getCirculatingSupplyHandlerFn(cliCtx)).Methods("GET")
+	registerQueryRoutes(cliCtx, r)
 
 }
