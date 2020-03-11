bb01e75b6d3638b6854698242605f07fdc4a0c54
denalimarsh <denalimarsh@gmail.com>
2020-03-11 16:07:52 -0700

    params tests

---
+++ x/bep3/keeper/params_test.go
@@ -0,0 +1,91 @@
+package keeper_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/kava-labs/kava/app"
+	"github.com/kava-labs/kava/x/bep3/keeper"
+	"github.com/stretchr/testify/suite"
+	abci "github.com/tendermint/tendermint/abci/types"
+	tmtime "github.com/tendermint/tendermint/types/time"
+)
+
+type ParamsTestSuite struct {
+	suite.Suite
+
+	keeper keeper.Keeper
+	addrs  []sdk.AccAddress
+	app    app.TestApp
+	ctx    sdk.Context
+}
+
+func (suite *ParamsTestSuite) SetupTest() {
+	tApp := app.NewTestApp()
+	ctx := tApp.NewContext(true, abci.Header{Height: 1, Time: tmtime.Now()})
+	_, addrs := app.GeneratePrivKeyAddressPairs(10)
+	tApp.InitializeFromGenesisStates(
+		NewBep3GenStateMulti(),
+	)
+	suite.keeper = tApp.GetBep3Keeper()
+	suite.ctx = ctx
+	suite.addrs = addrs
+}
+
+func (suite *ParamsTestSuite) TestGetSetBnbDeputyAddress() {
+	params := suite.keeper.GetParams(suite.ctx)
+	params.BnbDeputyAddress = suite.addrs[0]
+	suite.NotPanics(func() { suite.keeper.SetParams(suite.ctx, params) })
+
+	params = suite.keeper.GetParams(suite.ctx)
+	suite.Equal(suite.addrs[0], params.BnbDeputyAddress)
+	addr := suite.keeper.GetBnbDeputyAddress(suite.ctx)
+	suite.Equal(suite.addrs[0], addr)
+}
+
+func (suite *ParamsTestSuite) TestGetMaxBlockLock() {
+	params := suite.keeper.GetParams(suite.ctx)
+	maxBlockLock := params.MaxBlockLock
+
+	res := suite.keeper.GetMaxBlockLock(suite.ctx)
+	suite.Equal(maxBlockLock, res)
+}
+
+func (suite *ParamsTestSuite) TestGetMinBlockLock() {
+	params := suite.keeper.GetParams(suite.ctx)
+	minBlockLock := params.MinBlockLock
+
+	res := suite.keeper.GetMinBlockLock(suite.ctx)
+	suite.Equal(minBlockLock, res)
+}
+
+func (suite *ParamsTestSuite) TestGetAssets() {
+	params := suite.keeper.GetParams(suite.ctx)
+	assets := params.SupportedAssets
+
+	res, found := suite.keeper.GetAssets(suite.ctx)
+	suite.True(found)
+	suite.Equal(assets, res)
+}
+
+func (suite *ParamsTestSuite) TestGetAssetByDenom() {
+	params := suite.keeper.GetParams(suite.ctx)
+	asset := params.SupportedAssets[0]
+
+	res, found := suite.keeper.GetAssetByDenom(suite.ctx, asset.Denom)
+	suite.True(found)
+	suite.Equal(asset, res)
+}
+
+func (suite *ParamsTestSuite) TestGetAssetByCoinID() {
+	params := suite.keeper.GetParams(suite.ctx)
+	asset := params.SupportedAssets[0]
+
+	res, found := suite.keeper.GetAssetByCoinID(suite.ctx, asset.CoinID)
+	suite.True(found)
+	suite.Equal(asset, res)
+}
+
+func TestParamsTestSuite(t *testing.T) {
+	suite.Run(t, new(ParamsTestSuite))
+}
