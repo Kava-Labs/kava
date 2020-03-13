677d9eb53cd2a16aa2245db9a048bb54faaed6a7
denalimarsh <denalimarsh@gmail.com>
2020-03-12 20:57:50 -0700

    implement genesis types test

---
+++ x/bep3/types/genesis_test.go
@@ -0,0 +1,76 @@
+package types_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/kava-labs/kava/x/bep3/types"
+	"github.com/stretchr/testify/suite"
+)
+
+type GenesisTestSuite struct {
+	suite.Suite
+	swaps types.AtomicSwaps
+}
+
+func (suite *GenesisTestSuite) SetupTest() {
+	suite.swaps = atomicSwaps(10)
+	return
+}
+
+func (suite *GenesisTestSuite) TestValidate() {
+	type args struct {
+		swaps  types.AtomicSwaps
+		assets []sdk.Coin
+	}
+	testCases := []struct {
+		name       string
+		args       args
+		expectPass bool
+	}{
+		{
+			"default",
+			args{
+				swaps:  make(types.AtomicSwaps, 0),
+				assets: []sdk.Coin{},
+			},
+			true,
+		},
+		{
+			"with swaps",
+			args{
+				swaps:  suite.swaps,
+				assets: []sdk.Coin{},
+			},
+			true,
+		},
+		{
+			"duplicate swaps",
+			args{
+				swaps:  types.AtomicSwaps{suite.swaps[2], suite.swaps[2]},
+				assets: []sdk.Coin{},
+			},
+			false,
+		},
+	}
+
+	for _, tc := range testCases {
+		var gs types.GenesisState
+		if tc.name == "default" {
+			gs = types.DefaultGenesisState()
+		} else {
+			gs = types.NewGenesisState(types.DefaultParams(), tc.args.swaps, tc.args.assets)
+		}
+
+		err := gs.Validate()
+		if tc.expectPass {
+			suite.Nil(err)
+		} else {
+			suite.Error(err)
+		}
+	}
+}
+
+func TestGenesisTestSuite(t *testing.T) {
+	suite.Run(t, new(GenesisTestSuite))
+}
