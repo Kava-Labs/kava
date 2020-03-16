3f40bc7fa3e1b58b90e616e42ae954d0f907aff2
denalimarsh <denalimarsh@gmail.com>
2020-03-16 10:54:28 -0700

    abci test updates

--- x/bep3/abci_test.go
+++ x/bep3/abci_test.go
@@ -1,11 +1,11 @@
 package bep3_test
 
 import (
-	"encoding/hex"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/kava-labs/kava/app"
+	"github.com/kava-labs/kava/x/bep3"
 	"github.com/kava-labs/kava/x/bep3/keeper"
 	"github.com/kava-labs/kava/x/bep3/types"
 	"github.com/stretchr/testify/suite"
@@ -15,19 +15,19 @@ import (
 )
 
 const (
-	TestSenderOtherChain    = "bnb1uky3me9ggqypmrsvxk7ur6hqkzq7zmv4ed4ng7"
-	TestRecipientOtherChain = "bnb1urfermcg92dwq36572cx4xg84wpk3lfpksr5g7"
+	TestSenderOtherChain        = "bnb1uky3me9ggqypmrsvxk7ur6hqkzq7zmv4ed4ng7"
+	TestRecipientOtherChain     = "bnb1urfermcg92dwq36572cx4xg84wpk3lfpksr5g7"
+	SwapLongtermStorageDuration = 86400
 )
 
 type ABCITestSuite struct {
 	suite.Suite
-	keeper   keeper.Keeper
-	app      app.TestApp
-	ctx      sdk.Context
-	querier  sdk.Querier
-	addrs    []sdk.AccAddress
-	swapIDs  []cmn.HexBytes
-	isSwapID map[string]bool
+	keeper        keeper.Keeper
+	app           app.TestApp
+	ctx           sdk.Context
+	addrs         []sdk.AccAddress
+	swapIDs       []cmn.HexBytes
+	randomNumbers []cmn.HexBytes
 }
 
 func (suite *ABCITestSuite) SetupTest() {
@@ -35,27 +35,26 @@ func (suite *ABCITestSuite) SetupTest() {
 	ctx := tApp.NewContext(true, abci.Header{Height: 1, Time: tmtime.Now()})
 
 	// Set up auth GenesisState
-	_, addrs := app.GeneratePrivKeyAddressPairs(10)
+	_, addrs := app.GeneratePrivKeyAddressPairs(11)
 	coins := []sdk.Coins{}
-	for j := 0; j < 10; j++ {
+	for j := 0; j < 11; j++ {
 		coins = append(coins, cs(c("bnb", 10000000000), c("ukava", 10000000000)))
 	}
 	authGS := app.NewAuthGenState(addrs, coins)
-
-	tApp.InitializeFromGenesisStates(
-		authGS,
-		NewBep3GenStateMulti(),
-	)
+	// Initialize test app
+	tApp.InitializeFromGenesisStates(authGS, NewBep3GenStateMulti())
 
 	suite.ctx = ctx
 	suite.app = tApp
-	suite.keeper = tApp.GetBep3Keeper()
-	suite.querier = keeper.NewQuerier(suite.keeper)
 	suite.addrs = addrs
+	suite.ResetKeeper()
+}
 
-	// Create atomic swaps and save IDs
+func (suite *ABCITestSuite) ResetKeeper() {
+	suite.keeper = suite.app.GetBep3Keeper()
+
 	var swapIDs []cmn.HexBytes
-	isSwapID := make(map[string]bool)
+	var randomNumbers []cmn.HexBytes
 	for i := 0; i < 10; i++ {
 		// Set up atomic swap variables
 		expireHeight := int64(360)
@@ -64,33 +63,89 @@ func (suite *ABCITestSuite) SetupTest() {
 		randomNumber, _ := types.GenerateSecureRandomNumber()
 		randomNumberHash := types.CalculateRandomHash(randomNumber.Bytes(), timestamp)
 
-		// Create atomic swap and check err
+		// Create atomic swap and check err to confirm creation
 		err := suite.keeper.CreateAtomicSwap(suite.ctx, randomNumberHash, timestamp, expireHeight,
 			suite.addrs[i], suite.addrs[i], TestSenderOtherChain, TestRecipientOtherChain,
 			amount, amount.String())
 		suite.Nil(err)
 
-		// Calculate swap ID and save
+		// Store swap's calculated ID and secret random number
 		swapID := types.CalculateSwapID(randomNumberHash, suite.addrs[i], TestSenderOtherChain)
-		swapIDs = append(swapIDs, swapID)
-		isSwapID[hex.EncodeToString(swapID)] = true
+		suite.swapIDs = append(swapIDs, swapID)
+		suite.randomNumbers = append(randomNumbers, randomNumber.Bytes())
 	}
-	suite.swapIDs = swapIDs
-	suite.isSwapID = isSwapID
 }
 
 func (suite *ABCITestSuite) TestBeginBlocker() {
-	// 1. Attempt to refund an atomic swap - should be rejected
-	// 2. Claim an atomic swap - should be accepted and closed
-	// 3. Move block time past expiration - all swaps should be closed
-	// 4. Attempt to refund an atomic swap - should be accepted
-	// 5. Move block time past deletion time - all swaps should be deleted
+	testCases := []struct {
+		name            string
+		firstCtx        sdk.Context
+		secondCtx       sdk.Context
+		expectedStatus  types.SwapStatus
+		expectInStorage bool
+	}{
+		{
+			name:            "normal",
+			firstCtx:        suite.ctx,
+			secondCtx:       suite.ctx.WithBlockHeight(suite.ctx.BlockHeight() + 10),
+			expectedStatus:  types.Open,
+			expectInStorage: true,
+		},
+		{
+			name:            "after expiration",
+			firstCtx:        suite.ctx.WithBlockHeight(suite.ctx.BlockHeight() + 400),
+			secondCtx:       suite.ctx.WithBlockHeight(suite.ctx.BlockHeight() + 410),
+			expectedStatus:  types.Expired,
+			expectInStorage: true,
+		},
+		{
+			name:            "after completion",
+			firstCtx:        suite.ctx,
+			secondCtx:       suite.ctx.WithBlockHeight(suite.ctx.BlockHeight() + 10),
+			expectedStatus:  types.Completed,
+			expectInStorage: true,
+		},
+		{
+			name:            "after deletion",
+			firstCtx:        suite.ctx.WithBlockHeight(suite.ctx.BlockHeight() + 400),
+			secondCtx:       suite.ctx.WithBlockHeight(suite.ctx.BlockHeight() + 400 + SwapLongtermStorageDuration),
+			expectedStatus:  types.NULL,
+			expectInStorage: false,
+		},
+	}
 
-	// TODO:
-	// Run the endblocker, simulating a block time 1ns before swap expiry
-	// Check swap has not been closed yet
-	// Run the endblocker, simulating a block time equal to swap expiry
-	// Check swap has been deleted
+	for _, tc := range testCases {
+		// Reset keeper and run the initial begin blocker
+		suite.ResetKeeper()
+		bep3.BeginBlocker(tc.firstCtx, suite.keeper)
+
+		switch tc.expectedStatus {
+		case types.Completed:
+			for i, swapID := range suite.swapIDs {
+				err := suite.keeper.ClaimAtomicSwap(tc.firstCtx, suite.addrs[10], swapID, suite.randomNumbers[i])
+				suite.Nil(err)
+			}
+		case types.NULL:
+			for _, swapID := range suite.swapIDs {
+				err := suite.keeper.RefundAtomicSwap(tc.firstCtx, suite.addrs[10], swapID)
+				suite.Nil(err)
+			}
+		}
+
+		// Run the second begin blocker
+		bep3.BeginBlocker(tc.secondCtx, suite.keeper)
+
+		// Check each swap's availibility and status
+		for _, swapID := range suite.swapIDs {
+			storedSwap, found := suite.keeper.GetAtomicSwap(tc.secondCtx, swapID)
+			if tc.expectInStorage {
+				suite.True(found)
+			} else {
+				suite.False(found)
+			}
+			suite.Equal(tc.expectedStatus, storedSwap.Status)
+		}
+	}
 }
 
 func TestABCITestSuite(t *testing.T) {
