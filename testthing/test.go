0bffb3898e06c72411d566a1cb6e7c5158146a4a
John Maheswaran <john@kava.io>
2020-03-17 11:56:28 -0400

    used for testing and generating transactions and testing hooks

--- testthing/test.go
+++ testthing/test.go
@@ -32,48 +32,48 @@ func init() {
 func main() {
 	sendCoins()
 
-	proposalContent := gov.ContentFromProposalType("A Test Title", "A test description on this proposal.", gov.ProposalTypeText)
-	addr, err := sdk.AccAddressFromBech32("kava1ffv7nhd3z6sych2qpqkk03ec6hzkmufy0r2s4c") // validator
-	if err != nil {
-		panic(err)
-	}
+	// proposalContent := gov.ContentFromProposalType("A Test Title", "A test description on this proposal.", gov.ProposalTypeText)
+	// addr, err := sdk.AccAddressFromBech32("kava1ffv7nhd3z6sych2qpqkk03ec6hzkmufy0r2s4c") // validator
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	// create a message to send to the blockchain
-	msg := gov.NewMsgSubmitProposal(
-		proposalContent,
-		sdk.NewCoins(sdk.NewInt64Coin("stake", 1000)),
-		addr,
-	)
+	// // create a message to send to the blockchain
+	// msg := gov.NewMsgSubmitProposal(
+	// 	proposalContent,
+	// 	sdk.NewCoins(sdk.NewInt64Coin("stake", 1000)),
+	// 	addr,
+	// )
 
 	// helper methods for transactions
-	cdc := app.MakeCodec() // make codec for the app
+	// cdc := app.MakeCodec() // make codec for the app
 
-	// create a keybase
-	// TODO - IMPORTANT - this needs to be set manually and does NOT work with tilde i.e. ~/ does NOT work
-	keybase, err := keys.NewKeyBaseFromDir("/Users/john/.kvcli/")
-	if err != nil {
-		panic(err)
-	}
-	_, err = keybase.List()
-	// fmt.Printf("Keys: %s\n\n", all)
-	if err != nil {
-		panic(err)
-	}
+	// // create a keybase
+	// // TODO - IMPORTANT - this needs to be set manually and does NOT work with tilde i.e. ~/ does NOT work
+	// keybase, err := keys.NewKeyBaseFromDir("/Users/john/.kvcli/")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// _, err = keybase.List()
+	// // fmt.Printf("Keys: %s\n\n", all)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
 	// the test address - TODO IMPORTANT make sure this lines up with startchain.sh
-	address := "kava1ffv7nhd3z6sych2qpqkk03ec6hzkmufy0r2s4c"
+	// address := "kava1ffv7nhd3z6sych2qpqkk03ec6hzkmufy0r2s4c"
 
 	// SEND THE PROPOSAL
 
 	// get the account number and sequence number
-	accountNumber, sequenceNumber := getAccountNumberAndSequenceNumber(cdc, address)
-	// cast to the generic msg type
-	msgToSend := []sdk.Msg{msg}
-	keyname := "vlad"      // TODO - IMPORTANT this must match the keys in the startchain.sh script
-	password := "password" // TODO - IMPORTANT this must match the keys in the startchain.sh script
+	// accountNumber, sequenceNumber := getAccountNumberAndSequenceNumber(cdc, address)
+	// // cast to the generic msg type
+	// msgToSend := []sdk.Msg{msg}
+	// keyname := "vlad"      // TODO - IMPORTANT this must match the keys in the startchain.sh script
+	// password := "password" // TODO - IMPORTANT this must match the keys in the startchain.sh script
 
 	// send the PROPOSAL message to the blockchain
-	sendMsgToBlockchain(cdc, accountNumber, sequenceNumber, keyname, password, msgToSend, keybase)
+	// sendMsgToBlockchain(cdc, accountNumber, sequenceNumber, keyname, password, msgToSend, keybase)
 }
 
 func sendDeposit() {
@@ -303,9 +303,9 @@ func sendMsgToBlockchain(cdc *amino.Codec, accountNumber uint64, sequenceNumber 
 	txBldr := auth.NewTxBuilderFromCLI().
 		WithTxEncoder(authclient.GetTxEncoder(cdc)).WithChainID("testing").
 		WithKeybase(keybase).WithAccountNumber(accountNumber).
-		WithSequence(sequenceNumber)
+		WithSequence(sequenceNumber).WithFees("2000stake")
 
-		// build and sign the transaction
+	// build and sign the transaction
 	// this is the *Amino* encoded version of the transaction
 	// fmt.Printf("%+v", txBldr.Keybase())
 	txBytes, err := txBldr.BuildAndSign("vlad", "password", msg)
@@ -330,7 +330,7 @@ func sendMsgToBlockchain(cdc *amino.Codec, accountNumber uint64, sequenceNumber 
 	if err != nil {
 		panic(err)
 	}
-	// fmt.Println("post body: ", string(jsonBytes))
+	fmt.Println("valid transaction: ", string(jsonBytes))
 
 	resp, err := http.Post("http://localhost:1317/txs", "application/json", bytes.NewBuffer(jsonBytes))
 	if err != nil {
@@ -338,13 +338,13 @@ func sendMsgToBlockchain(cdc *amino.Codec, accountNumber uint64, sequenceNumber 
 	}
 
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	_, err = ioutil.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("\n\nBody:\n\n")
-	fmt.Println(string(body))
+	// fmt.Printf("\n\nBody:\n\n")
+	// fmt.Println(string(body))
 
 }
 
