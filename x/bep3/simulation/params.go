ba506404d896563665e54356fe0b28ef3af745a2
denalimarsh <denalimarsh@gmail.com>
2020-04-02 13:05:35 -0700

    gen random param changes

--- x/bep3/simulation/params.go
+++ x/bep3/simulation/params.go
@@ -1,14 +1,47 @@
 package simulation
 
 import (
+	"fmt"
 	"math/rand"
 
 	"github.com/cosmos/cosmos-sdk/x/simulation"
+
+	"github.com/kava-labs/kava/x/bep3/types"
+)
+
+const (
+	keyBnbDeputyAddress = "BnbDeputyAddress"
+	keyMinBlockLock     = "MinBlockLock"
+	keyMaxBlockLock     = "MaxBlockLock"
+	keySupportedAssets  = "SupportedAssets"
 )
 
 // ParamChanges defines the parameters that can be modified by param change proposals
 // on the simulation
 func ParamChanges(r *rand.Rand) []simulation.ParamChange {
-	// TODO implement this
-	return []simulation.ParamChange{}
+	// We generate MinBlockLock first because the result is required by GenMaxBlockLock()
+	minBlockLockVal := GenMinBlockLock(r)
+
+	return []simulation.ParamChange{
+		simulation.NewSimParamChange(types.ModuleName, keyBnbDeputyAddress, "",
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%s\"", GenBnbDeputyAddress(r))
+			},
+		),
+		simulation.NewSimParamChange(types.ModuleName, keyMinBlockLock, "",
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%d\"", minBlockLockVal)
+			},
+		),
+		simulation.NewSimParamChange(types.ModuleName, keyMaxBlockLock, "",
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%d\"", GenMaxBlockLock(r, minBlockLockVal))
+			},
+		),
+		simulation.NewSimParamChange(types.ModuleName, keySupportedAssets, "",
+			func(r *rand.Rand) string {
+				return fmt.Sprintf("\"%v\"", GenSupportedAssets(r))
+			},
+		),
+	}
 }
