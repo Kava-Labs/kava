dd8d84fa9acb9465158ee9132abafcd4949f6f63
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-04-08 15:25:50 +0100

    untested refactor of sim ops and genesis

--- x/auction/simulation/genesis.go
+++ x/auction/simulation/genesis.go
@@ -18,55 +18,75 @@ import (
 // RandomizedGenState generates a random GenesisState for auction
 func RandomizedGenState(simState *module.SimulationState) {
 
-	// TODO implement this fully
-	// - randomly generating the genesis params
-	// - overwriting with genesis provided to simulation
+	// Start wih the default genesis state
 	auctionGenesis := types.DefaultGenesisState()
 
-	// FIXME temporarily add an auction
-	a := types.NewDebtAuction(
-		cdptypes.LiquidatorMacc, // using cdp account rather than generic test one to avoid having to set permissions on the supply keeper
-		sdk.NewInt64Coin("usdx", 100),
-		sdk.NewInt64Coin("ukava", 1000000000000),
-		simState.GenTimestamp.Add(time.Hour*5),
-		sdk.NewInt64Coin("debt", 100), // same as usdx
-	) // ID is zero
-	auctionGenesis.Auctions = append(auctionGenesis.Auctions, a)
-	auctionGenesis.NextAuctionID = 0 + 1
+	// Add auctions
+	auctions := types.GenesisAuctions{
+		types.NewDebtAuction(
+			cdptypes.LiquidatorMacc, // using cdp account rather than generic test one to avoid having to set permissions on the supply keeper
+			sdk.NewInt64Coin("usdx", 100),
+			sdk.NewInt64Coin("ukava", 1000000000000),
+			simState.GenTimestamp.Add(time.Hour*5),
+			sdk.NewInt64Coin("debt", 100), // same as usdx
+		),
+	}
+	var startingID = auctionGenesis.NextAuctionID
+	var ok bool
+	var totalAuctionCoins sdk.Coins
+	for i, a := range auctions {
+		auctions[i], ok = a.WithID(uint64(i) + startingID).(types.GenesisAuction)
+		if !ok {
+			panic("can't convert Auction to GenesisAuction")
+		}
+		totalAuctionCoins = totalAuctionCoins.Add(a.GetModuleAccountCoins())
+	}
+	auctionGenesis.NextAuctionID = startingID + uint64(len(auctions))
+	auctionGenesis.Auctions = append(auctionGenesis.Auctions, auctions...)
 
-	// Add auction mod account with debt
-	// also add usdx coins to the bidder account so that they can actually place a bid
+	// Also need to update the auction module account (to reflect the coins held in the auctions)
 	var authGenesis auth.GenesisState
 	simState.Cdc.MustUnmarshalJSON(simState.GenState[auth.ModuleName], &authGenesis)
 
-	auctionModAcc := supply.NewEmptyModuleAccount(types.ModuleName)
-	auctionModAcc.SetCoins(sdk.NewCoins(sdk.NewInt64Coin("debt", 100))) // same as sum of auctions.
-	authGenesis.Accounts = append(authGenesis.Accounts, auctionModAcc)  // TODO check if it exists first
+	auctionModAcc, found := getAccount(authGenesis.Accounts, supply.NewModuleAddress(types.ModuleName))
+	if !found {
+		auctionModAcc = supply.NewEmptyModuleAccount(types.ModuleName)
+	}
+	if err := auctionModAcc.SetCoins(totalAuctionCoins); err != nil {
+		panic(err)
+	}
+	authGenesis.Accounts = replaceOrAppendAccount(authGenesis.Accounts, auctionModAcc)
 
+	// TODO adding bidder coins as well - this should be moved elsewhere
 	bidder, found := getAccount(authGenesis.Accounts, simState.Accounts[0].Address) // 0 is the bidder // FIXME
 	if !found {
 		panic("bidder not found")
 	}
-	bidder.SetCoins(bidder.GetCoins().Add(sdk.NewCoins(sdk.NewInt64Coin("usdx", 10000000000))))
-	authGenesis.Accounts = replaceAccount(authGenesis.Accounts, bidder)
+	bidderCoins := sdk.NewCoins(sdk.NewInt64Coin("usdx", 10000000000))
+	if err := bidder.SetCoins(bidder.GetCoins().Add(bidderCoins)); err != nil {
+		panic(err)
+	}
+	authGenesis.Accounts = replaceOrAppendAccount(authGenesis.Accounts, bidder)
 
 	simState.GenState[auth.ModuleName] = simState.Cdc.MustMarshalJSON(authGenesis)
 
-	// Also need to update supply's genesis state because everything's terrible
+	// Update the supply genesis state to reflect the new coins
+	// TODO find some way for this to happen automatically / move it elsewhere
 	var supplyGenesis supply.GenesisState
 	simState.Cdc.MustUnmarshalJSON(simState.GenState[supply.ModuleName], &supplyGenesis)
-	supplyGenesis.Supply = supplyGenesis.Supply.Add(sdk.NewCoins(sdk.NewInt64Coin("debt", 100), sdk.NewInt64Coin("usdx", 10000000000)))
+	supplyGenesis.Supply = supplyGenesis.Supply.Add(totalAuctionCoins).Add(bidderCoins)
 	simState.GenState[supply.ModuleName] = simState.Cdc.MustMarshalJSON(supplyGenesis)
 
 	// TODO liquidator mod account doesn't need to be initialized for this example
-	// it just mints kava, doesn't need a balance
-	// and supply.GetModuleAccount creates one if it doesn't exist
+	// - it just mints kava, doesn't need a starting balance
+	// - and supply.GetModuleAccount creates one if it doesn't exist
 
 	// Note: this line prints out the auction genesis state, not just the auction parameters. Some sdk modules print out just the parameters.
 	fmt.Printf("Selected randomly generated %s parameters:\n%s\n", types.ModuleName, codec.MustMarshalJSONIndent(simState.Cdc, auctionGenesis))
 	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(auctionGenesis)
 }
 
+// Return an account from a list of accounts that matches an address.
 func getAccount(accounts []authexported.GenesisAccount, addr sdk.AccAddress) (authexported.GenesisAccount, bool) {
 	for _, a := range accounts {
 		if a.GetAddress().Equals(addr) {
@@ -76,7 +96,8 @@ func getAccount(accounts []authexported.GenesisAccount, addr sdk.AccAddress) (au
 	return nil, false
 }
 
-func replaceAccount(accounts []authexported.GenesisAccount, acc authexported.GenesisAccount) []authexported.GenesisAccount {
+// In a list of accounts, replace the first account found with the same address. If not found, append the account.
+func replaceOrAppendAccount(accounts []authexported.GenesisAccount, acc authexported.GenesisAccount) []authexported.GenesisAccount {
 	newAccounts := accounts
 	for i, a := range accounts {
 		if a.GetAddress().Equals(acc.GetAddress()) {
@@ -84,5 +105,5 @@ func replaceAccount(accounts []authexported.GenesisAccount, acc authexported.Gen
 			return newAccounts
 		}
 	}
-	return newAccounts
+	return append(newAccounts, acc)
 }
