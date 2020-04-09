c5a3f9d5251d914b86a919180294a747c6e787ab
Kevin Davis <kjydavis3@gmail.com>
2020-04-09 09:54:23 -0400

    feat: add gen state, post price messages

--- x/pricefeed/simulation/operations/msg.go
+++ x/pricefeed/simulation/operations/msg.go
@@ -11,10 +11,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 
 	"github.com/kava-labs/kava/x/pricefeed"
-	"github.com/kava-labs/kava/x/pricefeed/keeper"
 	"github.com/kava-labs/kava/x/pricefeed/types"
-
-	tmtime "github.com/tendermint/tendermint/types/time"
 )
 
 var (
@@ -25,9 +22,9 @@ var (
 
 // SimulateMsgUpdatePrices updates the prices of various assets
 // TODO - MUST include BNB, USDX, BTC as HARD REQUIREMENTS - asset prices needed by CDP sims
-func SimulateMsgUpdatePrices(authKeeper auth.AccountKeeper, keeper keeper.Keeper) simulation.Operation {
+func SimulateMsgUpdatePrices(authKeeper auth.AccountKeeper, pfk pricefeed.Keeper) simulation.Operation {
 	// get a pricefeed handler
-	handler := pricefeed.NewHandler(keeper)
+	handler := pricefeed.NewHandler(pfk)
 
 	// There's two error paths
 	// - return a OpMessage, but nil error - this will log a message but keep running the simulation
@@ -42,100 +39,79 @@ func SimulateMsgUpdatePrices(authKeeper auth.AccountKeeper, keeper keeper.Keeper
 		// (4) MULTIPLY THE CURRENT PRICE BY THE RANDOM NUMBER
 		// (5) POST THE NEW PRICE TO THE KEEPER
 
-		// get the address for the account
-		// TODO QUESTION do we need to check if this address exists on chain or is an oracle for the price or does it not matter
-		address := sdk.AccAddress("kava1ffv7nhd3z6sych2qpqkk03ec6hzkmufy0r2s4c")
-
 		// pick a random asset out of BNB and BTC
-		assetCode, err := pickRandomAsset(r)
-		if err != nil {
-			return noOpMsg, nil, fmt.Errorf("Error picking random asset")
-		}
+		market := pickRandomMarket(ctx, pfk, r)
 
-		fmt.Printf("Picked asset: %s\n", assetCode)
+		fmt.Printf("Picked market: %s\n", market.MarketID)
 
 		// TODO QUESTION - GET THE CURRENT PRICE OF THE ASSET?? HOW TO DO THIS?
-		currentPrice := int64(100)
-
-		// generate a new random price based off the current price
-		price, err := pickNewRandomPrice(r, currentPrice)
+		currentPrice, err := pfk.GetCurrentPrice(ctx, market.MarketID)
 		if err != nil {
-			return noOpMsg, nil, fmt.Errorf("Error picking random price")
+			return noOpMsg, nil, fmt.Errorf("Error getting current price: %v", err)
 		}
 
+		oracle := getRandomOracle(r, market)
+
+		// generate a new random price based off the current price
+		price := pickNewRandomPrice(r, currentPrice.Price)
+
 		// get the expiry time based off the current time
-		expiry := getExpiryTime()
+		expiry := getExpiryTime(ctx)
 
 		// MSG POST PRICE
 		// GENERATE THE MSG TO SEND TO THE KEEPER
 		// now create the msg to post price
-		msg := types.NewMsgPostPrice(address, assetCode, price, expiry)
+		msg := types.NewMsgPostPrice(oracle, market.MarketID, price, expiry)
 
 		// Perform basic validation of the msg - don't submit errors that fail ValidateBasic, use unit tests for testing ValidateBasic
 		if err := msg.ValidateBasic(); err != nil {
-			return noOpMsg, nil, fmt.Errorf("expected msg to pass ValidateBasic: %s", msg.GetSignBytes())
+			return noOpMsg, nil, fmt.Errorf("expected msg to pass ValidateBasic: %v", err)
 		}
 
-		// now we submit the pricefeed update message
-		// TODO QUESTION - this is failing for some reason? Any ideas why?
 		if ok := submitMsg(ctx, handler, msg); !ok {
 			return noOpMsg, nil, fmt.Errorf("could not submit pricefeed msg")
 		}
-		fmt.Println("Pricefeed update sumbitted!")
-		return simulation.NewOperationMsg(msg, true, "pricefeed update submitted"), nil, nil // TODO what should go in comment field?
+		return simulation.NewOperationMsg(msg, true, "pricefeed update price"), nil, nil
 	}
 }
 
-// pickRandomAsset picks a random asset out of BNB and BTC with equal probability
-func pickRandomAsset(r *rand.Rand) (assetCode string, err error) {
+// pickRandomMarket picks a random asset from params
+func pickRandomMarket(ctx sdk.Context, pfk pricefeed.Keeper, r *rand.Rand) pricefeed.Market {
 	// randomly pick an asset
-	randomNum, err := simulation.RandPositiveInt(r, sdk.NewInt(100)) // get a random number
-	if err != nil {
-		return "", err
-	}
-	// pick each asset with 50% probability
-	if randomNum.LTE(sdk.NewInt(50)) {
-		assetCode = "BNB"
-	} else {
-		assetCode = "BTC"
-	}
-	return assetCode, nil
+	markets := pfk.GetMarkets(ctx)
+	return markets[simulation.RandIntBetween(r, 0, len(markets))]
 }
 
 // getExpiryTime gets a price expiry time by taking the current time and adding a delta to it
-func getExpiryTime() (t time.Time) {
-	t = tmtime.Now().Add(time.Second * 100000)
+func getExpiryTime(ctx sdk.Context) (t time.Time) {
+	t = ctx.BlockTime().Add(time.Hour * 24)
 	return t
 }
 
 // pickNewRandomPrice picks a new random price given the current price
 // It takes the current price then generates a random number to multiply it by to create variation while
 // still being in the similar range. Random walk style.
-func pickNewRandomPrice(r *rand.Rand, currentPrice int64) (price sdk.Dec, err sdk.Error) {
+func pickNewRandomPrice(r *rand.Rand, currentPrice sdk.Dec) (price sdk.Dec) {
 	// Pick random price
-	got, err := sdk.NewDecFromStr("0.4")
-	if err != nil {
-		return sdk.ZeroDec(), err
-	}
+	randomNoise := sdk.MustNewDecFromStr("0.4")
 
-	randomPriceMultiplier := simulation.RandomDecAmount(r, got) // get a random number
-	if err != nil {
-		return sdk.ZeroDec(), err
-		// return noOpMsg, nil, fmt.Errorf("Error picking random price")
-	}
+	randomPriceMultiplier := simulation.RandomDecAmount(r, randomNoise) // get a random number
 
-	offset, err := sdk.NewDecFromStr("0.8")
-	if err != nil {
-		return sdk.ZeroDec(), err
-		// return noOpMsg, nil, fmt.Errorf("Error picking random price")
-	}
+	offset := sdk.MustNewDecFromStr("0.8")
 
 	randomPriceMultiplier = randomPriceMultiplier.Add(offset) // gives a result in range 0.8-1.2 inclusive
 
 	// MULTIPLY CURRENT PRICE BY RANDOM PRICE MULTIPLER
-	price = randomPriceMultiplier.Mul(sdk.NewDec(int64(currentPrice)))
+	price = randomPriceMultiplier.Mul(currentPrice)
 	// return the price
-	return price, nil
+	return price
+}
+
+func getRandomOracle(r *rand.Rand, market pricefeed.Market) sdk.AccAddress {
+	fmt.Printf("picking random oracle for %v\n", market)
+	randn := simulation.RandIntBetween(r, 0, len(market.Oracles))
+	oracle := market.Oracles[randn]
+	return oracle
 }
 
 // submitMsg submits a message to the current instance of the keeper and returns a boolean whether the operation completed successfully or not
@@ -144,6 +120,9 @@ func submitMsg(ctx sdk.Context, handler sdk.Handler, msg sdk.Msg) (ok bool) {
 	ok = handler(ctx, msg).IsOK()
 	if ok {
 		write()
+	} else {
+		log := handler(ctx, msg).Log
+		fmt.Printf("Error when submitting msg to handler: %v\n", log)
 	}
 	return ok
 }
