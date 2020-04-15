d2f49d1aa2633a4bc08a82ee349ed3a1564de61e
John Maheswaran <john@noreply>
2020-04-15 15:43:24 -0400

    file for testing and to show random walk behavior. will remove before merging to master

---
+++ x/pricefeed/simulation/operations/pricegen_test.go
@@ -0,0 +1,37 @@
+package operations
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"testing"
+	"time"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/kava-labs/kava/x/pricefeed/types"
+)
+
+func TestPriceGen(t *testing.T) {
+	now := time.Now()
+	r := rand.New(rand.NewSource(now.Unix()))
+
+	n := int64(10000)
+
+	price := sdk.ZeroDec()
+	marketID := "btc:usd"
+	prices := make([]sdk.Dec, n)
+	for i := int64(0); i < n; i++ {
+		prices[i] = price
+		price = pickNewRandomPrice(r, marketID)
+	}
+
+	bz, err := types.ModuleCdc.MarshalJSONIndent(prices, "", "  ")
+	if err != nil {
+		t.FailNow()
+	}
+	err = ioutil.WriteFile(fmt.Sprintf("./pricefeed_test(%s).json", now), bz, 0644)
+	if err != nil {
+		t.FailNow()
+	}
+
+}
