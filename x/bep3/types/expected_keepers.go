dcceda48dc241fb7fae7cedea70bb3c67a0c0b69
denalimarsh <denalimarsh@gmail.com>
2020-05-01 13:00:16 -0700

    use expected keepers pattern

--- x/bep3/types/expected_keepers.go
+++ x/bep3/types/expected_keepers.go
@@ -6,7 +6,7 @@ import (
 	supplyexported "github.com/cosmos/cosmos-sdk/x/supply/exported"
 )
 
-// SupplyKeeper defines the expected supply keeper
+// SupplyKeeper defines the expected supply keeper (noalias)
 type SupplyKeeper interface {
 	GetModuleAddress(name string) sdk.AccAddress
 	GetModuleAccount(ctx sdk.Context, moduleName string) supplyexported.ModuleAccountI
@@ -19,7 +19,7 @@ type SupplyKeeper interface {
 	MintCoins(ctx sdk.Context, name string, amt sdk.Coins) error
 }
 
-// AccountKeeper defines the expected account keeper
+// AccountKeeper defines the expected account keeper (noalias)
 type AccountKeeper interface {
 	GetAccount(ctx sdk.Context, addr sdk.AccAddress) authexported.Account
 	NewAccountWithAddress(ctx sdk.Context, addr sdk.AccAddress) authexported.Account
