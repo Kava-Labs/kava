69b6f4c62ed45ac19778361ee3f3bbde8720118d
denalimarsh <denalimarsh@gmail.com>
2020-05-21 14:40:23 -0700

    cli auction query flags

--- x/auction/client/cli/query.go
+++ x/auction/client/cli/query.go
@@ -3,14 +3,24 @@ package cli
 import (
 	"fmt"
 	"strconv"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/kava-labs/kava/x/auction/types"
+)
+
+// Query auction flags
+const (
+	flagType  = "type"
+	flagDenom = "denom"
+	flagPhase = "phase"
 )
 
 // GetQueryCmd returns the cli query commands for this module
@@ -68,30 +78,95 @@ func QueryGetAuctionCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 
 // QueryGetAuctionsCmd queries the auctions in the store
 func QueryGetAuctionsCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
-	return &cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "auctions",
-		Short: "get a list of active auctions",
-		Args:  cobra.NoArgs,
+		Short: "query auctions with optional filters",
+		Long: strings.TrimSpace(`Query for all paginated auctions that match optional filters:
+Example:
+$ kvcli q auction auctions --type=(Collateral|Surplus|Debt)
+$ kvcli q auction auctions --denom=bnb
+$ kvcli q auction auctions --phase=(Forward|Reverse)
+$ kvcli q auction auctions --page=2 --limit=100
+`,
+		),
 		RunE: func(cmd *cobra.Command, args []string) error {
+			strType := viper.GetString(flagType)
+			strDenom := viper.GetString(flagDenom)
+			strPhase := viper.GetString(flagPhase)
+			page := viper.GetInt(flags.FlagPage)
+			limit := viper.GetInt(flags.FlagLimit)
+
+			var auctionType string
+			var auctionDenom string
+			var auctionPhase string
+
+			params := types.NewQueryAllAuctionParams(page, limit, auctionType, auctionDenom, auctionPhase)
+
+			if len(strType) != 0 {
+				auctionType = strings.ToLower(strings.TrimSpace(strType))
+				if auctionType != "collateral" && auctionType != "surplus" && auctionType != "debt" {
+					return fmt.Errorf("invalid auction type %s", strType)
+				}
+				params.Type = auctionType
+			}
+
+			if len(strDenom) != 0 {
+				auctionDenom := strings.TrimSpace(strDenom)
+				err := sdk.ValidateDenom(auctionDenom)
+				if err != nil {
+					return err
+				}
+				params.Denom = auctionDenom
+			}
+
+			if len(strPhase) != 0 {
+				auctionPhase := strings.ToLower(strings.TrimSpace(strPhase))
+				if auctionType != "collateral" && auctionType != "" {
+					return fmt.Errorf("cannot apply phase flag to non-collateral auction type")
+				}
+				if auctionPhase != "forward" && auctionPhase != "reverse" {
+					return fmt.Errorf("invalid auction phase %s", strPhase)
+				}
+				params.Phase = auctionPhase
+			}
+
+			bz, err := cdc.MarshalJSON(params)
+			if err != nil {
+				return err
+			}
+
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
 
 			// Query
-			res, _, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/%s", queryRoute, types.QueryGetAuctions), nil)
+			res, height, err := cliCtx.QueryWithData(fmt.Sprintf("custom/%s/%s", queryRoute, types.QueryGetAuctions), bz)
 			if err != nil {
 				return err
 			}
 
 			// Decode and print results
-			var auctions types.Auctions
-			cdc.MustUnmarshalJSON(res, &auctions)
+			var matchingAuctions types.Auctions
+			cdc.MustUnmarshalJSON(res, &matchingAuctions)
+
+			if len(matchingAuctions) == 0 {
+				return fmt.Errorf("No matching auctions found")
+			}
 
 			auctionsWithPhase := []types.AuctionWithPhase{} // using empty slice so json returns [] instead of null when there's no auctions
-			for _, a := range auctions {
+			for _, a := range matchingAuctions {
 				auctionsWithPhase = append(auctionsWithPhase, types.NewAuctionWithPhase(a))
 			}
-			return cliCtx.PrintOutput(auctionsWithPhase)
+			cliCtx = cliCtx.WithHeight(height)
+			return cliCtx.PrintOutput(auctionsWithPhase) // nolint:errcheck
 		},
 	}
+
+	cmd.Flags().Int(flags.FlagPage, 1, "pagination page of auctions to to query for")
+	cmd.Flags().Int(flags.FlagLimit, 100, "pagination limit of auctions to query for")
+	cmd.Flags().String(flagType, "", "(optional) filter by auction type, type: collateral, debt, surplus")
+	cmd.Flags().String(flagDenom, "", "(optional) filter by auction denom")
+	cmd.Flags().String(flagPhase, "", "(optional) filter by collateral auction phase, phase: forward/reverse")
+
+	return cmd
 }
 
 // QueryParamsCmd queries the auction module parameters
