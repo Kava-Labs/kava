7d039bc03e2a75da1af9701795a07fa42d041e97
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-05-24 23:14:37 +0100

    remove go-amino dependency from go.mod also tidy up unused indirect dependencies

--- go.mod
+++ go.mod
@@ -3,18 +3,15 @@ module github.com/kava-labs/kava
 go 1.13
 
 require (
-	github.com/btcsuite/btcd v0.20.1-beta // indirect
 	github.com/cosmos/cosmos-sdk v0.38.4
 	github.com/gorilla/mux v1.7.3
 	github.com/spf13/cobra v0.0.6
 	github.com/spf13/viper v1.6.2
 	github.com/stretchr/testify v1.5.1
-	github.com/tendermint/go-amino v0.15.1
 	github.com/tendermint/tendermint v0.33.3
 	github.com/tendermint/tm-db v0.5.0
-	golang.org/x/net v0.0.0-20190930134127-c5a3c61f89f3 // indirect
 	gopkg.in/yaml.v2 v2.2.8
 )
 
-// patch bech32 decoding
+// patch bech32 decoding to enable larger string lengths
 replace github.com/btcsuite/btcutil => github.com/kava-labs/btcutil v0.0.0-20200522184203-886d33430f06
--- migrate/v0_8/tendermint/v0_32/codec.go
+++ migrate/v0_8/tendermint/v0_32/codec.go
@@ -1,19 +1,23 @@
 package v032
 
 import (
-	amino "github.com/tendermint/go-amino"
+	"github.com/cosmos/cosmos-sdk/codec"
+	//amino "github.com/tendermint/go-amino"
 	cryptoAmino "github.com/tendermint/tendermint/crypto/encoding/amino"
 
 	"github.com/tendermint/tendermint/types"
 )
 
-var Cdc = amino.NewCodec()
+// Replace amino codec with sdk codec to avoid an explicit amino import in go.mod.
+// This will use a different version of amino from tendermint v0.32, but they are backwards compatible.
+// var Cdc = amino.NewCodec()
+var Cdc = codec.New()
 
 func init() {
 	RegisterBlockAmino(Cdc)
 }
 
-func RegisterBlockAmino(cdc *amino.Codec) {
+func RegisterBlockAmino(cdc *codec.Codec) {
 	cryptoAmino.RegisterAmino(cdc)
 	types.RegisterEvidences(cdc) // v0.33 is backwards compatible with v0.32 here
 }
