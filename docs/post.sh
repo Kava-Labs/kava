3fa2f3f5fb24e5755a7cae4704fd1aa607399ad9
denalimarsh <denalimarsh@gmail.com>
2020-06-01 13:42:42 -0700

    add tools, client docs to pre/post bas scripts

--- docs/post.sh
+++ docs/post.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
+# Module specs
 for D in ../x/*; do
   if [ -d "${D}" ]; then
     rm -rf "./$(echo $D | awk -F/ '{print $NF}')"
   fi
 done
+
+# JavaScript SDK docs
+ rm -rf "./building-on-kava"
+
+ # Kava Tools docs
+ rm -rf "./kava-tools"
--- docs/pre.sh
+++ docs/pre.sh
@@ -1,8 +1,27 @@
 #!/usr/bin/env bash
 
+# Module specs
 for D in ../x/*; do
   if [ -d "${D}" ]; then
     rm -rf "./$(echo $D | awk -F/ '{print $NF}')"
     mkdir -p "./$(echo $D | awk -F/ '{print $NF}')" && cp -r $D/spec/* "$_" && mv "./$_/README.md" "./$_/00_README.md" 
   fi
+done
+
+baseGitUrl="https://raw.githubusercontent.com/Kava-Labs"
+
+# Client docs (JavaScript SDK)
+clientGitRepo="javascript-sdk"
+clientDir="building-on-kava"
+
+mkdir -p "./${clientDir}"
+curl "${baseGitUrl}/${clientGitRepo}/master/README.md" -o "./${clientDir}/${clientGitRepo}.md"
+
+# Kava Tools docs
+toolsGitRepo="kava-tools"
+toolDocs=("auction" "oracle")
+
+mkdir -p "./${toolsGitRepo}"
+for T in ${toolDocs[@]}; do
+  curl "${baseGitUrl}/${toolsGitRepo}/master/${T}/README.md" -o "./${toolsGitRepo}/${T}.md"
 done
