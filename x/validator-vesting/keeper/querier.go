96b39767e65dbe064dba1c86a61cb3f2c94e348e
Kevin Davis <kjydavis3@gmail.com>
2020-06-12 19:54:24 -0400

    use complete circulating supply schedule

--- x/validator-vesting/keeper/querier.go
+++ x/validator-vesting/keeper/querier.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	"time"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
@@ -34,10 +36,51 @@ func queryGetTotalSupply(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) 
 }
 
 func queryGetCirculatingSupply(ctx sdk.Context, req abci.RequestQuery, keeper Keeper) ([]byte, error) {
-	supplyInt := sdk.NewInt(27190672)
+	supplyInt := getCirculatingSupply(ctx.BlockTime())
 	bz, err := keeper.cdc.MarshalJSON(supplyInt)
 	if err != nil {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
 	}
 	return bz, nil
+}
+
+func getCirculatingSupply(blockTime time.Time) sdk.Int {
+	vestingDates := []time.Time{
+		time.Date(2020, 9, 5, 14, 0, 0, 0, time.UTC),
+		time.Date(2020, 11, 5, 14, 0, 0, 0, time.UTC),
+		time.Date(2021, 2, 5, 14, 0, 0, 0, time.UTC),
+		time.Date(2021, 5, 5, 14, 0, 0, 0, time.UTC),
+		time.Date(2021, 8, 5, 14, 0, 0, 0, time.UTC),
+		time.Date(2021, 11, 5, 14, 0, 0, 0, time.UTC),
+		time.Date(2022, 2, 5, 14, 0, 0, 0, time.UTC),
+		time.Date(2022, 5, 5, 14, 0, 0, 0, time.UTC),
+		time.Date(2022, 8, 5, 14, 0, 0, 0, time.UTC),
+		time.Date(2022, 11, 5, 14, 0, 0, 0, time.UTC),
+	}
+
+	switch {
+	case blockTime.Before(vestingDates[0]):
+		return sdk.NewInt(27190672)
+	case blockTime.After(vestingDates[0]) && blockTime.Before(vestingDates[1]) || blockTime.Equal(vestingDates[0]):
+		return sdk.NewInt(29442227)
+	case blockTime.After(vestingDates[1]) && blockTime.Before(vestingDates[2]) || blockTime.Equal(vestingDates[1]):
+		return sdk.NewInt(46876230)
+	case blockTime.After(vestingDates[2]) && blockTime.Before(vestingDates[3]) || blockTime.Equal(vestingDates[2]):
+		return sdk.NewInt(58524186)
+	case blockTime.After(vestingDates[3]) && blockTime.Before(vestingDates[4]) || blockTime.Equal(vestingDates[3]):
+		return sdk.NewInt(70172142)
+	case blockTime.After(vestingDates[4]) && blockTime.Before(vestingDates[5]) || blockTime.Equal(vestingDates[4]):
+		return sdk.NewInt(81443180)
+	case blockTime.After(vestingDates[5]) && blockTime.Before(vestingDates[6]) || blockTime.Equal(vestingDates[5]):
+		return sdk.NewInt(90625000)
+	case blockTime.After(vestingDates[6]) && blockTime.Before(vestingDates[7]) || blockTime.Equal(vestingDates[6]):
+		return sdk.NewInt(92968750)
+	case blockTime.After(vestingDates[7]) && blockTime.Before(vestingDates[8]) || blockTime.Equal(vestingDates[7]):
+		return sdk.NewInt(95312500)
+	case blockTime.After(vestingDates[8]) && blockTime.Before(vestingDates[9]) || blockTime.Equal(vestingDates[8]):
+		return sdk.NewInt(97656250)
+	default:
+		return sdk.NewInt(100000000)
+	}
+
 }
