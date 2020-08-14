2392e3f4a0dbec7c4841fe4059786ad53299e3d0
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2020-08-14 20:52:22 +0100

    bump sdk version, make changes to get kvd running

--- cmd/kvd/main.go
+++ cmd/kvd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"io"
 
 	"github.com/spf13/cobra"
@@ -17,9 +16,7 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
 	"github.com/cosmos/cosmos-sdk/client/flags"
-	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/server"
-	srvconfig "github.com/cosmos/cosmos-sdk/server/config"
 	"github.com/cosmos/cosmos-sdk/store"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
@@ -27,7 +24,6 @@ import (
 	"github.com/cosmos/cosmos-sdk/x/staking"
 
 	"github.com/kava-labs/kava/app"
-	kava3 "github.com/kava-labs/kava/contrib/kava-3"
 	"github.com/kava-labs/kava/migrate"
 )
 
@@ -49,14 +45,13 @@ func main() {
 	rootCmd := &cobra.Command{
 		Use:               "kvd",
 		Short:             "Kava Daemon (server)",
-		PersistentPreRunE: persistentPreRunEFn(ctx),
+		PersistentPreRunE: server.PersistentPreRunEFn(ctx),
 	}
 
 	rootCmd.AddCommand(
 		genutilcli.InitCmd(ctx, cdc, app.ModuleBasics, app.DefaultNodeHome),
 		genutilcli.CollectGenTxsCmd(ctx, cdc, auth.GenesisAccountIterator{}, app.DefaultNodeHome),
 		migrate.MigrateGenesisCmd(ctx, cdc),
-		writeParamsAndConfigCmd(cdc),
 		genutilcli.GenTxCmd(
 			ctx,
 			cdc,
@@ -95,9 +90,14 @@ func newApp(logger log.Logger, db dbm.DB, traceStore io.Writer) abci.Application
 		skipUpgradeHeights[int64(h)] = true
 	}
 
+	pruningOpts, err := server.GetPruningOptionsFromFlags()
+	if err != nil {
+		panic(err)
+	}
+
 	return app.NewApp(
 		logger, db, traceStore, true, skipUpgradeHeights, invCheckPeriod,
-		baseapp.SetPruning(store.NewPruningOptionsFromString(viper.GetString("pruning"))),
+		baseapp.SetPruning(pruningOpts),
 		baseapp.SetMinGasPrices(viper.GetString(server.FlagMinGasPrices)),
 		baseapp.SetHaltHeight(viper.GetUint64(server.FlagHaltHeight)),
 		baseapp.SetHaltTime(viper.GetUint64(server.FlagHaltTime)),
@@ -119,58 +119,4 @@ func exportAppStateAndTMValidators(
 	}
 	tempApp := app.NewApp(logger, db, traceStore, true, map[int64]bool{}, uint(1))
 	return tempApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
-}
-
-// persistentPreRunEFn wraps the sdk function server.PersistentPreRunEFn to error on invaid pruning config.
-func persistentPreRunEFn(ctx *server.Context) func(*cobra.Command, []string) error {
-
-	originalFunc := server.PersistentPreRunEFn(ctx)
-
-	return func(cmd *cobra.Command, args []string) error {
-
-		if err := originalFunc(cmd, args); err != nil {
-			return err
-		}
-
-		// check pruning config for `kvd start`
-		if cmd.Name() == "start" {
-			if viper.GetString("pruning") == store.PruningStrategySyncable {
-				return fmt.Errorf(
-					"invalid app config: pruning == '%s'. Update config (%s) with pruning set to '%s' or '%s'.",
-					store.PruningStrategySyncable, viper.ConfigFileUsed(), store.PruningStrategyNothing, store.PruningStrategyEverything,
-				)
-			}
-		}
-		return nil
-	}
-}
-
-// writeParamsAndConfigCmd patches the write-params cmd to additionally update the app pruning config.
-func writeParamsAndConfigCmd(cdc *codec.Codec) *cobra.Command {
-	cmd := kava3.WriteGenesisParamsCmd(cdc)
-	originalFunc := cmd.RunE
-
-	wrappedFunc := func(cmd *cobra.Command, args []string) error {
-
-		if err := originalFunc(cmd, args); err != nil {
-			return err
-		}
-
-		// fetch the app config from viper
-		cfg, err := srvconfig.ParseConfig()
-		if err != nil {
-			return nil // don't return errors since as failures aren't critical
-		}
-		// don't prune any state, ie store everything
-		cfg.Pruning = store.PruningStrategyNothing
-		// write updated config
-		if viper.ConfigFileUsed() == "" {
-			return nil
-		}
-		srvconfig.WriteConfigFile(viper.ConfigFileUsed(), cfg)
-		return nil
-	}
-
-	cmd.RunE = wrappedFunc
-	return cmd
 }
