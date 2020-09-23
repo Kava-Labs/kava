5077aebdeab489c23036ea8f30b5574c8554c08b
karzak <kjydavis3@gmail.com>
2020-09-23 14:24:01 -0400

    draft kava-4 bep3 migration

--- migrate/v0_11/migrate.go
+++ migrate/v0_11/migrate.go
@@ -5,7 +5,7 @@ import (
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	v0_11bep3 "github.com/kava-labs/kava/x/bep3/legacy/v0_11"
+	v0_11bep3 "github.com/kava-labs/kava/x/bep3"
 	v0_9bep3 "github.com/kava-labs/kava/x/bep3/legacy/v0_9"
 )
 
@@ -16,7 +16,7 @@ func MigrateBep3(oldGenState v0_9bep3.GenesisState) v0_11bep3.GenesisState {
 	v0_9Params := oldGenState.Params
 
 	for _, asset := range v0_9Params.SupportedAssets {
-		v10AssetParam := v0_11bep3.AssetParam{
+		v11AssetParam := v0_11bep3.AssetParam{
 			Active:        asset.Active,
 			Denom:         asset.Denom,
 			CoinID:        asset.CoinID,
@@ -33,7 +33,7 @@ func MigrateBep3(oldGenState v0_9bep3.GenesisState) v0_11bep3.GenesisState {
 				TimeBasedLimit: sdk.ZeroInt(),
 			},
 		}
-		assetParams = append(assetParams, v10AssetParam)
+		assetParams = append(assetParams, v11AssetParam)
 	}
 	for _, supply := range oldGenState.AssetSupplies {
 		newSupply := v0_11bep3.NewAssetSupply(supply.IncomingSupply, supply.OutgoingSupply, supply.CurrentSupply, sdk.NewCoin(supply.CurrentSupply.Denom, sdk.ZeroInt()), time.Duration(0))
@@ -57,6 +57,27 @@ func MigrateBep3(oldGenState v0_9bep3.GenesisState) v0_11bep3.GenesisState {
 		}
 		swaps = append(swaps, newSwap)
 	}
+
+	// -------------- ADD BTCB To BEP3 params --------------------
+	btcbAssetParam := v0_11bep3.NewAssetParam(
+		"btcb",
+		0,
+		v0_11bep3.SupplyLimit{
+			Limit:          sdk.NewInt(10000000000), // TODO: 100 BTC limit at launch?
+			TimeLimited:    false,                   // TODO: Rate limiting?
+			TimePeriod:     time.Duration(0),
+			TimeBasedLimit: sdk.ZeroInt()},
+		true,
+		v0_9Params.BnbDeputyAddress, // TODO do we want to get an additional deputy address from binance?
+		v0_9Params.BnbDeputyFixedFee,
+		sdk.OneInt(),
+		sdk.NewInt(1000000000), // TODO: 10 BTC max swap?
+		220,
+		270,
+	)
+	btcbAssetSupply := v0_11bep3.NewAssetSupply(sdk.NewCoin("btcb", sdk.ZeroInt()), sdk.NewCoin("btcb", sdk.ZeroInt()), sdk.NewCoin("btcb", sdk.ZeroInt()), sdk.NewCoin("btcb", sdk.ZeroInt()), time.Duration(0))
+	assetParams = append(assetParams, btcbAssetParam)
+	assetSupplies = append(assetSupplies, btcbAssetSupply)
 	return v0_11bep3.GenesisState{
 		Params: v0_11bep3.Params{
 			AssetParams: assetParams},
