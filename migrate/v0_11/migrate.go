bdf98138bb0cef420059db8304c86990c1d665b9
karzak <kjydavis3@gmail.com>
2020-09-23 16:39:45 -0400

    draft kava-3 to kava-4 incentive migration

--- migrate/v0_11/migrate.go
+++ migrate/v0_11/migrate.go
@@ -7,6 +7,8 @@ import (
 
 	v0_11bep3 "github.com/kava-labs/kava/x/bep3/legacy/v0_11"
 	v0_9bep3 "github.com/kava-labs/kava/x/bep3/legacy/v0_9"
+	v0_11incentive "github.com/kava-labs/kava/x/incentive"
+	v0_9incentive "github.com/kava-labs/kava/x/incentive/legacy/v0_9"
 )
 
 // MigrateBep3 migrates from a v0.9 (or v0.10) bep3 genesis state to a v0.11 bep3 genesis state
@@ -64,4 +66,47 @@ func MigrateBep3(oldGenState v0_9bep3.GenesisState) v0_11bep3.GenesisState {
 		Supplies:          assetSupplies,
 		PreviousBlockTime: v0_11bep3.DefaultPreviousBlockTime,
 	}
+}
+
+// MigrateIncentive migrates from a v0.9 (or v0.10) incentive genesis state to a v0.11 incentive genesis state
+func MigrateIncentive(oldGenState v0_9incentive.GenesisState) v0_11incentive.GenesisState {
+	var newRewards v0_11incentive.Rewards
+	var newRewardPeriods v0_11incentive.RewardPeriods
+	var newClaimPeriods v0_11incentive.ClaimPeriods
+	var newClaims v0_11incentive.Claims
+	var newClaimPeriodIds v0_11incentive.GenesisClaimPeriodIDs
+
+	newMultiplier := v0_11incentive.NewMultiplier(v0_11incentive.Large, 12, sdk.OneDec())
+
+	for _, oldReward := range oldGenState.Params.Rewards {
+		newReward := v0_11incentive.NewReward(oldReward.Active, oldReward.Denom, oldReward.AvailableRewards, oldReward.Duration, v0_11incentive.Multipliers{newMultiplier}, oldReward.ClaimDuration)
+		newRewards = append(newRewards, newReward)
+	}
+	newParams := v0_11incentive.NewParams(true, newRewards)
+
+	for _, oldRewardPeriod := range oldGenState.RewardPeriods {
+
+		newRewardPeriod := v0_11incentive.NewRewardPeriod(oldRewardPeriod.Denom, oldRewardPeriod.Start, oldRewardPeriod.End, oldRewardPeriod.Reward, oldRewardPeriod.ClaimEnd, v0_11incentive.Multipliers{newMultiplier})
+		newRewardPeriods = append(newRewardPeriods, newRewardPeriod)
+	}
+
+	for _, oldClaimPeriod := range oldGenState.ClaimPeriods {
+		newClaimPeriod := v0_11incentive.NewClaimPeriod(oldClaimPeriod.Denom, oldClaimPeriod.ID, oldClaimPeriod.End, v0_11incentive.Multipliers{newMultiplier})
+		newClaimPeriods = append(newClaimPeriods, newClaimPeriod)
+	}
+
+	for _, oldClaim := range oldGenState.Claims {
+		newClaim := v0_11incentive.NewClaim(oldClaim.Owner, oldClaim.Reward, oldClaim.Denom, oldClaim.ClaimPeriodID)
+		newClaims = append(newClaims, newClaim)
+	}
+
+	for _, oldClaimPeriodID := range oldGenState.NextClaimPeriodIDs {
+		newClaimPeriodID := v0_11incentive.GenesisClaimPeriodID{
+			CollateralType: oldClaimPeriodID.Denom,
+			ID:             oldClaimPeriodID.ID,
+		}
+		newClaimPeriodIds = append(newClaimPeriodIds, newClaimPeriodID)
+	}
+
+	return v0_11incentive.NewGenesisState(newParams, oldGenState.PreviousBlockTime, newRewardPeriods, newClaimPeriods, newClaims, newClaimPeriodIds)
 }
--- migrate/v0_11/migrate_test.go
+++ migrate/v0_11/migrate_test.go
@@ -12,6 +12,7 @@ import (
 
 	"github.com/kava-labs/kava/app"
 	v0_9bep3 "github.com/kava-labs/kava/x/bep3/legacy/v0_9"
+	v0_9incentive "github.com/kava-labs/kava/x/incentive/legacy/v0_9"
 )
 
 func TestMain(m *testing.M) {
@@ -32,6 +33,20 @@ func TestMigrateBep3(t *testing.T) {
 	})
 
 	newGenState := MigrateBep3(oldGenState)
+	err = newGenState.Validate()
+	require.NoError(t, err)
+}
+
+func TestMigrateIncentive(t *testing.T) {
+	bz, err := ioutil.ReadFile(filepath.Join("testdata", "incentive-v09.json"))
+	require.NoError(t, err)
+	var oldGenState v0_9incentive.GenesisState
+	cdc := app.MakeCodec()
+	require.NotPanics(t, func() {
+		cdc.MustUnmarshalJSON(bz, &oldGenState)
+	})
+
+	newGenState := MigrateIncentive(oldGenState)
 	err = newGenState.Validate()
 	require.NoError(t, err)
 }
