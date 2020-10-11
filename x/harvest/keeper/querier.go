0f104b5b557a06815ff8d9fd7abc908ecd888d59
karzak <kjydavis3@gmail.com>
2020-10-11 12:44:42 -0400

    resolve KAV-01

--- x/harvest/keeper/querier.go
+++ x/harvest/keeper/querier.go
@@ -78,19 +78,9 @@ func queryGetDeposits(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte,
 	if err != nil {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
 	}
-	depositDenom := false
-	owner := false
-	depositType := false
-
-	if len(params.DepositDenom) > 0 {
-		depositDenom = true
-	}
-	if len(params.Owner) > 0 {
-		owner = true
-	}
-	if len(params.DepositType) > 0 {
-		depositType = true
-	}
+	depositDenom := len(params.DepositDenom) > 0
+	owner := len(params.Owner) > 0
+	depositType := len(params.DepositType) > 0
 
 	var deposits []types.Deposit
 	if depositDenom && owner && depositType {
