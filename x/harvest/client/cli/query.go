295405be8dfa3fe2497b828c79ac8b6bda4fb1f7
denalimarsh <denalimarsh@gmail.com>
2020-10-30 10:55:56 +0100

    address minor revisions

--- x/harvest/client/cli/query.go
+++ x/harvest/client/cli/query.go
@@ -21,7 +21,6 @@ import (
 const (
 	flagName         = "name"
 	flagDepositDenom = "deposit-denom"
-	flagBorrowDenom  = "borrow-denom"
 	flagOwner        = "owner"
 	flagDepositType  = "deposit-type"
 )
@@ -307,6 +306,5 @@ func queryBorrowsCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
 	cmd.Flags().Int(flags.FlagPage, 1, "pagination page to query for")
 	cmd.Flags().Int(flags.FlagLimit, 100, "pagination limit (max 100)")
 	cmd.Flags().String(flagOwner, "", "(optional) filter for borrows by owner address")
-	cmd.Flags().String(flagBorrowDenom, "", "(optional) filter for borrows by denom")
 	return cmd
 }
