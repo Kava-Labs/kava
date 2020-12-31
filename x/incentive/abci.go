9947663c3548f197cf006684503fc5fcfdf0aae3
karzak <kjydavis3@gmail.com>
2020-12-30 17:36:41 -0700

    add begin blocker

--- x/incentive/abci.go
+++ x/incentive/abci.go
@@ -8,5 +8,10 @@ import (
 
 // BeginBlocker runs at the start of every block
 func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
-
+	for _, rp := range k.GetParams(ctx).RewardPeriods {
+		err := k.AccumulateRewards(ctx, rp)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
