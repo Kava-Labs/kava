3c9837bc0f3e521bc74d19c9e81d3831ec63d828
denalimarsh <denalimarsh@gmail.com>
2021-01-13 20:37:16 +0100

    hook function definitions in hard module

--- x/hard/types/hooks.go
+++ x/hard/types/hooks.go
@@ -11,29 +11,29 @@ func NewMultiHARDHooks(hooks ...HARDHooks) MultiHARDHooks {
 }
 
 // BeforeDepositCreated runs before a deposit is created
-func (h MultiHARDHooks) BeforeDepositCreated(ctx sdk.Context, deposit Deposit) {
+func (h MultiHARDHooks) BeforeDepositCreated(ctx sdk.Context, deposit Deposit, denom string) {
 	for i := range h {
-		h[i].BeforeDepositCreated(ctx, deposit)
+		h[i].BeforeDepositCreated(ctx, deposit, denom)
 	}
 }
 
 // BeforeDepositModified runs before a deposit is modified
-func (h MultiHARDHooks) BeforeDepositModified(ctx sdk.Context, deposit Deposit) {
+func (h MultiHARDHooks) BeforeDepositModified(ctx sdk.Context, deposit Deposit, denom string) {
 	for i := range h {
-		h[i].BeforeDepositModified(ctx, deposit)
+		h[i].BeforeDepositModified(ctx, deposit, denom)
 	}
 }
 
 // BeforeBorrowCreated runs before a borrow is created
-func (h MultiHARDHooks) BeforeBorrowCreated(ctx sdk.Context, borrow Borrow) {
+func (h MultiHARDHooks) BeforeBorrowCreated(ctx sdk.Context, borrow Borrow, denom string) {
 	for i := range h {
-		h[i].BeforeBorrowCreated(ctx, borrow)
+		h[i].BeforeBorrowCreated(ctx, borrow, denom)
 	}
 }
 
 // BeforeBorrowModified runs before a borrow is modified
-func (h MultiHARDHooks) BeforeBorrowModified(ctx sdk.Context, borrow Borrow) {
+func (h MultiHARDHooks) BeforeBorrowModified(ctx sdk.Context, borrow Borrow, denom string) {
 	for i := range h {
-		h[i].BeforeBorrowModified(ctx, borrow)
+		h[i].BeforeBorrowModified(ctx, borrow, denom)
 	}
 }
