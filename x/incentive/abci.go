8f528ea4454d797790b875c9c9926a6ba97a3f33
denalimarsh <denalimarsh@gmail.com>
2021-01-18 22:23:47 +0100

    add hard accumulators to abci

--- x/incentive/abci.go
+++ x/incentive/abci.go
@@ -8,8 +8,21 @@ import (
 
 // BeginBlocker runs at the start of every block
 func BeginBlocker(ctx sdk.Context, k keeper.Keeper) {
-	for _, rp := range k.GetParams(ctx).USDXMintingRewardPeriods {
+	params := k.GetParams(ctx)
+	for _, rp := range params.USDXMintingRewardPeriods {
 		err := k.AccumulateUSDXMintingRewards(ctx, rp)
+		if err != nil {
+			panic(err)
+		}
+	}
+	for _, rp := range params.HardSupplyRewardPeriods {
+		err := k.AccumulateHardSupplyRewards(ctx, rp)
+		if err != nil {
+			panic(err)
+		}
+	}
+	for _, rp := range params.HardBorrowRewardPeriods {
+		err := k.AccumulateHardBorrowRewards(ctx, rp)
 		if err != nil {
 			panic(err)
 		}
