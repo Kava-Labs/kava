682b7987ded856271f7ed9d9c758743394ec803f
karzak <kjydavis3@gmail.com>
2021-02-22 14:14:19 -0700

    add validator vesting migrations

--- migrate/v0_13/migrate_test.go
+++ migrate/v0_13/migrate_test.go
@@ -22,6 +22,7 @@ import (
 	v0_11hard "github.com/kava-labs/kava/x/hard/legacy/v0_11"
 	v0_13incentive "github.com/kava-labs/kava/x/incentive"
 	v0_11incentive "github.com/kava-labs/kava/x/incentive/legacy/v0_11"
+	validatorvesting "github.com/kava-labs/kava/x/validator-vesting"
 
 	"github.com/stretchr/testify/require"
 )
@@ -55,6 +56,14 @@ func TestCDP(t *testing.T) {
 }
 
 func TestAuth(t *testing.T) {
+	validatorVestingChangeAddress, err := sdk.AccAddressFromBech32("kava1a3qmze57knfj29a5knqs5ptewh76v4fg23xsvn")
+	if err != nil {
+		panic(err)
+	}
+	validatorVestingUpdatedValAddress, err := sdk.ConsAddressFromBech32("kavavalcons1ucxhn6zh7y2zun49m36psjffrhmux7ukqxdcte")
+	if err != nil {
+		panic(err)
+	}
 	bz, err := ioutil.ReadFile(filepath.Join("testdata", "kava-4-auth-state-block-500000.json"))
 	require.NoError(t, err)
 	var oldGenState auth.GenesisState
@@ -65,6 +74,13 @@ func TestAuth(t *testing.T) {
 	harvestCoins := getModuleAccount(oldGenState.Accounts, "harvest").GetCoins()
 
 	newGenState := Auth(oldGenState)
+	for _, acc := range newGenState.Accounts {
+		if acc.GetAddress().Equals(validatorVestingChangeAddress) {
+			vacc := acc.(*validatorvesting.ValidatorVestingAccount)
+			require.Equal(t, int64(0), vacc.CurrentPeriodProgress.MissedBlocks)
+			require.Equal(t, validatorVestingUpdatedValAddress, vacc.ValidatorAddress)
+		}
+	}
 
 	err = auth.ValidateGenesis(newGenState)
 	require.NoError(t, err)
