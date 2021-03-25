ec162349058a277aaee9ac61d498fba0a45a4f6e
denalimarsh <denalimarsh@gmail.com>
2021-03-25 15:52:03 +0100

    unsynced querier keeper methods

--- x/incentive/keeper/querier.go
+++ x/incentive/keeper/querier.go
@@ -19,8 +19,12 @@ func NewQuerier(k Keeper) sdk.Querier {
 			return queryGetParams(ctx, req, k)
 		case types.QueryGetHardRewards:
 			return queryGetHardRewards(ctx, req, k)
+		case types.QueryGetHardRewardsUnsynced:
+			return queryGetHardRewardsUnsynced(ctx, req, k)
 		case types.QueryGetUSDXMintingRewards:
 			return queryGetUSDXMintingRewards(ctx, req, k)
+		case types.QueryGetUSDXMintingRewardsUnsynced:
+			return queryGetUSDXMintingRewardsUnsynced(ctx, req, k)
 		default:
 			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unknown %s query endpoint", types.ModuleName)
 		}
@@ -81,6 +85,41 @@ func queryGetHardRewards(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]by
 	return bz, nil
 }
 
+func queryGetHardRewardsUnsynced(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, error) {
+	var params types.QueryHardRewardsUnsyncedParams
+	err := types.ModuleCdc.UnmarshalJSON(req.Data, &params)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
+	}
+	owner := len(params.Owner) > 0
+
+	var hardClaims types.HardLiquidityProviderClaims
+	switch {
+	case owner:
+		hardClaim, foundHardClaim := k.GetHardLiquidityProviderClaim(ctx, params.Owner)
+		if foundHardClaim {
+			hardClaims = append(hardClaims, hardClaim)
+		}
+	default:
+		hardClaims = k.GetAllHardLiquidityProviderClaims(ctx)
+	}
+
+	var paginatedHardClaims types.HardLiquidityProviderClaims
+	startH, endH := client.Paginate(len(hardClaims), params.Page, params.Limit, 100)
+	if startH < 0 || endH < 0 {
+		paginatedHardClaims = types.HardLiquidityProviderClaims{}
+	} else {
+		paginatedHardClaims = hardClaims[startH:endH]
+	}
+
+	// Marshal Hard claims
+	bz, err := codec.MarshalJSONIndent(k.cdc, paginatedHardClaims)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	return bz, nil
+}
+
 func queryGetUSDXMintingRewards(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, error) {
 	var params types.QueryUSDXMintingRewardsParams
 	err := types.ModuleCdc.UnmarshalJSON(req.Data, &params)
@@ -116,6 +155,41 @@ func queryGetUSDXMintingRewards(ctx sdk.Context, req abci.RequestQuery, k Keeper
 
 	// Marshal USDX minting claims
 	bz, err := codec.MarshalJSONIndent(k.cdc, augmentedUsdxMintingClaims)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	return bz, nil
+}
+
+func queryGetUSDXMintingRewardsUnsynced(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, error) {
+	var params types.QueryUSDXMintingRewardsUnsyncedParams
+	err := types.ModuleCdc.UnmarshalJSON(req.Data, &params)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
+	}
+	owner := len(params.Owner) > 0
+
+	var usdxMintingClaims types.USDXMintingClaims
+	switch {
+	case owner:
+		usdxMintingClaim, foundUsdxMintingClaim := k.GetUSDXMintingClaim(ctx, params.Owner)
+		if foundUsdxMintingClaim {
+			usdxMintingClaims = append(usdxMintingClaims, usdxMintingClaim)
+		}
+	default:
+		usdxMintingClaims = k.GetAllUSDXMintingClaims(ctx)
+	}
+
+	var paginatedUsdxMintingClaims types.USDXMintingClaims
+	startU, endU := client.Paginate(len(usdxMintingClaims), params.Page, params.Limit, 100)
+	if startU < 0 || endU < 0 {
+		paginatedUsdxMintingClaims = types.USDXMintingClaims{}
+	} else {
+		paginatedUsdxMintingClaims = usdxMintingClaims[startU:endU]
+	}
+
+	// Marshal USDX minting claims
+	bz, err := codec.MarshalJSONIndent(k.cdc, paginatedUsdxMintingClaims)
 	if err != nil {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
 	}
