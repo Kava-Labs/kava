3a80c9b377fb247ab70c494ef38f5201906865a8
denalimarsh <denalimarsh@gmail.com>
2021-04-20 16:06:19 +0200

    refactor to committee interface

--- x/committee/types/committee.go
+++ x/committee/types/committee.go
@@ -13,68 +13,55 @@ import (
 
 const MaxCommitteeDescriptionLength int = 512
 
-// ------------------------------------------
-//				Committees
-// ------------------------------------------
-
-type VotingOption int
-
-const (
-	MembersOnly VotingOption = iota // 0
-	CoinVoting  VotingOption = iota // 1
-)
-
 type TallyOption int
 
 const (
-	FirstPastThePost VotingOption = iota // 0
-	Deadline         VotingOption = iota // 1
+	FirstPastThePost TallyOption = iota // 0
+	Deadline         TallyOption = iota // 1
 )
 
-// A Committee is a collection of addresses that are allowed to vote and enact any governance proposal that passes their permissions.
-type Committee struct {
-	ID               uint64           `json:"id" yaml:"id"`
-	Description      string           `json:"description" yaml:"description"`
-	Members          []sdk.AccAddress `json:"members" yaml:"members"` // Only used in MembersOnly committees
-	Permissions      []Permission     `json:"permissions" yaml:"permissions"`
-	VoteThreshold    sdk.Dec          `json:"vote_threshold" yaml:"vote_threshold"`       // Smallest percentage of members that must vote for a proposal to pass. Only used in MembersOnly committees.
-	ProposalDuration time.Duration    `json:"proposal_duration" yaml:"proposal_duration"` // The length of time a proposal remains active for. Proposals will close earlier if they get enough votes.
-	VotingOption     VotingOption     `json:"voting_option" yaml:"voting_option"`
-	Quorum           sdk.Dec          `json:"quorum" yaml:"quorum"`           // Only used in CoinVoting committees
-	TallyDenom       string           `json:"tally_denom" yaml:"tally_denom"` // Only used in CoinVoting committees
-	TallyOption      TallyOption      `json:"tally_option" yaml:"tally_option"`
+const (
+	MemberCommitteeType = "member"
+	TokenCommitteeType  = "token"
+)
+
+// Committee is an interface for handling common actions on committees
+type Committee interface {
+	GetID() uint64
+	GetDescription() string
+	GetType() string
+	GetPermissions() []Permission
+	HasPermissionsFor(ctx sdk.Context, appCdc *codec.Codec, pk ParamKeeper, proposal PubProposal) bool
+	GetVoteThreshold() sdk.Dec
+	GetTallyOption() TallyOption
+	Validate() error
 }
 
-func NewCommittee(id uint64, description string, members []sdk.AccAddress, permissions []Permission,
-	threshold sdk.Dec, duration time.Duration, votingOption VotingOption, quorum sdk.Dec, tallyOption TallyOption) Committee {
-	return Committee{
-		ID:               id,
-		Description:      description,
-		Members:          members,
-		Permissions:      permissions,
-		VoteThreshold:    threshold,
-		ProposalDuration: duration,
-		VotingOption:     votingOption,
-		Quorum:           quorum,
-		TallyOption:      tallyOption,
-	}
+// Committees is a slice of committees
+type Committees []Committee
+
+// BaseCommittee is a common type shared by all Committees
+type BaseCommittee struct {
+	ID               uint64        `json:"id" yaml:"id"`
+	Description      string        `json:"description" yaml:"description"`
+	Permissions      []Permission  `json:"permissions" yaml:"permissions"`
+	VoteThreshold    sdk.Dec       `json:"vote_threshold" yaml:"vote_threshold"`       // Smallest percentage that must vote for a proposal to pass
+	ProposalDuration time.Duration `json:"proposal_duration" yaml:"proposal_duration"` // The length of time a proposal remains active for. Proposals will close earlier if they get enough votes.
+	TallyOption      TallyOption   `json:"tally_option" yaml:"tally_option"`
 }
 
-func (c Committee) HasMember(addr sdk.AccAddress) bool {
-	if c.VotingOption == CoinVoting {
-		return false
-	}
-	for _, m := range c.Members {
-		if m.Equals(addr) {
-			return true
-		}
-	}
-	return false
-}
+// GetID is a getter for committee ID
+func (c BaseCommittee) GetID() uint64 { return c.ID }
+
+// GetDescription is a getter for committee Description
+func (c BaseCommittee) GetDescription() string { return c.Description }
+
+// GetPermissions is a getter for committee Permissions
+func (c BaseCommittee) GetPermissions() []Permission { return c.Permissions }
 
 // HasPermissionsFor returns whether the committee is authorized to enact a proposal.
 // As long as one permission allows the proposal then it goes through. Its the OR of all permissions.
-func (c Committee) HasPermissionsFor(ctx sdk.Context, appCdc *codec.Codec, pk ParamKeeper, proposal PubProposal) bool {
+func (c BaseCommittee) HasPermissionsFor(ctx sdk.Context, appCdc *codec.Codec, pk ParamKeeper, proposal PubProposal) bool {
 	for _, p := range c.Permissions {
 		if p.Allows(ctx, appCdc, pk, proposal) {
 			return true
@@ -83,21 +70,17 @@ func (c Committee) HasPermissionsFor(ctx sdk.Context, appCdc *codec.Codec, pk Pa
 	return false
 }
 
-func (c Committee) Validate() error {
+// GetVoteThreshold is a getter for committee VoteThreshold
+func (c BaseCommittee) GetVoteThreshold() sdk.Dec { return c.VoteThreshold }
 
-	addressMap := make(map[string]bool, len(c.Members))
-	for _, m := range c.Members {
-		// check there are no duplicate members
-		if _, ok := addressMap[m.String()]; ok {
-			return fmt.Errorf("committe cannot have duplicate members, %s", m)
-		}
-		// check for valid addresses
-		if m.Empty() {
-			return fmt.Errorf("committee cannot have empty member address")
-		}
-		addressMap[m.String()] = true
-	}
+// GetProposalDuration is a getter for committee ProposalDuration
+func (c BaseCommittee) GetProposalDuration() time.Duration { return c.ProposalDuration }
 
+// GetTallyOption is a getter for committee TallyOption
+func (c BaseCommittee) GetTallyOption() TallyOption { return c.TallyOption }
+
+// Validate validates BaseCommittee fields
+func (c BaseCommittee) Validate() error {
 	if len(c.Description) > MaxCommitteeDescriptionLength {
 		return fmt.Errorf("description length %d longer than max allowed %d", len(c.Description), MaxCommitteeDescriptionLength)
 	}
@@ -112,29 +95,131 @@ func (c Committee) Validate() error {
 		return fmt.Errorf("invalid proposal duration: %s", c.ProposalDuration)
 	}
 
-	switch c.VotingOption {
-	case MembersOnly:
-		if len(c.Members) == 0 {
-			return fmt.Errorf("committee cannot have zero members")
-		}
+	// threshold must be in the range (0,1]
+	if c.VoteThreshold.IsNil() || c.VoteThreshold.LTE(sdk.ZeroDec()) || c.VoteThreshold.GT(sdk.NewDec(1)) {
+		return fmt.Errorf("invalid threshold: %s", c.VoteThreshold)
+	}
 
-		// threshold must be in the range (0,1]
-		if c.VoteThreshold.IsNil() || c.VoteThreshold.LTE(sdk.ZeroDec()) || c.VoteThreshold.GT(sdk.NewDec(1)) {
-			return fmt.Errorf("invalid threshold: %s", c.VoteThreshold)
-		}
-	case CoinVoting:
-		err := sdk.ValidateDenom(c.TallyDenom)
-		if err != nil {
-			return err
-		}
+	return nil
+}
 
-		if len(c.Members) > 0 {
-			return fmt.Errorf("invalid members")
-		}
+func (c BaseCommittee) String() string {
+	return fmt.Sprintf(`Committee %d:
+  Description:              %s
+  Permissions:               			%s
+  VoteThreshold:            		  %s
+  ProposalDuration:        						%s
+  TallyOption:   						%d`,
+		c.ID, c.Description, c.Permissions,
+		c.VoteThreshold.String(), c.ProposalDuration.String(),
+		c.TallyOption,
+	)
+}
 
-		if c.Quorum.IsNegative() {
-			return fmt.Errorf("invalid quroum percentage: %s", c.Quorum)
+//
+type MemberCommittee struct {
+	BaseCommittee `json:"base_committee" yaml:"base_committee"`
+	Members       []sdk.AccAddress `json:"members" yaml:"members"`
+}
+
+func NewMemberCommittee(id uint64, description string, permissions []Permission, threshold sdk.Dec,
+	duration time.Duration, tallyOption TallyOption, members []sdk.AccAddress) MemberCommittee {
+	return MemberCommittee{
+		BaseCommittee: BaseCommittee{
+			ID:               id,
+			Description:      description,
+			Permissions:      permissions,
+			VoteThreshold:    threshold,
+			ProposalDuration: duration,
+			TallyOption:      tallyOption,
+		},
+		Members: members,
+	}
+}
+
+func (c MemberCommittee) GetType() string { return MemberCommitteeType }
+
+func (c MemberCommittee) GetMembers() []sdk.AccAddress { return c.Members }
+
+func (c MemberCommittee) HasMember(addr sdk.AccAddress) bool {
+	for _, m := range c.Members {
+		if m.Equals(addr) {
+			return true
 		}
+	}
+	return false
+}
+
+// Validate validates the MemberCommittee fields values
+func (c MemberCommittee) Validate() error {
+	if len(c.Members) <= 0 {
+		return fmt.Errorf("committee must have members")
+	}
+
+	addressMap := make(map[string]bool, len(c.Members))
+	for _, m := range c.Members {
+		// check there are no duplicate members
+		if _, ok := addressMap[m.String()]; ok {
+			return fmt.Errorf("committe cannot have duplicate members, %s", m)
+		}
+		// check for valid addresses
+		if m.Empty() {
+			return fmt.Errorf("committee cannot have empty member address")
+		}
+		addressMap[m.String()] = true
+	}
+
+	return c.BaseCommittee.Validate()
+}
+
+func (c MemberCommittee) String() string {
+	return fmt.Sprintf(`Committee %d:
+  Type:               %s
+  Description:              %s
+  Members:               %s
+  Permissions:               			%s
+  VoteThreshold:            		  %s
+  ProposalDuration:        						%s
+  TallyOption:   						%d`,
+		c.ID, c.GetType(), c.Description, c.GetMembers(), c.Permissions,
+		c.VoteThreshold.String(), c.ProposalDuration.String(),
+		c.TallyOption,
+	)
+}
+
+//
+type TokenCommittee struct {
+	BaseCommittee `json:"base_committee" yaml:"base_committee"`
+	Quorum        sdk.Dec `json:"quorum" yaml:"quorum"`
+	TallyDenom    string  `json:"tally_denom" yaml:"tally_denom"`
+}
+
+func NewTokenCommittee(id uint64, description string, permissions []Permission, threshold sdk.Dec,
+	duration time.Duration, tallyOption TallyOption, quorum sdk.Dec, tallyDenom string) TokenCommittee {
+	return TokenCommittee{
+		BaseCommittee: BaseCommittee{
+			ID:               id,
+			Description:      description,
+			Permissions:      permissions,
+			VoteThreshold:    threshold,
+			ProposalDuration: duration,
+			TallyOption:      tallyOption,
+		},
+		Quorum:     quorum,
+		TallyDenom: tallyDenom,
+	}
+}
+
+func (c TokenCommittee) GetType() string { return TokenCommitteeType }
+
+func (c TokenCommittee) Validate() error {
+	err := sdk.ValidateDenom(c.TallyDenom)
+	if err != nil {
+		return err
+	}
+
+	if c.Quorum.IsNegative() {
+		return fmt.Errorf("invalid quroum: %s", c.Quorum)
 	}
 
 	return nil
