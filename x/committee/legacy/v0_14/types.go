225ea3960d96db351c0d043a74fab2cd523cd864
denalimarsh <denalimarsh@gmail.com>
2021-05-06 15:29:28 +0200

    addRegisterCodec() to committee v14 legacy types

--- x/committee/legacy/v0_14/types.go
+++ x/committee/legacy/v0_14/types.go
@@ -982,3 +982,18 @@ func (gs GenesisState) Validate() error {
 	}
 	return nil
 }
+
+// RegisterCodec registers the necessary types for the module
+func RegisterCodec(cdc *codec.Codec) {
+
+	// Proposals
+	cdc.RegisterInterface((*PubProposal)(nil), nil)
+
+	// Permissions
+	cdc.RegisterInterface((*Permission)(nil), nil)
+	cdc.RegisterConcrete(GodPermission{}, "kava/GodPermission", nil)
+	cdc.RegisterConcrete(SimpleParamChangePermission{}, "kava/SimpleParamChangePermission", nil)
+	cdc.RegisterConcrete(TextPermission{}, "kava/TextPermission", nil)
+	cdc.RegisterConcrete(SoftwareUpgradePermission{}, "kava/SoftwareUpgradePermission", nil)
+	cdc.RegisterConcrete(SubParamChangePermission{}, "kava/SubParamChangePermission", nil)
+}
