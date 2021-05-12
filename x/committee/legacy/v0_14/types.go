bea0a6d9a6957cbcdacf13cccb3ecaed37ef5d91
denalimarsh <denalimarsh@gmail.com>
2021-05-12 13:21:56 +0200

    register msgs on legacy codec

--- x/committee/legacy/v0_14/types.go
+++ x/committee/legacy/v0_14/types.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/cosmos/cosmos-sdk/x/params"
 	paramstypes "github.com/cosmos/cosmos-sdk/x/params/types"
@@ -22,10 +23,18 @@ import (
 )
 
 const (
-	ModuleName                        = "committee"
-	MaxCommitteeDescriptionLength int = 512
-	// DefaultNextProposalID is the starting poiint for proposal IDs.
-	DefaultNextProposalID uint64 = 1
+	ModuleName                           = "committee"
+	RouterKey                            = ModuleName // RouterKey Top level router key
+	MaxCommitteeDescriptionLength int    = 512
+	DefaultNextProposalID         uint64 = 1                            // DefaultNextProposalID is the starting poiint for proposal IDs
+	TypeMsgSubmitProposal                = "commmittee_submit_proposal" // 'committee' prefix appended to avoid potential conflicts with gov msg types
+	TypeMsgVote                          = "committee_vote"
+)
+
+var (
+	ErrInvalidPubProposal = sdkerrors.New(ModuleName, 6, "invalid pubproposal")                                  // Declare custom error type, but don't register it here
+	ModuleCdc             *codec.Codec                                                                           // ModuleCdc is a generic codec to be used throughout module
+	_, _                  sdk.Msg                                               = MsgSubmitProposal{}, MsgVote{} // Enforce msg interface compliance
 )
 
 // Permission is anything with a method that validates whether a proposal is allowed by it or not.
@@ -442,6 +451,7 @@ func (perm SubParamChangePermission) Allows(ctx sdk.Context, appCdc *codec.Codec
 			return false // invalid json value, so just disallow
 		}
 	}
+
 	// only check if there was a proposed change
 	if foundIncomingAPs {
 		// Get the current value of the SupportedAssets
@@ -996,4 +1006,89 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(TextPermission{}, "kava/TextPermission", nil)
 	cdc.RegisterConcrete(SoftwareUpgradePermission{}, "kava/SoftwareUpgradePermission", nil)
 	cdc.RegisterConcrete(SubParamChangePermission{}, "kava/SubParamChangePermission", nil)
+
+	// Msgs
+	cdc.RegisterConcrete(MsgSubmitProposal{}, "kava/MsgSubmitProposal", nil)
+	cdc.RegisterConcrete(MsgVote{}, "kava/MsgVote", nil)
+}
+
+// MsgSubmitProposal is used by committee members to create a new proposal that they can vote on.
+type MsgSubmitProposal struct {
+	PubProposal PubProposal    `json:"pub_proposal" yaml:"pub_proposal"`
+	Proposer    sdk.AccAddress `json:"proposer" yaml:"proposer"`
+	CommitteeID uint64         `json:"committee_id" yaml:"committee_id"`
+}
+
+// NewMsgSubmitProposal creates a new MsgSubmitProposal instance
+func NewMsgSubmitProposal(pubProposal PubProposal, proposer sdk.AccAddress, committeeID uint64) MsgSubmitProposal {
+	return MsgSubmitProposal{
+		PubProposal: pubProposal,
+		Proposer:    proposer,
+		CommitteeID: committeeID,
+	}
+}
+
+// Route return the message type used for routing the message.
+func (msg MsgSubmitProposal) Route() string { return RouterKey }
+
+// Type returns a human-readable string for the message, intended for utilization within events.
+func (msg MsgSubmitProposal) Type() string { return TypeMsgSubmitProposal }
+
+// ValidateBasic does a simple validation check that doesn't require access to any other information.
+func (msg MsgSubmitProposal) ValidateBasic() error {
+	if msg.PubProposal == nil {
+		return sdkerrors.Wrap(ErrInvalidPubProposal, "pub proposal cannot be nil")
+	}
+	if msg.Proposer.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "proposer address cannot be empty")
+	}
+
+	return msg.PubProposal.ValidateBasic()
+}
+
+// GetSignBytes gets the canonical byte representation of the Msg.
+func (msg MsgSubmitProposal) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+// GetSigners returns the addresses of signers that must sign.
+func (msg MsgSubmitProposal) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{msg.Proposer}
+}
+
+// MsgVote is submitted by committee members to vote on proposals.
+type MsgVote struct {
+	ProposalID uint64         `json:"proposal_id" yaml:"proposal_id"`
+	Voter      sdk.AccAddress `json:"voter" yaml:"voter"`
+}
+
+// NewMsgVote creates a message to cast a vote on an active proposal
+func NewMsgVote(voter sdk.AccAddress, proposalID uint64) MsgVote {
+	return MsgVote{proposalID, voter}
+}
+
+// Route return the message type used for routing the message.
+func (msg MsgVote) Route() string { return RouterKey }
+
+// Type returns a human-readable string for the message, intended for utilization within events.
+func (msg MsgVote) Type() string { return TypeMsgVote }
+
+// ValidateBasic does a simple validation check that doesn't require access to any other information.
+func (msg MsgVote) ValidateBasic() error {
+	if msg.Voter.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "voter address cannot be empty")
+	}
+	return nil
+}
+
+// GetSignBytes gets the canonical byte representation of the Msg.
+func (msg MsgVote) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+// GetSigners returns the addresses of signers that must sign.
+func (msg MsgVote) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{msg.Voter}
 }
