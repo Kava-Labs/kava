6ff8ce03f6e482fa7e082966b874b264d210712a
denalimarsh <denalimarsh@gmail.com>
2021-06-04 18:46:40 +0200

    add genesis tests

--- x/swap/types/genesis_test.go
+++ x/swap/types/genesis_test.go
@@ -38,3 +38,129 @@ func TestGenesis_NotEmpty(t *testing.T) {
 	}
 	assert.False(t, nonEmptyGenesis.IsEmpty())
 }
+
+func TestGenesis_Validate_SwapFee(t *testing.T) {
+	type args struct {
+		name      string
+		swapFee   sdk.Dec
+		expectErr bool
+	}
+	// More comprehensive swap fee tests are in prams_test.go
+	testCases := []args{
+		{
+			"normal",
+			sdk.MustNewDecFromStr("0.25"),
+			false,
+		},
+		{
+			"negative",
+			sdk.MustNewDecFromStr("-0.5"),
+			true,
+		},
+		{
+			"greater than 1.0",
+			sdk.MustNewDecFromStr("1.001"),
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+
+		genesisState := types.GenesisState{
+			Params: types.Params{
+				Pairs:   types.DefaultPairs,
+				SwapFee: tc.swapFee,
+			},
+		}
+
+		err := genesisState.Validate()
+		if tc.expectErr {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+		}
+	}
+}
+
+func TestGenesis_Validate_Pairs(t *testing.T) {
+	type args struct {
+		name      string
+		pairs     types.Pairs
+		expectErr bool
+	}
+	// More comprehensive pair validation tests are in pair_test.go, params_test.go
+	testCases := []args{
+		{
+			"normal",
+			types.DefaultPairs,
+			false,
+		},
+		{
+			"invalid",
+			types.Pairs{
+				{
+					TokenA: "same",
+					TokenB: "same",
+				},
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+
+		genesisState := types.GenesisState{
+			Params: types.Params{
+				Pairs:   tc.pairs,
+				SwapFee: types.DefaultSwapFee,
+			},
+		}
+
+		err := genesisState.Validate()
+		if tc.expectErr {
+			assert.NotNil(t, err)
+		} else {
+			assert.Nil(t, err)
+		}
+	}
+}
+
+func TestGenesis_Equal(t *testing.T) {
+	params := types.Params{
+		types.NewPairs(types.NewPair("ukava", "usdx")),
+		sdk.MustNewDecFromStr("0.85"),
+	}
+
+	genesisA := types.GenesisState{params}
+	genesisB := types.GenesisState{params}
+
+	assert.True(t, genesisA.Equal(genesisB))
+}
+
+func TestGenesis_NotEqual(t *testing.T) {
+	baseParams := types.Params{
+		types.NewPairs(types.NewPair("ukava", "usdx")),
+		sdk.MustNewDecFromStr("0.85"),
+	}
+
+	// Base params
+	genesisAParams := baseParams
+	genesisA := types.GenesisState{genesisAParams}
+
+	// Different swap fee
+	genesisBParams := baseParams
+	genesisBParams.SwapFee = sdk.MustNewDecFromStr("0.84")
+	genesisB := types.GenesisState{genesisBParams}
+
+	// Different pairs
+	genesisCParams := baseParams
+	genesisCParams.Pairs = types.NewPairs(types.NewPair("ukava", "hard"))
+	genesisC := types.GenesisState{genesisCParams}
+
+	// A and B have different swap fees
+	assert.False(t, genesisA.Equal(genesisB))
+	// A and C have different pair token B denoms
+	assert.False(t, genesisA.Equal(genesisC))
+	// A and B and different swap fees and pair token B denoms
+	assert.False(t, genesisA.Equal(genesisB))
+}
