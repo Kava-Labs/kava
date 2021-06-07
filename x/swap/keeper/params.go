c686f3a89b2fef56b2a2724a4281360defd93799
Nick DeLuca <nickdeluca08@gmail.com>
2021-06-07 14:15:19 -0700

    remove unused method

--- x/swap/keeper/params.go
+++ x/swap/keeper/params.go
@@ -17,15 +17,3 @@ func (k Keeper) GetParams(ctx sdk.Context) types.Params {
 func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramSubspace.SetParamSet(ctx, &params)
 }
-
-// GetPairsWithDenom all pairs with the given token denom
-func (k Keeper) GetPairsWithDenom(ctx sdk.Context, denom string) types.Pairs {
-	params := k.GetParams(ctx)
-	pairs := types.Pairs{}
-	for _, p := range params.Pairs {
-		if p.TokenA == denom || p.TokenB == denom {
-			pairs = append(pairs, p)
-		}
-	}
-	return pairs
-}
