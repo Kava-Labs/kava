4fe658fb27c38b72da894d4a4c9cd1a11b089621
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2021-06-09 15:00:20 +0100

    migrate cdp test to new test state builders

--- x/incentive/keeper/cdp_test.go
+++ x/incentive/keeper/cdp_test.go
@@ -1,70 +1,69 @@
 package keeper_test
 
 import (
+	"testing"
 	"time"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
 	abci "github.com/tendermint/tendermint/abci/types"
 
-	cdptypes "github.com/kava-labs/kava/x/cdp/types"
+	"github.com/kava-labs/kava/app"
 	"github.com/kava-labs/kava/x/incentive/types"
 )
 
-func (suite *KeeperTestSuite) TestRiskyCDPsAccumulateRewards() {
-	suite.SetupWithGenState()
-	initialTime := suite.ctx.BlockTime()
+func TestRiskyCDPsAccumulateRewards(t *testing.T) {
+	genesisTime := time.Date(2020, 12, 15, 14, 0, 0, 0, time.UTC)
+	_, addrs := app.GeneratePrivKeyAddressPairs(5)
 
-	// Setup incentive state
+	initialCollateral := c("bnb", 1_000_000_000)
+	user := addrs[0]
+	authBuilder := app.NewAuthGenesisBuilder().
+		WithSimpleAccount(user, cs(initialCollateral))
+
 	collateralType := "bnb-a"
 	rewardsPerSecond := c(types.USDXMintingRewardDenom, 1_000_000)
-	params := types.NewParams(
-		types.RewardPeriods{types.NewRewardPeriod(true, collateralType, initialTime, initialTime.Add(4*oneYear), rewardsPerSecond)},
-		nil, // hard rewards not needed
-		nil,
-		nil, // delegator rewards not needed
-		types.Multipliers{types.NewMultiplier(types.MultiplierName("small"), 1, d("0.25")), types.NewMultiplier(types.MultiplierName("large"), 12, d("1.0"))},
-		initialTime.Add(5*oneYear),
+
+	incentBuilder := NewIncentiveGenesisBuilder().
+		WithGenesisTime(genesisTime).
+		WithSimpleUSDXRewardPeriod(collateralType, rewardsPerSecond)
+
+	tApp := app.NewTestApp()
+	tApp.InitializeFromGenesisStates(
+		authBuilder.BuildMarshalled(),
+		NewPricefeedGenStateMultiFromTime(genesisTime),
+		NewCDPGenStateMulti(),
+		incentBuilder.BuildMarshalled(),
 	)
-	suite.keeper.SetParams(suite.ctx, params)
-	suite.keeper.SetPreviousUSDXMintingAccrualTime(suite.ctx, collateralType, initialTime)
-	suite.keeper.SetUSDXMintingRewardFactor(suite.ctx, collateralType, sdk.ZeroDec())
+	ctx := tApp.NewContext(true, abci.Header{Height: 1, Time: genesisTime})
 
 	// Setup cdp state containing one CDP
-	cdpKeeper := suite.app.GetCDPKeeper()
-	initialCollateral := c("bnb", 1_000_000_000)
-	initialPrincipal := c("usdx", 100_000_000)
-	cdpKeeper.SetPreviousAccrualTime(suite.ctx, collateralType, suite.ctx.BlockTime())
-	cdpKeeper.SetInterestFactor(suite.ctx, collateralType, sdk.OneDec())
-	// add coins to user's address // TODO move this to auth genesis setup
-	sk := suite.app.GetSupplyKeeper()
-	sk.MintCoins(suite.ctx, cdptypes.ModuleName, cs(initialCollateral))
-	sk.SendCoinsFromModuleToAccount(suite.ctx, cdptypes.ModuleName, suite.addrs[0], cs(initialCollateral))
-
-	err := cdpKeeper.AddCdp(suite.ctx, suite.addrs[0], initialCollateral, initialPrincipal, collateralType)
-	suite.Require().NoError(err)
+	cdpKeeper := tApp.GetCDPKeeper()
+	err := cdpKeeper.AddCdp(ctx, user, initialCollateral, c("usdx", 100_000_000), collateralType)
+	require.NoError(t, err)
 
 	// Skip ahead two blocks to accumulate both interest and usdx reward for the cdp
 	// Two blocks are required because the cdp begin blocker runs before incentive begin blocker.
 	// In the first begin block the cdp is synced, which triggers its claim to sync. But no global rewards have accumulated yet so the sync does nothing.
 	// Global rewards accumulate immediately after during the incentive begin blocker.
 	// Rewards are added to the cdp's claim in the next block when the cdp is synced.
-	_ = suite.app.EndBlocker(suite.ctx, abci.RequestEndBlock{})
-	suite.ctx = suite.ctx.WithBlockTime(suite.ctx.BlockTime().Add(10 * time.Minute))
-	_ = suite.app.BeginBlocker(suite.ctx, abci.RequestBeginBlock{}) // height and time in header are ignored by module begin blockers
+	_ = tApp.EndBlocker(ctx, abci.RequestEndBlock{})
+	ctx = ctx.WithBlockTime(ctx.BlockTime().Add(10 * time.Minute))
+	_ = tApp.BeginBlocker(ctx, abci.RequestBeginBlock{}) // height and time in header are ignored by module begin blockers
 
-	_ = suite.app.EndBlocker(suite.ctx, abci.RequestEndBlock{})
-	suite.ctx = suite.ctx.WithBlockTime(suite.ctx.BlockTime().Add(10 * time.Minute))
-	_ = suite.app.BeginBlocker(suite.ctx, abci.RequestBeginBlock{})
+	_ = tApp.EndBlocker(ctx, abci.RequestEndBlock{})
+	ctx = ctx.WithBlockTime(ctx.BlockTime().Add(10 * time.Minute))
+	_ = tApp.BeginBlocker(ctx, abci.RequestBeginBlock{})
 
 	// check cdp rewards
-	cdp, found := cdpKeeper.GetCdpByOwnerAndCollateralType(suite.ctx, suite.addrs[0], collateralType)
-	suite.Require().True(found)
+	cdp, found := cdpKeeper.GetCdpByOwnerAndCollateralType(ctx, user, collateralType)
+	require.True(t, found)
 	// This additional sync adds the rewards accumulated at the end of the last begin block.
 	// They weren't added during the begin blocker as the incentive BB runs after the CDP BB.
-	suite.keeper.SynchronizeUSDXMintingReward(suite.ctx, cdp)
-	claim, found := suite.keeper.GetUSDXMintingClaim(suite.ctx, suite.addrs[0])
-	suite.Require().True(found)
+	incentiveKeeper := tApp.GetIncentiveKeeper()
+	incentiveKeeper.SynchronizeUSDXMintingReward(ctx, cdp)
+	claim, found := incentiveKeeper.GetUSDXMintingClaim(ctx, user)
+	require.True(t, found)
 
 	// rewards are roughly rewardsPerSecond * secondsElapsed (10mins) * num blocks (2)
-	suite.Require().Equal(c(types.USDXMintingRewardDenom, 1_200_001_671), claim.Reward)
+	require.Equal(t, c(types.USDXMintingRewardDenom, 1_200_000_557), claim.Reward)
 }
