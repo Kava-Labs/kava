401c645419fcd0f1d9318347773542d42849f8c7
denalimarsh <denalimarsh@gmail.com>
2021-06-11 16:14:21 +0200

    tx deposit rest

--- x/swap/client/rest/rest.go
+++ x/swap/client/rest/rest.go
@@ -4,6 +4,8 @@ import (
 	"github.com/gorilla/mux"
 
 	"github.com/cosmos/cosmos-sdk/client/context"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/rest"
 )
 
 // REST variable names
@@ -14,4 +16,12 @@ const ()
 func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
 	registerQueryRoutes(cliCtx, r)
 	registerTxRoutes(cliCtx, r)
+}
+
+// PostDepositReq defines the properties of a deposit create request's body
+type PostCreateDepositReq struct {
+	BaseReq rest.BaseReq   `json:"base_req" yaml:"base_req"`
+	From    sdk.AccAddress `json:"from" yaml:"from"`
+	TokenA  sdk.Coin       `json:"token_a" yaml:"token_a"`
+	TokenB  sdk.Coin       `json:"token_b" yaml:"token_b"`
 }
