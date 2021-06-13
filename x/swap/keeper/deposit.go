9ab89a7879382ba80300f798711507b1259f6e57
Nick DeLuca <nickdeluca08@gmail.com>
2021-06-13 10:26:54 -0700

    make helper methods private. they are tested via deposit method, and unit testing them would make test suite brittle and refactoring difficult

--- x/swap/keeper/deposit.go
+++ x/swap/keeper/deposit.go
@@ -16,7 +16,7 @@ func (k Keeper) Deposit(ctx sdk.Context, depositor sdk.AccAddress, amountA sdk.C
 		return sdkerrors.Wrap(types.ErrNotImplemented, fmt.Sprintf("can not deposit into existing pool '%s'", poolName))
 	}
 
-	err := k.DepositAllowed(ctx, poolName, amountA, amountB)
+	err := k.depositAllowed(ctx, poolName, amountA, amountB)
 	if err != nil {
 		return err
 	}
@@ -28,7 +28,7 @@ func (k Keeper) Deposit(ctx sdk.Context, depositor sdk.AccAddress, amountA sdk.C
 		return err
 	}
 
-	k.InitializePool(ctx, depositor, amountA, amountB)
+	k.initializePool(ctx, depositor, amountA, amountB)
 
 	ctx.EventManager().EmitEvent(
 		sdk.NewEvent(
@@ -42,17 +42,7 @@ func (k Keeper) Deposit(ctx sdk.Context, depositor sdk.AccAddress, amountA sdk.C
 	return nil
 }
 
-func (k Keeper) InitializePool(ctx sdk.Context, depositor sdk.AccAddress, amountA, amountB sdk.Coin) error {
-	pool, err := types.NewPool(amountA, amountB)
-	if err != nil {
-		return err
-	}
-	k.SetPool(ctx, pool)
-	k.SetDepositorShares(ctx, depositor, pool.Name(), pool.TotalShares)
-	return nil
-}
-
-func (k Keeper) DepositAllowed(ctx sdk.Context, poolName string, amountA, amountB sdk.Coin) error {
+func (k Keeper) depositAllowed(ctx sdk.Context, poolName string, amountA, amountB sdk.Coin) error {
 	params := k.GetParams(ctx)
 	for _, p := range params.AllowedPools {
 		if p.TokenA == amountA.Denom && p.TokenB == amountB.Denom {
@@ -60,4 +50,14 @@ func (k Keeper) DepositAllowed(ctx sdk.Context, poolName string, amountA, amount
 		}
 	}
 	return sdkerrors.Wrap(types.ErrNotAllowed, fmt.Sprintf("can not create pool '%s'", poolName))
+}
+
+func (k Keeper) initializePool(ctx sdk.Context, depositor sdk.AccAddress, amountA, amountB sdk.Coin) error {
+	pool, err := types.NewPool(amountA, amountB)
+	if err != nil {
+		return err
+	}
+	k.SetPool(ctx, pool)
+	k.SetDepositorShares(ctx, depositor, pool.Name(), pool.TotalShares)
+	return nil
 }
