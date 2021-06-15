16d48141981eb6fdfd6c61c15c5a1185cfc2992a
Nick DeLuca <nickdeluca08@gmail.com>
2021-06-15 12:17:16 -0700

    add ErrInvalidPool

--- x/swap/types/errors.go
+++ x/swap/types/errors.go
@@ -8,5 +8,6 @@ var (
 	ErrNotAllowed       = sdkerrors.Register(ModuleName, 2, "not allowed")
 	ErrInvalidDeadline  = sdkerrors.Register(ModuleName, 3, "invalid deadline")
 	ErrDeadlineExceeded = sdkerrors.Register(ModuleName, 4, "deadline exceeded")
-	ErrNotImplemented   = sdkerrors.Register(ModuleName, 5, "not implemented")
+	ErrInvalidPool      = sdkerrors.Register(ModuleName, 5, "invalid pool")
+	ErrNotImplemented   = sdkerrors.Register(ModuleName, 6, "not implemented")
 )
