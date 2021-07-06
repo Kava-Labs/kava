483a77546fed5a1b92ccd03929e0fc4ea51652cc
denalimarsh <denalimarsh@gmail.com>
2021-07-06 20:20:03 +0200

    add new msg type

--- x/incentive/types/msg.go
+++ x/incentive/types/msg.go
@@ -146,7 +146,7 @@ type MsgClaimHardRewardVVesting struct {
 	MultiplierName string         `json:"multiplier_name" yaml:"multiplier_name"`
 }
 
-// NewMsgClaimHardRewardVVesting returns a new MsgClaimHardReward.
+// NewMsgClaimHardRewardVVesting returns a new MsgClaimHardRewardVVesting.
 func NewMsgClaimHardRewardVVesting(sender, receiver sdk.AccAddress, multiplierName string) MsgClaimHardRewardVVesting {
 	return MsgClaimHardRewardVVesting{
 		Sender:         sender,
@@ -182,5 +182,92 @@ func (msg MsgClaimHardRewardVVesting) GetSignBytes() []byte {
 
 // GetSigners returns the addresses of signers that must sign.
 func (msg MsgClaimHardRewardVVesting) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{msg.Sender}
+}
+
+// MsgClaimDelegatorReward message type used to claim delegator rewards
+type MsgClaimDelegatorReward struct {
+	Sender         sdk.AccAddress `json:"sender" yaml:"sender"`
+	MultiplierName string         `json:"multiplier_name" yaml:"multiplier_name"`
+}
+
+// NewMsgClaimDelegatorReward returns a new MsgClaimDelegatorReward.
+func NewMsgClaimDelegatorReward(sender sdk.AccAddress, multiplierName string) MsgClaimDelegatorReward {
+	return MsgClaimDelegatorReward{
+		Sender:         sender,
+		MultiplierName: multiplierName,
+	}
+}
+
+// Route return the message type used for routing the message.
+func (msg MsgClaimDelegatorReward) Route() string { return RouterKey }
+
+// Type returns a human-readable string for the message, intended for utilization within tags.
+func (msg MsgClaimDelegatorReward) Type() string {
+	return "claim_delegator_reward"
+}
+
+// ValidateBasic does a simple validation check that doesn't require access to state.
+func (msg MsgClaimDelegatorReward) ValidateBasic() error {
+	if msg.Sender.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "sender address cannot be empty")
+	}
+	return MultiplierName(strings.ToLower(msg.MultiplierName)).IsValid()
+}
+
+// GetSignBytes gets the canonical byte representation of the Msg.
+func (msg MsgClaimDelegatorReward) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+// GetSigners returns the addresses of signers that must sign.
+func (msg MsgClaimDelegatorReward) GetSigners() []sdk.AccAddress {
+	return []sdk.AccAddress{msg.Sender}
+}
+
+// MsgClaimDelegatorRewardVVesting message type used to claim delegator rewards for validator vesting accounts
+type MsgClaimDelegatorRewardVVesting struct {
+	Sender         sdk.AccAddress `json:"sender" yaml:"sender"`
+	Receiver       sdk.AccAddress `json:"receiver" yaml:"receiver"`
+	MultiplierName string         `json:"multiplier_name" yaml:"multiplier_name"`
+}
+
+// MsgClaimDelegatorRewardVVesting returns a new MsgClaimDelegatorRewardVVesting.
+func NewMsgClaimDelegatorRewardVVesting(sender, receiver sdk.AccAddress, multiplierName string) MsgClaimDelegatorRewardVVesting {
+	return MsgClaimDelegatorRewardVVesting{
+		Sender:         sender,
+		Receiver:       receiver,
+		MultiplierName: multiplierName,
+	}
+}
+
+// Route return the message type used for routing the message.
+func (msg MsgClaimDelegatorRewardVVesting) Route() string { return RouterKey }
+
+// Type returns a human-readable string for the message, intended for utilization within tags.
+func (msg MsgClaimDelegatorRewardVVesting) Type() string {
+	return "claim_delegator_reward_vvesting"
+}
+
+// ValidateBasic does a simple validation check that doesn't require access to state.
+func (msg MsgClaimDelegatorRewardVVesting) ValidateBasic() error {
+	if msg.Sender.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "sender address cannot be empty")
+	}
+	if msg.Receiver.Empty() {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "receiver address cannot be empty")
+	}
+	return MultiplierName(strings.ToLower(msg.MultiplierName)).IsValid()
+}
+
+// GetSignBytes gets the canonical byte representation of the Msg.
+func (msg MsgClaimDelegatorRewardVVesting) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(msg)
+	return sdk.MustSortJSON(bz)
+}
+
+// GetSigners returns the addresses of signers that must sign.
+func (msg MsgClaimDelegatorRewardVVesting) GetSigners() []sdk.AccAddress {
 	return []sdk.AccAddress{msg.Sender}
 }
