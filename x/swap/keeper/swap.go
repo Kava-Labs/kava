94e41c1c4de40d7c86d82b3e91850fd59e02a10d
Nick DeLuca <nickdeluca08@gmail.com>
2021-07-13 13:27:24 -0500

    dry up swap keeper methods

--- x/swap/keeper/swap.go
+++ x/swap/keeper/swap.go
@@ -11,16 +11,9 @@ import (
 
 // SwapExactForTokens swaps an exact coin a input for a coin b output
 func (k *Keeper) SwapExactForTokens(ctx sdk.Context, requester sdk.AccAddress, exactCoinA, coinB sdk.Coin, slippageLimit sdk.Dec) error {
-	poolID := types.PoolID(exactCoinA.Denom, coinB.Denom)
-
-	poolRecord, found := k.GetPool(ctx, poolID)
-	if !found {
-		return sdkerrors.Wrapf(types.ErrInvalidPool, "pool %s not found", poolID)
-	}
-
-	pool, err := types.NewDenominatedPoolWithExistingShares(poolRecord.Reserves(), poolRecord.TotalShares)
+	poolID, pool, err := k.loadPool(ctx, exactCoinA.Denom, coinB.Denom)
 	if err != nil {
-		panic(fmt.Sprintf("invalid pool %s: %s", poolID, err))
+		return err
 	}
 
 	swapOutput, feePaid := pool.SwapWithExactInput(exactCoinA, k.GetSwapFee(ctx))
@@ -29,48 +22,22 @@ func (k *Keeper) SwapExactForTokens(ctx sdk.Context, requester sdk.AccAddress, e
 	}
 
 	priceChange := swapOutput.Amount.ToDec().Quo(coinB.Amount.ToDec())
-	slippage := sdk.OneDec().Sub(priceChange)
-	if slippage.GT(slippageLimit) {
-		return sdkerrors.Wrapf(types.ErrSlippageExceeded, "slippage %s > limit %s", slippage, slippageLimit)
-	}
-
-	k.SetPool(ctx, types.NewPoolRecord(pool))
-
-	if err := k.supplyKeeper.SendCoinsFromAccountToModule(ctx, requester, types.ModuleAccountName, sdk.NewCoins(exactCoinA)); err != nil {
+	if err := k.assertSlippageWithinLimit(priceChange, slippageLimit); err != nil {
 		return err
 	}
 
-	if err := k.supplyKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleAccountName, requester, sdk.NewCoins(swapOutput)); err != nil {
-		panic(err)
+	if err := k.commitSwap(ctx, poolID, pool, requester, exactCoinA, swapOutput, feePaid, "input"); err != nil {
+		return err
 	}
-
-	ctx.EventManager().EmitEvent(
-		sdk.NewEvent(
-			types.EventTypeSwapTrade,
-			sdk.NewAttribute(types.AttributeKeyPoolID, poolID),
-			sdk.NewAttribute(types.AttributeKeyRequester, requester.String()),
-			sdk.NewAttribute(types.AttributeKeySwapInput, exactCoinA.String()),
-			sdk.NewAttribute(types.AttributeKeySwapOutput, swapOutput.String()),
-			sdk.NewAttribute(types.AttributeKeyFeePaid, feePaid.String()),
-			sdk.NewAttribute(types.AttributeKeyExactDirection, "input"),
-		),
-	)
 
 	return nil
 }
 
 // SwapForExactTokens swaps a coin a input for an exact coin b output
 func (k *Keeper) SwapForExactTokens(ctx sdk.Context, requester sdk.AccAddress, coinA, exactCoinB sdk.Coin, slippageLimit sdk.Dec) error {
-	poolID := types.PoolID(coinA.Denom, exactCoinB.Denom)
-
-	poolRecord, found := k.GetPool(ctx, poolID)
-	if !found {
-		return sdkerrors.Wrapf(types.ErrInvalidPool, "pool %s not found", poolID)
-	}
-
-	pool, err := types.NewDenominatedPoolWithExistingShares(poolRecord.Reserves(), poolRecord.TotalShares)
+	poolID, pool, err := k.loadPool(ctx, coinA.Denom, exactCoinB.Denom)
 	if err != nil {
-		panic(fmt.Sprintf("invalid pool %s: %s", poolID, err))
+		return err
 	}
 
 	if exactCoinB.Amount.GTE(pool.Reserves().AmountOf(exactCoinB.Denom)) {
@@ -83,18 +50,59 @@ func (k *Keeper) SwapForExactTokens(ctx sdk.Context, requester sdk.AccAddress, c
 	swapInput, feePaid := pool.SwapWithExactOutput(exactCoinB, k.GetSwapFee(ctx))
 
 	priceChange := coinA.Amount.ToDec().Quo(swapInput.Sub(feePaid).Amount.ToDec())
+	if err := k.assertSlippageWithinLimit(priceChange, slippageLimit); err != nil {
+		return err
+	}
+
+	if err := k.commitSwap(ctx, poolID, pool, requester, swapInput, exactCoinB, feePaid, "output"); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (k Keeper) loadPool(ctx sdk.Context, denomA string, denomB string) (string, *types.DenominatedPool, error) {
+	poolID := types.PoolID(denomA, denomB)
+
+	poolRecord, found := k.GetPool(ctx, poolID)
+	if !found {
+		return poolID, nil, sdkerrors.Wrapf(types.ErrInvalidPool, "pool %s not found", poolID)
+	}
+
+	pool, err := types.NewDenominatedPoolWithExistingShares(poolRecord.Reserves(), poolRecord.TotalShares)
+	if err != nil {
+		panic(fmt.Sprintf("invalid pool %s: %s", poolID, err))
+	}
+
+	return poolID, pool, nil
+}
+
+func (k Keeper) assertSlippageWithinLimit(priceChange sdk.Dec, slippageLimit sdk.Dec) error {
 	slippage := sdk.OneDec().Sub(priceChange)
 	if slippage.GT(slippageLimit) {
 		return sdkerrors.Wrapf(types.ErrSlippageExceeded, "slippage %s > limit %s", slippage, slippageLimit)
 	}
 
+	return nil
+}
+
+func (k Keeper) commitSwap(
+	ctx sdk.Context,
+	poolID string,
+	pool *types.DenominatedPool,
+	requester sdk.AccAddress,
+	swapInput sdk.Coin,
+	swapOutput sdk.Coin,
+	feePaid sdk.Coin,
+	exactDirection string,
+) error {
 	k.SetPool(ctx, types.NewPoolRecord(pool))
 
 	if err := k.supplyKeeper.SendCoinsFromAccountToModule(ctx, requester, types.ModuleAccountName, sdk.NewCoins(swapInput)); err != nil {
 		return err
 	}
 
-	if err := k.supplyKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleAccountName, requester, sdk.NewCoins(exactCoinB)); err != nil {
+	if err := k.supplyKeeper.SendCoinsFromModuleToAccount(ctx, types.ModuleAccountName, requester, sdk.NewCoins(swapOutput)); err != nil {
 		panic(err)
 	}
 
@@ -104,9 +112,9 @@ func (k *Keeper) SwapForExactTokens(ctx sdk.Context, requester sdk.AccAddress, c
 			sdk.NewAttribute(types.AttributeKeyPoolID, poolID),
 			sdk.NewAttribute(types.AttributeKeyRequester, requester.String()),
 			sdk.NewAttribute(types.AttributeKeySwapInput, swapInput.String()),
-			sdk.NewAttribute(types.AttributeKeySwapOutput, exactCoinB.String()),
+			sdk.NewAttribute(types.AttributeKeySwapOutput, swapOutput.String()),
 			sdk.NewAttribute(types.AttributeKeyFeePaid, feePaid.String()),
-			sdk.NewAttribute(types.AttributeKeyExactDirection, "output"),
+			sdk.NewAttribute(types.AttributeKeyExactDirection, exactDirection),
 		),
 	)
 
