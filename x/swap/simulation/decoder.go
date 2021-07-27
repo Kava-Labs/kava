c252a1b99bf8869d659a84a704b60f3ac6e02868
Denali Marsh <denali@kava.io>
2021-07-27 12:57:23 +0200

    Swap module simulations: swap msg operations (#972)
    * simulate MsgSwapExactForTokens
    
    * extract deadline generation to custom method
    
    * simulate MsgSwapForExactTokens
    
    * implement simulation decoder
    
    * decoder test
    
    * add fee to msgs
    
    * update comments

--- x/swap/simulation/decoder.go
+++ x/swap/simulation/decoder.go
@@ -1,13 +1,29 @@
 package simulation
 
 import (
-	"github.com/tendermint/tendermint/libs/kv"
+	"bytes"
+	"fmt"
 
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/tendermint/tendermint/libs/kv"
+
+	"github.com/kava-labs/kava/x/swap/types"
 )
 
 // DecodeStore unmarshals the KVPair's Value to the module's corresponding type
 func DecodeStore(cdc *codec.Codec, kvA, kvB kv.Pair) string {
-	// TODO: as store keys are added to the module, test marshal/unmarshal of each key prefix
-	return ""
+	switch {
+	case bytes.Equal(kvA.Key[:1], types.PoolKeyPrefix):
+		var poolRecordA, poolRecordB types.PoolRecord
+		cdc.MustUnmarshalBinaryLengthPrefixed(kvA.Value, &poolRecordA)
+		cdc.MustUnmarshalBinaryLengthPrefixed(kvB.Value, &poolRecordB)
+		return fmt.Sprintf("%v\n%v", poolRecordA, poolRecordB)
+	case bytes.Equal(kvA.Key[:1], types.DepositorPoolSharesPrefix):
+		var shareRecordA, shareRecordB types.ShareRecord
+		cdc.MustUnmarshalBinaryLengthPrefixed(kvA.Value, &shareRecordA)
+		cdc.MustUnmarshalBinaryLengthPrefixed(kvB.Value, &shareRecordB)
+		return fmt.Sprintf("%v\n%v", shareRecordA, shareRecordB)
+	default:
+		panic(fmt.Sprintf("invalid %s key prefix %X", types.ModuleName, kvA.Key[:1]))
+	}
 }
