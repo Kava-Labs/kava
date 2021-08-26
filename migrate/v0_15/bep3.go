441479f8074dddd05c38fc752dc0b102284b6b11
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2021-08-26 18:40:21 +0100

    add bep3 height resets

--- migrate/v0_15/bep3.go
+++ migrate/v0_15/bep3.go
@@ -4,7 +4,26 @@ import (
 	v0_15bep3 "github.com/kava-labs/kava/x/bep3/types"
 )
 
-// Bep3 migrates from a v0.14 bep3 genesis state to a v0.15 incentive genesis state
-func Bep3(bep3GS v0_15bep3.GenesisState) v0_15bep3.GenesisState {
-	return bep3GS
+// Bep3 resets the swap expire/close heights for a chain starting at height 0.
+func Bep3(genesisState v0_15bep3.GenesisState) v0_15bep3.GenesisState {
+
+	var newSwaps v0_15bep3.AtomicSwaps
+	for _, swap := range genesisState.AtomicSwaps {
+
+		if swap.Status == v0_15bep3.Completed {
+			// reset closed block to one so completed swaps are removed from long term storage properly
+			swap.ClosedBlock = 1
+		}
+
+		if swap.Status == v0_15bep3.Open || swap.Status == v0_15bep3.Expired {
+			swap.Status = v0_15bep3.Expired // set open swaps to expired so they can be refunded after chain start
+			swap.ExpireHeight = 1           // set expire on first block as well to be safe
+		}
+
+		newSwaps = append(newSwaps, swap)
+	}
+
+	genesisState.AtomicSwaps = newSwaps
+
+	return genesisState
 }
