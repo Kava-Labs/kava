1a1e97865c24c1ef5caeca7fa61a90c222130026
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2021-10-21 13:27:37 +0100

    revert commented out params file

--- app/params/params.go
+++ app/params/params.go
@@ -1,27 +1,27 @@
 package params
 
-// TODO
+// TODO this has not been updated for v0.44
 
-// // Simulation parameter constants
-// const (
-// 	StakePerAccount           = "stake_per_account"
-// 	InitiallyBondedValidators = "initially_bonded_validators"
-// )
+// Simulation parameter constants
+const (
+	StakePerAccount           = "stake_per_account"
+	InitiallyBondedValidators = "initially_bonded_validators"
+)
 
-// // Default simulation operation weights for messages and gov proposals
-// const (
-// 	DefaultWeightMsgPlaceBid              int = 20
-// 	DefaultWeightMsgCreateAtomicSwap      int = 20
-// 	DefaultWeightMsgUpdatePrices          int = 20
-// 	DefaultWeightMsgCdp                   int = 20
-// 	DefaultWeightMsgClaimReward           int = 20
-// 	DefaultWeightMsgDeposit               int = 20
-// 	DefaultWeightMsgWithdraw              int = 20
-// 	DefaultWeightMsgSwapExactForTokens    int = 20
-// 	DefaultWeightMsgSwapForExactTokens    int = 20
-// 	DefaultWeightMsgIssue                 int = 20
-// 	DefaultWeightMsgRedeem                int = 20
-// 	DefaultWeightMsgBlock                 int = 20
-// 	DefaultWeightMsgPause                 int = 20
-// 	OpWeightSubmitCommitteeChangeProposal int = 20
-// )
+// Default simulation operation weights for messages and gov proposals
+const (
+	DefaultWeightMsgPlaceBid              int = 20
+	DefaultWeightMsgCreateAtomicSwap      int = 20
+	DefaultWeightMsgUpdatePrices          int = 20
+	DefaultWeightMsgCdp                   int = 20
+	DefaultWeightMsgClaimReward           int = 20
+	DefaultWeightMsgDeposit               int = 20
+	DefaultWeightMsgWithdraw              int = 20
+	DefaultWeightMsgSwapExactForTokens    int = 20
+	DefaultWeightMsgSwapForExactTokens    int = 20
+	DefaultWeightMsgIssue                 int = 20
+	DefaultWeightMsgRedeem                int = 20
+	DefaultWeightMsgBlock                 int = 20
+	DefaultWeightMsgPause                 int = 20
+	OpWeightSubmitCommitteeChangeProposal int = 20
+)
