e5724e3060b909d046e77c5f7f4c494ecb1d023c
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2021-10-21 13:10:12 +0100

    add cmd packages back in

---
+++ cmd/kvcli/cmd_keys.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/keys"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/kava-labs/kava/app"
+)
+
+/*
+NOTE TO FUTURE IMPLEMENTERS
+This monkey patches the sdk `keys` command, therefore needs to be reviewed on any sdk updates.
+
+The patch adds support for using kava's legacy bip44 coin type to the cli.
+Coin types are used to create a bip44 derivation path, which is used as a mapping from mnemonic to private key.
+
+In cosmos-sdk v0.38.3, all private keys are stored without reference to the mnemonic or bip44 derivation path, except ledger keys.
+Ledger keys are just references to a private key on a ledger device. They contain the bip44 derivation path.
+To patch the cli, we only need to modify:
+- when new ledger references are created
+- anything to do with converting a mnemonic to a private key.
+
+These only happen in `kvcli keys add` cmd.
+For private key generation, use a --legacy-hd-path flag to enable old coin type.
+The current cosmos ledger app (v1.5.3) only supports the legacy coin type. So we only need to ensure ledger reference creation doesn't use the new coin type.
+
+Signing txs:
+- with local keys
+	- the stored the priv key is used to sign, mnemonics or bip44 paths not involved
+- with ledger
+	- the stored bip44 path is used to instruct the ledger which key to sign with
+*/
+
+const flagLegacyHDPath = "legacy-hd-path"
+const flagHDPath = "hd-path" // this is copied from keys add cmd because it's not exported
+
+// getModifiedKeysCmd returns the standard cosmos-sdk/client/keys cmd but modified to support new and old bip44 coin types supported by kava.
+func getModifiedKeysCmd() *cobra.Command {
+	keysCmd := keys.Commands()
+	for _, c := range keysCmd.Commands() {
+		if c.Name() == "add" {
+			monkeyPatchCmdKeysAdd(c)
+		}
+		if c.Name() == "parse" {
+			monkeyPatchCmdKeysParse(c)
+		}
+	}
+	return keysCmd
+}
+
+func monkeyPatchCmdKeysParse(keysParseCmd *cobra.Command) {
+
+	// replace the run function with a wrapped version that sets the old coin type in the global config
+	oldRun := keysParseCmd.RunE
+	keysParseCmd.RunE = func(cmd *cobra.Command, args []string) error {
+
+		sdk.GetConfig().Seal()
+		return oldRun(cmd, args)
+	}
+}
+
+// monkeyPatchCmdKeysAdd modifies the `keys add` command to use the old bip44 coin type when a flag is passed.
+func monkeyPatchCmdKeysAdd(keysAddCmd *cobra.Command) {
+	// add flag
+	keysAddCmd.Flags().Bool(flagLegacyHDPath, false, fmt.Sprintf("Use the old bip44 coin type (%d) to derive addresses from mnemonics.", sdk.CoinType))
+
+	// replace description
+	keysAddCmd.Long = fmt.Sprintf(`Derive a new private key and encrypt to disk.
+	Optionally specify a BIP39 mnemonic, a BIP39 passphrase to further secure the mnemonic,
+	and BIP44 account/index numbers to derive a specific key. The key will be stored under the given name
+	and encrypted with the given password.
+
+	NOTE: This cli defaults to Kava's BIP44 coin type %d. Use the --%s flag to use the old one (%d).
+
+	The flag --recover allows one to recover a key from a seed passphrase.
+	If run with --dry-run, a key would be generated (or recovered) but not stored to the
+	local keystore.
+	Use the --pubkey flag to add arbitrary public keys to the keystore for constructing
+	multisig transactions.
+
+	You can add a multisig key by passing the list of key names you want the public
+	key to be composed of to the --multisig flag and the minimum number of signatures
+	required through --multisig-threshold. The keys are sorted by address, unless
+	the flag --nosort is set.
+	`, app.Bip44CoinType, flagLegacyHDPath, sdk.CoinType)
+
+	// replace the run function with a wrapped version that sets the old coin type in the global config
+	oldRun := keysAddCmd.RunE
+	keysAddCmd.RunE = func(cmd *cobra.Command, args []string) error {
+
+		if !viper.GetBool(flagLegacyHDPath) && viper.GetBool(flags.FlagUseLedger) {
+			return fmt.Errorf("cosmos ledger app only supports legacy bip44 coin type, must use --%s flag when adding ledger key", flagLegacyHDPath)
+		}
+		if viper.GetBool(flagLegacyHDPath) && viper.IsSet(flagHDPath) {
+			return fmt.Errorf("cannot use a custom hd path (--%s) and legacy bip44 coin type (--%s) at the same time", flagHDPath, flagLegacyHDPath)
+		}
+		if viper.GetBool(flagLegacyHDPath) {
+			preExistingCoinType := sdk.GetConfig().GetCoinType()
+			sdk.GetConfig().SetCoinType(sdk.CoinType) // set old coin type
+			err := oldRun(cmd, args)
+			sdk.GetConfig().SetCoinType(preExistingCoinType) // revert to pre-existing coin type
+			return err
+		}
+		return oldRun(cmd, args)
+	}
+}
---
+++ cmd/kvd/main.go
@@ -0,0 +1,177 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+
+	dbm "github.com/tendermint/tm-db"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+	"github.com/tendermint/tendermint/libs/cli"
+	"github.com/tendermint/tendermint/libs/log"
+	tmtypes "github.com/tendermint/tendermint/types"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/store"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	genutilcli "github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
+	"github.com/cosmos/cosmos-sdk/x/staking"
+
+	"github.com/kava-labs/kava/app"
+	"github.com/kava-labs/kava/migrate"
+)
+
+// kvd custom flags
+const (
+	flagInvCheckPeriod       = "inv-check-period"
+	flagMempoolEnableAuth    = "mempool.enable-authentication"
+	flagMempoolAuthAddresses = "mempool.authorized-addresses"
+)
+
+var invCheckPeriod uint
+
+func main() {
+	cdc := app.MakeCodec()
+
+	config := sdk.GetConfig()
+	app.SetBech32AddressPrefixes(config)
+	app.SetBip44CoinType(config)
+	config.Seal()
+
+	ctx := server.NewDefaultContext()
+	cobra.EnableCommandSorting = false
+	rootCmd := &cobra.Command{
+		Use:               "kvd",
+		Short:             "Kava Daemon (server)",
+		PersistentPreRunE: server.PersistentPreRunEFn(ctx),
+	}
+
+	rootCmd.AddCommand(
+		genutilcli.InitCmd(ctx, cdc, app.ModuleBasics, app.DefaultNodeHome),
+		genutilcli.CollectGenTxsCmd(ctx, cdc, auth.GenesisAccountIterator{}, app.DefaultNodeHome),
+		migrate.MigrateGenesisCmd(ctx, cdc),
+		migrate.AssertInvariantsCmd(ctx, cdc),
+		genutilcli.GenTxCmd(
+			ctx,
+			cdc,
+			app.ModuleBasics,
+			staking.AppModuleBasic{},
+			auth.GenesisAccountIterator{},
+			app.DefaultNodeHome,
+			app.DefaultCLIHome),
+		genutilcli.ValidateGenesisCmd(ctx, cdc, app.ModuleBasics),
+		AddGenesisAccountCmd(ctx, cdc, app.DefaultNodeHome, app.DefaultCLIHome),
+		testnetCmd(ctx, cdc, app.ModuleBasics, auth.GenesisAccountIterator{}),
+		flags.NewCompletionCmd(rootCmd, true),
+	)
+
+	server.AddCommands(ctx, cdc, rootCmd, newApp, exportAppStateAndTMValidators)
+
+	// prepare and add flags
+	executor := cli.PrepareBaseCmd(rootCmd, "KA", app.DefaultNodeHome)
+	rootCmd.PersistentFlags().UintVar(&invCheckPeriod, flagInvCheckPeriod,
+		0, "Assert registered invariants every N blocks")
+	startCmd, _, err := rootCmd.Find([]string{"start"})
+	if err != nil {
+		panic(fmt.Sprintf("could not find 'start' command on root command: %s", err))
+	}
+	startCmd.Flags().Bool(flagMempoolEnableAuth, false, "Configure the mempool to only accept transactions from authorized addresses")
+	err = viper.BindPFlag(flagMempoolEnableAuth, startCmd.Flags().Lookup(flagMempoolEnableAuth))
+	if err != nil {
+		panic(fmt.Sprintf("failed to bind flag: %s", err))
+	}
+	startCmd.Flags().StringSlice(flagMempoolAuthAddresses, []string{}, "Additional addresses to accept transactions from when the mempool is running in authorized mode (comma separated kava addresses)")
+	err = viper.BindPFlag(flagMempoolAuthAddresses, startCmd.Flags().Lookup(flagMempoolAuthAddresses))
+	if err != nil {
+		panic(fmt.Sprintf("failed to bind flag: %s", err))
+	}
+
+	// run main command
+	err = executor.Execute()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func newApp(logger log.Logger, db dbm.DB, traceStore io.Writer) abci.Application {
+	var cache sdk.MultiStorePersistentCache
+
+	if viper.GetBool(server.FlagInterBlockCache) {
+		cache = store.NewCommitKVStoreCacheManager()
+	}
+
+	skipUpgradeHeights := make(map[int64]bool)
+	for _, h := range viper.GetIntSlice(server.FlagUnsafeSkipUpgrades) {
+		skipUpgradeHeights[int64(h)] = true
+	}
+
+	pruningOpts, err := server.GetPruningOptionsFromFlags()
+	if err != nil {
+		panic(err)
+	}
+
+	mempoolEnableAuth := viper.GetBool(flagMempoolEnableAuth)
+	mempoolAuthAddresses, err := accAddressesFromBech32(viper.GetStringSlice(flagMempoolAuthAddresses)...)
+	if err != nil {
+		panic(fmt.Sprintf("could not get authorized address from config: %v", err))
+	}
+
+	return app.NewApp(
+		logger, db, traceStore,
+		app.AppOptions{
+			SkipLoadLatest:       false,
+			SkipUpgradeHeights:   skipUpgradeHeights,
+			InvariantCheckPeriod: invCheckPeriod,
+			MempoolEnableAuth:    mempoolEnableAuth,
+			MempoolAuthAddresses: mempoolAuthAddresses,
+		},
+		baseapp.SetPruning(pruningOpts),
+		baseapp.SetMinGasPrices(viper.GetString(server.FlagMinGasPrices)),
+		baseapp.SetHaltHeight(viper.GetUint64(server.FlagHaltHeight)),
+		baseapp.SetHaltTime(viper.GetUint64(server.FlagHaltTime)),
+		baseapp.SetInterBlockCache(cache),
+	)
+}
+
+func exportAppStateAndTMValidators(
+	logger log.Logger, db dbm.DB, traceStore io.Writer, height int64, forZeroHeight bool, jailWhiteList []string,
+) (json.RawMessage, []tmtypes.GenesisValidator, error) {
+
+	if height != -1 {
+		opts := app.AppOptions{
+			SkipLoadLatest:       true,
+			InvariantCheckPeriod: uint(1),
+		}
+		tempApp := app.NewApp(logger, db, traceStore, opts)
+		err := tempApp.LoadHeight(height)
+		if err != nil {
+			return nil, nil, err
+		}
+		return tempApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
+	}
+	opts := app.AppOptions{
+		SkipLoadLatest:       false,
+		InvariantCheckPeriod: uint(1),
+	}
+	tempApp := app.NewApp(logger, db, traceStore, opts)
+	return tempApp.ExportAppStateAndValidators(forZeroHeight, jailWhiteList)
+}
+
+func accAddressesFromBech32(addresses ...string) ([]sdk.AccAddress, error) {
+	var decodedAddresses []sdk.AccAddress
+	for _, s := range addresses {
+		a, err := sdk.AccAddressFromBech32(s)
+		if err != nil {
+			return nil, err
+		}
+		decodedAddresses = append(decodedAddresses, a)
+	}
+	return decodedAddresses, nil
+}
