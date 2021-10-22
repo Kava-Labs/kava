cc606ff224e164b84ce3eb8a16697c31076a38f4
DracoLi <draco@dracoli.com>
2021-10-22 16:07:20 -0400

    fix bad kavadist rest import

--- x/kavadist/client/rest/utils.go
+++ x/kavadist/client/rest/utils.go
@@ -4,7 +4,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/rest"
 
-	"github.com/Kava-Labs/draco-test/x/kavadist/types"
+	"github.com/kava-labs/kava/x/kavadist/types"
 )
 
 type (
