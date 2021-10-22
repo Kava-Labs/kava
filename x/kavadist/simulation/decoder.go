7893c6946d04664b397c87fdb3b82afdab105a7a
DracoLi <draco@dracoli.com>
2021-10-22 15:48:49 -0400

    upgrade simulation

--- x/kavadist/simulation/decoder.go
+++ x/kavadist/simulation/decoder.go
@@ -5,20 +5,18 @@ import (
 	"fmt"
 	"time"
 
-	"github.com/tendermint/tendermint/libs/kv"
-
-	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/types/kv"
 
 	"github.com/kava-labs/kava/x/kavadist/types"
 )
 
 // DecodeStore unmarshals the KVPair's Value to the corresponding cdp type
-func DecodeStore(cdc *codec.Codec, kvA, kvB kv.Pair) string {
+func DecodeStore(kvA, kvB kv.Pair) string {
 	switch {
 	case bytes.Equal(kvA.Key[:1], types.PreviousBlockTimeKey):
 		var timeA, timeB time.Time
-		cdc.MustUnmarshalBinaryLengthPrefixed(kvA.Value, &timeA)
-		cdc.MustUnmarshalBinaryLengthPrefixed(kvB.Value, &timeB)
+		timeA.UnmarshalBinary(kvA.Value)
+		timeB.UnmarshalBinary(kvB.Value)
 		return fmt.Sprintf("%s\n%s", timeA, timeB)
 
 	default:
