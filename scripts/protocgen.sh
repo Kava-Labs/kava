d851297319fc8b42106c4013fb7023e22ad7ff22
Derrick Lee <derrick@dlee.dev>
2021-10-27 08:54:16 -0700

    Update protocgen.sh output to reference kava
    Co-authored-by: Ruaridh <rhuairahrighairidh@users.noreply.github.com>

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -6,7 +6,7 @@ set -eo pipefail
 
 protoc_gen_gocosmos() {
   if ! grep "github.com/gogo/protobuf => github.com/regen-network/protobuf" go.mod &>/dev/null ; then
-    echo -e "\tPlease run this command from somewhere inside the cosmos-sdk folder."
+    echo -e "\tPlease run this command from somewhere inside the root kava folder."
     return 1
   fi
 
