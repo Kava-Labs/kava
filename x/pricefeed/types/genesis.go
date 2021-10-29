ebecc15868410ce990252d173e9e10f5c81f47dc
drklee3 <derrick@dlee.dev>
2021-10-29 16:26:42 -0700

    Remove GenesisState.Equal()

--- x/pricefeed/types/genesis.go
+++ x/pricefeed/types/genesis.go
@@ -1,9 +1,5 @@
 package types
 
-import (
-	"bytes"
-)
-
 // NewGenesisState creates a new genesis state for the pricefeed module
 func NewGenesisState(p Params, pp []PostedPrice) GenesisState {
 	return GenesisState{
@@ -18,18 +14,6 @@ func DefaultGenesisState() GenesisState {
 		DefaultParams(),
 		[]PostedPrice{},
 	)
-}
-
-// Equal checks whether two gov GenesisState structs are equivalent
-func (gs GenesisState) Equal(gs2 GenesisState) bool {
-	b1 := ModuleCdc.MustMarshal(&gs)
-	b2 := ModuleCdc.MustMarshal(&gs2)
-	return bytes.Equal(b1, b2)
-}
-
-// IsEmpty returns true if a GenesisState is empty
-func (gs GenesisState) IsEmpty() bool {
-	return gs.Equal(GenesisState{})
 }
 
 // Validate performs basic validation of genesis data returning an
