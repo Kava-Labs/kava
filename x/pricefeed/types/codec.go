88e06e1e57e01329da9163438f83c180aa62d6df
drklee3 <derrick@dlee.dev>
2021-10-29 16:21:48 -0700

    Remove copied codec comment

--- x/pricefeed/types/codec.go
+++ x/pricefeed/types/codec.go
@@ -25,14 +25,7 @@ func RegisterInterfaces(registry types.InterfaceRegistry) {
 }
 
 var (
-	amino = codec.NewLegacyAmino()
-
-	// ModuleCdc references the global x/gov module codec. Note, the codec should
-	// ONLY be used in certain instances of tests and for JSON encoding as Amino is
-	// still used for that purpose.
-	//
-	// The actual codec used for serialization should be provided to x/gov and
-	// defined at the application level.
+	amino     = codec.NewLegacyAmino()
 	ModuleCdc = codec.NewAminoCodec(amino)
 )
 
