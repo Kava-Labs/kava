a9c73932500565200155bb3904ea011926dd4b3e
Nick DeLuca <nickdeluca08@gmail.com>
2021-11-05 18:32:43 +0000

    use addr equals

--- x/swap/keeper/querier.go
+++ x/swap/keeper/querier.go
@@ -141,7 +141,7 @@ func filterShareRecords(ctx sdk.Context, records types.ShareRecords, params type
 
 		// match owner address (if supplied)
 		if len(params.Owner) > 0 {
-			matchOwner = s.Depositor.String() == params.Owner.String()
+			matchOwner = s.Depositor.Equals(params.Owner)
 		}
 
 		// match pool ID (if supplied)
