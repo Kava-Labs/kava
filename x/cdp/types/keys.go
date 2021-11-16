b877874e399722b6e7c34b45e88118889834d1ba
drklee3 <derrick@dlee.dev>
2021-11-16 11:21:56 -0800

    Use collateralType string for keys

--- x/cdp/types/keys.go
+++ x/cdp/types/keys.go
@@ -74,8 +74,8 @@ func GetCdpIDFromBytes(bz []byte) (cdpID uint64) {
 }
 
 // CdpKey key of a specific cdp in the store
-func CdpKey(denomByte byte, cdpID uint64) []byte {
-	return createKey([]byte{denomByte}, sep, GetCdpIDBytes(cdpID))
+func CdpKey(collateralType string, cdpID uint64) []byte {
+	return createKey([]byte(collateralType), sep, GetCdpIDBytes(cdpID))
 }
 
 // SplitCdpKey returns the component parts of a cdp key
@@ -85,8 +85,8 @@ func SplitCdpKey(key []byte) (string, uint64) {
 }
 
 // DenomIterKey returns the key for iterating over cdps of a certain denom in the store
-func DenomIterKey(denomByte byte) []byte {
-	return append([]byte{denomByte}, sep...)
+func DenomIterKey(collateralType string) []byte {
+	return append([]byte(collateralType), sep...)
 }
 
 // SplitDenomIterKey returns the component part of a key for iterating over cdps by denom
@@ -128,11 +128,11 @@ func CollateralRatioBytes(ratio sdk.Dec) []byte {
 }
 
 // CollateralRatioKey returns the key for querying a cdp by its liquidation ratio
-func CollateralRatioKey(denomByte byte, cdpID uint64, ratio sdk.Dec) []byte {
+func CollateralRatioKey(collateralType string, cdpID uint64, ratio sdk.Dec) []byte {
 	ratioBytes := CollateralRatioBytes(ratio)
 	idBytes := GetCdpIDBytes(cdpID)
 
-	return createKey([]byte{denomByte}, sep, ratioBytes, sep, idBytes)
+	return createKey([]byte(collateralType), sep, ratioBytes, sep, idBytes)
 }
 
 // SplitCollateralRatioKey split the collateral ratio key and return the denom, cdp id, and collateral:debt ratio
@@ -150,9 +150,9 @@ func SplitCollateralRatioKey(key []byte) (string, uint64, sdk.Dec) {
 }
 
 // CollateralRatioIterKey returns the key for iterating over cdps by denom and liquidation ratio
-func CollateralRatioIterKey(denomByte byte, ratio sdk.Dec) []byte {
+func CollateralRatioIterKey(collateralType string, ratio sdk.Dec) []byte {
 	ratioBytes := CollateralRatioBytes(ratio)
-	return createKey([]byte{denomByte}, sep, ratioBytes)
+	return createKey([]byte(collateralType), sep, ratioBytes)
 }
 
 // SplitCollateralRatioIterKey split the collateral ratio key and return the denom, cdp id, and collateral:debt ratio
