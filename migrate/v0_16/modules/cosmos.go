956fe1b9d3053c6484a77b2a989ca0fb45195c02
DracoLi <draco@dracoli.com>
2021-11-17 14:30:38 -0500

    update some notes

--- migrate/v0_16/modules/cosmos.go
+++ migrate/v0_16/modules/cosmos.go
@@ -13,12 +13,17 @@ import (
 func MigrateCosmosAppState(appState genutiltypes.AppMap, clientCtx client.Context) genutiltypes.AppMap {
 	app.SetSDKConfig()
 
-	// Remove module states that we don't want genutil to process.
-	// The auth state might require removal here due to issues with migrating validator vesting.
-	// TODO:
+	// To Be Removed Notes:
+	// To avoid copying over migrations from genutil that we can reuse, we can remove app states
+	// from modules that we don't want genutil to process before using genutil.Migrate.
+	// In most cases, its the v40 migrations that are going to be an issue since it processes all interfaces
+	// and we have a number of interfaces registered with cosmos modules.
+	// We will need to copy over the code and process those manually.
+	// Example:
 	// authState := appState[v039auth.ModuleName]
 	// delete(appState, v039auth.ModuleName)
 	// fmt.Println(authState)
+	// MigrateAuthState(authState, clientCtx)
 
 	appState = v040genutil.Migrate(appState, clientCtx)
 	appState = v043genutil.Migrate(appState, clientCtx)
