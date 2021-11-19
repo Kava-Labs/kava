a4322e41ea71f31108455ed5d480f60b8f203a0e
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2021-11-19 22:38:04 +0000

    reinstate grpc querier

--- x/auction/module.go
+++ x/auction/module.go
@@ -137,7 +137,7 @@ func (AppModule) ConsensusVersion() uint64 {
 // RegisterServices registers module services.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
-	// types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
+	types.RegisterQueryServer(cfg.QueryServer(), keeper.NewQueryServer(am.keeper))
 }
 
 // InitGenesis module init-genesis
