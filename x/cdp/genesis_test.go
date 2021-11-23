a675ca7c1cab6c3fa1f135e26490720719e7c517
drklee3 <derrick@dlee.dev>
2021-11-23 12:30:38 -0800

    Use MustMarshalJson for test genesis state

--- x/cdp/genesis_test.go
+++ x/cdp/genesis_test.go
@@ -154,7 +154,7 @@ func (suite *GenesisTestSuite) TestValidGenState() {
 
 	cdpGS := NewCDPGenStateMulti(cdc)
 	gs := types.GenesisState{}
-	suite.app.AppCodec().UnmarshalJSON(cdpGS["cdp"], &gs)
+	suite.app.AppCodec().MustUnmarshalJSON(cdpGS["cdp"], &gs)
 	gs.CDPs = cdps()
 	gs.StartingCdpID = uint64(5)
 	appGS := app.GenesisState{"cdp": suite.app.AppCodec().MustMarshalJSON(&gs)}
