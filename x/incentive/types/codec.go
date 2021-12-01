3410adb0186c0138f96b726c26b175e3f06de5bb
denalimarsh <denalimarsh@gmail.com>
2021-12-01 13:50:28 +0100

    revisions

--- x/incentive/types/codec.go
+++ x/incentive/types/codec.go
@@ -57,12 +57,6 @@ func RegisterInterfaces(registry types.InterfaceRegistry) {
 var (
 	amino = codec.NewLegacyAmino()
 
-	// ModuleCdc references the global x/gov module codec. Note, the codec should
-	// ONLY be used in certain instances of tests and for JSON encoding as Amino is
-	// still used for that purpose.
-	//
-	// The actual codec used for serialization should be provided to x/gov and
-	// defined at the application level.
 	ModuleCdc = codec.NewAminoCodec(amino)
 )
 
