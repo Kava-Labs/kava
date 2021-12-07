602b8ad41f5b4115796356f4881d037551a4bf07
drklee3 <derrick@dlee.dev>
2021-12-07 10:08:30 -0800

    Upgrade incentive integration/genesis test

---
+++ x/incentive/genesis_test.go
@@ -0,0 +1,350 @@
+package incentive_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/suite"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	"github.com/kava-labs/kava/app"
+	hardtypes "github.com/kava-labs/kava/x/hard/types"
+	"github.com/kava-labs/kava/x/incentive"
+	"github.com/kava-labs/kava/x/incentive/keeper"
+	"github.com/kava-labs/kava/x/incentive/types"
+	kavadisttypes "github.com/kava-labs/kava/x/kavadist/types"
+)
+
+const (
+	oneYear time.Duration = 365 * 24 * time.Hour
+)
+
+type GenesisTestSuite struct {
+	suite.Suite
+
+	ctx    sdk.Context
+	app    app.TestApp
+	keeper keeper.Keeper
+	addrs  []sdk.AccAddress
+
+	genesisTime time.Time
+}
+
+func (suite *GenesisTestSuite) SetupTest() {
+	tApp := app.NewTestApp()
+	keeper := tApp.GetIncentiveKeeper()
+	suite.genesisTime = time.Date(1998, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	_, addrs := app.GeneratePrivKeyAddressPairs(5)
+
+	authBuilder := app.NewAuthBankGenesisBuilder().
+		WithSimpleAccount(addrs[0], cs(c("bnb", 1e10), c("ukava", 1e10))).
+		WithSimpleModuleAccount(kavadisttypes.KavaDistMacc, cs(c("hard", 1e15), c("ukava", 1e15)))
+
+	loanToValue, _ := sdk.NewDecFromStr("0.6")
+	borrowLimit := sdk.NewDec(1000000000000000)
+	hardGS := hardtypes.NewGenesisState(
+		hardtypes.NewParams(
+			hardtypes.MoneyMarkets{
+				hardtypes.NewMoneyMarket("ukava", hardtypes.NewBorrowLimit(false, borrowLimit, loanToValue), "kava:usd", sdk.NewInt(1000000), hardtypes.NewInterestRateModel(sdk.MustNewDecFromStr("0.05"), sdk.MustNewDecFromStr("2"), sdk.MustNewDecFromStr("0.8"), sdk.MustNewDecFromStr("10")), sdk.MustNewDecFromStr("0.05"), sdk.ZeroDec()),
+				hardtypes.NewMoneyMarket("bnb", hardtypes.NewBorrowLimit(false, borrowLimit, loanToValue), "bnb:usd", sdk.NewInt(1000000), hardtypes.NewInterestRateModel(sdk.MustNewDecFromStr("0.05"), sdk.MustNewDecFromStr("2"), sdk.MustNewDecFromStr("0.8"), sdk.MustNewDecFromStr("10")), sdk.MustNewDecFromStr("0.05"), sdk.ZeroDec()),
+			},
+			sdk.NewDec(10),
+		),
+		hardtypes.DefaultAccumulationTimes,
+		hardtypes.DefaultDeposits,
+		hardtypes.DefaultBorrows,
+		hardtypes.DefaultTotalSupplied,
+		hardtypes.DefaultTotalBorrowed,
+		hardtypes.DefaultTotalReserves,
+	)
+	incentiveGS := types.NewGenesisState(
+		types.NewParams(
+			types.RewardPeriods{types.NewRewardPeriod(true, "bnb-a", suite.genesisTime.Add(-1*oneYear), suite.genesisTime.Add(oneYear), c("ukava", 122354))},
+			types.MultiRewardPeriods{types.NewMultiRewardPeriod(true, "bnb", suite.genesisTime.Add(-1*oneYear), suite.genesisTime.Add(oneYear), cs(c("hard", 122354)))},
+			types.MultiRewardPeriods{types.NewMultiRewardPeriod(true, "bnb", suite.genesisTime.Add(-1*oneYear), suite.genesisTime.Add(oneYear), cs(c("hard", 122354)))},
+			types.MultiRewardPeriods{types.NewMultiRewardPeriod(true, "ukava", suite.genesisTime.Add(-1*oneYear), suite.genesisTime.Add(oneYear), cs(c("hard", 122354)))},
+			types.MultiRewardPeriods{types.NewMultiRewardPeriod(true, "btcb/usdx", suite.genesisTime.Add(-1*oneYear), suite.genesisTime.Add(oneYear), cs(c("swp", 122354)))},
+			types.MultipliersPerDenoms{
+				{
+					Denom: "ukava",
+					Multipliers: types.Multipliers{
+						types.NewMultiplier(types.MULTIPLIER_NAME_LARGE, 12, d("1.0")),
+					},
+				},
+				{
+					Denom: "hard",
+					Multipliers: types.Multipliers{
+						types.NewMultiplier(types.MULTIPLIER_NAME_SMALL, 1, d("0.25")),
+						types.NewMultiplier(types.MULTIPLIER_NAME_LARGE, 12, d("1.0")),
+					},
+				},
+				{
+					Denom: "swp",
+					Multipliers: types.Multipliers{
+						types.NewMultiplier(types.MULTIPLIER_NAME_SMALL, 1, d("0.25")),
+						types.NewMultiplier(types.MULTIPLIER_NAME_MEDIUM, 6, d("0.8")),
+					},
+				},
+			},
+			suite.genesisTime.Add(5*oneYear),
+		),
+		types.DefaultGenesisRewardState,
+		types.DefaultGenesisRewardState,
+		types.DefaultGenesisRewardState,
+		types.DefaultGenesisRewardState,
+		types.DefaultGenesisRewardState,
+		types.DefaultUSDXClaims,
+		types.DefaultHardClaims,
+		types.DefaultDelegatorClaims,
+		types.DefaultSwapClaims,
+	)
+	tApp.InitializeFromGenesisStatesWithTime(
+		suite.genesisTime,
+		authBuilder.BuildMarshalled(suite.app.AppCodec()),
+		app.GenesisState{types.ModuleName: types.ModuleCdc.MustMarshalJSON(&incentiveGS)},
+		app.GenesisState{hardtypes.ModuleName: hardtypes.ModuleCdc.MustMarshalJSON(&hardGS)},
+		NewCDPGenStateMulti(),
+		NewPricefeedGenStateMultiFromTime(suite.genesisTime),
+	)
+
+	ctx := tApp.NewContext(true, tmproto.Header{Height: 1, Time: suite.genesisTime})
+
+	suite.addrs = addrs
+	suite.keeper = keeper
+	suite.app = tApp
+	suite.ctx = ctx
+}
+
+func (suite *GenesisTestSuite) TestExportedGenesisMatchesImported() {
+	genesisTime := time.Date(1998, 1, 1, 0, 0, 0, 0, time.UTC)
+	genesisState := types.NewGenesisState(
+		types.NewParams(
+			types.RewardPeriods{types.NewRewardPeriod(true, "bnb-a", genesisTime.Add(-1*oneYear), genesisTime.Add(oneYear), c("ukava", 122354))},
+			types.MultiRewardPeriods{types.NewMultiRewardPeriod(true, "bnb", genesisTime.Add(-1*oneYear), genesisTime.Add(oneYear), cs(c("hard", 122354)))},
+			types.MultiRewardPeriods{types.NewMultiRewardPeriod(true, "bnb", genesisTime.Add(-1*oneYear), genesisTime.Add(oneYear), cs(c("hard", 122354)))},
+			types.MultiRewardPeriods{types.NewMultiRewardPeriod(true, "ukava", genesisTime.Add(-1*oneYear), genesisTime.Add(oneYear), cs(c("hard", 122354)))},
+			types.MultiRewardPeriods{types.NewMultiRewardPeriod(true, "btcb/usdx", genesisTime.Add(-1*oneYear), genesisTime.Add(oneYear), cs(c("swp", 122354)))},
+			types.MultipliersPerDenoms{
+				{
+					Denom: "ukava",
+					Multipliers: types.Multipliers{
+						types.NewMultiplier(types.MULTIPLIER_NAME_LARGE, 12, d("1.0")),
+					},
+				},
+				{
+					Denom: "hard",
+					Multipliers: types.Multipliers{
+						types.NewMultiplier(types.MULTIPLIER_NAME_SMALL, 1, d("0.25")),
+						types.NewMultiplier(types.MULTIPLIER_NAME_LARGE, 12, d("1.0")),
+					},
+				},
+				{
+					Denom: "swp",
+					Multipliers: types.Multipliers{
+						types.NewMultiplier(types.MULTIPLIER_NAME_SMALL, 1, d("0.25")),
+						types.NewMultiplier(types.MULTIPLIER_NAME_MEDIUM, 6, d("0.8")),
+					},
+				},
+			},
+			genesisTime.Add(5*oneYear),
+		),
+		types.NewGenesisRewardState(
+			types.AccumulationTimes{
+				types.NewAccumulationTime("bnb-a", genesisTime),
+			},
+			types.MultiRewardIndexes{
+				types.NewMultiRewardIndex("bnb-a", types.RewardIndexes{{CollateralType: "ukava", RewardFactor: d("0.3")}}),
+			},
+		),
+		types.NewGenesisRewardState(
+			types.AccumulationTimes{
+				types.NewAccumulationTime("bnb", genesisTime.Add(-1*time.Hour)),
+			},
+			types.MultiRewardIndexes{
+				types.NewMultiRewardIndex("bnb", types.RewardIndexes{{CollateralType: "hard", RewardFactor: d("0.1")}}),
+			},
+		),
+		types.NewGenesisRewardState(
+			types.AccumulationTimes{
+				types.NewAccumulationTime("bnb", genesisTime.Add(-2*time.Hour)),
+			},
+			types.MultiRewardIndexes{
+				types.NewMultiRewardIndex("bnb", types.RewardIndexes{{CollateralType: "hard", RewardFactor: d("0.05")}}),
+			},
+		),
+		types.NewGenesisRewardState(
+			types.AccumulationTimes{
+				types.NewAccumulationTime("ukava", genesisTime.Add(-3*time.Hour)),
+			},
+			types.MultiRewardIndexes{
+				types.NewMultiRewardIndex("ukava", types.RewardIndexes{{CollateralType: "hard", RewardFactor: d("0.2")}}),
+			},
+		),
+		types.NewGenesisRewardState(
+			types.AccumulationTimes{
+				types.NewAccumulationTime("bctb/usdx", genesisTime.Add(-4*time.Hour)),
+			},
+			types.MultiRewardIndexes{
+				types.NewMultiRewardIndex("btcb/usdx", types.RewardIndexes{{CollateralType: "swap", RewardFactor: d("0.001")}}),
+			},
+		),
+		types.USDXMintingClaims{
+			types.NewUSDXMintingClaim(
+				suite.addrs[0],
+				c("ukava", 1e9),
+				types.RewardIndexes{{CollateralType: "bnb-a", RewardFactor: d("0.3")}},
+			),
+			types.NewUSDXMintingClaim(
+				suite.addrs[1],
+				c("ukava", 1),
+				types.RewardIndexes{{CollateralType: "bnb-a", RewardFactor: d("0.001")}},
+			),
+		},
+		types.HardLiquidityProviderClaims{
+			types.NewHardLiquidityProviderClaim(
+				suite.addrs[0],
+				cs(c("ukava", 1e9), c("hard", 1e9)),
+				types.MultiRewardIndexes{{CollateralType: "bnb", RewardIndexes: types.RewardIndexes{{CollateralType: "hard", RewardFactor: d("0.01")}}}},
+				types.MultiRewardIndexes{{CollateralType: "bnb", RewardIndexes: types.RewardIndexes{{CollateralType: "hard", RewardFactor: d("0.0")}}}},
+			),
+			types.NewHardLiquidityProviderClaim(
+				suite.addrs[1],
+				cs(c("hard", 1)),
+				types.MultiRewardIndexes{{CollateralType: "bnb", RewardIndexes: types.RewardIndexes{{CollateralType: "hard", RewardFactor: d("0.1")}}}},
+				types.MultiRewardIndexes{{CollateralType: "bnb", RewardIndexes: types.RewardIndexes{{CollateralType: "hard", RewardFactor: d("0.0")}}}},
+			),
+		},
+		types.DelegatorClaims{
+			types.NewDelegatorClaim(
+				suite.addrs[2],
+				cs(c("hard", 5)),
+				types.MultiRewardIndexes{{CollateralType: "ukava", RewardIndexes: types.RewardIndexes{{CollateralType: "hard", RewardFactor: d("0.2")}}}},
+			),
+		},
+		types.SwapClaims{
+			types.NewSwapClaim(
+				suite.addrs[3],
+				nil,
+				types.MultiRewardIndexes{{CollateralType: "btcb/usdx", RewardIndexes: types.RewardIndexes{{CollateralType: "swap", RewardFactor: d("0.0")}}}},
+			),
+		},
+	)
+
+	tApp := app.NewTestApp()
+	ctx := tApp.NewContext(true, tmproto.Header{Height: 0, Time: genesisTime})
+
+	// Incentive init genesis reads from the cdp keeper to check params are ok. So it needs to be initialized first.
+	// Then the cdp keeper reads from pricefeed keeper to check its params are ok. So it also need initialization.
+	tApp.InitializeFromGenesisStates(
+		NewCDPGenStateMulti(),
+		NewPricefeedGenStateMultiFromTime(genesisTime),
+	)
+
+	incentive.InitGenesis(
+		ctx,
+		tApp.GetIncentiveKeeper(),
+		tApp.GetAccountKeeper(),
+		tApp.GetBankKeeper(),
+		tApp.GetCDPKeeper(),
+		genesisState,
+	)
+
+	exportedGenesisState := incentive.ExportGenesis(ctx, tApp.GetIncentiveKeeper())
+
+	suite.Equal(genesisState, exportedGenesisState)
+}
+
+func (suite *GenesisTestSuite) TestInitGenesisPanicsWhenAccumulationTimesToLongAgo() {
+	genesisTime := time.Date(1998, 1, 1, 0, 0, 0, 0, time.UTC)
+	invalidRewardState := types.NewGenesisRewardState(
+		types.AccumulationTimes{
+			types.NewAccumulationTime(
+				"bnb",
+				genesisTime.Add(-23*incentive.EarliestValidAccumulationTime).Add(-time.Nanosecond),
+			),
+		},
+		types.MultiRewardIndexes{},
+	)
+	minimalParams := types.Params{
+		ClaimEnd: genesisTime.Add(5 * oneYear),
+	}
+
+	testCases := []struct {
+		genesisState types.GenesisState
+	}{
+		{
+			types.GenesisState{
+				Params:          minimalParams,
+				USDXRewardState: invalidRewardState,
+			},
+		},
+		{
+			types.GenesisState{
+				Params:                minimalParams,
+				HardSupplyRewardState: invalidRewardState,
+			},
+		},
+		{
+			types.GenesisState{
+				Params:                minimalParams,
+				HardBorrowRewardState: invalidRewardState,
+			},
+		},
+		{
+			types.GenesisState{
+				Params:               minimalParams,
+				DelegatorRewardState: invalidRewardState,
+			},
+		},
+		{
+			types.GenesisState{
+				Params:          minimalParams,
+				SwapRewardState: invalidRewardState,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+
+		tApp := app.NewTestApp()
+		ctx := tApp.NewContext(true, tmproto.Header{Height: 0, Time: genesisTime})
+
+		// Incentive init genesis reads from the cdp keeper to check params are ok. So it needs to be initialized first.
+		// Then the cdp keeper reads from pricefeed keeper to check its params are ok. So it also need initialization.
+		tApp.InitializeFromGenesisStates(
+			NewCDPGenStateMulti(),
+			NewPricefeedGenStateMultiFromTime(genesisTime),
+		)
+
+		suite.PanicsWithValue(
+			"found accumulation time '1975-01-06 23:59:59.999999999 +0000 UTC' more than '8760h0m0s' behind genesis time '1998-01-01 00:00:00 +0000 UTC'",
+			func() {
+				incentive.InitGenesis(
+					ctx, tApp.GetIncentiveKeeper(),
+					tApp.GetAccountKeeper(),
+					tApp.GetBankKeeper(),
+					tApp.GetCDPKeeper(),
+					tc.genesisState,
+				)
+			},
+		)
+	}
+}
+
+func (suite *GenesisTestSuite) TestValidateAccumulationTime() {
+	genTime := time.Date(1998, 1, 1, 0, 0, 0, 0, time.UTC)
+
+	err := incentive.ValidateAccumulationTime(
+		genTime.Add(-incentive.EarliestValidAccumulationTime).Add(-time.Nanosecond),
+		genTime,
+	)
+	suite.Error(err)
+}
+
+func TestGenesisTestSuite(t *testing.T) {
+	suite.Run(t, new(GenesisTestSuite))
+}
--- x/incentive/integration_test.go
+++ x/incentive/integration_test.go
@@ -4,13 +4,13 @@ import (
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/staking"
+	stakingtypes "github.com/cosmos/cosmos-sdk/x/staking/types"
 
 	"github.com/kava-labs/kava/app"
-	"github.com/kava-labs/kava/x/cdp"
+	cdptypes "github.com/kava-labs/kava/x/cdp/types"
 	"github.com/kava-labs/kava/x/incentive/testutil"
-	"github.com/kava-labs/kava/x/pricefeed"
-	"github.com/kava-labs/kava/x/swap"
+	pricefeedtypes "github.com/kava-labs/kava/x/pricefeed/types"
+	swaptypes "github.com/kava-labs/kava/x/swap/types"
 )
 
 // Avoid cluttering test cases with long function names
@@ -20,14 +20,14 @@ func cs(coins ...sdk.Coin) sdk.Coins        { return sdk.NewCoins(coins...) }
 func c(denom string, amount int64) sdk.Coin { return sdk.NewInt64Coin(denom, amount) }
 
 func NewCDPGenStateMulti() app.GenesisState {
-	cdpGenesis := cdp.GenesisState{
-		Params: cdp.Params{
+	cdpGenesis := cdptypes.GenesisState{
+		Params: cdptypes.Params{
 			GlobalDebtLimit:         sdk.NewInt64Coin("usdx", 2000000000000),
-			SurplusAuctionThreshold: cdp.DefaultSurplusThreshold,
-			SurplusAuctionLot:       cdp.DefaultSurplusLot,
-			DebtAuctionThreshold:    cdp.DefaultDebtThreshold,
-			DebtAuctionLot:          cdp.DefaultDebtLot,
-			CollateralParams: cdp.CollateralParams{
+			SurplusAuctionThreshold: cdptypes.DefaultSurplusThreshold,
+			SurplusAuctionLot:       cdptypes.DefaultSurplusLot,
+			DebtAuctionThreshold:    cdptypes.DefaultDebtThreshold,
+			DebtAuctionLot:          cdptypes.DefaultDebtLot,
+			CollateralParams: cdptypes.CollateralParams{
 				{
 					Denom:               "xrp",
 					Type:                "xrp-a",
@@ -36,7 +36,6 @@ func NewCDPGenStateMulti() app.GenesisState {
 					StabilityFee:        sdk.MustNewDecFromStr("1.000000001547125958"), // %5 apr
 					LiquidationPenalty:  d("0.05"),
 					AuctionSize:         i(7000000000),
-					Prefix:              0x20,
 					SpotMarketID:        "xrp:usd",
 					LiquidationMarketID: "xrp:usd",
 					ConversionFactor:    i(6),
@@ -49,7 +48,6 @@ func NewCDPGenStateMulti() app.GenesisState {
 					StabilityFee:        sdk.MustNewDecFromStr("1.000000000782997609"), // %2.5 apr
 					LiquidationPenalty:  d("0.025"),
 					AuctionSize:         i(10000000),
-					Prefix:              0x21,
 					SpotMarketID:        "btc:usd",
 					LiquidationMarketID: "btc:usd",
 					ConversionFactor:    i(8),
@@ -62,7 +60,6 @@ func NewCDPGenStateMulti() app.GenesisState {
 					StabilityFee:        sdk.MustNewDecFromStr("1.000000001547125958"), // %5 apr
 					LiquidationPenalty:  d("0.05"),
 					AuctionSize:         i(50000000000),
-					Prefix:              0x22,
 					SpotMarketID:        "bnb:usd",
 					LiquidationMarketID: "bnb:usd",
 					ConversionFactor:    i(8),
@@ -75,43 +72,41 @@ func NewCDPGenStateMulti() app.GenesisState {
 					StabilityFee:        sdk.OneDec(), // %0 apr
 					LiquidationPenalty:  d("0.05"),
 					AuctionSize:         i(10000000000),
-					Prefix:              0x23,
-					SpotMarketID:        "busd:usd",
 					LiquidationMarketID: "busd:usd",
 					ConversionFactor:    i(8),
 				},
 			},
-			DebtParam: cdp.DebtParam{
+			DebtParam: cdptypes.DebtParam{
 				Denom:            "usdx",
 				ReferenceAsset:   "usd",
 				ConversionFactor: i(6),
 				DebtFloor:        i(10000000),
 			},
 		},
-		StartingCdpID: cdp.DefaultCdpStartingID,
-		DebtDenom:     cdp.DefaultDebtDenom,
-		GovDenom:      cdp.DefaultGovDenom,
-		CDPs:          cdp.CDPs{},
-		PreviousAccumulationTimes: cdp.GenesisAccumulationTimes{
-			cdp.NewGenesisAccumulationTime("btc-a", time.Time{}, sdk.OneDec()),
-			cdp.NewGenesisAccumulationTime("xrp-a", time.Time{}, sdk.OneDec()),
-			cdp.NewGenesisAccumulationTime("busd-a", time.Time{}, sdk.OneDec()),
-			cdp.NewGenesisAccumulationTime("bnb-a", time.Time{}, sdk.OneDec()),
+		StartingCdpID: cdptypes.DefaultCdpStartingID,
+		DebtDenom:     cdptypes.DefaultDebtDenom,
+		GovDenom:      cdptypes.DefaultGovDenom,
+		CDPs:          cdptypes.CDPs{},
+		PreviousAccumulationTimes: cdptypes.GenesisAccumulationTimes{
+			cdptypes.NewGenesisAccumulationTime("btc-a", time.Time{}, sdk.OneDec()),
+			cdptypes.NewGenesisAccumulationTime("xrp-a", time.Time{}, sdk.OneDec()),
+			cdptypes.NewGenesisAccumulationTime("busd-a", time.Time{}, sdk.OneDec()),
+			cdptypes.NewGenesisAccumulationTime("bnb-a", time.Time{}, sdk.OneDec()),
 		},
-		TotalPrincipals: cdp.GenesisTotalPrincipals{
-			cdp.NewGenesisTotalPrincipal("btc-a", sdk.ZeroInt()),
-			cdp.NewGenesisTotalPrincipal("xrp-a", sdk.ZeroInt()),
-			cdp.NewGenesisTotalPrincipal("busd-a", sdk.ZeroInt()),
-			cdp.NewGenesisTotalPrincipal("bnb-a", sdk.ZeroInt()),
+		TotalPrincipals: cdptypes.GenesisTotalPrincipals{
+			cdptypes.NewGenesisTotalPrincipal("btc-a", sdk.ZeroInt()),
+			cdptypes.NewGenesisTotalPrincipal("xrp-a", sdk.ZeroInt()),
+			cdptypes.NewGenesisTotalPrincipal("busd-a", sdk.ZeroInt()),
+			cdptypes.NewGenesisTotalPrincipal("bnb-a", sdk.ZeroInt()),
 		},
 	}
-	return app.GenesisState{cdp.ModuleName: cdp.ModuleCdc.MustMarshalJSON(cdpGenesis)}
+	return app.GenesisState{cdptypes.ModuleName: cdptypes.ModuleCdc.MustMarshalJSON(&cdpGenesis)}
 }
 
 func NewPricefeedGenStateMultiFromTime(t time.Time) app.GenesisState {
-	pfGenesis := pricefeed.GenesisState{
-		Params: pricefeed.Params{
-			Markets: []pricefeed.Market{
+	pfGenesis := pricefeedtypes.GenesisState{
+		Params: pricefeedtypes.Params{
+			Markets: []pricefeedtypes.Market{
 				{MarketID: "kava:usd", BaseAsset: "kava", QuoteAsset: "usd", Oracles: []sdk.AccAddress{}, Active: true},
 				{MarketID: "btc:usd", BaseAsset: "btc", QuoteAsset: "usd", Oracles: []sdk.AccAddress{}, Active: true},
 				{MarketID: "xrp:usd", BaseAsset: "xrp", QuoteAsset: "usd", Oracles: []sdk.AccAddress{}, Active: true},
@@ -120,7 +115,7 @@ func NewPricefeedGenStateMultiFromTime(t time.Time) app.GenesisState {
 				{MarketID: "zzz:usd", BaseAsset: "zzz", QuoteAsset: "usd", Oracles: []sdk.AccAddress{}, Active: true},
 			},
 		},
-		PostedPrices: []pricefeed.PostedPrice{
+		PostedPrices: []pricefeedtypes.PostedPrice{
 			{
 				MarketID:      "kava:usd",
 				OracleAddress: sdk.AccAddress{},
@@ -159,7 +154,7 @@ func NewPricefeedGenStateMultiFromTime(t time.Time) app.GenesisState {
 			},
 		},
 	}
-	return app.GenesisState{pricefeed.ModuleName: pricefeed.ModuleCdc.MustMarshalJSON(pfGenesis)}
+	return app.GenesisState{pricefeedtypes.ModuleName: pricefeedtypes.ModuleCdc.MustMarshalJSON(&pfGenesis)}
 }
 
 func NewHardGenStateMulti(genTime time.Time) testutil.HardGenesisBuilder {
@@ -179,23 +174,23 @@ func NewHardGenStateMulti(genTime time.Time) testutil.HardGenesisBuilder {
 }
 
 func NewStakingGenesisState() app.GenesisState {
-	genState := staking.DefaultGenesisState()
+	genState := stakingtypes.DefaultGenesisState()
 	genState.Params.BondDenom = "ukava"
 	return app.GenesisState{
-		staking.ModuleName: staking.ModuleCdc.MustMarshalJSON(genState),
+		stakingtypes.ModuleName: stakingtypes.ModuleCdc.MustMarshalJSON(genState),
 	}
 }
 
 func NewSwapGenesisState() app.GenesisState {
-	genesis := swap.NewGenesisState(
-		swap.NewParams(
-			swap.NewAllowedPools(swap.NewAllowedPool("busd", "ukava")),
+	genesis := swaptypes.NewGenesisState(
+		swaptypes.NewParams(
+			swaptypes.NewAllowedPools(swaptypes.NewAllowedPool("busd", "ukava")),
 			d("0.0"),
 		),
-		swap.DefaultPoolRecords,
-		swap.DefaultShareRecords,
+		swaptypes.DefaultPoolRecords,
+		swaptypes.DefaultShareRecords,
 	)
 	return app.GenesisState{
-		swap.ModuleName: swap.ModuleCdc.MustMarshalJSON(genesis),
+		swaptypes.ModuleName: swaptypes.ModuleCdc.MustMarshalJSON(&genesis),
 	}
 }
