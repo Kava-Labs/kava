87e2c80917e2c0c5dc1a82967506de580679fc86
drklee3 <derrick@dlee.dev>
2021-12-17 11:01:06 -0800

    Use grpc query struct for bep3

--- x/bep3/module.go
+++ x/bep3/module.go
@@ -136,7 +136,7 @@ func (AppModule) QuerierRoute() string {
 // RegisterServices registers module services.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
 	types.RegisterMsgServer(cfg.MsgServer(), NewMsgServerImpl(am.keeper))
-	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
+	types.RegisterQueryServer(cfg.QueryServer(), keeper.NewQueryServerImpl(am.keeper))
 }
 
 // InitGenesis performs genesis initialization for the bep3 module. It returns
