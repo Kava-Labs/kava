6c95213c5173d940b6cc336e7c327105347890ce
karzak <kjydavis3@gmail.com>
2021-12-21 18:34:39 -0700

    feat: remove support for MsgCreateVestingAccount

---
+++ app/ante/vesting.go
@@ -0,0 +1,27 @@
+package ante
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	vesting "github.com/cosmos/cosmos-sdk/x/auth/vesting/types"
+)
+
+// VestingAccountDecorator blocks MsgCreateVestingAccount from reaching the mempool
+type VestingAccountDecorator struct{}
+
+func NewVestingAccountDecorator() VestingAccountDecorator {
+	return VestingAccountDecorator{}
+}
+
+func (vad VestingAccountDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
+	msgs := tx.GetMsgs()
+	for _, msg := range msgs {
+		_, ok := msg.(*vesting.MsgCreateVestingAccount)
+		if ok {
+			// return ctx, fmt.Errorf("MsgCreateVestingAccount not supported")
+			return ctx, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "MsgCreateVestingAccount not supported")
+
+		}
+	}
+	return next(ctx, tx, simulate)
+}
