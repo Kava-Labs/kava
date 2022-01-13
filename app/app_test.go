abf2f6ae11d74aef2e7d2184d21a1ab9387b598f
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2022-01-13 14:34:23 +0000

    reinstate and expand export test

--- app/app_test.go
+++ app/app_test.go
@@ -1,10 +1,17 @@
 package app
 
 import (
+	"encoding/json"
 	"os"
+	"sort"
 	"testing"
+	"time"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/libs/log"
+	tmtypes "github.com/tendermint/tendermint/types"
 	db "github.com/tendermint/tm-db"
 )
 
@@ -20,43 +27,52 @@ func TestNewApp(t *testing.T) {
 	)
 }
 
-// func TestExport(t *testing.T) {
-// 	db := db.NewMemDB()
-// 	app := NewApp(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, AppOptions{})
-// 	setGenesis(app)
+func TestExport(t *testing.T) {
+	db := db.NewMemDB()
+	app := NewApp(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, DefaultNodeHome, nil, MakeEncodingConfig(), Options{})
 
-// 	// Making a new app object with the db, so that initchain hasn't been called
-// 	newApp := NewApp(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, AppOptions{})
-// 	_, _, err := newApp.ExportAppStateAndValidators(false, []string{})
-// 	require.NoError(t, err, "ExportAppStateAndValidators should not have an error")
-// }
+	stateBytes, err := json.Marshal(NewDefaultGenesisState())
+	require.NoError(t, err)
 
-// // ensure that black listed addresses are properly set in bank keeper
-// func TestBlackListedAddrs(t *testing.T) {
-// 	db := db.NewMemDB()
-// 	app := NewApp(log.NewTMLogger(log.NewSyncWriter(os.Stdout)), db, nil, AppOptions{})
+	initRequest := abci.RequestInitChain{
+		Time:            time.Date(1998, 1, 1, 0, 0, 0, 0, time.UTC),
+		ChainId:         "kava-test",
+		InitialHeight:   1,
+		ConsensusParams: tmtypes.TM2PB.ConsensusParams(tmtypes.DefaultConsensusParams()),
+		Validators:      nil,
+		AppStateBytes:   stateBytes,
+	}
+	app.InitChain(initRequest)
+	app.Commit()
 
-// 	for acc := range mAccPerms {
-// 		require.Equal(t, !allowedReceivingModAcc[acc], app.bankKeeper.BlacklistedAddr(app.supplyKeeper.GetModuleAddress(acc)))
-// 	}
-// }
+	exportedApp, err := app.ExportAppStateAndValidators(false, []string{})
+	require.NoError(t, err)
 
-// func setGenesis(app *App) error {
-// 	genesisState := NewDefaultGenesisState()
+	// Assume each module is exported correctly, so only check modules in genesis are present in export
+	initialModules, err := unmarshalJSONKeys(initRequest.AppStateBytes)
+	require.NoError(t, err)
+	exportedModules, err := unmarshalJSONKeys(exportedApp.AppState)
+	require.NoError(t, err)
+	assert.ElementsMatch(t, initialModules, exportedModules)
 
-// 	stateBytes, err := codec.MarshalJSONIndent(app.cdc, genesisState)
-// 	if err != nil {
-// 		return err
-// 	}
+	assert.Equal(t, initRequest.InitialHeight+1, exportedApp.Height) // app.Commit() increments height
+	assert.Equal(t, initRequest.ConsensusParams, exportedApp.ConsensusParams)
+	assert.Equal(t, []tmtypes.GenesisValidator(nil), exportedApp.Validators) // no validators set in default genesis
+}
 
-// 	// Initialize the chain
-// 	app.InitChain(
-// 		abci.RequestInitChain{
-// 			Validators:    []abci.ValidatorUpdate{},
-// 			AppStateBytes: stateBytes,
-// 		},
-// 	)
-// 	app.Commit()
+// unmarshalJSONKeys extracts keys from the top level of a json blob.
+func unmarshalJSONKeys(jsonBytes []byte) ([]string, error) {
+	var jsonMap map[string]json.RawMessage
+	err := json.Unmarshal(jsonBytes, &jsonMap)
+	if err != nil {
+		return nil, err
+	}
 
-// 	return nil
-// }
+	keys := make([]string, 0, len(jsonMap))
+	for k, _ := range jsonMap {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	return keys, nil
+}
