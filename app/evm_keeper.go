884f955bbfbaf86a4add372839792f8cf3751599
drklee3 <derrick@dlee.dev>
2022-01-28 17:01:29 -0800

    Add doc comments, remove temp actualAmt vars
    actualAmt vars replaced with inline calls to make it more clear that the
    converted value is being used, as opposed to accidentally reusing the
    raw EVM amt.

--- app/evm_keeper.go
+++ app/evm_keeper.go
@@ -10,49 +10,58 @@ import (
 // 12 decimal difference, so 1_000_000_000_000.
 var conversionMultiplier = sdk.NewInt(1_000_000_000_000)
 
-// EVMBankKeeper is a wrapper for bank keeper that converts between EVM and native decimals
+// EVMBankKeeper is a wrapper for bank keeper that converts between EVM (18) and
+// native decimals (6).
 type EVMBankKeeper struct {
 	bankKeeper evmtypes.BankKeeper
 }
 
 var _ evmtypes.BankKeeper = (*EVMBankKeeper)(nil)
 
-// NewEVMBankKeeper returns a wrapped bank keeper that converts between EVM and native decimals
+// NewEVMBankKeeper returns a wrapped bank keeper that converts between EVM (18)
+// and native decimals (6)
 func NewEVMBankKeeper(bk bankkeeper.Keeper) EVMBankKeeper {
 	return EVMBankKeeper{
 		bankKeeper: bk,
 	}
 }
 
+// GetBalance returns the 18 decimal balance of a specific denomination for a
+// given account by address.
 func (bk EVMBankKeeper) GetBalance(ctx sdk.Context, addr sdk.AccAddress, denom string) sdk.Coin {
 	bal := bk.bankKeeper.GetBalance(ctx, addr, denom)
-
 	return convertCoinToEvm(bal)
 }
 
+// SendCoinsFromModuleToAccount transfers 18 decimal coins from a ModuleAccount
+// to an AccAddress. It will panic if the module account does not exist. An
+// error is returned if the recipient address is black-listed or if sending the
+// tokens fails.
 func (bk EVMBankKeeper) SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error {
-	actualAmt := convertCoinsFromEvm(amt)
-
-	return bk.bankKeeper.SendCoinsFromModuleToAccount(ctx, senderModule, recipientAddr, actualAmt)
+	return bk.bankKeeper.SendCoinsFromModuleToAccount(ctx, senderModule, recipientAddr, convertCoinsFromEvm(amt))
 }
 
+// SendCoinsFromAccountToModule transfers 18 decimal coins from an AccAddress to
+// a ModuleAccount. It will panic if the module account does not exist.
 func (bk EVMBankKeeper) SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error {
-	actualAmt := convertCoinsFromEvm(amt)
-
-	return bk.bankKeeper.SendCoinsFromAccountToModule(ctx, senderAddr, recipientModule, actualAmt)
+	return bk.bankKeeper.SendCoinsFromAccountToModule(ctx, senderAddr, recipientModule, convertCoinsFromEvm(amt))
 }
 
+// MintCoins creates new 18 decimal coins from thin air and adds it to the
+// module account. It will panic if the module account does not exist or is
+// unauthorized.
 func (bk EVMBankKeeper) MintCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error {
-	actualAmt := convertCoinsFromEvm(amt)
-
-	return bk.bankKeeper.MintCoins(ctx, moduleName, actualAmt)
+	return bk.bankKeeper.MintCoins(ctx, moduleName, convertCoinsFromEvm(amt))
 }
 
+// BurnCoins burns 18 decimal coins deletes coins from the balance of the module
+// account. It will panic if the module account does not exist or is
+// unauthorized.
 func (bk EVMBankKeeper) BurnCoins(ctx sdk.Context, moduleName string, amt sdk.Coins) error {
-	actualAmt := convertCoinsFromEvm(amt)
-
-	return bk.bankKeeper.BurnCoins(ctx, moduleName, actualAmt)
+	return bk.bankKeeper.BurnCoins(ctx, moduleName, convertCoinsFromEvm(amt))
 }
+
+//____________________________________________________________________________
 
 // convertCoinToEvm converts a sdk.Coin with native decimals to an sdk.Coin with evm decimals
 func convertCoinToEvm(coin sdk.Coin) sdk.Coin {
