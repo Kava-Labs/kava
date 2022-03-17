435e46c1fdbb3d36c4fc90bb55c4bcde2dff988a
denalimarsh <denalimarsh@gmail.com>
2022-03-17 17:50:41 +0100

    generate updates proto types

--- x/savings/types/genesis.pb.go
+++ x/savings/types/genesis.pb.go
@@ -25,7 +25,7 @@ const _ = proto.GoGoProtoPackageIsVersion3 // please upgrade the proto package
 
 // GenesisState defines the savings module's genesis state.
 type GenesisState struct {
-	// params defines all the paramaters of the module.
+	// params defines all the parameters of the module.
 	Params Params `protobuf:"bytes,1,opt,name=params,proto3" json:"params"`
 }
 
