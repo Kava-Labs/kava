1cfd76e67f1bc18002f6971b37520e0326ed64bc
denalimarsh <denalimarsh@gmail.com>
2022-03-21 22:14:32 +0100

    keeper logic for deposits queries

--- x/savings/keeper/grpc_query.go
+++ x/savings/keeper/grpc_query.go
@@ -6,7 +6,10 @@ import (
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 
+	"github.com/cosmos/cosmos-sdk/client"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/types/query"
 
 	"github.com/kava-labs/kava/x/savings/types"
 )
@@ -32,4 +35,71 @@ func (s queryServer) Params(c context.Context, req *types.QueryParamsRequest) (*
 	params := s.keeper.GetParams(sdkCtx)
 
 	return &types.QueryParamsResponse{Params: params}, nil
+}
+
+func (s queryServer) Deposits(ctx context.Context, req *types.QueryDepositsRequest) (*types.QueryDepositsResponse, error) {
+	if req == nil {
+		return nil, status.Errorf(codes.InvalidArgument, "empty request")
+	}
+
+	sdkCtx := sdk.UnwrapSDKContext(ctx)
+
+	hasDenom := len(req.Denom) > 0
+	hasOwner := len(req.Owner) > 0
+
+	var owner sdk.AccAddress
+	var err error
+	if hasOwner {
+		owner, err = sdk.AccAddressFromBech32(req.Owner)
+		if err != nil {
+			return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, err.Error())
+		}
+	}
+
+	var deposits types.Deposits
+	switch {
+	case hasOwner && hasDenom:
+		deposit, found := s.keeper.GetDeposit(sdkCtx, owner)
+		if found {
+			for _, coin := range deposit.Amount {
+				if coin.Denom == req.Denom {
+					deposits = append(deposits, deposit)
+				}
+			}
+		}
+	case hasOwner:
+		deposit, found := s.keeper.GetDeposit(sdkCtx, owner)
+		if found {
+			deposits = append(deposits, deposit)
+		}
+	case hasDenom:
+		s.keeper.IterateDeposits(sdkCtx, func(deposit types.Deposit) (stop bool) {
+			if deposit.Amount.AmountOf(req.Denom).IsPositive() {
+				deposits = append(deposits, deposit)
+			}
+			return false
+		})
+	default:
+		s.keeper.IterateDeposits(sdkCtx, func(deposit types.Deposit) (stop bool) {
+			deposits = append(deposits, deposit)
+			return false
+		})
+	}
+
+	page, limit, err := query.ParsePagination(req.Pagination)
+	if err != nil {
+		return nil, err
+	}
+
+	start, end := client.Paginate(len(deposits), page, limit, 100)
+	if start < 0 || end < 0 {
+		deposits = types.Deposits{}
+	} else {
+		deposits = deposits[start:end]
+	}
+
+	return &types.QueryDepositsResponse{
+		Deposits:   deposits,
+		Pagination: nil,
+	}, nil
 }
--- x/savings/keeper/querier.go
+++ x/savings/keeper/querier.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	abci "github.com/tendermint/tendermint/abci/types"
 
+	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -16,6 +17,8 @@ func NewQuerier(keeper Keeper, legacyQuerierCdc *codec.LegacyAmino) sdk.Querier 
 		switch path[0] {
 		case types.QueryGetParams:
 			return queryGetParams(ctx, req, keeper, legacyQuerierCdc)
+		case types.QueryGetDeposits:
+			return queryGetDeposits(ctx, req, keeper, legacyQuerierCdc)
 		default:
 			return nil, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "unknown %s query endpoint", types.ModuleName)
 		}
@@ -29,6 +32,62 @@ func queryGetParams(ctx sdk.Context, req abci.RequestQuery, keeper Keeper, legac
 
 	// Encode results
 	bz, err := codec.MarshalJSONIndent(legacyQuerierCdc, params)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
+	}
+	return bz, nil
+}
+
+func queryGetDeposits(ctx sdk.Context, req abci.RequestQuery, k Keeper, legacyQuerierCdc *codec.LegacyAmino) ([]byte, error) {
+
+	var params types.QueryDepositsParams
+	err := legacyQuerierCdc.UnmarshalJSON(req.Data, &params)
+	if err != nil {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONUnmarshal, err.Error())
+	}
+	denom := len(params.Denom) > 0
+	owner := len(params.Owner) > 0
+
+	var deposits types.Deposits
+	switch {
+	case owner && denom:
+		deposit, found := k.GetDeposit(ctx, params.Owner)
+		if found {
+			for _, coin := range deposit.Amount {
+				if coin.Denom == params.Denom {
+					deposits = append(deposits, deposit)
+				}
+			}
+		}
+	case owner:
+		deposit, found := k.GetDeposit(ctx, params.Owner)
+		if found {
+			deposits = append(deposits, deposit)
+		}
+	case denom:
+		k.IterateDeposits(ctx, func(deposit types.Deposit) (stop bool) {
+			if deposit.Amount.AmountOf(params.Denom).IsPositive() {
+				deposits = append(deposits, deposit)
+			}
+			return false
+		})
+	default:
+		k.IterateDeposits(ctx, func(deposit types.Deposit) (stop bool) {
+			deposits = append(deposits, deposit)
+			return false
+		})
+	}
+
+	var bz []byte
+
+	start, end := client.Paginate(len(deposits), params.Page, params.Limit, 100)
+	if start < 0 || end < 0 {
+		deposits = types.Deposits{}
+	} else {
+		deposits = deposits[start:end]
+	}
+
+	bz, err = codec.MarshalJSONIndent(legacyQuerierCdc, deposits)
 	if err != nil {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
 	}
