451bc05f4714e3733c3647e22aad612a803f056b
Denali Marsh <denalimarsh@gmail.com>
2022-03-23 14:27:54 +0100

    Savings module params (#1190)
    * module files
    
    * proto types
    
    * types and generated proto types
    
    * keeper
    
    * client scaffold
    
    * add savings module to app
    
    * remove placeholder types file
    
    * implement rest and add to module
    
    * update proto types
    
    * validation for supported denoms
    
    * generate updates proto types
    
    * update comments
    
    * update comments
    
    * remove unused imports from proto files
    
    * regenerate proto files
    
    * remove abci
    
    * remove refs to other modules
    
    * remove endblocker call
    
    * genesis init test for module account
    
    * update genesis test with params
    
    * add get/set params test

--- x/savings/types/params.go
+++ x/savings/types/params.go
@@ -1,20 +1,27 @@
 package types
 
 import (
+	"fmt"
+
 	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 )
 
 // Parameter keys
-var ()
+var (
+	KeySupportedDenoms     = []byte("SupportedDenoms")
+	DefaultSupportedDenoms = []string{}
+)
 
 // NewParams creates a new Params object
-func NewParams() Params {
-	return Params{}
+func NewParams(supportedDenoms []string) Params {
+	return Params{
+		SupportedDenoms: supportedDenoms,
+	}
 }
 
 // DefaultParams default params for savings
 func DefaultParams() Params {
-	return NewParams()
+	return NewParams(DefaultSupportedDenoms)
 }
 
 // ParamKeyTable Key declaration for parameters
@@ -25,10 +32,28 @@ func ParamKeyTable() paramtypes.KeyTable {
 // ParamSetPairs implements the ParamSet interface and returns all the key/value pairs
 // pairs of savings module's parameters.
 func (p *Params) ParamSetPairs() paramtypes.ParamSetPairs {
-	return paramtypes.ParamSetPairs{}
+	return paramtypes.ParamSetPairs{
+		paramtypes.NewParamSetPair(KeySupportedDenoms, &p.SupportedDenoms, validateSupportedDenoms),
+	}
 }
 
 // Validate ensure that params have valid values
 func (p Params) Validate() error {
+	return validateSupportedDenoms(p.SupportedDenoms)
+}
+
+func validateSupportedDenoms(i interface{}) error {
+	supportedDenoms, ok := i.([]string)
+	if !ok {
+		return fmt.Errorf("invalid parameter type: %T", i)
+	}
+
+	seenDenoms := make(map[string]bool)
+	for _, denom := range supportedDenoms {
+		if seenDenoms[denom] {
+			return fmt.Errorf("duplicated denom %s", denom)
+		}
+		seenDenoms[denom] = true
+	}
 	return nil
 }
