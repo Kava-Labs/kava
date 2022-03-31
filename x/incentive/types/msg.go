981dd734e5aa63ff8a1d81b5dfbb664a9fd3fcff
denalimarsh <denalimarsh@gmail.com>
2022-03-31 12:02:18 +0200

    implement savings claim msg + test

--- x/incentive/types/msg.go
+++ x/incentive/types/msg.go
@@ -12,6 +12,7 @@ var _ sdk.Msg = &MsgClaimUSDXMintingReward{}
 var _ sdk.Msg = &MsgClaimHardReward{}
 var _ sdk.Msg = &MsgClaimDelegatorReward{}
 var _ sdk.Msg = &MsgClaimSwapReward{}
+var _ sdk.Msg = &MsgClaimSavingsReward{}
 
 // NewMsgClaimUSDXMintingReward returns a new MsgClaimUSDXMintingReward.
 func NewMsgClaimUSDXMintingReward(sender string, multiplierName string) MsgClaimUSDXMintingReward {
@@ -176,6 +177,49 @@ func (msg MsgClaimSwapReward) GetSignBytes() []byte {
 
 // GetSigners returns the addresses of signers that must sign.
 func (msg MsgClaimSwapReward) GetSigners() []sdk.AccAddress {
+	sender, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		panic(err)
+	}
+	return []sdk.AccAddress{sender}
+}
+
+// NewMsgClaimSavingsReward returns a new MsgClaimSavingsReward.
+func NewMsgClaimSavingsReward(sender string, denomsToClaim Selections) MsgClaimSavingsReward {
+	return MsgClaimSavingsReward{
+		Sender:        sender,
+		DenomsToClaim: denomsToClaim,
+	}
+}
+
+// Route return the message type used for routing the message.
+func (msg MsgClaimSavingsReward) Route() string { return RouterKey }
+
+// Type returns a human-readable string for the message, intended for utilization within tags.
+func (msg MsgClaimSavingsReward) Type() string {
+	return "claim_savings_reward"
+}
+
+// ValidateBasic does a simple validation check that doesn't require access to state.
+func (msg MsgClaimSavingsReward) ValidateBasic() error {
+	_, err := sdk.AccAddressFromBech32(msg.Sender)
+	if err != nil {
+		return sdkerrors.Wrap(sdkerrors.ErrInvalidAddress, "sender address cannot be empty or invalid")
+	}
+	if err := msg.DenomsToClaim.Validate(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// GetSignBytes gets the canonical byte representation of the Msg.
+func (msg MsgClaimSavingsReward) GetSignBytes() []byte {
+	bz := ModuleCdc.MustMarshalJSON(&msg)
+	return sdk.MustSortJSON(bz)
+}
+
+// GetSigners returns the addresses of signers that must sign.
+func (msg MsgClaimSavingsReward) GetSigners() []sdk.AccAddress {
 	sender, err := sdk.AccAddressFromBech32(msg.Sender)
 	if err != nil {
 		panic(err)
