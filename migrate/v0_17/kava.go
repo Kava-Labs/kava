ecda31ad27054e0c775bb22ef1a4af35cc20a484
drklee3 <derrick@dlee.dev>
2022-04-20 12:09:37 -0700

    Add bridge migration

--- migrate/v0_17/kava.go
+++ migrate/v0_17/kava.go
@@ -3,12 +3,15 @@ package v0_17
 import (
 	"github.com/cosmos/cosmos-sdk/client"
 
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	authz "github.com/cosmos/cosmos-sdk/x/authz"
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	evmtypes "github.com/tharsis/ethermint/x/evm/types"
 	feemarkettypes "github.com/tharsis/ethermint/x/feemarket/types"
 
 	evmutiltypes "github.com/kava-labs/kava/x/evmutil/types"
+
+	bridgetypes "github.com/kava-labs/kava-bridge/x/bridge/types"
 )
 
 func migrateAppState(appState genutiltypes.AppMap, clientCtx client.Context) {
@@ -30,6 +33,21 @@ func migrateAppState(appState genutiltypes.AppMap, clientCtx client.Context) {
 		},
 	}
 	appState[evmtypes.ModuleName] = codec.MustMarshalJSON(evmGenState)
+
+	// x/bridge
+	bridgeGenState := &bridgetypes.GenesisState{
+		// Start with no ERC20 tokens that can be bridged, no relayer set.
+		Params: bridgetypes.NewParams(
+			false, // bridge disabled
+			bridgetypes.EnabledERC20Tokens{},
+			nil, // no relayer
+			bridgetypes.ConversionPairs{},
+		),
+		// No ERC20 tokens have been bridged yet.
+		ERC20BridgePairs:     bridgetypes.ERC20BridgePairs{},
+		NextWithdrawSequence: sdk.OneInt(),
+	}
+	appState[bridgetypes.ModuleName] = codec.MustMarshalJSON(bridgeGenState)
 
 	// x/feemarket
 	feemarketState := feemarkettypes.DefaultGenesisState()
