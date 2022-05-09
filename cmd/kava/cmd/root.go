bc28e8babebd266c01ebe4096fe99698fdba264f
Nick DeLuca <nickdeluca08@gmail.com>
2022-05-09 23:00:00 +0000

    remove migration cmd for testnet

--- cmd/kava/cmd/root.go
+++ cmd/kava/cmd/root.go
@@ -21,7 +21,6 @@ import (
 	"github.com/kava-labs/kava/app"
 	"github.com/kava-labs/kava/app/params"
 	kavaclient "github.com/kava-labs/kava/client"
-	"github.com/kava-labs/kava/migrate"
 )
 
 // EnvPrefix is the prefix environment variables must have to configure the app.
@@ -83,8 +82,6 @@ func addSubCmds(rootCmd *cobra.Command, encodingConfig params.EncodingConfig, de
 			genutilcli.InitCmd(app.ModuleBasics, defaultNodeHome),
 		),
 		genutilcli.CollectGenTxsCmd(banktypes.GenesisBalancesIterator{}, defaultNodeHome),
-		migrate.MigrateGenesisCmd(),
-		migrate.AssertInvariantsCmd(encodingConfig),
 		genutilcli.GenTxCmd(app.ModuleBasics, encodingConfig.TxConfig, banktypes.GenesisBalancesIterator{}, defaultNodeHome),
 		genutilcli.ValidateGenesisCmd(app.ModuleBasics),
 		AddGenesisAccountCmd(defaultNodeHome),
