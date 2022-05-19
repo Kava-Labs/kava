22cdbd53189bd70ffc311fc82633c934c5472d09
Nick DeLuca <nickdeluca08@gmail.com>
2022-05-19 13:45:13 +0000

    add doc comments

--- app/ante/min_gas_filter.go
+++ app/ante/min_gas_filter.go
@@ -7,20 +7,24 @@ import (
 
 var _ sdk.AnteDecorator = EvmMinGasFilter{}
 
+// EVMKeeper specifies the interface that EvmMinGasFilter requires
 type EVMKeeper interface {
 	GetParams(ctx sdk.Context) evmtypes.Params
 }
 
+// EvmMinGasFilter filters out the EvmDenom min gas price and calls the next ante handle with an updated context
 type EvmMinGasFilter struct {
 	evmKeeper EVMKeeper
 }
 
+// NewEvmMinGasFilter takes an EVMKeeper and returns a new min gas filter for it's EvmDenom
 func NewEvmMinGasFilter(evmKeeper EVMKeeper) EvmMinGasFilter {
 	return EvmMinGasFilter{
 		evmKeeper: evmKeeper,
 	}
 }
 
+// AnteHandle checks the EvmDenom from the evmKeeper and filters out the EvmDenom from the ctx
 func (emgf EvmMinGasFilter) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
 	evmDenom := emgf.evmKeeper.GetParams(ctx).EvmDenom
 
