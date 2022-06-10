5cb074dc97097801a44008cb355fdc6ae04b109f
denalimarsh <denalimarsh@gmail.com>
2022-06-10 18:29:55 +0200

    genesis/module top level files

---
+++ x/liquidstaking/genesis.go
@@ -0,0 +1,25 @@
+package liquidstaking
+
+import (
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/kava-labs/kava/x/liquidstaking/keeper"
+	"github.com/kava-labs/kava/x/liquidstaking/types"
+)
+
+// InitGenesis initializes genesis state
+func InitGenesis(ctx sdk.Context, k keeper.Keeper, ak types.AccountKeeper, gs types.GenesisState) {
+	if err := gs.Validate(); err != nil {
+		panic(fmt.Sprintf("failed to validate %s genesis state: %s", types.ModuleName, err))
+	}
+
+	k.SetParams(ctx, gs.Params)
+}
+
+// ExportGenesis returns a GenesisState for a given context and keeper
+func ExportGenesis(ctx sdk.Context, k keeper.Keeper) types.GenesisState {
+	params := k.GetParams(ctx)
+	return types.NewGenesisState(params)
+}
