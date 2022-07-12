e99266a758b04c7e3fdca3e6f151e2e909b24e76
drklee3 <derrick@dlee.dev>
2022-07-12 09:03:40 -0700

    Add basic earn types and interfaces

---
+++ x/earn/keeper/strategy_stablecoin.go
@@ -0,0 +1,28 @@
+package keeper
+
+import sdk "github.com/cosmos/cosmos-sdk/types"
+
+// StableCoinStrategy defines the stablecoin strategy:
+// 1. Mint USDX from stablecoin (USDC, BUSD, USTD, DAI)
+// 2. Supply USDX to Lend
+type StableCoinStrategy Keeper
+
+func (s *StableCoinStrategy) GetName() string {
+	return "stablecoin"
+}
+
+func (s *StableCoinStrategy) GetDescription() string {
+	return "Mint USDX from stablecoin, then supply the USDX to Lend"
+}
+
+func (s *StableCoinStrategy) GetSupportedDenoms() []string {
+	return []string{"busd", "usdc", "usdt", "dai"}
+}
+
+func (s *StableCoinStrategy) GetEstimatedTotalAssets(denom string) (sdk.Coin, error) {
+	// Amount of USDX in Lend
+
+	// Convert USDX to denom in Mint
+
+	return sdk.Coin{}, nil
+}
