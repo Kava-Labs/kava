315d6ef4d13d1ab831fd5b4eac04f8b129c7fe37
drklee3 <derrick@dlee.dev>
2022-07-18 10:32:15 -0700

    Add hard and savings keepers

--- x/earn/types/expected_keepers.go
+++ x/earn/types/expected_keepers.go
@@ -3,6 +3,9 @@ package types
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth/types"
+
+	hardtypes "github.com/kava-labs/kava/x/hard/types"
+	savingstypes "github.com/kava-labs/kava/x/savings/types"
 )
 
 // AccountKeeper defines the expected account keeper
@@ -20,4 +23,22 @@ type BankKeeper interface {
 	SendCoinsFromModuleToModule(ctx sdk.Context, senderModule, recipientModule string, amt sdk.Coins) error
 	SendCoinsFromModuleToAccount(ctx sdk.Context, senderModule string, recipientAddr sdk.AccAddress, amt sdk.Coins) error
 	SendCoinsFromAccountToModule(ctx sdk.Context, senderAddr sdk.AccAddress, recipientModule string, amt sdk.Coins) error
+}
+
+// HardKeeper defines the expected interface needed for the hard strategy.
+type HardKeeper interface {
+	// State modification methods
+	Deposit(ctx sdk.Context, depositor sdk.AccAddress, coins sdk.Coins) error
+	Withdraw(ctx sdk.Context, depositor sdk.AccAddress, coins sdk.Coins) error
+
+	// Read only methods
+	GetSyncedBorrow(ctx sdk.Context, borrower sdk.AccAddress) (hardtypes.Borrow, bool)
+}
+
+// SavingsKeeper defines the expected interface needed for the savings strategy.
+type SavingsKeeper interface {
+	Deposit(ctx sdk.Context, depositor sdk.AccAddress, coins sdk.Coins) error
+	Withdraw(ctx sdk.Context, depositor sdk.AccAddress, coins sdk.Coins) error
+
+	GetDeposit(ctx sdk.Context, depositor sdk.AccAddress) (savingstypes.Deposit, bool)
 }
