31f24e3b1fe3e0846c15ebf44edc578d0e70a991
drklee3 <derrick@dlee.dev>
2022-07-21 16:10:36 -0700

    Add Vaults, TotalDeposited queries

--- x/earn/keeper/grpc_query_test.go
+++ x/earn/keeper/grpc_query_test.go
@@ -55,36 +55,173 @@ func (suite *grpcQueryTestSuite) TestQueryParams() {
 	)
 }
 
+func (suite *grpcQueryTestSuite) TestVaults_ZeroSupply() {
+	// Add vaults
+	suite.CreateVault("usdx", types.STRATEGY_TYPE_HARD)
+	suite.CreateVault("busd", types.STRATEGY_TYPE_HARD)
+
+	suite.Run("single", func() {
+		res, err := suite.queryClient.Vaults(context.Background(), types.NewQueryVaultsRequest("usdx"))
+		suite.Require().NoError(err)
+		suite.Require().Len(res.Vaults, 1)
+		suite.Require().Equal(
+			types.VaultResponse{
+				Denom:         "usdx",
+				VaultStrategy: types.STRATEGY_TYPE_HARD,
+				TotalSupplied: sdk.NewInt(0),
+				TotalValue:    sdk.NewInt(0),
+			},
+			res.Vaults[0],
+		)
+	})
+
+	suite.Run("all", func() {
+		res, err := suite.queryClient.Vaults(context.Background(), types.NewQueryVaultsRequest(""))
+		suite.Require().NoError(err)
+		suite.Require().Len(res.Vaults, 2)
+		suite.Require().ElementsMatch(
+			[]types.VaultResponse{
+				{
+					Denom:         "usdx",
+					VaultStrategy: types.STRATEGY_TYPE_HARD,
+					TotalSupplied: sdk.NewInt(0),
+					TotalValue:    sdk.NewInt(0),
+				},
+				{
+					Denom:         "busd",
+					VaultStrategy: types.STRATEGY_TYPE_HARD,
+					TotalSupplied: sdk.NewInt(0),
+					TotalValue:    sdk.NewInt(0),
+				},
+			},
+			res.Vaults,
+		)
+	})
+}
+
+func (suite *grpcQueryTestSuite) TestVaults_WithSupply() {
+	vaultDenom := "usdx"
+
+	startBalance := sdk.NewInt64Coin(vaultDenom, 1000)
+	depositAmount := sdk.NewInt64Coin(vaultDenom, 100)
+
+	suite.CreateVault(vaultDenom, types.STRATEGY_TYPE_HARD)
+
+	acc := suite.CreateAccount(sdk.NewCoins(startBalance), 0)
+
+	err := suite.Keeper.Deposit(suite.Ctx, acc.GetAddress(), depositAmount)
+	suite.Require().NoError(err)
+
+	res, err := suite.queryClient.Vaults(context.Background(), types.NewQueryVaultsRequest("usdx"))
+	suite.Require().NoError(err)
+	suite.Require().Len(res.Vaults, 1)
+	suite.Require().Equal(
+		types.VaultResponse{
+			Denom:         "usdx",
+			VaultStrategy: types.STRATEGY_TYPE_HARD,
+			TotalSupplied: depositAmount.Amount,
+			TotalValue:    depositAmount.Amount,
+		},
+		res.Vaults[0],
+	)
+}
+
 func (suite *grpcQueryTestSuite) TestTotalDeposited_NoSupply() {
 	// Add vaults
 	suite.CreateVault("usdx", types.STRATEGY_TYPE_HARD)
 	suite.CreateVault("cats", types.STRATEGY_TYPE_HARD)
 
-	// Query again for added vault
 	res, err := suite.queryClient.TotalDeposited(context.Background(), types.NewQueryTotalDepositedRequest(""))
 	suite.Require().NoError(err)
 	suite.Require().True(res.SuppliedCoins.Empty(), "supplied coins should be empty")
 }
 
 func (suite *grpcQueryTestSuite) TestTotalDeposited_All() {
+	vault1Denom := "usdx"
+	vault2Denom := "busd"
+
 	// Add vaults
-	suite.CreateVault("usdx", types.STRATEGY_TYPE_HARD)
-	suite.CreateVault("cats", types.STRATEGY_TYPE_HARD)
+	suite.CreateVault(vault1Denom, types.STRATEGY_TYPE_HARD)
+	suite.CreateVault(vault2Denom, types.STRATEGY_TYPE_HARD)
 
-	vaultDenom := "usdx"
-	startBalance := sdk.NewInt64Coin(vaultDenom, 1000)
-	depositAmount := sdk.NewInt64Coin(vaultDenom, 100)
+	startBalance := sdk.NewCoins(
+		sdk.NewInt64Coin(vault1Denom, 1000),
+		sdk.NewInt64Coin(vault2Denom, 1000),
+	)
+	deposit1Amount := sdk.NewInt64Coin(vault1Denom, 100)
+	deposit2Amount := sdk.NewInt64Coin(vault2Denom, 100)
 
-	acc := suite.CreateAccount(sdk.NewCoins(startBalance), 0).GetAddress()
-	err := suite.Keeper.Deposit(suite.Ctx, acc, depositAmount)
+	acc := suite.CreateAccount(startBalance, 0).GetAddress()
+	err := suite.Keeper.Deposit(suite.Ctx, acc, deposit1Amount)
 	suite.Require().NoError(err)
 
-	// Query again for added vault
-	res, err := suite.queryClient.TotalDeposited(context.Background(), types.NewQueryTotalDepositedRequest(""))
+	res, err := suite.queryClient.TotalDeposited(
+		context.Background(),
+		types.NewQueryTotalDepositedRequest(""), // query all
+	)
 	suite.Require().NoError(err)
 	suite.Require().Equal(
-		sdk.NewCoins(depositAmount),
+		sdk.NewCoins(deposit1Amount),
 		res.SuppliedCoins,
 		"supplied coins should be sum of all supplied coins",
+	)
+
+	err = suite.Keeper.Deposit(suite.Ctx, acc, deposit2Amount)
+	suite.Require().NoError(err)
+
+	res, err = suite.queryClient.TotalDeposited(
+		context.Background(),
+		types.NewQueryTotalDepositedRequest(""), // query all
+	)
+	suite.Require().NoError(err)
+	suite.Require().Equal(
+		sdk.NewCoins(deposit1Amount, deposit2Amount),
+		res.SuppliedCoins,
+		"supplied coins should be sum of all supplied coins for multiple coins",
+	)
+}
+
+func (suite *grpcQueryTestSuite) TestTotalDeposited_Single() {
+	vault1Denom := "usdx"
+	vault2Denom := "busd"
+
+	// Add vaults
+	suite.CreateVault(vault1Denom, types.STRATEGY_TYPE_HARD)
+	suite.CreateVault(vault2Denom, types.STRATEGY_TYPE_HARD)
+
+	startBalance := sdk.NewCoins(
+		sdk.NewInt64Coin(vault1Denom, 1000),
+		sdk.NewInt64Coin(vault2Denom, 1000),
+	)
+	deposit1Amount := sdk.NewInt64Coin(vault1Denom, 100)
+	deposit2Amount := sdk.NewInt64Coin(vault2Denom, 100)
+
+	acc := suite.CreateAccount(startBalance, 0).GetAddress()
+	err := suite.Keeper.Deposit(suite.Ctx, acc, deposit1Amount)
+	suite.Require().NoError(err)
+
+	err = suite.Keeper.Deposit(suite.Ctx, acc, deposit2Amount)
+	suite.Require().NoError(err)
+
+	res, err := suite.queryClient.TotalDeposited(
+		context.Background(),
+		types.NewQueryTotalDepositedRequest(vault1Denom),
+	)
+	suite.Require().NoError(err)
+	suite.Require().Equal(
+		sdk.NewCoins(deposit1Amount),
+		res.SuppliedCoins,
+		"should only contain queried denom",
+	)
+
+	res, err = suite.queryClient.TotalDeposited(
+		context.Background(),
+		types.NewQueryTotalDepositedRequest(vault2Denom),
+	)
+	suite.Require().NoError(err)
+	suite.Require().Equal(
+		sdk.NewCoins(deposit2Amount),
+		res.SuppliedCoins,
+		"should only contain queried denom",
 	)
 }
