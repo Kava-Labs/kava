fba821e6184a20690f22eed7eea7e2cd80da71b0
drklee3 <derrick@dlee.dev>
2022-07-21 15:44:35 -0700

    Add query deposits

--- x/earn/keeper/grpc_query_test.go
+++ x/earn/keeper/grpc_query_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/kava-labs/kava/x/earn/keeper"
 	"github.com/kava-labs/kava/x/earn/testutil"
@@ -51,5 +52,39 @@ func (suite *grpcQueryTestSuite) TestQueryParams() {
 			types.NewAllowedVault(vaultDenom, types.STRATEGY_TYPE_HARD),
 		},
 		res.Params.AllowedVaults,
+	)
+}
+
+func (suite *grpcQueryTestSuite) TestTotalDeposited_NoSupply() {
+	// Add vaults
+	suite.CreateVault("usdx", types.STRATEGY_TYPE_HARD)
+	suite.CreateVault("cats", types.STRATEGY_TYPE_HARD)
+
+	// Query again for added vault
+	res, err := suite.queryClient.TotalDeposited(context.Background(), types.NewQueryTotalDepositedRequest(""))
+	suite.Require().NoError(err)
+	suite.Require().True(res.SuppliedCoins.Empty(), "supplied coins should be empty")
+}
+
+func (suite *grpcQueryTestSuite) TestTotalDeposited_All() {
+	// Add vaults
+	suite.CreateVault("usdx", types.STRATEGY_TYPE_HARD)
+	suite.CreateVault("cats", types.STRATEGY_TYPE_HARD)
+
+	vaultDenom := "usdx"
+	startBalance := sdk.NewInt64Coin(vaultDenom, 1000)
+	depositAmount := sdk.NewInt64Coin(vaultDenom, 100)
+
+	acc := suite.CreateAccount(sdk.NewCoins(startBalance), 0).GetAddress()
+	err := suite.Keeper.Deposit(suite.Ctx, acc, depositAmount)
+	suite.Require().NoError(err)
+
+	// Query again for added vault
+	res, err := suite.queryClient.TotalDeposited(context.Background(), types.NewQueryTotalDepositedRequest(""))
+	suite.Require().NoError(err)
+	suite.Require().Equal(
+		sdk.NewCoins(depositAmount),
+		res.SuppliedCoins,
+		"supplied coins should be sum of all supplied coins",
 	)
 }
--- x/earn/types/query.go
+++ x/earn/types/query.go
@@ -1,5 +1,35 @@
 package types
 
+import "github.com/cosmos/cosmos-sdk/types/query"
+
+// NewQueryParamsRequest returns a new QueryParamsRequest
 func NewQueryParamsRequest() *QueryParamsRequest {
 	return &QueryParamsRequest{}
+}
+
+// NewQueryVaultsRequest returns a new QueryVaultsRequest
+func NewQueryVaultsRequest(denom string) *QueryVaultsRequest {
+	return &QueryVaultsRequest{
+		Denom: denom,
+	}
+}
+
+// NewQueryDepositsRequest returns a new QueryDepositsRequest
+func NewQueryDepositsRequest(
+	owner string,
+	denom string,
+	pagination *query.PageRequest,
+) *QueryDepositsRequest {
+	return &QueryDepositsRequest{
+		Owner:      owner,
+		Denom:      denom,
+		Pagination: pagination,
+	}
+}
+
+// NewQueryTotalDepositedRequest returns a new QueryTotalDepositedRequest
+func NewQueryTotalDepositedRequest(denom string) *QueryTotalDepositedRequest {
+	return &QueryTotalDepositedRequest{
+		Denom: denom,
+	}
 }
