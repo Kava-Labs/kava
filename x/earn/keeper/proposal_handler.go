6d89a98c0d90ae964084fd4cd35ad54750012622
karzak <kjydavis3@gmail.com>
2022-07-26 11:45:41 -0500

    wip: initial deposit logic

--- x/earn/keeper/proposal_handler.go
+++ x/earn/keeper/proposal_handler.go
@@ -8,6 +8,6 @@ import (
 
 // HandleCommunityPoolDepositProposal is a handler for executing a passed community pool deposit proposal
 func HandleCommunityPoolDepositProposal(ctx sdk.Context, k Keeper, p *types.CommunityPoolDepositProposal) error {
-
-	return nil
+	// kava1jv65s3grqf6v6jl3dp4t6c9t9rk99cd8m2splc
+	return k.Deposit(ctx, k.accountKeeper.GetModuleAddress("distribution"), p.Amount)
 }
