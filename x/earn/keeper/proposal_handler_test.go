c4498a33ee38a84292cb3ee22234d19baa4fed13
karzak <kjydavis3@gmail.com>
2022-07-27 11:43:50 -0500

    feat: add deposit proposal test

---
+++ x/earn/keeper/proposal_handler_test.go
@@ -0,0 +1,45 @@
+package keeper_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/kava-labs/kava/x/earn/keeper"
+	"github.com/kava-labs/kava/x/earn/testutil"
+	"github.com/kava-labs/kava/x/earn/types"
+	"github.com/stretchr/testify/suite"
+)
+
+type proposalTestSuite struct {
+	testutil.Suite
+}
+
+func (suite *proposalTestSuite) SetupTest() {
+	suite.Suite.SetupTest()
+	suite.Keeper.SetParams(suite.Ctx, types.DefaultParams())
+}
+
+func TestProposalTestSuite(t *testing.T) {
+	suite.Run(t, new(proposalTestSuite))
+}
+
+func (suite *proposalTestSuite) TestCommunityDepositProposal() {
+	distKeeper := suite.App.GetDistrKeeper()
+	ctx := suite.Ctx
+	macc := distKeeper.GetDistributionAccount(ctx)
+	fundAmount := sdk.NewCoins(sdk.NewInt64Coin("ukava", 100000000))
+	depositAmount := sdk.NewCoin("ukava", sdk.NewInt(10000000))
+	suite.Require().NoError(suite.App.FundModuleAccount(ctx, macc.GetName(), fundAmount))
+	feePool := distKeeper.GetFeePool(ctx)
+	feePool.CommunityPool = sdk.NewDecCoinsFromCoins(fundAmount...)
+	distKeeper.SetFeePool(ctx, feePool)
+	// TODO update to STRATEGY_TYPE_SAVINGS once implemented
+	suite.CreateVault("ukava", types.STRATEGY_TYPE_HARD)
+	prop := types.NewCommunityPoolDepositProposal("test title",
+		"desc", depositAmount)
+	err := keeper.HandleCommunityPoolDepositProposal(ctx, suite.Keeper, prop)
+	suite.Require().NoError(err)
+
+	balance := suite.BankKeeper.GetAllBalances(ctx, macc.GetAddress())
+	suite.Require().Equal(fundAmount.Sub(sdk.NewCoins(depositAmount)), balance)
+}
