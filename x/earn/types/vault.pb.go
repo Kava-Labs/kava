5f9c703f7a964fc67efb8cba868f73d4920440c7
drklee3 <derrick@dlee.dev>
2022-07-27 13:20:29 -0700

    Change vault supply to shares

--- x/earn/types/vault.pb.go
+++ x/earn/types/vault.pb.go
@@ -6,8 +6,8 @@ package types
 import (
 	fmt "fmt"
 	_ "github.com/cosmos/cosmos-proto"
+	_ "github.com/cosmos/cosmos-sdk/types"
 	github_com_cosmos_cosmos_sdk_types "github.com/cosmos/cosmos-sdk/types"
-	types "github.com/cosmos/cosmos-sdk/types"
 	_ "github.com/gogo/protobuf/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
 	io "io"
@@ -82,15 +82,10 @@ func (m *AllowedVault) GetVaultStrategy() StrategyType {
 	return STRATEGY_TYPE_UNSPECIFIED
 }
 
-// VaultRecord is the state of a vault and is used to store the state of a
-// vault.
+// VaultRecord is the state of a vault.
 type VaultRecord struct {
-	// Denom is the only supported denomination of the vault for deposits and
-	// withdrawals.
-	Denom string `protobuf:"bytes,1,opt,name=denom,proto3" json:"denom,omitempty"`
-	// TotalSupply is the total supply of the vault, denominated **only** in the
-	// user deposit/withdrawal denom, must be the same as the Denom field.
-	TotalSupply types.Coin `protobuf:"bytes,2,opt,name=total_supply,json=totalSupply,proto3" json:"total_supply"`
+	// TotalShares is the total distributed number of shares in the vault.
+	TotalShares VaultShare `protobuf:"bytes,1,opt,name=total_shares,json=totalShares,proto3" json:"total_shares"`
 }
 
 func (m *VaultRecord) Reset()         { *m = VaultRecord{} }
@@ -126,27 +121,19 @@ func (m *VaultRecord) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_VaultRecord proto.InternalMessageInfo
 
-func (m *VaultRecord) GetDenom() string {
+func (m *VaultRecord) GetTotalShares() VaultShare {
 	if m != nil {
-		return m.Denom
+		return m.TotalShares
 	}
-	return ""
-}
-
-func (m *VaultRecord) GetTotalSupply() types.Coin {
-	if m != nil {
-		return m.TotalSupply
-	}
-	return types.Coin{}
+	return VaultShare{}
 }
 
 // VaultShareRecord defines the vault shares owned by a depositor.
 type VaultShareRecord struct {
 	// Depositor represents the owner of the shares
 	Depositor github_com_cosmos_cosmos_sdk_types.AccAddress `protobuf:"bytes,1,opt,name=depositor,proto3,casttype=github.com/cosmos/cosmos-sdk/types.AccAddress" json:"depositor,omitempty"`
-	// AmountSupplied represents the total amount a depositor has supplied to the
-	// vault. The vault is determined by the coin denom.
-	AmountSupplied github_com_cosmos_cosmos_sdk_types.Coins `protobuf:"bytes,2,rep,name=amount_supplied,json=amountSupplied,proto3,castrepeated=github.com/cosmos/cosmos-sdk/types.Coins" json:"amount_supplied"`
+	// Shares represent the vault shares owned by the depositor.
+	Shares VaultShares `protobuf:"bytes,2,rep,name=shares,proto3,castrepeated=VaultShares" json:"shares"`
 }
 
 func (m *VaultShareRecord) Reset()         { *m = VaultShareRecord{} }
@@ -189,49 +176,97 @@ func (m *VaultShareRecord) GetDepositor() github_com_cosmos_cosmos_sdk_types.Acc
 	return nil
 }
 
-func (m *VaultShareRecord) GetAmountSupplied() github_com_cosmos_cosmos_sdk_types.Coins {
+func (m *VaultShareRecord) GetShares() VaultShares {
 	if m != nil {
-		return m.AmountSupplied
+		return m.Shares
 	}
 	return nil
+}
+
+// VaultShare defines shares of a vault owned by a depositor.
+type VaultShare struct {
+	Denom  string                                 `protobuf:"bytes,1,opt,name=denom,proto3" json:"denom,omitempty"`
+	Amount github_com_cosmos_cosmos_sdk_types.Int `protobuf:"bytes,2,opt,name=amount,proto3,customtype=github.com/cosmos/cosmos-sdk/types.Int" json:"amount"`
+}
+
+func (m *VaultShare) Reset()         { *m = VaultShare{} }
+func (m *VaultShare) String() string { return proto.CompactTextString(m) }
+func (*VaultShare) ProtoMessage()    {}
+func (*VaultShare) Descriptor() ([]byte, []int) {
+	return fileDescriptor_884eb89509fbdc04, []int{3}
+}
+func (m *VaultShare) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *VaultShare) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_VaultShare.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *VaultShare) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_VaultShare.Merge(m, src)
+}
+func (m *VaultShare) XXX_Size() int {
+	return m.Size()
+}
+func (m *VaultShare) XXX_DiscardUnknown() {
+	xxx_messageInfo_VaultShare.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_VaultShare proto.InternalMessageInfo
+
+func (m *VaultShare) GetDenom() string {
+	if m != nil {
+		return m.Denom
+	}
+	return ""
 }
 
 func init() {
 	proto.RegisterType((*AllowedVault)(nil), "kava.earn.v1beta1.AllowedVault")
 	proto.RegisterType((*VaultRecord)(nil), "kava.earn.v1beta1.VaultRecord")
 	proto.RegisterType((*VaultShareRecord)(nil), "kava.earn.v1beta1.VaultShareRecord")
+	proto.RegisterType((*VaultShare)(nil), "kava.earn.v1beta1.VaultShare")
 }
 
 func init() { proto.RegisterFile("kava/earn/v1beta1/vault.proto", fileDescriptor_884eb89509fbdc04) }
 
 var fileDescriptor_884eb89509fbdc04 = []byte{
-	// 417 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x92, 0xbd, 0x8e, 0xd3, 0x40,
-	0x10, 0xc7, 0xed, 0xe3, 0x43, 0xba, 0x4d, 0x08, 0x60, 0xae, 0xc8, 0x9d, 0x84, 0x6d, 0xa5, 0x40,
-	0x69, 0xbc, 0xe6, 0x8e, 0x17, 0x20, 0x46, 0x42, 0xd4, 0x36, 0xa2, 0xa0, 0x89, 0xd6, 0xde, 0xc5,
-	0xb1, 0x62, 0x7b, 0x2c, 0xef, 0x3a, 0xe0, 0xb7, 0xe0, 0x39, 0xa8, 0x79, 0x88, 0x94, 0x11, 0x15,
-	0x55, 0x40, 0xc9, 0x0b, 0x50, 0x53, 0xa1, 0xfd, 0x48, 0x40, 0x8a, 0x40, 0x54, 0xde, 0x9d, 0x99,
-	0xff, 0xfc, 0xfe, 0x33, 0x5e, 0xf4, 0x78, 0x49, 0x56, 0x24, 0x64, 0xa4, 0xad, 0xc3, 0xd5, 0x75,
-	0xca, 0x04, 0xb9, 0x0e, 0x57, 0xa4, 0x2b, 0x05, 0x6e, 0x5a, 0x10, 0xe0, 0x3c, 0x94, 0x69, 0x2c,
-	0xd3, 0xd8, 0xa4, 0xaf, 0x2e, 0x33, 0xe0, 0x15, 0xf0, 0xb9, 0x2a, 0x08, 0xf5, 0x45, 0x57, 0x5f,
-	0xb9, 0xfa, 0x16, 0xa6, 0x84, 0xb3, 0x63, 0xbb, 0x0c, 0x8a, 0xda, 0xe4, 0x2f, 0x72, 0xc8, 0x41,
-	0xeb, 0xe4, 0xc9, 0x44, 0xfd, 0x53, 0x0b, 0x5c, 0xb4, 0x44, 0xb0, 0xbc, 0xd7, 0x15, 0x93, 0x12,
-	0x0d, 0x67, 0x65, 0x09, 0xef, 0x19, 0x7d, 0x23, 0xbd, 0x39, 0x17, 0xe8, 0x0e, 0x65, 0x35, 0x54,
-	0x63, 0xdb, 0xb7, 0xa7, 0xe7, 0xb1, 0xbe, 0x38, 0x2f, 0xd1, 0x48, 0x59, 0x9f, 0x1f, 0xd4, 0xe3,
-	0x33, 0xdf, 0x9e, 0x8e, 0x6e, 0x3c, 0x7c, 0x32, 0x04, 0x4e, 0x4c, 0xc9, 0xeb, 0xbe, 0x61, 0xf1,
-	0x3d, 0x25, 0x3b, 0x84, 0x26, 0x39, 0x1a, 0x28, 0x4c, 0xcc, 0x32, 0x68, 0xe9, 0x5f, 0x60, 0x11,
-	0x1a, 0x0a, 0x10, 0xa4, 0x9c, 0xf3, 0xae, 0x69, 0x4a, 0x8d, 0x1a, 0xdc, 0x5c, 0x62, 0xb3, 0x0f,
-	0xb9, 0x81, 0x23, 0xec, 0x05, 0x14, 0x75, 0x74, 0x7b, 0xbd, 0xf5, 0xac, 0x78, 0xa0, 0x44, 0x89,
-	0xd2, 0x4c, 0x7e, 0xd8, 0xe8, 0x81, 0x22, 0x25, 0x0b, 0xd2, 0x32, 0x83, 0x7b, 0x87, 0xce, 0x29,
-	0x6b, 0x80, 0x17, 0x02, 0x5a, 0x85, 0x1c, 0x46, 0xaf, 0x7e, 0x6e, 0xbd, 0x20, 0x2f, 0xc4, 0xa2,
-	0x4b, 0x71, 0x06, 0x95, 0xd9, 0xb9, 0xf9, 0x04, 0x9c, 0x2e, 0x43, 0xd1, 0x37, 0x8c, 0xe3, 0x59,
-	0x96, 0xcd, 0x28, 0x6d, 0x19, 0xe7, 0x5f, 0x3e, 0x07, 0x8f, 0x8c, 0x13, 0x13, 0x89, 0x7a, 0xc1,
-	0x78, 0xfc, 0xbb, 0xb5, 0x23, 0xd0, 0x7d, 0x52, 0x41, 0x57, 0x0b, 0x3d, 0x41, 0xc1, 0xe8, 0xf8,
-	0xcc, 0xbf, 0xf5, 0xef, 0x19, 0x9e, 0xca, 0x19, 0x3e, 0x7d, 0xf3, 0xa6, 0xff, 0x61, 0x46, 0x0a,
-	0x78, 0x3c, 0xd2, 0x8c, 0xc4, 0x20, 0xa2, 0xe7, 0xeb, 0x9d, 0x6b, 0x6f, 0x76, 0xae, 0xfd, 0x7d,
-	0xe7, 0xda, 0x1f, 0xf7, 0xae, 0xb5, 0xd9, 0xbb, 0xd6, 0xd7, 0xbd, 0x6b, 0xbd, 0x7d, 0xf2, 0x47,
-	0x4f, 0xf9, 0xbf, 0x82, 0x92, 0xa4, 0x5c, 0x9d, 0xc2, 0x0f, 0xfa, 0x71, 0xa8, 0xbe, 0xe9, 0x5d,
-	0xf5, 0x24, 0x9e, 0xfd, 0x0a, 0x00, 0x00, 0xff, 0xff, 0x6e, 0x80, 0xa4, 0xff, 0xb9, 0x02, 0x00,
+	// 433 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0x8c, 0x52, 0x31, 0xcf, 0xd2, 0x40,
+	0x18, 0x6e, 0x3f, 0x95, 0x84, 0x2b, 0x12, 0x2d, 0x0c, 0x48, 0x42, 0xdb, 0x74, 0x30, 0x2c, 0x6d,
+	0x03, 0xae, 0x0e, 0xd2, 0x81, 0x88, 0x63, 0x41, 0x07, 0x17, 0x72, 0x6d, 0xcf, 0x42, 0x68, 0x7b,
+	0x4d, 0xef, 0x40, 0xfa, 0x2f, 0xfc, 0x1d, 0xce, 0xfc, 0x06, 0xc3, 0x48, 0x98, 0x8c, 0x03, 0x1a,
+	0xf8, 0x17, 0x4e, 0xe6, 0xae, 0x87, 0x35, 0x41, 0xe3, 0x37, 0xf5, 0xde, 0xe7, 0x7d, 0x9e, 0xe7,
+	0x9e, 0xb7, 0xef, 0x81, 0xde, 0x0a, 0x6e, 0xa0, 0x83, 0x60, 0x9e, 0x3a, 0x9b, 0x81, 0x8f, 0x28,
+	0x1c, 0x38, 0x1b, 0xb8, 0x8e, 0xa9, 0x9d, 0xe5, 0x98, 0x62, 0xf5, 0x29, 0x6b, 0xdb, 0xac, 0x6d,
+	0x8b, 0x76, 0xf7, 0x59, 0x80, 0x49, 0x82, 0xc9, 0x9c, 0x13, 0x9c, 0xb2, 0x28, 0xd9, 0x5d, 0xad,
+	0xac, 0x1c, 0x1f, 0x12, 0xf4, 0xdb, 0x2e, 0xc0, 0xcb, 0x54, 0xf4, 0xdb, 0x11, 0x8e, 0x70, 0xa9,
+	0x63, 0x27, 0x81, 0x1a, 0xb7, 0x11, 0x08, 0xcd, 0x21, 0x45, 0x51, 0x51, 0x32, 0xcc, 0x18, 0x34,
+	0x46, 0x71, 0x8c, 0x3f, 0xa2, 0xf0, 0x1d, 0xcb, 0xa6, 0xb6, 0xc1, 0xa3, 0x10, 0xa5, 0x38, 0xe9,
+	0xc8, 0x86, 0xdc, 0xaf, 0x7b, 0x65, 0xa1, 0x8e, 0x41, 0x93, 0x47, 0x9f, 0x5f, 0xd5, 0x9d, 0x3b,
+	0x43, 0xee, 0x37, 0x87, 0xba, 0x7d, 0x33, 0x84, 0x3d, 0x15, 0x94, 0x59, 0x91, 0x21, 0xef, 0x31,
+	0x97, 0x5d, 0x21, 0xf3, 0x2d, 0x50, 0xf8, 0x35, 0x1e, 0x0a, 0x70, 0x1e, 0xaa, 0x63, 0xd0, 0xa0,
+	0x98, 0xc2, 0x78, 0x4e, 0x16, 0x30, 0x47, 0x84, 0xdf, 0xa9, 0x0c, 0x7b, 0x7f, 0x31, 0xe5, 0xaa,
+	0x29, 0x63, 0xb9, 0x0f, 0xf7, 0x27, 0x5d, 0xf2, 0x14, 0x2e, 0xe4, 0x08, 0x31, 0xbf, 0xc8, 0xe0,
+	0x49, 0xc5, 0x10, 0xe6, 0x1f, 0x40, 0x3d, 0x44, 0x19, 0x26, 0x4b, 0x8a, 0x73, 0xee, 0xdc, 0x70,
+	0x5f, 0xff, 0x3c, 0xe9, 0x56, 0xb4, 0xa4, 0x8b, 0xb5, 0x6f, 0x07, 0x38, 0x11, 0x7f, 0x58, 0x7c,
+	0x2c, 0x12, 0xae, 0x1c, 0x5a, 0x64, 0x88, 0xd8, 0xa3, 0x20, 0x18, 0x85, 0x61, 0x8e, 0x08, 0x39,
+	0xee, 0xac, 0x96, 0xd8, 0x83, 0x40, 0xdc, 0x82, 0x22, 0xe2, 0x55, 0xd6, 0xea, 0x1b, 0x50, 0x13,
+	0xf1, 0xef, 0x8c, 0x07, 0xff, 0x8f, 0xdf, 0x62, 0xf1, 0x3f, 0x7f, 0xd7, 0x95, 0x0a, 0x23, 0x9e,
+	0x70, 0x30, 0xb7, 0x00, 0x54, 0xf0, 0x3f, 0x76, 0x31, 0x03, 0x35, 0x98, 0xe0, 0x75, 0x4a, 0xf9,
+	0x0e, 0xea, 0xee, 0x4b, 0x66, 0xf8, 0xed, 0xa4, 0x3f, 0xbf, 0xc7, 0x60, 0x93, 0x94, 0x1e, 0x77,
+	0x16, 0x10, 0x13, 0x4d, 0x52, 0xea, 0x09, 0x2f, 0xf7, 0xd5, 0xfe, 0xac, 0xc9, 0x87, 0xb3, 0x26,
+	0xff, 0x38, 0x6b, 0xf2, 0xa7, 0x8b, 0x26, 0x1d, 0x2e, 0x9a, 0xf4, 0xf5, 0xa2, 0x49, 0xef, 0xff,
+	0xf4, 0x65, 0x93, 0x59, 0x31, 0xf4, 0x09, 0x3f, 0x39, 0xdb, 0xf2, 0x69, 0x71, 0x6f, 0xbf, 0xc6,
+	0x1f, 0xd4, 0x8b, 0x5f, 0x01, 0x00, 0x00, 0xff, 0xff, 0x52, 0x5f, 0xe7, 0x5a, 0xf7, 0x02, 0x00,
 	0x00,
 }
 
@@ -291,7 +326,7 @@ func (m *VaultRecord) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	var l int
 	_ = l
 	{
-		size, err := m.TotalSupply.MarshalToSizedBuffer(dAtA[:i])
+		size, err := m.TotalShares.MarshalToSizedBuffer(dAtA[:i])
 		if err != nil {
 			return 0, err
 		}
@@ -299,14 +334,7 @@ func (m *VaultRecord) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i = encodeVarintVault(dAtA, i, uint64(size))
 	}
 	i--
-	dAtA[i] = 0x12
-	if len(m.Denom) > 0 {
-		i -= len(m.Denom)
-		copy(dAtA[i:], m.Denom)
-		i = encodeVarintVault(dAtA, i, uint64(len(m.Denom)))
-		i--
-		dAtA[i] = 0xa
-	}
+	dAtA[i] = 0xa
 	return len(dAtA) - i, nil
 }
 
@@ -330,10 +358,10 @@ func (m *VaultShareRecord) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 	_ = i
 	var l int
 	_ = l
-	if len(m.AmountSupplied) > 0 {
-		for iNdEx := len(m.AmountSupplied) - 1; iNdEx >= 0; iNdEx-- {
+	if len(m.Shares) > 0 {
+		for iNdEx := len(m.Shares) - 1; iNdEx >= 0; iNdEx-- {
 			{
-				size, err := m.AmountSupplied[iNdEx].MarshalToSizedBuffer(dAtA[:i])
+				size, err := m.Shares[iNdEx].MarshalToSizedBuffer(dAtA[:i])
 				if err != nil {
 					return 0, err
 				}
@@ -348,6 +376,46 @@ func (m *VaultShareRecord) MarshalToSizedBuffer(dAtA []byte) (int, error) {
 		i -= len(m.Depositor)
 		copy(dAtA[i:], m.Depositor)
 		i = encodeVarintVault(dAtA, i, uint64(len(m.Depositor)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *VaultShare) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *VaultShare) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *VaultShare) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	{
+		size := m.Amount.Size()
+		i -= size
+		if _, err := m.Amount.MarshalTo(dAtA[i:]); err != nil {
+			return 0, err
+		}
+		i = encodeVarintVault(dAtA, i, uint64(size))
+	}
+	i--
+	dAtA[i] = 0x12
+	if len(m.Denom) > 0 {
+		i -= len(m.Denom)
+		copy(dAtA[i:], m.Denom)
+		i = encodeVarintVault(dAtA, i, uint64(len(m.Denom)))
 		i--
 		dAtA[i] = 0xa
 	}
@@ -387,11 +455,7 @@ func (m *VaultRecord) Size() (n int) {
 	}
 	var l int
 	_ = l
-	l = len(m.Denom)
-	if l > 0 {
-		n += 1 + l + sovVault(uint64(l))
-	}
-	l = m.TotalSupply.Size()
+	l = m.TotalShares.Size()
 	n += 1 + l + sovVault(uint64(l))
 	return n
 }
@@ -406,12 +470,27 @@ func (m *VaultShareRecord) Size() (n int) {
 	if l > 0 {
 		n += 1 + l + sovVault(uint64(l))
 	}
-	if len(m.AmountSupplied) > 0 {
-		for _, e := range m.AmountSupplied {
+	if len(m.Shares) > 0 {
+		for _, e := range m.Shares {
 			l = e.Size()
 			n += 1 + l + sovVault(uint64(l))
 		}
 	}
+	return n
+}
+
+func (m *VaultShare) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Denom)
+	if l > 0 {
+		n += 1 + l + sovVault(uint64(l))
+	}
+	l = m.Amount.Size()
+	n += 1 + l + sovVault(uint64(l))
 	return n
 }
 
@@ -553,39 +632,7 @@ func (m *VaultRecord) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field Denom", wireType)
-			}
-			var stringLen uint64
-			for shift := uint(0); ; shift += 7 {
-				if shift >= 64 {
-					return ErrIntOverflowVault
-				}
-				if iNdEx >= l {
-					return io.ErrUnexpectedEOF
-				}
-				b := dAtA[iNdEx]
-				iNdEx++
-				stringLen |= uint64(b&0x7F) << shift
-				if b < 0x80 {
-					break
-				}
-			}
-			intStringLen := int(stringLen)
-			if intStringLen < 0 {
-				return ErrInvalidLengthVault
-			}
-			postIndex := iNdEx + intStringLen
-			if postIndex < 0 {
-				return ErrInvalidLengthVault
-			}
-			if postIndex > l {
-				return io.ErrUnexpectedEOF
-			}
-			m.Denom = string(dAtA[iNdEx:postIndex])
-			iNdEx = postIndex
-		case 2:
-			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field TotalSupply", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field TotalShares", wireType)
 			}
 			var msglen int
 			for shift := uint(0); ; shift += 7 {
@@ -612,7 +659,7 @@ func (m *VaultRecord) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			if err := m.TotalSupply.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			if err := m.TotalShares.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
@@ -702,7 +749,7 @@ func (m *VaultShareRecord) Unmarshal(dAtA []byte) error {
 			iNdEx = postIndex
 		case 2:
 			if wireType != 2 {
-				return fmt.Errorf("proto: wrong wireType = %d for field AmountSupplied", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Shares", wireType)
 			}
 			var msglen int
 			for shift := uint(0); ; shift += 7 {
@@ -729,8 +776,124 @@ func (m *VaultShareRecord) Unmarshal(dAtA []byte) error {
 			if postIndex > l {
 				return io.ErrUnexpectedEOF
 			}
-			m.AmountSupplied = append(m.AmountSupplied, types.Coin{})
-			if err := m.AmountSupplied[len(m.AmountSupplied)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+			m.Shares = append(m.Shares, VaultShare{})
+			if err := m.Shares[len(m.Shares)-1].Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
+				return err
+			}
+			iNdEx = postIndex
+		default:
+			iNdEx = preIndex
+			skippy, err := skipVault(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthVault
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *VaultShare) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowVault
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: VaultShare: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: VaultShare: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Denom", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowVault
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthVault
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthVault
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Denom = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Amount", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowVault
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthVault
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthVault
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			if err := m.Amount.Unmarshal(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
 			iNdEx = postIndex
