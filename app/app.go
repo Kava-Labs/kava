18f879112d4d6f1f0557071deaa62617c22827b1
drklee3 <derrick@dlee.dev>
2022-07-29 09:37:48 -0700

    Pass hard and savings keeper as pointer to earn keeper

--- app/app.go
+++ app/app.go
@@ -630,8 +630,8 @@ func NewApp(
 		earnSubspace,
 		app.accountKeeper,
 		app.bankKeeper,
-		hardKeeper,
-		savingsKeeper,
+		&hardKeeper,
+		&savingsKeeper,
 	)
 
 	// create committee keeper with router
