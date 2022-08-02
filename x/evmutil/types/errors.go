124d0192e6bdc078f33c59f0ba60ec4062d0dfd5
DracoLi <draco@dracoli.com>
2022-08-02 11:58:31 -0400

    remove

--- x/evmutil/types/errors.go
+++ x/evmutil/types/errors.go
@@ -7,6 +7,5 @@ import (
 // errors
 var (
 	ErrABIPack              = sdkerrors.Register(ModuleName, 2, "contract ABI pack failed")
-	ErrABIUnpack            = sdkerrors.Register(ModuleName, 3, "contract ABI unpack failed")
 	ErrConversionNotEnabled = sdkerrors.Register(ModuleName, 4, "ERC20 token not enabled to convert to sdk.Coin")
 )
