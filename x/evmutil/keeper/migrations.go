70e1716d34e0434eeb9c742a79e4a5716f7cd678
DracoLi <draco@dracoli.com>
2022-08-18 11:52:48 -0400

    fix evm migration issues

--- x/evmutil/keeper/migrations.go
+++ x/evmutil/keeper/migrations.go
@@ -1,23 +1,22 @@
-package migrations
+package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
 
 	v2 "github.com/kava-labs/kava/x/evmutil/migrations/v2"
 )
 
 // Migrator is a struct for handling in-place store migrations.
 type Migrator struct {
-	legacySubspace paramtypes.Subspace
+	keeper Keeper
 }
 
 // NewMigrator returns a new Migrator.
-func NewMigrator(ss paramtypes.Subspace) Migrator {
-	return Migrator{legacySubspace: ss}
+func NewMigrator(keeper Keeper) Migrator {
+	return Migrator{keeper: keeper}
 }
 
 // Migrate1to2 migrates from version 1 to 2.
 func (m Migrator) Migrate1to2(ctx sdk.Context) error {
-	return v2.MigrateStore(ctx, m.legacySubspace)
+	return v2.MigrateStore(ctx, m.keeper.paramSubspace)
 }
