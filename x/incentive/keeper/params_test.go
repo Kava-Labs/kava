b47bd4264379a6b03ac25d8dce21c625bf799c47
drklee3 <derrick@dlee.dev>
2022-09-08 15:52:59 -0700

    Add additional tests for genesis and params

--- x/incentive/keeper/params_test.go
+++ x/incentive/keeper/params_test.go
@@ -45,8 +45,6 @@ func (suite *ParamsTests) TestGetMultiplierByDenom() {
 	}
 	suite.keeper = suite.NewKeeper(subspace, nil, nil, nil, nil, nil, nil, nil, nil, nil)
 
-	suite.T().Logf("params :%v", suite.keeper.GetParams(suite.ctx).ClaimMultipliers)
-
 	tests := []struct {
 		name       string
 		denom      string
@@ -88,5 +86,32 @@ func (suite *ParamsTests) TestGetMultiplierByDenom() {
 			suite.Require().True(found)
 			suite.Require().Equal(tc.expected, multiplier)
 		})
+	}
+}
+
+func (suite *ParamsTests) TestEmptyModuleNameByDefault() {
+	subspace := &fakeParamSubspace{
+		params: types.Params{
+			ClaimMultipliers: types.MultipliersPerDenoms{
+				{
+					Denom: "hard",
+					Multipliers: types.Multipliers{
+						types.NewMultiplier("small", 1, d("0.2")),
+					},
+				},
+				{
+					Denom: "ukava",
+					Multipliers: types.Multipliers{
+						types.NewMultiplier("large", 0, d("1")),
+					},
+				},
+			},
+		},
+	}
+	suite.keeper = suite.NewKeeper(subspace, nil, nil, nil, nil, nil, nil, nil, nil, nil)
+
+	params := suite.keeper.GetParams(suite.ctx)
+	for _, m := range params.ClaimMultipliers {
+		suite.Require().Equal("", m.ModuleName)
 	}
 }
