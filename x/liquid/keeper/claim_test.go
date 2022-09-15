a38144ffb38d927edfa38f552887952b7693607e
drklee3 <derrick@dlee.dev>
2022-09-15 13:10:53 -0700

    Add claim test

--- x/liquid/keeper/claim_test.go
+++ x/liquid/keeper/claim_test.go
@@ -2,8 +2,11 @@ package keeper_test
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/staking"
 	"github.com/kava-labs/kava/app"
 	"github.com/kava-labs/kava/x/liquid/types"
+
+	distrtypes "github.com/cosmos/cosmos-sdk/x/distribution/types"
 )
 
 func (suite *KeeperTestSuite) TestCollectStakingRewards() {
@@ -12,31 +15,52 @@ func (suite *KeeperTestSuite) TestCollectStakingRewards() {
 	valAddr1 := sdk.ValAddress(valAccAddr1)
 
 	initialBalance := i(1e9)
-	vestedBalance := i(500e6)
 	delegateAmount := i(100e6)
 
+	err := suite.App.FundModuleAccount(
+		suite.Ctx,
+		distrtypes.ModuleName,
+		sdk.NewCoins(
+			sdk.NewCoin("ukava", initialBalance),
+		),
+	)
+	suite.NoError(err)
+
 	suite.CreateAccountWithAddress(valAccAddr1, suite.NewBondCoins(initialBalance))
-	suite.CreateVestingAccountWithAddress(delegator, suite.NewBondCoins(initialBalance), suite.NewBondCoins(vestedBalance))
+	suite.CreateAccountWithAddress(delegator, suite.NewBondCoins(initialBalance))
 
 	suite.CreateNewUnbondedValidator(valAddr1, initialBalance)
 	suite.CreateDelegation(valAddr1, delegator, delegateAmount)
+	staking.EndBlocker(suite.Ctx, suite.StakingKeeper)
 
-	_, err := suite.Keeper.MintDerivative(suite.Ctx, delegator, valAddr1, suite.NewBondCoin(delegateAmount))
+	// Transfers delegation to module account
+	_, err = suite.Keeper.MintDerivative(suite.Ctx, delegator, valAddr1, suite.NewBondCoin(delegateAmount))
 	suite.Require().NoError(err)
 
 	validator, found := suite.StakingKeeper.GetValidator(suite.Ctx, valAddr1)
 	suite.Require().True(found)
 
-	rewardCoins := sdk.NewDecCoins(sdk.NewDecCoin("ukava", sdk.NewInt(100e6)))
+	suite.Ctx = suite.Ctx.WithBlockHeight(2)
+
+	rewardCoins := sdk.NewDecCoins(sdk.NewDecCoin("ukava", sdk.NewInt(500e6)))
 
 	distrKeeper := suite.App.GetDistrKeeper()
+	stakingKeeper := suite.App.GetStakingKeeper()
+	accKeeper := suite.App.GetAccountKeeper()
+	liquidMacc := accKeeper.GetModuleAccount(suite.Ctx, types.ModuleAccountName)
+
 	distrKeeper.AllocateTokensToValidator(suite.Ctx, validator, rewardCoins)
 
-	derivativeDenom := suite.Keeper.GetLiquidStakingTokenDenom(valAddr1)
-	suite.T().Logf("derivativeDenom: %s", derivativeDenom)
+	delegation, found := stakingKeeper.GetDelegation(suite.Ctx, liquidMacc.GetAddress(), valAddr1)
+	suite.Require().True(found)
 
+	endingPeriod := distrKeeper.IncrementValidatorPeriod(suite.Ctx, validator)
+	delegationRewards := distrKeeper.CalculateDelegationRewards(suite.Ctx, validator, delegation, endingPeriod)
+	truncatedRewards, _ := delegationRewards.TruncateDecimal()
+
+	derivativeDenom := suite.Keeper.GetLiquidStakingTokenDenom(valAddr1)
 	rewards, err := suite.Keeper.CollectStakingRewardsByDenom(suite.Ctx, derivativeDenom, types.ModuleName)
 	suite.Require().NoError(err)
 
-	suite.Require().Equal(rewardCoins, (rewards))
+	suite.Require().Equal(truncatedRewards, rewards)
 }
