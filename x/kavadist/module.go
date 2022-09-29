51069b552dc5848d579854ed94256307ccff6413
Draco <draco@dracoli.com>
2022-09-29 17:12:32 -0400

    update kavadist consensus version

--- x/kavadist/module.go
+++ x/kavadist/module.go
@@ -160,7 +160,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 1 }
+func (AppModule) ConsensusVersion() uint64 { return ConsensusVersion }
 
 // BeginBlock executes all ABCI BeginBlock logic respective to kavadist module.
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
