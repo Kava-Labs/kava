7086f1d46a2d2eb052b163900adf1986c97ca6ff
drklee3 <derrick@dlee.dev>
2022-09-29 10:40:57 -0700

    Add param change for cosmos x/mint

--- app/upgrades.go
+++ app/upgrades.go
@@ -4,6 +4,7 @@ import (
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
+	mintkeeper "github.com/cosmos/cosmos-sdk/x/mint/keeper"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	earntypes "github.com/kava-labs/kava/x/earn/types"
 )
@@ -13,6 +14,9 @@ const UpgradeName = "v0.19.0"
 func (app App) RegisterUpgradeHandlers() {
 	app.upgradeKeeper.SetUpgradeHandler(UpgradeName,
 		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
+			// 100% inflation -> 75% inflation
+			UpdateCosmosMintInflation(ctx, app.mintKeeper)
+
 			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 		},
 	)
@@ -35,4 +39,13 @@ func (app App) RegisterUpgradeHandlers() {
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
 	}
+}
+
+func UpdateCosmosMintInflation(ctx sdk.Context, mintKeeper mintkeeper.Keeper) {
+	mintParams := mintKeeper.GetParams(ctx)
+	// 0.75
+	mintParams.InflationMin = sdk.NewDecWithPrec(75, 2)
+	mintParams.InflationMax = sdk.NewDecWithPrec(75, 2)
+
+	mintKeeper.SetParams(ctx, mintParams)
 }
