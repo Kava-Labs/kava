81cda46c178735be186e0df8616b028dbbeed452
Derrick Lee <derrick@dlee.dev>
2022-09-30 12:40:22 -0700

    Add migration to add ukava, bkava, usdc to x/savings supported denoms (#1324)
    * Add supported denoms to x/savings
    
    * Add logging for param migrations

--- app/upgrades.go
+++ app/upgrades.go
@@ -7,6 +7,8 @@ import (
 	mintkeeper "github.com/cosmos/cosmos-sdk/x/mint/keeper"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
 	earntypes "github.com/kava-labs/kava/x/earn/types"
+	savingskeeper "github.com/kava-labs/kava/x/savings/keeper"
+	savingstypes "github.com/kava-labs/kava/x/savings/types"
 )
 
 const UpgradeName = "v0.19.0"
@@ -15,7 +17,11 @@ func (app App) RegisterUpgradeHandlers() {
 	app.upgradeKeeper.SetUpgradeHandler(UpgradeName,
 		func(ctx sdk.Context, plan upgradetypes.Plan, fromVM module.VersionMap) (module.VersionMap, error) {
 			// 100% inflation -> 75% inflation
+			app.Logger().Info("updating x/mint params with inflation 100% -> 75%")
 			UpdateCosmosMintInflation(ctx, app.mintKeeper)
+
+			app.Logger().Info("updating x/savings params with new supported denoms")
+			UpdateSavingsParams(ctx, app.savingsKeeper)
 
 			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 		},
@@ -48,4 +54,14 @@ func UpdateCosmosMintInflation(ctx sdk.Context, mintKeeper mintkeeper.Keeper) {
 	mintParams.InflationMax = sdk.NewDecWithPrec(75, 2)
 
 	mintKeeper.SetParams(ctx, mintParams)
+}
+
+func UpdateSavingsParams(ctx sdk.Context, savingsKeeper savingskeeper.Keeper) {
+	savingsParams := savingstypes.NewParams([]string{
+		"ukava",
+		"bkava",
+		"erc20/multichain/usdc",
+	})
+
+	savingsKeeper.SetParams(ctx, savingsParams)
 }
