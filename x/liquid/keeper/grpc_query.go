584abd7147d4a9119500701c25dfd5a52984e713
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2022-10-03 16:55:18 -0700

    add basic implementation

--- x/liquid/keeper/grpc_query.go
+++ x/liquid/keeper/grpc_query.go
@@ -2,6 +2,12 @@ package keeper
 
 import (
 	"context"
+	"fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	vestingexported "github.com/cosmos/cosmos-sdk/x/auth/vesting/exported"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 
 	"github.com/kava-labs/kava/x/liquid/types"
 )
@@ -18,8 +24,69 @@ func NewQueryServerImpl(k Keeper) types.QueryServer {
 var _ types.QueryServer = queryServer{}
 
 func (s queryServer) DelegatedBalance(
-	ctx context.Context,
+	goCtx context.Context,
 	req *types.QueryDelegatedBalanceRequest,
 ) (*types.QueryDelegatedBalanceResponse, error) {
-	return &types.QueryDelegatedBalanceResponse{}, nil
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	delegator, err := sdk.AccAddressFromBech32(req.Delegator)
+	if err != nil {
+		return nil, status.Errorf(codes.InvalidArgument, "invalid delegator address: %s", err)
+	}
+
+	delegated := s.getDelegatedBalance(ctx, delegator)
+	unbonding := s.getUnbondingBalance(ctx, delegator)
+
+	bondDenom := s.keeper.stakingKeeper.BondDenom(ctx)
+	vesting := s.getVesting(ctx, delegator).AmountOf(bondDenom)
+
+	totalStaked := delegated.Add(unbonding)
+	vestingDelegated := sdk.MinInt(vesting, totalStaked)
+	vestedDelegated := totalStaked.Sub(vestingDelegated)
+
+	res := types.QueryDelegatedBalanceResponse{
+		Vested:  sdk.NewCoin(bondDenom, vestedDelegated),
+		Vesting: sdk.NewCoin(bondDenom, vestingDelegated),
+	}
+	return &res, nil
+}
+
+func (s queryServer) getDelegatedBalance(ctx sdk.Context, delegator sdk.AccAddress) sdk.Int {
+	balance := sdk.ZeroInt()
+
+	delegations := s.keeper.stakingKeeper.GetDelegatorDelegations(ctx, delegator, 1000) // TODO what to set max to?
+	for _, delegation := range delegations {
+		validator, found := s.keeper.stakingKeeper.GetValidator(ctx, delegation.GetValidatorAddr())
+		if !found {
+			panic(fmt.Sprintf("validator %s for delegation not found", delegation.GetValidatorAddr()))
+		}
+		balance = balance.Add(validator.TokensFromShares(delegation.GetShares()).TruncateInt())
+	}
+	return balance
+}
+
+func (s queryServer) getUnbondingBalance(ctx sdk.Context, delegator sdk.AccAddress) sdk.Int {
+	balance := sdk.ZeroInt()
+
+	ubds := s.keeper.stakingKeeper.GetUnbondingDelegations(ctx, delegator, 1000) // TODO what to set max to?
+	for _, ubd := range ubds {
+		for _, entry := range ubd.Entries {
+			balance = balance.Add(entry.Balance)
+		}
+	}
+	return balance
+}
+
+func (s queryServer) getVesting(ctx sdk.Context, delegator sdk.AccAddress) sdk.Coins {
+	acc := s.keeper.accountKeeper.GetAccount(ctx, delegator)
+	if acc == nil {
+		// account doesn't exist so amount vesting is 0
+		return nil
+	}
+	vestAcc, ok := acc.(vestingexported.VestingAccount)
+	if !ok {
+		// account is not vesting type, so amount vesting is 0
+		return nil
+	}
+	return vestAcc.GetVestingCoins(ctx.BlockTime())
 }
--- x/liquid/keeper/grpc_query_test.go
+++ x/liquid/keeper/grpc_query_test.go
@@ -5,8 +5,11 @@ import (
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/staking"
 	"github.com/stretchr/testify/suite"
 
+	"github.com/kava-labs/kava/app"
 	"github.com/kava-labs/kava/x/liquid/keeper"
 	"github.com/kava-labs/kava/x/liquid/types"
 )
@@ -31,6 +34,135 @@ func TestGrpcQueryTestSuite(t *testing.T) {
 }
 
 func (suite *grpcQueryTestSuite) TestQueryDelegatedBalance() {
-	_, err := suite.queryClient.DelegatedBalance(context.Background(), &types.QueryDelegatedBalanceRequest{})
-	suite.Require().NoError(err)
+	zeroResponse := &types.QueryDelegatedBalanceResponse{
+		Vested:  suite.NewBondCoin(sdk.ZeroInt()),
+		Vesting: suite.NewBondCoin(sdk.ZeroInt()),
+	}
+
+	testCases := []struct {
+		name        string
+		setup       func() string
+		expectedRes *types.QueryDelegatedBalanceResponse
+		expectedErr error
+	}{
+		{
+			name: "no account returns zeros",
+			setup: func() string {
+				return "kava10wlnqzyss4accfqmyxwx5jy5x9nfkwh6qm7n4t"
+			},
+			expectedRes: zeroResponse,
+		},
+		{
+			name: "base account no delegations returns zeros",
+			setup: func() string {
+				acc := suite.CreateAccount(suite.NewBondCoins(i(1e9)), 0)
+				return acc.GetAddress().String()
+			},
+			expectedRes: zeroResponse,
+		},
+		{
+			name: "base account with delegations returns delegated",
+			setup: func() string {
+				initBalance := suite.NewBondCoin(i(1e9))
+				val1Acc := suite.CreateAccount(sdk.NewCoins(initBalance), 0)
+				val2Acc := suite.CreateAccount(sdk.NewCoins(initBalance), 1)
+				delAcc := suite.CreateAccount(sdk.NewCoins(initBalance), 2)
+
+				suite.CreateNewUnbondedValidator(val1Acc.GetAddress().Bytes(), initBalance.Amount)
+				suite.CreateNewUnbondedValidator(val2Acc.GetAddress().Bytes(), initBalance.Amount)
+
+				suite.CreateDelegation(val1Acc.GetAddress().Bytes(), delAcc.GetAddress(), initBalance.Amount.QuoRaw(2))
+				suite.CreateDelegation(val2Acc.GetAddress().Bytes(), delAcc.GetAddress(), initBalance.Amount.QuoRaw(2))
+
+				return delAcc.GetAddress().String()
+			},
+			expectedRes: &types.QueryDelegatedBalanceResponse{
+				Vested:  suite.NewBondCoin(i(1e9)),
+				Vesting: suite.NewBondCoin(sdk.ZeroInt()),
+			},
+		},
+		{
+			name: "base account with delegations and unbonding delegations returns total",
+			setup: func() string {
+				initBalance := suite.NewBondCoin(i(1e9))
+				valAcc := suite.CreateAccount(sdk.NewCoins(initBalance), 0)
+				delAcc := suite.CreateAccount(sdk.NewCoins(initBalance), 1)
+
+				suite.CreateNewUnbondedValidator(valAcc.GetAddress().Bytes(), initBalance.Amount)
+				suite.CreateDelegation(valAcc.GetAddress().Bytes(), delAcc.GetAddress(), initBalance.Amount)
+				staking.EndBlocker(suite.Ctx, suite.StakingKeeper) // bond the validator
+
+				suite.CreateUnbondingDelegation(delAcc.GetAddress(), valAcc.GetAddress().Bytes(), initBalance.Amount.QuoRaw(2))
+
+				return delAcc.GetAddress().String()
+			},
+			expectedRes: &types.QueryDelegatedBalanceResponse{
+				Vested:  suite.NewBondCoin(i(1e9)),
+				Vesting: suite.NewBondCoin(sdk.ZeroInt()),
+			},
+		},
+		{
+			name: "base account with only unbonding delegations returns unbonding",
+			setup: func() string {
+				initBalance := suite.NewBondCoin(i(1e9))
+				valAcc := suite.CreateAccount(sdk.NewCoins(initBalance), 0)
+				delAcc := suite.CreateAccount(sdk.NewCoins(initBalance), 1)
+
+				suite.CreateNewUnbondedValidator(valAcc.GetAddress().Bytes(), initBalance.Amount)
+				suite.CreateDelegation(valAcc.GetAddress().Bytes(), delAcc.GetAddress(), initBalance.Amount)
+				staking.EndBlocker(suite.Ctx, suite.StakingKeeper) // bond the validator
+
+				suite.CreateUnbondingDelegation(delAcc.GetAddress(), valAcc.GetAddress().Bytes(), initBalance.Amount)
+
+				return delAcc.GetAddress().String()
+			},
+			expectedRes: &types.QueryDelegatedBalanceResponse{
+				Vested:  suite.NewBondCoin(i(1e9)),
+				Vesting: suite.NewBondCoin(sdk.ZeroInt()),
+			},
+		},
+		{
+			name: "vesting balance taken from delegated and unbonding balances",
+			setup: func() string {
+				initBalance := suite.NewBondCoin(i(1e9))
+				_, addrs := app.GeneratePrivKeyAddressPairs(2)
+				valAddr, delAddr := addrs[0], addrs[1]
+
+				suite.CreateAccountWithAddress(valAddr, sdk.NewCoins(initBalance))
+
+				threeQuarters := initBalance.Amount.ToDec().Mul(d("0.75")).TruncateInt()
+				suite.CreateVestingAccountWithAddress(delAddr, sdk.NewCoins(initBalance), suite.NewBondCoins(threeQuarters))
+
+				suite.CreateNewUnbondedValidator(sdk.ValAddress(valAddr), initBalance.Amount)
+				suite.CreateDelegation(sdk.ValAddress(valAddr), delAddr, initBalance.Amount)
+				staking.EndBlocker(suite.Ctx, suite.StakingKeeper) // bond the validator
+
+				suite.CreateUnbondingDelegation(delAddr, sdk.ValAddress(valAddr), initBalance.Amount.QuoRaw(2))
+
+				return delAddr.String()
+			},
+			expectedRes: &types.QueryDelegatedBalanceResponse{
+				Vested:  suite.NewBondCoin(i(250e6)),
+				Vesting: suite.NewBondCoin(i(750e6)),
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+			address := tc.setup()
+
+			res, err := suite.queryClient.DelegatedBalance(
+				context.Background(),
+				&types.QueryDelegatedBalanceRequest{
+					Delegator: address,
+				},
+			)
+			suite.ErrorIs(err, tc.expectedErr)
+			if err == nil {
+				suite.Equal(tc.expectedRes, res)
+			}
+		})
+	}
 }
