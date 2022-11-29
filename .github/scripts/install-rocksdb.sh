3a766030a3279b48047086d354144a3df564f525
Levi Schoen <levi.schoen@kava.io>
2022-11-29 12:22:48 -0800

    github action for ci (#1398)
    * add workflow for CI on PRs to master or release branch, and every push to master
    
    * sprint demo

---
+++ .github/scripts/install-rocksdb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -x
+
+# install build dependencies
+sudo apt-get install -y libgflags-dev libsnappy-dev zlib1g-dev libbz2-dev liblz4-dev libzstd-dev
+
+# get rocksdb sources
+git clone https://github.com/facebook/rocksdb.git /home/runner/rocksdb
+
+cd /home/runner/rocksdb
+
+git checkout v7.7.3
+
+# install rocksdb locally
+sudo make -j $(nproc --all) install-shared
