cca837130728f7f2d3c10d89c2e83fc4538333d2
Nick DeLuca <nickdeluca08@gmail.com>
2022-11-30 11:02:16 -0700

    Update proto breaking CI check to pass on release branches (#1407)
    * modify proto check breaking to check against the previous commit by
    default
    
    * add additional proto breaking check for pull requests to master
    
    * always check proto breaking against target branch when making a pull
    request

--- build/proto.mk
+++ build/proto.mk
@@ -43,15 +43,17 @@ proto-format: install-build-deps
 check-proto-format: proto-format
 	@git diff --exit-code proto/**/*.proto > /dev/null || (echo "Protobuf format is not up to date! Please run \`make proto-format\`."; exit 1)
 
+BUF_CHECK_BREAKING_AGAINST ?= ref=HEAD~1
+
 .PHONY: check-proto-breaking
 check-proto-breaking: install-build-deps
 	@echo "Checking for proto backward compatibility"
-	@$(BUF) breaking --against '.git#branch=master'
+	@$(BUF) breaking --against '.git#$(BUF_CHECK_BREAKING_AGAINST)'
 
 .PHONY: check-proto-breaking-remote
 check-proto-breaking-remote: install-build-deps
 	@echo "Checking for proto backward compatibility"
-	@$(BUF) breaking --against '$(HTTPS_GIT)#branch=master'
+	@$(BUF) breaking --against '$(HTTPS_GIT)#$(BUF_CHECK_BREAKING_AGAINST)'
 
 .PHONY: proto-gen-all
 proto-gen-all: proto-gen proto-gen-doc proto-gen-swagger
