937e5f339f97bd2451dc47fe9010d9c3c103ed87
Derrick Lee <derrick@dlee.dev>
2022-12-05 16:54:18 -0800

    Add incentive migrations for earn rewards (#1406)
    * Add initial earn claim migrations
    
    * Use existing types for migrations, add accural time migrations
    
    * Add MigrateRewardIndexes
    
    * Delete old state after migration
    
    * Update store test with multiple entries
    
    * Move key methods to keys.go
    
    * Update incentive consensus version to 3
    
    * Call MigrateRewardIndexes in main migration, remove debugging statements
    
    * Fix migration version to v3
    
    * Update module versions
    
    * Update outdated v1 comment

--- app/test_common.go
+++ app/test_common.go
@@ -56,13 +56,16 @@ var (
 // TestApp is a simple wrapper around an App. It exposes internal keepers for use in integration tests.
 // This file also contains test helpers. Ideally they would be in separate package.
 // Basic Usage:
-// 	Create a test app with NewTestApp, then all keepers and their methods can be accessed for test setup and execution.
+//
+//	Create a test app with NewTestApp, then all keepers and their methods can be accessed for test setup and execution.
+//
 // Advanced Usage:
-// 	Some tests call for an app to be initialized with some state. This can be achieved through keeper method calls (ie keeper.SetParams(...)).
-// 	However this leads to a lot of duplicated logic similar to InitGenesis methods.
-// 	So TestApp.InitializeFromGenesisStates() will call InitGenesis with the default genesis state.
+//
+//	Some tests call for an app to be initialized with some state. This can be achieved through keeper method calls (ie keeper.SetParams(...)).
+//	However this leads to a lot of duplicated logic similar to InitGenesis methods.
+//	So TestApp.InitializeFromGenesisStates() will call InitGenesis with the default genesis state.
 //	and TestApp.InitializeFromGenesisStates(authState, cdpState) will do the same but overwrite the auth and cdp sections of the default genesis state
-// 	Creating the genesis states can be combersome, but helper methods can make it easier such as NewAuthGenStateFromAccounts below.
+//	Creating the genesis states can be combersome, but helper methods can make it easier such as NewAuthGenStateFromAccounts below.
 type TestApp struct {
 	App
 }
@@ -114,6 +117,10 @@ func (tApp TestApp) GetFeeMarketKeeper() feemarketkeeper.Keeper { return tApp.fe
 func (tApp TestApp) GetLiquidKeeper() liquidkeeper.Keeper       { return tApp.liquidKeeper }
 func (tApp TestApp) GetEarnKeeper() earnkeeper.Keeper           { return tApp.earnKeeper }
 func (tApp TestApp) GetRouterKeeper() routerkeeper.Keeper       { return tApp.routerKeeper }
+
+func (tApp TestApp) GetKeys() map[string]*sdk.KVStoreKey {
+	return tApp.keys
+}
 
 // LegacyAmino returns the app's amino codec.
 func (app *App) LegacyAmino() *codec.LegacyAmino {
