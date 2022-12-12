dd9bc7de84ef4fe600ee59ba270fa35845057994
Robert Pirtle <astropirtle@gmail.com>
2022-12-12 11:21:38 -0800

    test CommunityLendWithdrawProposal

--- x/community/keeper/proposal_handler_test.go
+++ x/community/keeper/proposal_handler_test.go
@@ -1,10 +1,13 @@
 package keeper_test
 
 import (
+	"fmt"
 	"testing"
+	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/suite"
+	abcitypes "github.com/tendermint/tendermint/abci/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmtime "github.com/tendermint/tendermint/types/time"
 
@@ -87,6 +90,15 @@ func (suite *proposalTestSuite) SetupTest() {
 	suite.NoError(err)
 }
 
+func (suite *proposalTestSuite) NextBlock() {
+	newTime := suite.Ctx.BlockTime().Add(6 * time.Second)
+	newHeight := suite.Ctx.BlockHeight() + 1
+
+	suite.App.EndBlocker(suite.Ctx, abcitypes.RequestEndBlock{})
+	suite.Ctx = suite.Ctx.WithBlockTime(newTime).WithBlockHeight(newHeight).WithChainID(chainID)
+	suite.App.BeginBlocker(suite.Ctx, abcitypes.RequestBeginBlock{})
+}
+
 func (suite *proposalTestSuite) TestCommunityLendDepositProposal() {
 	testCases := []struct {
 		name             string
@@ -167,6 +179,134 @@ func (suite *proposalTestSuite) TestCommunityLendDepositProposal() {
 			for _, amt := range tc.expectedDeposits {
 				suite.Equal(amt, deposits[0].Amount, "expected amount to match")
 			}
+		})
+	}
+}
+
+func (suite *proposalTestSuite) TestCommunityLendWithdrawProposal() {
+	testCases := []struct {
+		name               string
+		initialDeposit     sdk.Coins
+		proposals          []*types.CommunityPoolLendWithdrawProposal
+		expectedErr        string
+		expectedWithdrawal sdk.Coins
+	}{
+		{
+			// in the week it would take a proposal to pass, the position would have grown
+			// to withdraw the entire position, it'd be safest to set a very high withdraw
+			name:           "valid - requesting withdrawal of more than total will withdraw all",
+			initialDeposit: ukava(1e9),
+			proposals: []*types.CommunityPoolLendWithdrawProposal{
+				{Amount: ukava(1e12)},
+			},
+			expectedErr:        "",
+			expectedWithdrawal: ukava(1e9),
+		},
+		{
+			name:           "valid - single proposal, single denom, full withdrawal",
+			initialDeposit: ukava(1e9),
+			proposals: []*types.CommunityPoolLendWithdrawProposal{
+				{Amount: ukava(1e9)},
+			},
+			expectedErr:        "",
+			expectedWithdrawal: ukava(1e9),
+		},
+		{
+			name:           "valid - single proposal, multiple denoms, full withdrawal",
+			initialDeposit: ukava(1e9).Add(usdx(1e9)...),
+			proposals: []*types.CommunityPoolLendWithdrawProposal{
+				{Amount: ukava(1e9).Add(usdx(1e9)...)},
+			},
+			expectedErr:        "",
+			expectedWithdrawal: ukava(1e9).Add(usdx(1e9)...),
+		},
+		{
+			name:           "valid - single proposal, partial withdrawal",
+			initialDeposit: ukava(1e9).Add(usdx(1e9)...),
+			proposals: []*types.CommunityPoolLendWithdrawProposal{
+				{Amount: ukava(1e8).Add(usdx(1e9)...)},
+			},
+			expectedErr:        "",
+			expectedWithdrawal: ukava(1e8).Add(usdx(1e9)...),
+		},
+		{
+			name:           "valid - multiple proposals, full withdrawal",
+			initialDeposit: ukava(1e9).Add(usdx(1e9)...),
+			proposals: []*types.CommunityPoolLendWithdrawProposal{
+				{Amount: ukava(1e9)},
+				{Amount: usdx(1e9)},
+			},
+			expectedErr:        "",
+			expectedWithdrawal: ukava(1e9).Add(usdx(1e9)...),
+		},
+		{
+			name:           "valid - multiple proposals, partial withdrawal",
+			initialDeposit: ukava(1e9).Add(usdx(1e9)...),
+			proposals: []*types.CommunityPoolLendWithdrawProposal{
+				{Amount: ukava(1e8)},
+				{Amount: usdx(1e8)},
+			},
+			expectedErr:        "",
+			expectedWithdrawal: ukava(1e8).Add(usdx(1e8)...),
+		},
+		{
+			name:           "invalid - nonexistent position, has no deposits",
+			initialDeposit: sdk.NewCoins(),
+			proposals: []*types.CommunityPoolLendWithdrawProposal{
+				{Amount: ukava(1e8)},
+			},
+			expectedErr:        "deposit not found",
+			expectedWithdrawal: sdk.NewCoins(),
+		},
+		{
+			name:           "invalid - nonexistent position, has deposits of different denom",
+			initialDeposit: ukava(1e8),
+			proposals: []*types.CommunityPoolLendWithdrawProposal{
+				{Amount: usdx(1e8)},
+			},
+			expectedErr:        "no coins of this type deposited",
+			expectedWithdrawal: sdk.NewCoins(),
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+
+			// setup initial deposit
+			if !tc.initialDeposit.IsZero() {
+				deposit := types.NewCommunityPoolLendDepositProposal("initial deposit", "has coins", tc.initialDeposit)
+				err := keeper.HandleCommunityPoolLendDepositProposal(suite.Ctx, suite.Keeper, deposit)
+				suite.NoError(err, "unexpected error while seeding lend deposit")
+			}
+
+			beforeBalance := suite.Keeper.GetModuleAccountBalance(suite.Ctx)
+
+			// run the proposals
+			for i, proposal := range tc.proposals {
+				fmt.Println("submitting proposal ", i, " ", suite.Ctx.ChainID())
+				err := keeper.HandleCommunityPoolLendWithdrawProposal(suite.Ctx, suite.Keeper, proposal)
+				if tc.expectedErr == "" {
+					suite.NoError(err)
+				} else {
+					suite.ErrorContains(err, tc.expectedErr)
+				}
+
+				suite.NextBlock()
+			}
+
+			// expect funds to be removed from hard deposit
+			expectedRemaining := tc.initialDeposit.Sub(tc.expectedWithdrawal)
+			deposits := suite.hardKeeper.GetDepositsByUser(suite.Ctx, suite.MaccAddress)
+			if expectedRemaining.IsZero() {
+				suite.Len(deposits, 0, "expected all deposits to be withdrawn")
+			} else {
+				suite.Len(deposits, 1, "expected user to have remaining deposit")
+				suite.Equal(expectedRemaining, deposits[0].Amount)
+			}
+
+			// expect funds to be distributed back to community pool
+			suite.App.CheckBalance(suite.T(), suite.Ctx, suite.MaccAddress, beforeBalance.Add(tc.expectedWithdrawal...))
 		})
 	}
 }
