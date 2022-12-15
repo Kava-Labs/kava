827768233d317a11100366ced8f20cf382fb9441
Robert Pirtle <astropirtle@gmail.com>
2022-12-15 10:03:03 -0800

    test subset of original x/distribution queries

--- app/query/distribution/query_test.go
+++ app/query/distribution/query_test.go
@@ -98,3 +98,73 @@ func (suite *queryTestSuite) Test_CommunityPoolOverride() {
 		})
 	}
 }
+
+// modified from sdk test of distribution querier
+func (suite *queryTestSuite) Test_OGDistributionQueries() {
+	suite.SetupTest()
+
+	ctx, distrKeeper, stakingKeeper := suite.Ctx, suite.App.GetDistrKeeper(), suite.App.GetStakingKeeper()
+
+	// setup a validator
+	addr := app.RandomAddress()
+	valAddr := sdk.ValAddress(addr)
+	bondDenom := stakingKeeper.BondDenom(ctx)
+
+	// test param queries
+	params := distrtypes.Params{
+		CommunityTax:        sdk.ZeroDec(),
+		BaseProposerReward:  sdk.NewDecWithPrec(2, 1),
+		BonusProposerReward: sdk.NewDecWithPrec(1, 1),
+		WithdrawAddrEnabled: true,
+	}
+	distrKeeper.SetParams(ctx, params)
+	r1, err := suite.queryClient.Params(context.Background(), &distrtypes.QueryParamsRequest{})
+	suite.NoError(err)
+	suite.Equal(params, r1.Params)
+
+	// test outstanding rewards query
+	outstandingRewards := sdk.NewDecCoins(sdk.NewInt64DecCoin(bondDenom, 100), sdk.NewInt64DecCoin("other", 10))
+	distrKeeper.SetValidatorOutstandingRewards(ctx, valAddr, distrtypes.ValidatorOutstandingRewards{Rewards: outstandingRewards})
+	r2, err := suite.queryClient.ValidatorOutstandingRewards(context.Background(), &distrtypes.QueryValidatorOutstandingRewardsRequest{ValidatorAddress: valAddr.String()})
+	suite.NoError(err)
+	suite.Equal(outstandingRewards, r2.Rewards.Rewards)
+
+	// test validator commission query
+	commission := sdk.DecCoins{{Denom: "token1", Amount: sdk.NewDec(4)}, {Denom: "token2", Amount: sdk.NewDec(2)}}
+	distrKeeper.SetValidatorAccumulatedCommission(ctx, valAddr, distrtypes.ValidatorAccumulatedCommission{Commission: commission})
+	r3, err := suite.queryClient.ValidatorCommission(context.Background(), &distrtypes.QueryValidatorCommissionRequest{ValidatorAddress: valAddr.String()})
+	suite.NoError(err)
+	suite.Equal(commission, r3.Commission.Commission)
+
+	// test delegator's total rewards query
+	r4, err := suite.queryClient.DelegationTotalRewards(context.Background(), &distrtypes.QueryDelegationTotalRewardsRequest{DelegatorAddress: addr.String()})
+	suite.NoError(err)
+	suite.Equal(&distrtypes.QueryDelegationTotalRewardsResponse{}, r4)
+
+	// test validator slashes query with height range
+	slashOne := distrtypes.NewValidatorSlashEvent(3, sdk.NewDecWithPrec(5, 1))
+	slashTwo := distrtypes.NewValidatorSlashEvent(7, sdk.NewDecWithPrec(6, 1))
+	distrKeeper.SetValidatorSlashEvent(ctx, valAddr, 3, 0, slashOne)
+	distrKeeper.SetValidatorSlashEvent(ctx, valAddr, 7, 0, slashTwo)
+	slashes := suite.getQueriedValidatorSlashes(valAddr, 0, 2)
+	suite.Equal(0, len(slashes))
+	slashes = suite.getQueriedValidatorSlashes(valAddr, 0, 5)
+	suite.Equal([]distrtypes.ValidatorSlashEvent{slashOne}, slashes)
+	slashes = suite.getQueriedValidatorSlashes(valAddr, 0, 10)
+	suite.Equal([]distrtypes.ValidatorSlashEvent{slashOne, slashTwo}, slashes)
+
+	// non-zero delegator reward queries are not tested here.
+}
+
+func (suite *queryTestSuite) getQueriedValidatorSlashes(validatorAddr sdk.ValAddress, startHeight uint64, endHeight uint64) (slashes []distrtypes.ValidatorSlashEvent) {
+	result, err := suite.queryClient.ValidatorSlashes(
+		context.Background(),
+		&distrtypes.QueryValidatorSlashesRequest{
+			ValidatorAddress: validatorAddr.String(),
+			StartingHeight:   startHeight,
+			EndingHeight:     endHeight,
+		},
+	)
+	suite.NoError(err)
+	return result.Slashes
+}
