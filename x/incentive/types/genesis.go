f24f24d8a10150302b16815d38c3daf55f077172
rhuairahrighairigh <ruaridh.odonnell@gmail.com>
2022-12-15 14:10:02 -0800

    Revert "Add incentive migrations for earn rewards (#1406)"
    This reverts commit 937e5f339f97bd2451dc47fe9010d9c3c103ed87.

--- x/incentive/types/genesis.go
+++ x/incentive/types/genesis.go
@@ -200,10 +200,6 @@ func NewAccrualTime(claimType ClaimType, collateralType string, prevTime time.Ti
 
 // Validate performs validation of AccrualTime
 func (at AccrualTime) Validate() error {
-	if at.PreviousAccumulationTime.IsZero() {
-		return fmt.Errorf("previous accumulation time cannot be zero")
-	}
-
 	if err := at.ClaimType.Validate(); err != nil {
 		return err
 	}
