022f5d7d82b616d994a127891be88d25ea4eab8d
Draco <draco@dracoli.com>
2023-01-11 11:00:15 -0500

    change evmutil authority to authorities

--- app/app.go
+++ app/app.go
@@ -500,7 +500,9 @@ func NewApp(
 		evmutilSubspace,
 		app.bankKeeper,
 		app.accountKeeper,
-		authtypes.NewModuleAddress(communitytypes.ModuleAccountName).String(),
+		[]string{
+			authtypes.NewModuleAddress(communitytypes.ModuleAccountName).String(),
+		},
 	)
 
 	evmBankKeeper := evmutilkeeper.NewEvmBankKeeper(app.evmutilKeeper, app.bankKeeper, app.accountKeeper)
