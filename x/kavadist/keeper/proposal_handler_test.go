02a19de9b4192ac445f8a0742afe1c34720b9ada
Robert Pirtle <astropirtle@gmail.com>
2023-01-24 16:43:56 -0800

    remove x/community from kavadist

--- x/kavadist/keeper/proposal_handler_test.go
+++ x/kavadist/keeper/proposal_handler_test.go
@@ -3,22 +3,21 @@ package keeper_test
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	communitytypes "github.com/kava-labs/kava/x/community/types"
 	"github.com/kava-labs/kava/x/kavadist/keeper"
 	"github.com/kava-labs/kava/x/kavadist/types"
 )
 
 func (suite *keeperTestSuite) TestHandleCommunityPoolMultiSpendProposal() {
-	addr, communityKeeper, ctx := suite.Addrs[0], suite.App.GetCommunityKeeper(), suite.Ctx
+	addr, distrKeeper, ctx := suite.Addrs[0], suite.App.GetDistrKeeper(), suite.Ctx
 	initBalances := suite.BankKeeper.GetAllBalances(ctx, addr)
 
-	// add coins to the module account and fund community pool
-	initialFunds := int64(1000000)
-	fundAmount := sdk.NewCoins(sdk.NewInt64Coin("ukava", initialFunds))
-	suite.Require().NoError(suite.App.FundModuleAccount(ctx, communitytypes.ModuleAccountName, fundAmount))
-	// expect funds to start in community pool
-	commPoolFunds := communityKeeper.GetModuleAccountBalance(ctx)
-	suite.Require().True(fundAmount.IsEqual(commPoolFunds))
+	// add coins to the module account and fund fee pool
+	macc := distrKeeper.GetDistributionAccount(ctx)
+	fundAmount := sdk.NewCoins(sdk.NewInt64Coin("ukava", 1000000))
+	suite.Require().NoError(suite.App.FundModuleAccount(ctx, macc.GetName(), fundAmount))
+	feePool := distrKeeper.GetFeePool(ctx)
+	feePool.CommunityPool = sdk.NewDecCoinsFromCoins(fundAmount...)
+	distrKeeper.SetFeePool(ctx, feePool)
 
 	proposalAmount1 := int64(1100)
 	proposalAmount2 := int64(1200)
@@ -36,12 +35,6 @@ func (suite *keeperTestSuite) TestHandleCommunityPoolMultiSpendProposal() {
 	suite.Require().Nil(err)
 
 	balances := suite.BankKeeper.GetAllBalances(ctx, addr)
-
-	// expect funds to be transferred to recipient
 	expected := initBalances.AmountOf("ukava").Add(sdk.NewInt(proposalAmount1 + proposalAmount2))
 	suite.Require().Equal(expected, balances.AmountOf("ukava"))
-
-	// expect funds to be deducted from community pool
-	expectedCommPool := commPoolFunds.AmountOf("ukava").SubRaw(proposalAmount1 + proposalAmount2)
-	suite.Require().Equal(expectedCommPool, communityKeeper.GetModuleAccountBalance(ctx).AmountOf("ukava"))
 }
