7ef18a3405d1d273ef2ebbeff5046b1f0fee8c85
Robert Pirtle <astropirtle@gmail.com>
2023-01-26 14:57:38 -0800

    hardcode v0.21.0 upgrade at block 3430160

--- app/app.go
+++ app/app.go
@@ -950,10 +950,11 @@ func NewApp(
 
 // BeginBlocker contains app specific logic for the BeginBlock abci call.
 func (app *App) BeginBlocker(ctx sdk.Context, req abci.RequestBeginBlock) abci.ResponseBeginBlock {
-	upgradeHeight := int64(3100005)
+	// export is height 3430142
+	upgradeHeight := int64(3430160)
 	if ctx.BlockHeight() == upgradeHeight-1 {
 		upgradePlan := upgradetypes.Plan{
-			Name:   "v0.20.0",
+			Name:   "v0.21.0",
 			Height: upgradeHeight,
 		}
 		if err := app.upgradeKeeper.ScheduleUpgrade(ctx, upgradePlan); err != nil {
