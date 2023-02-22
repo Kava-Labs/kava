04b58274e5269b4cfd74a884116688298f73c35d
drklee3 <derrick@dlee.dev>
2023-02-22 10:51:54 -0800

    Remove rest from proposal handlers

--- x/community/client/proposal_handler.go
+++ x/community/client/proposal_handler.go
@@ -1,35 +1,17 @@
 package client
 
 import (
-	"net/http"
-
-	"github.com/cosmos/cosmos-sdk/client"
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
-	govrest "github.com/cosmos/cosmos-sdk/x/gov/client/rest"
 
 	"github.com/kava-labs/kava/x/community/client/cli"
-	"github.com/kava-labs/kava/x/community/types"
 )
 
 // community-pool deposit/withdraw lend proposal handlers
 var (
 	LendDepositProposalHandler = govclient.NewProposalHandler(
 		cli.NewCmdSubmitCommunityPoolLendDepositProposal,
-		notImplementedRestHandler(types.ProposalTypeCommunityPoolLendDeposit),
 	)
 	LendWithdrawProposalHandler = govclient.NewProposalHandler(
 		cli.NewCmdSubmitCommunityPoolLendWithdrawProposal,
-		notImplementedRestHandler(types.ProposalTypeCommunityPoolLendDeposit),
 	)
 )
-
-func notImplementedRestHandler(subRoute string) govclient.RESTHandlerFn {
-	return func(ctx client.Context) govrest.ProposalRESTHandler {
-		return govrest.ProposalRESTHandler{
-			SubRoute: subRoute,
-			Handler: func(w http.ResponseWriter, r *http.Request) {
-				http.Error(w, "Unimplemented", http.StatusNotImplemented)
-			},
-		}
-	}
-}
--- x/earn/client/proposal_handler.go
+++ x/earn/client/proposal_handler.go
@@ -4,11 +4,10 @@ import (
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
 
 	"github.com/kava-labs/kava/x/earn/client/cli"
-	"github.com/kava-labs/kava/x/earn/client/rest"
 )
 
 // community-pool deposit/withdraw proposal handlers
 var (
-	DepositProposalHandler  = govclient.NewProposalHandler(cli.GetCmdSubmitCommunityPoolDepositProposal, rest.DepositProposalRESTHandler)
-	WithdrawProposalHandler = govclient.NewProposalHandler(cli.GetCmdSubmitCommunityPoolWithdrawProposal, rest.WithdrawProposalRESTHandler)
+	DepositProposalHandler  = govclient.NewProposalHandler(cli.GetCmdSubmitCommunityPoolDepositProposal)
+	WithdrawProposalHandler = govclient.NewProposalHandler(cli.GetCmdSubmitCommunityPoolWithdrawProposal)
 )
