15c8e62b45dbdfd0905cad2f89da00bee5bb337d
Robert Pirtle <astropirtle@gmail.com>
2023-05-17 13:23:03 -0700

    add v2 store migration for evmutil

---
+++ x/evmutil/migrations/v2/store.go
@@ -0,0 +1,23 @@
+package v2
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	paramtypes "github.com/cosmos/cosmos-sdk/x/params/types"
+
+	"github.com/kava-labs/kava/x/evmutil/types"
+)
+
+// MigrateStore performs in-place store migrations for consensus version 2
+// V2 adds the allowed_native_denoms param to parameters.
+func MigrateStore(ctx sdk.Context, paramstore paramtypes.Subspace) error {
+	migrateParamsStore(ctx, paramstore)
+	return nil
+}
+
+// migrateParamsStore ensures the param key table exists and has the allowed_native_denoms property
+func migrateParamsStore(ctx sdk.Context, paramstore paramtypes.Subspace) {
+	if !paramstore.HasKeyTable() {
+		paramstore.WithKeyTable(types.ParamKeyTable())
+	}
+	paramstore.Set(ctx, types.KeyAllowedNativeDenoms, types.DefaultAllowedNativeDenoms)
+}
