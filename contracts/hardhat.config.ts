9c04bde0b54c3587aee445ceb90be59b4ad987c2
Robert Pirtle <astropirtle@gmail.com>
2023-05-18 14:29:36 -0700

    configure solc with optimization and evm target

--- contracts/hardhat.config.ts
+++ contracts/hardhat.config.ts
@@ -2,7 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: {
+    version: "0.8.18",
+    settings: {
+      evmVersion: "istanbul",
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+    },
+  },
 };
 
 export default config;
