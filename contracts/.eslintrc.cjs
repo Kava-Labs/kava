3a8543024638d598c28334aa06cb0f66b7a0ffb3
Robert Pirtle <astropirtle@gmail.com>
2023-05-18 11:34:01 -0700

    setup prettier

--- contracts/.eslintrc.cjs
+++ contracts/.eslintrc.cjs
@@ -1,5 +1,9 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
