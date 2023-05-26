152729a23d8425dab6ec3429c18a715829950b8f
Robert Pirtle <astropirtle@gmail.com>
2023-05-26 12:38:30 -0700

    rewrite grpc query tests

--- x/evmutil/keeper/grpc_query_test.go
+++ x/evmutil/keeper/grpc_query_test.go
@@ -4,34 +4,58 @@ import (
 	"context"
 	"testing"
 
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/suite"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
+	"github.com/kava-labs/kava/app"
 	"github.com/kava-labs/kava/x/evmutil/keeper"
 	"github.com/kava-labs/kava/x/evmutil/testutil"
 	"github.com/kava-labs/kava/x/evmutil/types"
 )
 
-type GrpcQueryTestSuite struct {
-	testutil.Suite
+type grpcQueryTestSuite struct {
+	suite.Suite
 
-	msgServer types.MsgServer
+	App         app.TestApp
+	Keeper      keeper.Keeper
+	Ctx         sdk.Context
+	QueryClient types.QueryClient
 }
 
-func (suite *GrpcQueryTestSuite) SetupTest() {
-	suite.Suite.SetupTest()
-	suite.msgServer = keeper.NewMsgServerImpl(suite.App.GetEvmutilKeeper())
+func (suite *grpcQueryTestSuite) SetupTest() {
+	suite.App = app.NewTestApp()
+	suite.App.InitializeFromGenesisStates()
+
+	suite.Keeper = suite.App.GetEvmutilKeeper()
+	suite.Ctx = suite.App.NewContext(true, tmproto.Header{Height: 1})
+
+	queryHelper := baseapp.NewQueryServerTestHelper(suite.Ctx, suite.App.InterfaceRegistry())
+	types.RegisterQueryServer(queryHelper, keeper.NewQueryServerImpl(suite.App.GetEvmutilKeeper()))
+	suite.QueryClient = types.NewQueryClient(queryHelper)
 }
 
 func TestGrpcQueryTestSuite(t *testing.T) {
-	suite.Run(t, new(GrpcQueryTestSuite))
+	suite.Run(t, new(grpcQueryTestSuite))
 }
 
-func (suite *GrpcQueryTestSuite) TestQueryParams() {
+func (suite *grpcQueryTestSuite) TestQueryParams() {
+	expectedParams := types.DefaultParams()
+	expectedParams.AllowedCosmosDenoms = append(
+		expectedParams.AllowedCosmosDenoms,
+		types.NewAllowedCosmosCoinERC20Token("cosmos-denom", "Cosmos Coin", "COSMOS", 6),
+	)
+	expectedParams.EnabledConversionPairs = types.NewConversionPairs(
+		types.NewConversionPair(testutil.RandomInternalEVMAddress(), "evm-denom"),
+		types.NewConversionPair(testutil.RandomInternalEVMAddress(), "evm-denom2"),
+	)
+	suite.Keeper.SetParams(suite.Ctx, expectedParams)
+
 	params, err := suite.QueryClient.Params(
 		context.Background(),
 		&types.QueryParamsRequest{},
 	)
 	suite.Require().NoError(err)
-
-	suite.Require().Len(params.Params.EnabledConversionPairs, 1)
+	suite.Require().Equal(expectedParams, params.Params)
 }
