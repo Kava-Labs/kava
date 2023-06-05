c62b4e65df79d9be3f8bfad2a1ae6f6d65718ac7
drklee3 <derrick@dlee.dev>
2023-06-05 14:57:49 -0700

    Update internal testnet seed script

--- .github/scripts/seed-internal-testnet.sh
+++ .github/scripts/seed-internal-testnet.sh
@@ -101,7 +101,7 @@ do
 done
 
 # create a text proposal
-kava tx gov submit-proposal --deposit 1000000000ukava --type "Text" --title "Example Proposal" --description "This is an example proposal" --gas auto --gas-adjustment 1.2 --from dev-wallet --gas-prices 0.01ukava -y
+kava tx gov submit-legacy-proposal --deposit 1000000000ukava --type "Text" --title "Example Proposal" --description "This is an example proposal" --gas auto --gas-adjustment 1.2 --from dev-wallet --gas-prices 0.01ukava -y
 
 # setup god's wallet
 echo "${KAVA_TESTNET_GOD_MNEMONIC}" | kava keys add --recover god
