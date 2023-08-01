e6b8a02f0e5785587d1f1c5fbdd115e2eca12768
Robert Pirtle <astropirtle@gmail.com>
2023-08-01 16:36:51 -0700

    update e2e test for eip712 signing
    complex eip712 workflow now uses x/cdp instead of x/earn vault

--- tests/e2e/e2e_evm_contracts_test.go
+++ tests/e2e/e2e_evm_contracts_test.go
@@ -11,7 +11,7 @@ import (
 	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 
 	"github.com/kava-labs/kava/app"
-	earntypes "github.com/kava-labs/kava/x/earn/types"
+	cdptypes "github.com/kava-labs/kava/x/cdp/types"
 	evmutiltypes "github.com/kava-labs/kava/x/evmutil/types"
 
 	"github.com/kava-labs/kava/tests/e2e/contracts/greeter"
@@ -106,13 +106,16 @@ func (suite *IntegrationTestSuite) TestEip712BasicMessageAuthorization() {
 	suite.Equal(sdk.NewInt(1e3), balRes.Balance.Amount)
 }
 
-// Note that this test works because the deployed erc20 is configured in evmutil & earn params.
-func (suite *IntegrationTestSuite) TestEip712ConvertToCoinAndDepositToEarn() {
-	amount := sdk.NewInt(1e2) // 0.0002 USDC
+// Note that this test works because the deployed erc20 is configured in evmutil & cdp params.
+// This test matches the webapp's "USDT Earn" workflow
+func (suite *IntegrationTestSuite) TestEip712ConvertToCoinAndDepositToLend() {
+	// cdp requires minimum of $11 collateral
+	amount := sdk.NewInt(11e6) // 11 USDT
+	principal := sdk.NewCoin("usdx", sdk.NewInt(10e6))
 	sdkDenom := suite.DeployedErc20.CosmosDenom
 
 	// create new funded account
-	depositor := suite.Kava.NewFundedAccount("eip712-earn-depositor", sdk.NewCoins(ukava(1e5)))
+	depositor := suite.Kava.NewFundedAccount("eip712-lend-depositor", sdk.NewCoins(ukava(1e5)))
 	// give them erc20 balance to deposit
 	fundRes := suite.FundKavaErc20Balance(depositor.EvmAddress, amount.BigInt())
 	suite.NoError(fundRes.Err)
@@ -124,16 +127,17 @@ func (suite *IntegrationTestSuite) TestEip712ConvertToCoinAndDepositToEarn() {
 		evmutiltypes.NewInternalEVMAddress(suite.DeployedErc20.Address),
 		amount,
 	)
-	depositMsg := earntypes.NewMsgDeposit(
-		depositor.SdkAddress.String(),
+	depositMsg := cdptypes.NewMsgCreateCDP(
+		depositor.SdkAddress,
 		sdk.NewCoin(sdkDenom, amount),
-		earntypes.STRATEGY_TYPE_SAVINGS,
+		principal,
+		suite.DeployedErc20.CdpCollateralType,
 	)
 	msgs := []sdk.Msg{
 		// convert to coin
 		&convertMsg,
-		// deposit into earn
-		depositMsg,
+		// deposit into cdp (Mint), take out USDX
+		&depositMsg,
 	}
 
 	// create tx
@@ -143,7 +147,7 @@ func (suite *IntegrationTestSuite) TestEip712ConvertToCoinAndDepositToEarn() {
 		1e6,
 		sdk.NewCoins(ukava(1e4)),
 		msgs,
-		"depositing my USDC into Earn!",
+		"doing the USDT Earn workflow! erc20 -> sdk.Coin -> USDX hard deposit",
 	).GetTx()
 
 	txBytes, err := suite.Kava.EncodingConfig.TxConfig.TxEncoder()(tx)
@@ -158,26 +162,26 @@ func (suite *IntegrationTestSuite) TestEip712ConvertToCoinAndDepositToEarn() {
 	suite.Equal(sdkerrors.SuccessABCICode, res.TxResponse.Code)
 
 	_, err = util.WaitForSdkTxCommit(suite.Kava.Tx, res.TxResponse.TxHash, 6*time.Second)
-	suite.NoError(err)
+	suite.Require().NoError(err)
 
 	// check that depositor no longer has erc20 balance
 	balance := suite.Kava.GetErc20Balance(suite.DeployedErc20.Address, depositor.EvmAddress)
 	suite.BigIntsEqual(big.NewInt(0), balance, "expected no erc20 balance")
 
-	// check that account has an earn deposit position
-	earnRes, err := suite.Kava.Earn.Deposits(context.Background(), &earntypes.QueryDepositsRequest{
-		Depositor: depositor.SdkAddress.String(),
-		Denom:     sdkDenom,
+	// check that account has cdp
+	cdpRes, err := suite.Kava.Cdp.Cdp(context.Background(), &cdptypes.QueryCdpRequest{
+		CollateralType: suite.DeployedErc20.CdpCollateralType,
+		Owner:          depositor.SdkAddress.String(),
 	})
 	suite.NoError(err)
-	suite.Len(earnRes.Deposits, 1)
-	suite.Equal(sdk.NewDecFromInt(amount), earnRes.Deposits[0].Shares.AmountOf(sdkDenom))
+	suite.True(cdpRes.Cdp.Collateral.Amount.Equal(amount))
+	suite.True(cdpRes.Cdp.Principal.Equal(principal))
 
 	// withdraw deposit & convert back to erc20 (this allows refund to recover erc20s used in test)
-	withdraw := earntypes.NewMsgWithdraw(
-		depositor.SdkAddress.String(),
-		sdk.NewCoin(sdkDenom, amount),
-		earntypes.STRATEGY_TYPE_SAVINGS,
+	withdraw := cdptypes.NewMsgRepayDebt(
+		depositor.SdkAddress,
+		suite.DeployedErc20.CdpCollateralType,
+		principal,
 	)
 	convertBack := evmutiltypes.NewMsgConvertCoinToERC20(
 		depositor.SdkAddress.String(),
@@ -185,11 +189,14 @@ func (suite *IntegrationTestSuite) TestEip712ConvertToCoinAndDepositToEarn() {
 		sdk.NewCoin(sdkDenom, amount),
 	)
 	withdrawAndConvertBack := util.KavaMsgRequest{
-		Msgs:      []sdk.Msg{withdraw, &convertBack},
-		GasLimit:  3e5,
-		FeeAmount: sdk.NewCoins(ukava(300)),
-		Data:      "withdrawing from earn & converting back to erc20",
+		Msgs:      []sdk.Msg{&withdraw, &convertBack},
+		GasLimit:  1e6,
+		FeeAmount: sdk.NewCoins(ukava(1000)),
+		Data:      "withdrawing from mint & converting back to erc20",
 	}
 	lastRes := depositor.SignAndBroadcastKavaTx(withdrawAndConvertBack)
 	suite.NoError(lastRes.Err)
+
+	balance = suite.Kava.GetErc20Balance(suite.DeployedErc20.Address, depositor.EvmAddress)
+	suite.BigIntsEqual(amount.BigInt(), balance, "expected returned erc20 balance")
 }
