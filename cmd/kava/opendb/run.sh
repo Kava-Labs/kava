94752559b504cbfbdb96f4140358abee13019470
evgeniy-scherbina <evgeniy.shcherbina.es@gmail.com>
2023-09-01 14:34:58 -0400

    CR's fixes

--- cmd/kava/opendb/run.sh
+++
@@ -1,3 +0,0 @@
-CGO_CFLAGS="-I/opt/homebrew/opt/rocksdb/include" \
-CGO_LDFLAGS="-L/opt/homebrew/opt/rocksdb/lib -lrocksdb -lstdc++ -lm -lz -L/opt/homebrew/opt/snappy/lib -L/opt/homebrew/opt/lz4/lib -L/opt/homebrew/opt/zstd/lib" \
-  go test -v -tags=rocksdb
