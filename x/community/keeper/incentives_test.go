93a15fe714e470aa64b515dcdd2acd0b1a5501b6
Draco <draco@dracoli.com>
2023-09-21 13:44:43 -0400

    add reward payout tests

--- x/community/keeper/incentives_test.go
+++ x/community/keeper/incentives_test.go
@@ -1,12 +1,18 @@
 package keeper_test
 
 import (
+	"fmt"
+	"reflect"
 	"testing"
 	"time"
 
+	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	"github.com/stretchr/testify/suite"
+	abci "github.com/tendermint/tendermint/abci/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	tmtime "github.com/tendermint/tendermint/types/time"
 
@@ -48,6 +54,122 @@ func (suite *IncentivesTestSuite) SetupTest() {
 
 func TestIncentivesTestSuite(t *testing.T) {
 	suite.Run(t, new(IncentivesTestSuite))
+}
+
+func (suite *IncentivesTestSuite) TestCanPayoutStakingRewards() {
+	suite.Run("returns false if we have not upgrade", func() {
+		suite.SetupTest()
+		suite.False(suite.Keeper.CanPayoutStakingRewards(suite.Ctx))
+	})
+
+	suite.Run("returns true if we have upgraded", func() {
+		suite.SetupTest()
+		suite.Keeper.StartDisableInflationUpgrade(suite.Ctx)
+		suite.True(suite.Keeper.CanPayoutStakingRewards(suite.Ctx))
+	})
+}
+
+func (suite *IncentivesTestSuite) TestPayoutStakingRewards() {
+	suite.Run("errors if params are not set", func() {
+		suite.SetupTest()
+		store := suite.Ctx.KVStore(suite.App.GetKVStoreKey(types.StoreKey))
+		store.Delete(types.ParamsKey)
+		err := suite.Keeper.PayoutStakingRewards(suite.Ctx)
+		suite.Error(err)
+		suite.ErrorIs(err, types.ErrStakingRewardsPayout)
+	})
+
+	suite.Run("errors if previous block time is not set", func() {
+		suite.SetupTest()
+		_, found := suite.Keeper.GetPreviousBlockTime(suite.Ctx)
+		suite.False(found)
+		err := suite.Keeper.PayoutStakingRewards(suite.Ctx)
+		suite.Error(err)
+		suite.ErrorIs(err, types.ErrStakingRewardsPayout)
+	})
+
+	suite.Run("errors if community account is out of funds", func() {
+		suite.SetupTest()
+		suite.Keeper.StartDisableInflationUpgrade(suite.Ctx)
+		ctx := suite.Ctx.WithBlockTime(suite.Ctx.BlockTime().Add(5 * time.Second))
+		err := suite.Keeper.PayoutStakingRewards(ctx)
+		suite.Error(err)
+		suite.ErrorIs(err, sdkerrors.ErrInsufficientFunds)
+	})
+
+	suite.Run("errors if new block time is less than previous block", func() {
+		suite.SetupTest()
+		suite.Keeper.StartDisableInflationUpgrade(suite.Ctx)
+		ctx := suite.Ctx.WithBlockTime(suite.Ctx.BlockTime().Add(-1 * time.Second))
+		err := suite.Keeper.PayoutStakingRewards(ctx)
+		suite.Error(err)
+		suite.ErrorIs(err, types.ErrStakingRewardsPayout)
+	})
+
+	suite.Run("emits payout rewards event", func() {
+		suite.SetupTest()
+		suite.Keeper.StartDisableInflationUpgrade(suite.Ctx)
+		suite.fundCommunityAccount(sdkmath.NewInt(1_000_000_000))
+		ctx := suite.Ctx.WithBlockTime(suite.Ctx.BlockTime().Add(5 * time.Second))
+		err := suite.Keeper.PayoutStakingRewards(ctx)
+		suite.NoError(err)
+		events := suite.Ctx.EventManager().Events()
+		suite.EventsContains(
+			events,
+			sdk.NewEvent(
+				types.EventTypePayoutRewards,
+				sdk.NewAttribute(
+					types.AttributeKeyRewardsPerSecond, "744191",
+				),
+				sdk.NewAttribute(
+					sdk.AttributeKeyAmount, "3720955",
+				),
+			),
+		)
+	})
+
+	// pays out the correct staking rewards
+	testCases := []struct {
+		name          string
+		blockDuration time.Duration
+		expRewards    int64
+	}{
+		{
+			name:          "6.2 seconds block time",
+			blockDuration: suite.mustParseDuration("6.2s"),
+			expRewards:    4_465_146,
+		},
+		{
+			name:          "21.9 seconds block time",
+			blockDuration: suite.mustParseDuration("21.9s"),
+			expRewards:    15_628_011,
+		},
+		{
+			name:          "same block time as previous block",
+			blockDuration: suite.mustParseDuration("0s"),
+			expRewards:    0,
+		},
+	}
+	for _, tc := range testCases {
+		suite.Run(tc.name, func() {
+			suite.SetupTest()
+			bk := suite.App.GetBankKeeper()
+			feeCollectorAcc := suite.App.GetAccountKeeper().GetModuleAccount(suite.Ctx, authtypes.FeeCollectorName)
+
+			feeCollectorBal := bk.GetBalance(suite.Ctx, feeCollectorAcc.GetAddress(), "ukava")
+			suite.Equal(sdk.ZeroInt(), feeCollectorBal.Amount)
+
+			suite.Keeper.StartDisableInflationUpgrade(suite.Ctx)
+			ctx := suite.Ctx.WithBlockTime(suite.Ctx.BlockTime().Add(tc.blockDuration))
+			suite.fundCommunityAccount(sdkmath.NewInt(1_000_000_000))
+			err := suite.Keeper.PayoutStakingRewards(ctx)
+			suite.NoError(err)
+
+			// check that the fee collector account has the correct amount of funds
+			feeCollectorBal = bk.GetBalance(ctx, feeCollectorAcc.GetAddress(), "ukava")
+			suite.Equal(tc.expRewards, feeCollectorBal.Amount.Int64())
+		})
+	}
 }
 
 func (suite *IncentivesTestSuite) TestShouldStartDisableInflationUpgrade() {
@@ -139,4 +261,43 @@ func (suite *IncentivesTestSuite) setUpgradeTimeFromNow(t time.Duration) {
 	suite.True(found)
 	params.UpgradeTimeDisableInflation = suite.Ctx.BlockTime().Add(t)
 	suite.Keeper.SetParams(suite.Ctx, params)
+}
+
+// EventsContains asserts that the expected event is in the provided events
+func (suite *IncentivesTestSuite) EventsContains(events sdk.Events, expectedEvent sdk.Event) {
+	foundMatch := false
+	for _, event := range events {
+		if event.Type == expectedEvent.Type {
+			if reflect.DeepEqual(attrsToMap(expectedEvent.Attributes), attrsToMap(event.Attributes)) {
+				foundMatch = true
+			}
+		}
+	}
+
+	suite.True(foundMatch, fmt.Sprintf("event of type %s not found or did not match", expectedEvent.Type))
+}
+
+func attrsToMap(attrs []abci.EventAttribute) []sdk.Attribute { // new cosmos changed the event attribute type
+	out := []sdk.Attribute{}
+
+	for _, attr := range attrs {
+		out = append(out, sdk.NewAttribute(string(attr.Key), string(attr.Value)))
+	}
+
+	return out
+}
+
+func (suite *IncentivesTestSuite) fundCommunityAccount(amt sdkmath.Int) {
+	err := suite.App.FundModuleAccount(
+		suite.Ctx,
+		types.ModuleAccountName,
+		sdk.NewCoins(sdk.NewCoin("ukava", amt)),
+	)
+	suite.NoError(err)
+}
+
+func (suite *IncentivesTestSuite) mustParseDuration(dur string) time.Duration {
+	ret, err := time.ParseDuration(dur)
+	suite.NoError(err)
+	return ret
 }
