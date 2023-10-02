f2349c1f50ae1f09074b7df47f479cb60dc93981
Nick DeLuca <nickdeluca08@gmail.com>
2023-10-02 16:18:37 +0000

    refactor and pull out main calculation to private pure function with no dependence on keeper

--- x/community/keeper/staking.go
+++ x/community/keeper/staking.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	"time"
+
 	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
@@ -25,31 +27,21 @@ func (k Keeper) PayoutAccumulatedStakingRewards(ctx sdk.Context) {
 		return
 	}
 
-	// we get the duration since we last accumulated, then use nanoseconds for full precision available
-	durationSinceLastPayout := currentBlockTime.Sub(state.LastAccumulationTime)
-	nanosecondsSinceLastPayout := sdkmath.LegacyNewDec(durationSinceLastPayout.Nanoseconds())
+	// we fetch the community pool balance to ensure only accumulate rewards up to the current balance
+	communityPoolBalance := sdkmath.LegacyNewDecFromInt(k.bankKeeper.GetBalance(ctx, k.moduleAddress, "ukava").Amount)
 
-	// We multiply by nanoseconds first, then divide by conversion to avoid loss of precision.
-	// This multiplicaiton is also tested against very large values so we are safe from overflow
-	// in normal operations.
-	accumulatedRewards := nanosecondsSinceLastPayout.Mul(params.StakingRewardsPerSecond).QuoInt64(nanosecondsInOneSecond)
-	// Ensure we add any error from previous truncations
-	accumulatedRewards = accumulatedRewards.Add(state.LastTruncationError)
+	// calculate staking reward payout capped to community pool balance
+	truncatedRewards, truncationError := calculateStakingRewards(
+		currentBlockTime,
+		state.LastAccumulationTime,
+		state.LastTruncationError,
+		params.StakingRewardsPerSecond,
+		communityPoolBalance,
+	)
 
-	// If the community pool balance is less than the accumulated rewards we only accumulate rewards up
-	// to the pool balance.
-	poolBalance := sdkmath.LegacyNewDecFromInt(k.bankKeeper.GetBalance(ctx, k.moduleAddress, "ukava").Amount)
-	if poolBalance.LT(accumulatedRewards) {
-		accumulatedRewards = poolBalance
-	}
-
-	// we truncate since we can only transfer whole units
-	truncatedRewards := accumulatedRewards.TruncateDec()
-	// the truncation error to carry over to the next accumulation
-	truncationError := accumulatedRewards.Sub(truncatedRewards)
-
+	// only payout if the truncated rewards are non-zero
 	if !truncatedRewards.IsZero() {
-		transferAmount := sdk.NewCoins(sdk.NewCoin("ukava", truncatedRewards.TruncateInt()))
+		transferAmount := sdk.NewCoins(sdk.NewCoin("ukava", truncatedRewards))
 
 		if err := k.bankKeeper.SendCoinsFromModuleToModule(ctx, types.ModuleAccountName, authtypes.FeeCollectorName, transferAmount); err != nil {
 			// we check for a valid balance and rewards can never be negative so panic since this will only
@@ -60,10 +52,37 @@ func (k Keeper) PayoutAccumulatedStakingRewards(ctx sdk.Context) {
 
 	// update accumulation state
 	state.LastAccumulationTime = currentBlockTime
+	// if the community pool balance is zero, this also resets the truncation error
 	state.LastTruncationError = truncationError
 
 	// save state
 	k.SetStakingRewardsState(ctx, state)
 
 	return
+}
+
+// calculateStakingRewards takees the currentBlockTime, state of last accumulation, rewards per second, and the community pool balance
+// in order to calculate the total payout since the last accumulation time.  It returns the truncated payout amount and the truncation error.
+func calculateStakingRewards(currentBlockTime, lastAccumulationTime time.Time, lastTruncationError, stakingRewardsPerSecond, communityPoolBalance sdkmath.LegacyDec) (sdk.Int, sdkmath.LegacyDec) {
+	// we get the duration since we last accumulated, then use nanoseconds for full precision available
+	durationSinceLastPayout := currentBlockTime.Sub(lastAccumulationTime)
+	nanosecondsSinceLastPayout := sdkmath.LegacyNewDec(durationSinceLastPayout.Nanoseconds())
+
+	// We multiply by nanoseconds first, then divide by conversion to avoid loss of precision.
+	// This multiplicaiton is also tested against very large values so we are safe from overflow
+	// in normal operations.
+	accumulatedRewards := nanosecondsSinceLastPayout.Mul(stakingRewardsPerSecond).QuoInt64(nanosecondsInOneSecond)
+	// Ensure we add any error from previous truncations
+	accumulatedRewards = accumulatedRewards.Add(lastTruncationError)
+
+	if communityPoolBalance.LT(accumulatedRewards) {
+		accumulatedRewards = communityPoolBalance
+	}
+
+	// we truncate since we can only transfer whole units
+	truncatedRewards := accumulatedRewards.TruncateDec()
+	// the truncation error to carry over to the next accumulation
+	truncationError := accumulatedRewards.Sub(truncatedRewards)
+
+	return truncatedRewards.TruncateInt(), truncationError
 }
