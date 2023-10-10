3ed29c48b3de5d4a6c5dc602cf0d46e3f2d11a5b
drklee3 <derrick@dlee.dev>
2023-10-10 16:22:23 -0700

    Check total supply doesn't change

--- tests/e2e/e2e_upgrade_inflation_test.go
+++ tests/e2e/e2e_upgrade_inflation_test.go
@@ -7,6 +7,7 @@ import (
 
 	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/bank/types"
 	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
 	abci "github.com/tendermint/tendermint/abci/types"
 	coretypes "github.com/tendermint/tendermint/rpc/core/types"
@@ -116,49 +117,75 @@ func (suite *IntegrationTestSuite) TestUpgradeInflation_Disable() {
 		// Next 5 blocks after switchover, ensure there are no "mint" events
 		// in begin, end, or message events
 
+		kavaSupply := sdk.NewCoin(suite.Kava.StakingDenom, sdkmath.ZeroInt())
+
 		for i := 0; i < 5; i++ {
 			queryHeight := switchoverHeight + int64(i)
 
-			var block *coretypes.ResultBlockResults
+			suite.Run("x/mint events should with 0 amount", func() {
+				var block *coretypes.ResultBlockResults
+				suite.Require().Eventually(func() bool {
+					// Check begin block events
+					block, err = suite.Kava.TmSignClient.BlockResults(
+						context.Background(),
+						&queryHeight,
+					)
 
-			suite.Require().Eventually(func() bool {
-				// Check begin block events
-				block, err = suite.Kava.TmSignClient.BlockResults(
-					context.Background(),
-					&queryHeight,
-				)
+					return err == nil
+				}, 20*time.Second, 3*time.Second)
 
-				return err == nil
-			}, 20*time.Second, 3*time.Second)
+				var mintEvents []abci.Event
 
-			var mintEvents []abci.Event
+				// Mint events should only occur in begin block, but we just include
+				// everything else just in case anything changes in x/mint
+				mintEventsBegin := FilterEventsByType(block.BeginBlockEvents, minttypes.EventTypeMint)
+				mintEventsEnd := FilterEventsByType(block.EndBlockEvents, minttypes.EventTypeMint)
+				mintEventsTx := FilterTxEventsByType(block.TxsResults, minttypes.EventTypeMint)
 
-			// Mint events should only occur in begin block, but we just include
-			// everything else just in case anything changes in x/mint
-			mintEventsBegin := FilterEventsByType(block.BeginBlockEvents, minttypes.EventTypeMint)
-			mintEventsEnd := FilterEventsByType(block.EndBlockEvents, minttypes.EventTypeMint)
-			mintEventsTx := FilterTxEventsByType(block.TxsResults, minttypes.EventTypeMint)
+				mintEvents = append(mintEvents, mintEventsBegin...)
+				mintEvents = append(mintEvents, mintEventsEnd...)
+				mintEvents = append(mintEvents, mintEventsTx...)
 
-			mintEvents = append(mintEvents, mintEventsBegin...)
-			mintEvents = append(mintEvents, mintEventsEnd...)
-			mintEvents = append(mintEvents, mintEventsTx...)
+				suite.Require().NotEmpty(mintEvents, "mint events should still be emitted")
 
-			suite.Require().NotEmpty(mintEvents, "mint events should still be emitted")
+				// Ensure mint amounts are 0
+				for _, event := range mintEvents {
+					for _, attribute := range event.Attributes {
+						// Bonded ratio and annual provisions unchecked
 
-			// Ensure mint amounts are 0
-			for _, event := range mintEvents {
-				for _, attribute := range event.Attributes {
-					// Bonded ratio and annual provisions unchecked
+						if string(attribute.Key) == minttypes.AttributeKeyInflation {
+							suite.Equal(sdkmath.LegacyZeroDec().String(), string(attribute.Value))
+						}
 
-					if string(attribute.Key) == minttypes.AttributeKeyInflation {
-						suite.Equal(sdkmath.LegacyZeroDec().String(), string(attribute.Value))
-					}
-
-					if string(attribute.Key) == sdk.AttributeKeyAmount {
-						suite.Equal(sdkmath.ZeroInt().String(), string(attribute.Value))
+						if string(attribute.Key) == sdk.AttributeKeyAmount {
+							suite.Equal(sdkmath.ZeroInt().String(), string(attribute.Value))
+						}
 					}
 				}
-			}
+			})
+
+			// Run this after the events check, since that one waits for the
+			// new block if necessary
+			suite.Run("total staking denom supply should not change", func() {
+				supplyRes, err := suite.Kava.Bank.SupplyOf(
+					util.CtxAtHeight(queryHeight),
+					&types.QuerySupplyOfRequest{
+						Denom: suite.Kava.StakingDenom,
+					},
+				)
+				suite.Require().NoError(err)
+
+				if kavaSupply.IsZero() {
+					// First iteration, set supply
+					kavaSupply = supplyRes.Amount
+				} else {
+					suite.Require().Equal(
+						kavaSupply,
+						supplyRes.Amount,
+						"ukava supply should not change",
+					)
+				}
+			})
 		}
 	})
 }
