6a8d7d7ca4bac29589aee09f6285e2eaa8bdb1f7
drklee3 <derrick@dlee.dev>
2023-10-11 10:49:12 -0700

    Check inflation and events before switchover

--- tests/e2e/e2e_upgrade_inflation_test.go
+++ tests/e2e/e2e_upgrade_inflation_test.go
@@ -3,6 +3,7 @@ package e2e_test
 import (
 	"context"
 	"fmt"
+	"strconv"
 	"time"
 
 	sdkmath "cosmossdk.io/math"
@@ -49,16 +50,18 @@ func (suite *IntegrationTestSuite) TestUpgradeInflation_Disable() {
 	suite.Require().NoError(err)
 	suite.Require().NotZero(switchoverHeight)
 
+	// 1 block before switchover
+	beforeSwitchoverCtx := util.CtxAtHeight(switchoverHeight - 1)
 	afterSwitchoverCtx := util.CtxAtHeight(switchoverHeight)
 
 	suite.Run("x/mint inflation before switchover", func() {
 		mintParams, err := suite.Kava.Mint.Params(
-			afterUpgradeCtx,
+			beforeSwitchoverCtx,
 			&minttypes.QueryParamsRequest{},
 		)
 		suite.NoError(err)
 		kavaDistParams, err := suite.Kava.Kavadist.Params(
-			afterUpgradeCtx,
+			beforeSwitchoverCtx,
 			&kavadisttypes.QueryParamsRequest{},
 		)
 		suite.NoError(err)
@@ -111,81 +114,166 @@ func (suite *IntegrationTestSuite) TestUpgradeInflation_Disable() {
 		)
 	})
 
+	// Ensure inflation was still active before switchover
+	suite.Run("positive mint events before switchover", func() {
+		// 1 block before switchover
+		queryHeight := switchoverHeight - 1
+
+		block, err := suite.Kava.TmSignClient.BlockResults(
+			context.Background(),
+			&queryHeight,
+		)
+		suite.Require().NoError(err)
+
+		// Mint events should only occur in begin block
+		mintEvents := FilterEventsByType(block.BeginBlockEvents, minttypes.EventTypeMint)
+
+		suite.Require().NotEmpty(mintEvents, "mint events should be emitted")
+
+		// Ensure mint amounts are non-zero
+		found := false
+		for _, event := range mintEvents {
+			for _, attribute := range event.Attributes {
+				// Bonded ratio and annual provisions unchecked
+
+				if string(attribute.Key) == minttypes.AttributeKeyInflation {
+					suite.Equal(
+						sdkmath.LegacyMustNewDecFromStr("0.595000000000000000").String(),
+						string(attribute.Value),
+						"inflation should be 59.5%% before switchover",
+					)
+				}
+
+				if string(attribute.Key) == sdk.AttributeKeyAmount {
+					found = true
+					// Parse as native go int, not necessary to use sdk.Int
+					value, err := strconv.Atoi(string(attribute.Value))
+					suite.Require().NoError(err)
+
+					suite.NotZero(value, "mint amount should be non-zero")
+					suite.Positive(value, "mint amount should be positive")
+				}
+			}
+		}
+
+		suite.True(found, "mint amount should be found")
+	})
+
+	suite.Run("staking denom supply increases before switchover", func() {
+		queryHeight := switchoverHeight - 2
+
+		supply1, err := suite.Kava.Bank.SupplyOf(
+			util.CtxAtHeight(queryHeight),
+			&types.QuerySupplyOfRequest{
+				Denom: suite.Kava.StakingDenom,
+			},
+		)
+		suite.Require().NoError(err)
+
+		suite.NotZero(supply1.Amount, "ukava supply should be non-zero")
+
+		// Next block
+		queryHeight += 1
+		supply2, err := suite.Kava.Bank.SupplyOf(
+			util.CtxAtHeight(queryHeight),
+			&types.QuerySupplyOfRequest{
+				Denom: suite.Kava.StakingDenom,
+			},
+		)
+		suite.Require().NoError(err)
+
+		suite.NotZero(supply2.Amount, "ukava supply should be non-zero")
+
+		suite.Truef(
+			supply2.Amount.Amount.GT(supply1.Amount.Amount),
+			"ukava supply before switchover should increase between blocks, %s > %s",
+			supply2.Amount.Amount.String(),
+		)
+	})
+
 	// Check if inflation is ACTUALLY disabled... check if any coins are being
 	// minted in the blocks after switchover
 	suite.Run("no minting after switchover", func() {
-		// Next 5 blocks after switchover, ensure there are no "mint" events
-		// in begin, end, or message events
-
 		kavaSupply := sdk.NewCoin(suite.Kava.StakingDenom, sdkmath.ZeroInt())
 
+		// Next 5 blocks after switchover, ensure there's actually no more inflation
 		for i := 0; i < 5; i++ {
 			queryHeight := switchoverHeight + int64(i)
 
-			suite.Run("x/mint events should with 0 amount", func() {
-				var block *coretypes.ResultBlockResults
-				suite.Require().Eventually(func() bool {
-					// Check begin block events
-					block, err = suite.Kava.TmSignClient.BlockResults(
-						context.Background(),
-						&queryHeight,
-					)
+			suite.Run(
+				fmt.Sprintf("x/mint events should with 0 amount @ height=%d", queryHeight),
+				func() {
+					var block *coretypes.ResultBlockResults
+					suite.Require().Eventually(func() bool {
+						// Check begin block events
+						block, err = suite.Kava.TmSignClient.BlockResults(
+							context.Background(),
+							&queryHeight,
+						)
 
-					return err == nil
-				}, 20*time.Second, 3*time.Second)
+						return err == nil
+					}, 20*time.Second, 3*time.Second)
 
-				var mintEvents []abci.Event
+					var mintEvents []abci.Event
 
-				// Mint events should only occur in begin block, but we just include
-				// everything else just in case anything changes in x/mint
-				mintEventsBegin := FilterEventsByType(block.BeginBlockEvents, minttypes.EventTypeMint)
-				mintEventsEnd := FilterEventsByType(block.EndBlockEvents, minttypes.EventTypeMint)
-				mintEventsTx := FilterTxEventsByType(block.TxsResults, minttypes.EventTypeMint)
+					// Mint events should only occur in begin block, but we just include
+					// everything else just in case anything changes in x/mint
+					mintEventsBegin := FilterEventsByType(block.BeginBlockEvents, minttypes.EventTypeMint)
+					mintEventsEnd := FilterEventsByType(block.EndBlockEvents, minttypes.EventTypeMint)
+					mintEventsTx := FilterTxEventsByType(block.TxsResults, minttypes.EventTypeMint)
 
-				mintEvents = append(mintEvents, mintEventsBegin...)
-				mintEvents = append(mintEvents, mintEventsEnd...)
-				mintEvents = append(mintEvents, mintEventsTx...)
+					mintEvents = append(mintEvents, mintEventsBegin...)
+					mintEvents = append(mintEvents, mintEventsEnd...)
+					mintEvents = append(mintEvents, mintEventsTx...)
 
-				suite.Require().NotEmpty(mintEvents, "mint events should still be emitted")
+					suite.Require().NotEmpty(mintEvents, "mint events should still be emitted")
 
-				// Ensure mint amounts are 0
-				for _, event := range mintEvents {
-					for _, attribute := range event.Attributes {
-						// Bonded ratio and annual provisions unchecked
+					// Ensure mint amounts are 0
+					found := false
+					for _, event := range mintEvents {
+						for _, attribute := range event.Attributes {
+							// Bonded ratio and annual provisions unchecked
 
-						if string(attribute.Key) == minttypes.AttributeKeyInflation {
-							suite.Equal(sdkmath.LegacyZeroDec().String(), string(attribute.Value))
-						}
+							if string(attribute.Key) == minttypes.AttributeKeyInflation {
+								suite.Equal(sdkmath.LegacyZeroDec().String(), string(attribute.Value))
+							}
 
-						if string(attribute.Key) == sdk.AttributeKeyAmount {
-							suite.Equal(sdkmath.ZeroInt().String(), string(attribute.Value))
+							if string(attribute.Key) == sdk.AttributeKeyAmount {
+								found = true
+								suite.Equal(sdkmath.ZeroInt().String(), string(attribute.Value))
+							}
 						}
 					}
-				}
-			})
+
+					suite.True(found, "mint amount should be found")
+				},
+			)
 
 			// Run this after the events check, since that one waits for the
 			// new block if necessary
-			suite.Run("total staking denom supply should not change", func() {
-				supplyRes, err := suite.Kava.Bank.SupplyOf(
-					util.CtxAtHeight(queryHeight),
-					&types.QuerySupplyOfRequest{
-						Denom: suite.Kava.StakingDenom,
-					},
-				)
-				suite.Require().NoError(err)
-
-				if kavaSupply.IsZero() {
-					// First iteration, set supply
-					kavaSupply = supplyRes.Amount
-				} else {
-					suite.Require().Equal(
-						kavaSupply,
-						supplyRes.Amount,
-						"ukava supply should not change",
+			suite.Run(
+				fmt.Sprintf("total staking denom supply should not change @ height=%d", queryHeight),
+				func() {
+					supplyRes, err := suite.Kava.Bank.SupplyOf(
+						util.CtxAtHeight(queryHeight),
+						&types.QuerySupplyOfRequest{
+							Denom: suite.Kava.StakingDenom,
+						},
 					)
-				}
-			})
+					suite.Require().NoError(err)
+
+					if kavaSupply.IsZero() {
+						// First iteration, set supply
+						kavaSupply = supplyRes.Amount
+					} else {
+						suite.Require().Equal(
+							kavaSupply,
+							supplyRes.Amount,
+							"ukava supply should not change",
+						)
+					}
+				},
+			)
 		}
 	})
 }
