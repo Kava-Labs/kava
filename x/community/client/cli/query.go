79dc361d6f67a09f1d37cd93762bf03898e913d9
Robert Pirtle <astropirtle@gmail.com>
2023-10-25 11:08:17 -0700

    feat(community): add CLI cmd for annualized-rewards

--- x/community/client/cli/query.go
+++ x/community/client/cli/query.go
@@ -24,6 +24,7 @@ func GetQueryCmd() *cobra.Command {
 	commands := []*cobra.Command{
 		getCmdQueryParams(),
 		getCmdQueryBalance(),
+		getCmdQueryAnnualizedRewards(),
 	}
 
 	for _, cmd := range commands {
@@ -73,6 +74,29 @@ func getCmdQueryBalance() *cobra.Command {
 			queryClient := types.NewQueryClient(clientCtx)
 
 			res, err := queryClient.Balance(cmd.Context(), &types.QueryBalanceRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+}
+
+// getCmdQueryAnnualizedRewards implements a command to return the current annualized rewards.
+func getCmdQueryAnnualizedRewards() *cobra.Command {
+	return &cobra.Command{
+		Use:   "annualized-rewards",
+		Short: "Query a current calculation of annualized rewards for the chain.",
+		Args:  cobra.NoArgs,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, err := queryClient.AnnualizedRewards(cmd.Context(), &types.QueryAnnualizedRewardsRequest{})
 			if err != nil {
 				return err
 			}
