9c1dff5aad6341305118f230c6a4e6db88010aee
Robert Pirtle <Astropirtle@gmail.com>
2024-01-05 11:25:53 -0800

    feat(cli): add shard CLI command (#1785)
    * stub out shard CLI command
    
    * prune blocks before and after desired range
    
    * update rollback to use patched cometbft
    
    * temp override for local patched versions
    
    * handle pruning cometbft & block store state
    
    * include docs & support -1 for "latest"
    
    * update changelog
    
    * add --only-app-state flag to match cosmos-sdk prune cmd
    
    * give -1 magic number a name & reuse home from ctx
    
    * refactor to only open state.db & blockstore.db once
    
    * write rollback progress to one line
    
    * prevent attempting rollback of future blocks
    
    * make shard inclusive of endblock
    
    * use tagged cosmo-sdk & cometbft versions

--- cmd/kava/cmd/root.go
+++ cmd/kava/cmd/root.go
@@ -119,5 +119,6 @@ func addSubCmds(rootCmd *cobra.Command, encodingConfig params.EncodingConfig, de
 		newTxCmd(),
 		kavaclient.KeyCommands(app.DefaultNodeHome),
 		newIavlViewerCmd(opts),
+		newShardCmd(opts),
 	)
 }
