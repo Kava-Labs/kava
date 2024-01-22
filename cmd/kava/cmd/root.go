e7125a4d2f4c320369e577d8b3c7c51fbe1fcca4
Nick DeLuca <nickdeluca08@gmail.com>
2024-01-22 18:56:37 +0000

    port iavlviewer to kava v0.17.x to debug app hash

--- cmd/kava/cmd/root.go
+++ cmd/kava/cmd/root.go
@@ -126,5 +126,6 @@ func addSubCmds(rootCmd *cobra.Command, encodingConfig params.EncodingConfig, de
 		newQueryCmd(),
 		newTxCmd(),
 		kavaclient.KeyCommands(app.DefaultNodeHome),
+		newIavlViewerCmd(opts),
 	)
 }
