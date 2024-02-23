321cd1b24ec52b12d29cd213338014e1401de954
Draco <draco@dracoli.com>
2024-02-23 10:06:18 -0500

    Remove used legacy querier types
    These were left out by accident when the legacy queriers are removed from the modules in this commit https://github.com/Kava-Labs/kava/commit/3ba4078ec180e609a147e4fc0729ed3039c75eab

--- x/cdp/types/querier.go
+++ x/cdp/types/querier.go
@@ -4,36 +4,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-// Querier routes for the cdp module
-const (
-	QueryGetCdp                     = "cdp"
-	QueryGetCdps                    = "cdps"
-	QueryGetCdpDeposits             = "deposits"
-	QueryGetCdpsByCollateralization = "ratio"          // legacy query, maintained for REST API
-	QueryGetCdpsByCollateralType    = "collateralType" // legacy query, maintained for REST API
-	QueryGetParams                  = "params"
-	QueryGetAccounts                = "accounts"
-	QueryGetTotalPrincipal          = "totalPrincipal"
-	QueryGetTotalCollateral         = "totalCollateral"
-	RestOwner                       = "owner"
-	RestCollateralType              = "collateral-type"
-	RestRatio                       = "ratio"
-)
-
-// QueryCdpParams params for query /cdp/cdp
-type QueryCdpParams struct {
-	CollateralType string         // get CDPs with this collateral type
-	Owner          sdk.AccAddress // get CDPs belonging to this owner
-}
-
-// NewQueryCdpParams returns QueryCdpParams
-func NewQueryCdpParams(owner sdk.AccAddress, collateralType string) QueryCdpParams {
-	return QueryCdpParams{
-		Owner:          owner,
-		CollateralType: collateralType,
-	}
-}
-
 // QueryCdpsParams is the params for a filtered CDP query
 type QueryCdpsParams struct {
 	Page           int            `json:"page" yaml:"page"`
@@ -53,69 +23,5 @@ func NewQueryCdpsParams(page, limit int, collateralType string, owner sdk.AccAdd
 		Owner:          owner,
 		ID:             id,
 		Ratio:          ratio,
-	}
-}
-
-// QueryCdpDeposits params for query /cdp/deposits
-type QueryCdpDeposits struct {
-	CollateralType string         // get CDPs with this collateral type
-	Owner          sdk.AccAddress // get CDPs belonging to this owner
-}
-
-// NewQueryCdpDeposits returns QueryCdpDeposits
-func NewQueryCdpDeposits(owner sdk.AccAddress, collateralType string) QueryCdpDeposits {
-	return QueryCdpDeposits{
-		Owner:          owner,
-		CollateralType: collateralType,
-	}
-}
-
-// QueryCdpsByCollateralTypeParams params for query /cdp/cdps/{denom}
-type QueryCdpsByCollateralTypeParams struct {
-	CollateralType string // get CDPs with this collateral type
-}
-
-// NewQueryCdpsByCollateralTypeParams returns QueryCdpsByCollateralTypeParams
-func NewQueryCdpsByCollateralTypeParams(collateralType string) QueryCdpsByCollateralTypeParams {
-	return QueryCdpsByCollateralTypeParams{
-		CollateralType: collateralType,
-	}
-}
-
-// QueryCdpsByRatioParams params for query /cdp/cdps/ratio
-type QueryCdpsByRatioParams struct {
-	CollateralType string
-	Ratio          sdk.Dec // get CDPs below this collateral:debt ratio
-}
-
-// NewQueryCdpsByRatioParams returns QueryCdpsByRatioParams
-func NewQueryCdpsByRatioParams(collateralType string, ratio sdk.Dec) QueryCdpsByRatioParams {
-	return QueryCdpsByRatioParams{
-		CollateralType: collateralType,
-		Ratio:          ratio,
-	}
-}
-
-// QueryGetTotalPrincipalParams params for query /cdp/totalPrincipal
-type QueryGetTotalPrincipalParams struct {
-	CollateralType string
-}
-
-// NewQueryGetTotalPrincipalParams returns QueryGetTotalPrincipalParams
-func NewQueryGetTotalPrincipalParams(collateralType string) QueryGetTotalPrincipalParams {
-	return QueryGetTotalPrincipalParams{
-		CollateralType: collateralType,
-	}
-}
-
-// QueryGetTotalCollateralParams params for query /cdp/totalCollateral
-type QueryGetTotalCollateralParams struct {
-	CollateralType string
-}
-
-// NewQueryGetTotalCollateralParams returns QueryGetTotalCollateralParams
-func NewQueryGetTotalCollateralParams(collateralType string) QueryGetTotalCollateralParams {
-	return QueryGetTotalCollateralParams{
-		CollateralType: collateralType,
 	}
 }
