3e1e6fb2144fdc2b80e5d698227b80950d74d994
Nick DeLuca <nickdeluca08@gmail.com>
2024-03-28 23:21:12 +0000

    set CDP block interval to 100 to only run interest synchronization for risky cdps every 100 blocks instead of every block

--- app/upgrades.go
+++ app/upgrades.go
@@ -123,6 +123,16 @@ func upgradeHandler(
 		// dedicated x/consensus module.
 		baseapp.MigrateParams(ctx, baseAppLegacySS, &app.consensusParamsKeeper)
 
-		return app.mm.RunMigrations(ctx, app.configurator, fromVM)
+		// run migrations for all modules and return new consensus version map
+		versionMap, err := app.mm.RunMigrations(ctx, app.configurator, fromVM)
+
+		// Set risky CDP's to sync interest and liquidate every 100 blocks instead
+		// of every block.  This significantly improves performance as this cdp
+		// process is a signification porition of time spent during block execution.
+		cdpParams := app.cdpKeeper.GetParams(ctx)
+		cdpParams.LiquidationBlockInterval = int64(100)
+		app.cdpKeeper.SetParams(ctx, cdpParams)
+
+		return versionMap, err
 	}
 }
