9c819e34d6e2102e471fe1b30e8e352cbe9ecc16
Nick DeLuca <nickdeluca08@gmail.com>
2024-03-28 14:51:26 +0000

    revert back to iavl v1 to avoid hash changes on new modules like we are seeing on the v0.24.x to v0.25.x upgrade block.  Also, add replace statements for exp and rapid to match upstream cosmos-sdk

--- cmd/kava/cmd/shard.go
+++ cmd/kava/cmd/shard.go
@@ -220,11 +220,8 @@ func shardApplicationDb(multistore *rootmulti.Store, startBlock, endBlock int64)
 
 	if len(pruneHeights) > 0 {
 		// prune application state
-		fmt.Printf("pruning application state to height %d\n", startBlock)
-		for _, pruneHeight := range pruneHeights {
-			if err := multistore.PruneStores(pruneHeight); err != nil {
-				return fmt.Errorf("failed to prune application state: %s", err)
-			}
+		if err := multistore.PruneStores(true, pruneHeights); err != nil {
+			fmt.Printf("pruninn applicatior state to height %d\n", startBlock)
 		}
 	}
 
