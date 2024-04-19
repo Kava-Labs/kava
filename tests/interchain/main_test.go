3d5c110432baa69422fb48f863b1bc43b2d24bdb
Robert Pirtle <astropirtle@gmail.com>
2024-04-19 09:29:40 -0700

    e2e test of packet forwarding middleware

--- tests/interchain/main_test.go
+++ tests/interchain/main_test.go
@@ -39,16 +39,14 @@ func TestInterchainIBC(t *testing.T) {
 				// CoinType: "60", // might need this to sign evm txs. will need to override decimals to be 6 again.
 			},
 		},
-		{Name: "gaia", Version: "v7.0.0", ChainConfig: ibc.ChainConfig{
-			GasPrices: "0.0uatom",
-		}},
-		// {Name: "osmosis", Version: "v11.0.0"},
+		{Name: "gaia", Version: "v15.2.0", ChainConfig: ibc.ChainConfig{GasPrices: "0.0uatom"}},
+		{Name: "osmosis", Version: "v24.0.1"},
 	})
 
 	chains, err := cf.Chains(t.Name())
 	require.NoError(t, err)
 
-	kava, gaia := chains[0].(*cosmos.CosmosChain), chains[1].(*cosmos.CosmosChain)
+	kava, gaia, osmosis := chains[0].(*cosmos.CosmosChain), chains[1].(*cosmos.CosmosChain), chains[2].(*cosmos.CosmosChain)
 
 	// setup relayer
 	client, network := interchaintest.DockerSetup(t)
@@ -57,24 +55,24 @@ func TestInterchainIBC(t *testing.T) {
 
 	// configure interchain
 	const kavaGaiaIbcPath = "kava-gaia-demo"
-	// const kavaOsmosisIbcPath = "kava-osmo-demo"
+	const kavaOsmosisIbcPath = "kava-osmo-demo"
 	ic := interchaintest.NewInterchain().
 		AddChain(kava).
 		AddChain(gaia).
-		// AddChain(osmosis).
+		AddChain(osmosis).
 		AddRelayer(r, "relayer").
 		AddLink(interchaintest.InterchainLink{
 			Chain1:  kava,
 			Chain2:  gaia,
 			Relayer: r,
 			Path:    kavaGaiaIbcPath,
+		}).
+		AddLink(interchaintest.InterchainLink{
+			Chain1:  kava,
+			Chain2:  osmosis,
+			Relayer: r,
+			Path:    kavaOsmosisIbcPath,
 		})
-		// AddLink(interchaintest.InterchainLink{
-		// 	Chain1:  kava,
-		// 	Chain2:  osmosis,
-		// 	Relayer: r,
-		// 	Path:    kavaOsmosisIbcPath,
-		// })
 
 	// Log location
 	f, err := interchaintest.CreateLogFile(fmt.Sprintf("%d.json", time.Now().Unix()))
@@ -84,21 +82,26 @@ func TestInterchainIBC(t *testing.T) {
 	eRep := rep.RelayerExecReporter(t)
 
 	// Build interchain
-	require.NoError(t, ic.Build(ctx, eRep, interchaintest.InterchainBuildOptions{
+	err = ic.Build(ctx, eRep, interchaintest.InterchainBuildOptions{
 		TestName:         t.Name(),
 		Client:           client,
 		NetworkID:        network,
 		SkipPathCreation: false},
-	),
 	)
+	require.NoError(t, err)
+
+	// start the relayer so we don't need to manually Flush() packets
+	err = r.StartRelayer(ctx, eRep, kavaGaiaIbcPath, kavaOsmosisIbcPath)
+	require.NoError(t, err)
+	defer r.StopRelayer(ctx, eRep)
 
 	// Create and Fund User Wallets
 	fundAmount := math.NewInt(10_000_000)
 
-	users := interchaintest.GetAndFundTestUsers(t, ctx, "default", fundAmount, kava, gaia)
+	users := interchaintest.GetAndFundTestUsers(t, ctx, "default", fundAmount, kava, gaia, osmosis)
 	kavaUser := users[0]
 	gaiaUser := users[1]
-	// osmosisUser := users[2]
+	osmosisUser := users[2]
 
 	// wait for new block to ensure initial funding complete
 	height, err := kava.Height(ctx)
@@ -115,18 +118,27 @@ func TestInterchainIBC(t *testing.T) {
 	require.True(t, kavaUserBalInitial.Equal(fundAmount))
 
 	// get ibc channel ids
-	kavaChannelInfo, err := r.GetChannels(ctx, eRep, kava.Config().ChainID)
-	require.NoError(t, err)
-	kavaChannelID := kavaChannelInfo[0].ChannelID
-
 	gaiaChannelInfo, err := r.GetChannels(ctx, eRep, gaia.Config().ChainID)
 	require.NoError(t, err)
-	gaiaChannelID := gaiaChannelInfo[0].ChannelID
+	osmoChannelInfo, err := r.GetChannels(ctx, eRep, osmosis.Config().ChainID)
+	require.NoError(t, err)
+
+	gaiaToKavaChannelID := gaiaChannelInfo[0].ChannelID
+	kavaToGaiaChannelID := gaiaChannelInfo[0].Counterparty.ChannelID
+	osmoToKavaChannelID := osmoChannelInfo[0].ChannelID
+	kavaToOsmoChannelID := osmoChannelInfo[0].Counterparty.ChannelID
+
+	// determine ibc denoms
+	srcDenomTrace := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom("transfer", gaiaToKavaChannelID, kava.Config().Denom))
+	kavaOnGaiaDenom := srcDenomTrace.IBCDenom()
+	srcDenomTrace = transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom("transfer", osmoToKavaChannelID, kava.Config().Denom))
+	kavaOnOsmoDenom := srcDenomTrace.IBCDenom()
+
+	amountToSend := math.NewInt(1_000_000)
 
 	// IBC transfer kava to cosmoshub
 	// inspired by https://github.com/strangelove-ventures/interchaintest/blob/main/examples/ibc/learn_ibc_test.go
 	t.Run("ibc transfer kava -> cosmoshub", func(t *testing.T) {
-		amountToSend := math.NewInt(1_000_000)
 		dstAddress := gaiaUser.FormattedAddress()
 		transfer := ibc.WalletAmount{
 			Address: dstAddress,
@@ -134,12 +146,12 @@ func TestInterchainIBC(t *testing.T) {
 			Amount:  amountToSend,
 		}
 
-		tx, err := kava.SendIBCTransfer(ctx, kavaChannelID, kavaUser.KeyName(), transfer, ibc.TransferOptions{})
+		tx, err := kava.SendIBCTransfer(ctx, kavaToGaiaChannelID, kavaUser.KeyName(), transfer, ibc.TransferOptions{})
 		require.NoError(t, err)
 		require.NoError(t, tx.Validate())
 
-		// relay MsgRecvPacket to gaia, then MsgAcknowledgement back to kava
-		require.NoError(t, r.Flush(ctx, eRep, kavaGaiaIbcPath, kavaChannelID))
+		// manually flush packets so we don't need to wait for the relayer
+		require.NoError(t, r.Flush(ctx, eRep, kavaGaiaIbcPath, kavaToGaiaChannelID))
 
 		// verify balance deducted from kava account
 		expectedBal := kavaUserBalInitial.Sub(amountToSend)
@@ -147,13 +159,43 @@ func TestInterchainIBC(t *testing.T) {
 		require.NoError(t, err)
 		require.True(t, kavaUserBalNew.Equal(expectedBal))
 
-		// determine ibc denom on cosmoshub
-		srcDenomTrace := transfertypes.ParseDenomTrace(transfertypes.GetPrefixedDenom("transfer", gaiaChannelID, kava.Config().Denom))
-		dstIbcDenom := srcDenomTrace.IBCDenom()
-
 		// verify cosmoshub account received funds
-		gaiaUserBalNew, err := gaia.GetBalance(ctx, gaiaUser.FormattedAddress(), dstIbcDenom)
+		gaiaUserBalNew, err := gaia.GetBalance(ctx, gaiaUser.FormattedAddress(), kavaOnGaiaDenom)
 		require.NoError(t, err)
 		require.True(t, gaiaUserBalNew.Equal(amountToSend))
+	})
+
+	t.Run("packet forwarding middleware: cosmoshub -> kava -> osmosis", func(t *testing.T) {
+		dstAddress := osmosisUser.FormattedAddress()
+		transfer := ibc.WalletAmount{
+			Address: "pfm", // purposefully invalid b/c we are forwarding through kava onward to osmosis!
+			Denom:   kavaOnGaiaDenom,
+			Amount:  amountToSend,
+		}
+
+		tx, err := gaia.SendIBCTransfer(ctx, gaiaToKavaChannelID, gaiaUser.KeyName(), transfer, ibc.TransferOptions{
+			// packet forwarding middleware!
+			Memo: fmt.Sprintf(`{
+	"forward": {
+		"receiver": "%s",
+		"port": "transfer",
+		"channel": "%s"
+	}
+}`, dstAddress, kavaToOsmoChannelID),
+		})
+		require.NoError(t, err)
+		require.NoError(t, tx.Validate())
+
+		require.Eventually(t, func() bool {
+			// verify transfer to osmosis
+			osmosisUserBalNew, err := osmosis.GetBalance(ctx, osmosisUser.FormattedAddress(), kavaOnOsmoDenom)
+			require.NoError(t, err)
+			return osmosisUserBalNew.Equal(amountToSend)
+		}, 15*time.Second, time.Second, "osmosis never received funds")
+
+		// verify cosmoshub account no longer has the funds
+		gaiaUserBalNew, err := gaia.GetBalance(ctx, gaiaUser.FormattedAddress(), kavaOnGaiaDenom)
+		require.NoError(t, err)
+		require.True(t, gaiaUserBalNew.Equal(math.ZeroInt()))
 	})
 }
