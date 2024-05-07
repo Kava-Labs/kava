a968364891128f540c91be31810776364748b3e3
Robert Pirtle <astropirtle@gmail.com>
2024-05-07 10:57:49 -0700

    better names for int pointers

--- tests/e2e-ibc/conformance_test.go
+++ tests/e2e-ibc/conformance_test.go
@@ -18,8 +18,8 @@ import (
 )
 
 var (
-	zero = 0
-	one  = 1
+	numFullNodes  = 0
+	numValidators = 1
 )
 
 func TestIbcConformance(t *testing.T) {
@@ -37,10 +37,10 @@ func TestIbcConformance(t *testing.T) {
 				// override default number of nodes to limit the number of ports we need to bind to
 				// running conformance tests without these limits result in errors that look like
 				// tendermint rpc client status: post failed: Post "http://127.0.0.1:<port>": dial tcp 127.0.0.1:<port>: connect: can't assign requested address
-				NumValidators: &one,
-				NumFullNodes:  &zero,
+				NumValidators: &numValidators,
+				NumFullNodes:  &numFullNodes,
 			},
-			{Name: "osmosis", Version: "v24.0.1", NumValidators: &one, NumFullNodes: &zero},
+			{Name: "osmosis", Version: "v24.0.1", NumValidators: &numValidators, NumFullNodes: &numFullNodes},
 		}),
 	)
 
