dea4b65eb0d52ad590e41890e5835ea88b5d2d0b
drklee3 <derrick@dlee.dev>
2024-05-10 16:46:23 -0700

    feat: Add InitGenesis test case for accounts

--- x/precisebank/genesis.go
+++ x/precisebank/genesis.go
@@ -15,16 +15,20 @@ func InitGenesis(
 	ctx sdk.Context,
 	keeper keeper.Keeper,
 	ak types.AccountKeeper,
+	bk types.BankKeeper,
 	gs *types.GenesisState,
 ) {
+	// Ensure the genesis state is valid
 	if err := gs.Validate(); err != nil {
 		panic(fmt.Sprintf("failed to validate %s genesis state: %s", types.ModuleName, err))
 	}
 
-	// initialize module account
+	// Initialize module account
 	if moduleAcc := ak.GetModuleAccount(ctx, types.ModuleName); moduleAcc == nil {
 		panic(fmt.Sprintf("%s module account has not been set", types.ModuleName))
 	}
+
+	// Additional validations that requires checking balances
 
 	// TODO:
 	// - Set balances
--- x/precisebank/genesis_test.go
+++ x/precisebank/genesis_test.go
@@ -3,6 +3,9 @@ package precisebank_test
 import (
 	"testing"
 
+	sdkmath "cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
 	"github.com/kava-labs/kava/x/precisebank"
 	"github.com/kava-labs/kava/x/precisebank/testutil"
 	"github.com/kava-labs/kava/x/precisebank/types"
@@ -20,25 +23,52 @@ func TestGenesisTestSuite(t *testing.T) {
 func (suite *GenesisTestSuite) TestInitGenesis() {
 	tests := []struct {
 		name         string
+		setupFn      func()
 		genesisState *types.GenesisState
 		shouldPanic  bool
 		panicMsg     string
 	}{
 		{
 			"default genesisState",
+			func() {},
 			types.DefaultGenesisState(),
 			false,
 			"",
 		},
 		{
 			"empty genesisState",
+			func() {},
 			&types.GenesisState{},
-			false,
-			"",
+			true,
+			"failed to validate precisebank genesis state: nil remainder amount",
 		},
 		{
-			"TODO: invalid genesisState",
-			&types.GenesisState{},
+			// Other GenesisState.Validate() tests are in types/genesis_test.go
+			"invalid genesisState - GenesisState.Validate() is called",
+			func() {},
+			types.NewGenesisState(
+				types.FractionalBalances{
+					types.NewFractionalBalance(sdk.AccAddress{1}.String(), sdkmath.NewInt(1)),
+					types.NewFractionalBalance(sdk.AccAddress{1}.String(), sdkmath.NewInt(1)),
+				},
+				sdkmath.ZeroInt(),
+			),
+			true,
+			"failed to validate precisebank genesis state: invalid balances: duplicate address kava1qy0xn7za",
+		},
+		{
+			"sets module account",
+			func() {
+				// Delete the module account first to ensure it's created here
+				moduleAcc := suite.AccountKeeper.GetModuleAccount(suite.Ctx, types.ModuleName)
+				suite.AccountKeeper.RemoveAccount(suite.Ctx, moduleAcc)
+
+				// Ensure module account is deleted in state.
+				// GetModuleAccount() will always return non-nil and does not
+				// necessarily equate to the account being stored in the account store.
+				suite.Require().Nil(suite.AccountKeeper.GetAccount(suite.Ctx, moduleAcc.GetAddress()))
+			},
+			types.DefaultGenesisState(),
 			false,
 			"",
 		},
@@ -47,43 +77,55 @@ func (suite *GenesisTestSuite) TestInitGenesis() {
 	for _, tc := range tests {
 		suite.Run(tc.name, func() {
 			if tc.shouldPanic {
-				suite.Require().Panics(func() {
-					precisebank.InitGenesis(suite.Ctx, suite.Keeper, suite.AccountKeeper, tc.genesisState)
-				}, tc.panicMsg)
+				suite.Require().PanicsWithValue(
+					tc.panicMsg,
+					func() {
+						precisebank.InitGenesis(
+							suite.Ctx,
+							suite.Keeper,
+							suite.AccountKeeper,
+							suite.BankKeeper,
+							tc.genesisState,
+						)
+					},
+				)
 
 				return
 			}
 
 			suite.Require().NotPanics(func() {
-				precisebank.InitGenesis(suite.Ctx, suite.Keeper, suite.AccountKeeper, tc.genesisState)
+				precisebank.InitGenesis(
+					suite.Ctx,
+					suite.Keeper,
+					suite.AccountKeeper,
+					suite.BankKeeper,
+					tc.genesisState,
+				)
 			})
 
 			// Ensure module account is created
 			moduleAcc := suite.AccountKeeper.GetModuleAccount(suite.Ctx, types.ModuleName)
-			suite.NotNil(moduleAcc, "module account should be created")
+			suite.NotNil(moduleAcc)
+			suite.NotNil(
+				suite.AccountKeeper.GetAccount(suite.Ctx, moduleAcc.GetAddress()),
+				"module account should be created & stored in account store",
+			)
 
 			// TODO: Check module state once implemented
 
-			// - Verify balances
-			// - Ensure reserve account exists
-			// - Ensure reserve balance matches sum of all fractional balances
+			// Verify balances
+			// IterateBalances() or something
+
+			// Ensure reserve balance matches sum of all fractional balances
+			// sum up IterateBalances()
+
 			// - etc
 		})
 	}
 }
 
-func (suite *GenesisTestSuite) TestImportGenesis_ModuleAccountCreated() {
-	suite.Require().NotPanics(func() {
-		precisebank.InitGenesis(suite.Ctx, suite.Keeper, suite.AccountKeeper, types.DefaultGenesisState())
-	})
-
-	moduleAcc := suite.AccountKeeper.GetModuleAccount(suite.Ctx, types.ModuleName)
-	suite.NotNil(moduleAcc, "module account should be created")
-}
-
 func (suite *GenesisTestSuite) TestExportGenesis_Valid() {
 	// ExportGenesis(moduleState) should return a valid genesis state
-
 	tests := []struct {
 		name    string
 		maleate func()
@@ -95,6 +137,7 @@ func (suite *GenesisTestSuite) TestExportGenesis_Valid() {
 					suite.Ctx,
 					suite.Keeper,
 					suite.AccountKeeper,
+					suite.BankKeeper,
 					types.DefaultGenesisState(),
 				)
 			},
@@ -132,6 +175,7 @@ func (suite *GenesisTestSuite) TestExportImportedState() {
 					suite.Ctx,
 					suite.Keeper,
 					suite.AccountKeeper,
+					suite.BankKeeper,
 					tc.initGenesisState,
 				)
 			})
