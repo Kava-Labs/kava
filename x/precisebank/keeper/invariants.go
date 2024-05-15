9826027fc8ec670f83c9e38f977202f379e0edb9
drklee3 <derrick@dlee.dev>
2024-05-15 15:00:25 -0700

    feat: Add valid fractionalBalances & remainder invariants

--- x/precisebank/keeper/invariants.go
+++ x/precisebank/keeper/invariants.go
@@ -3,6 +3,7 @@ package keeper
 import (
 	"fmt"
 
+	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/kava-labs/kava/x/precisebank/types"
 )
@@ -13,7 +14,9 @@ func RegisterInvariants(
 	k Keeper,
 	bk types.BankKeeper,
 ) {
-	ir.RegisterRoute(types.ModuleName, "invalid-fractional-total", BalancedFractionalTotalInvariant(k))
+	ir.RegisterRoute(types.ModuleName, "balance-remainder-total", BalancedFractionalTotalInvariant(k))
+	ir.RegisterRoute(types.ModuleName, "valid-fractional-balances", ValidFractionalAmountsInvariant(k))
+	ir.RegisterRoute(types.ModuleName, "valid-remainder-amount", ValidRemainderAmountInvariant(k))
 }
 
 // AllInvariants runs all invariants of the X/precisebank module.
@@ -24,7 +27,72 @@ func AllInvariants(k Keeper) sdk.Invariant {
 			return res, stop
 		}
 
+		res, stop = ValidFractionalAmountsInvariant(k)(ctx)
+		if stop {
+			return res, stop
+		}
+
+		res, stop = ValidRemainderAmountInvariant(k)(ctx)
+		if stop {
+			return res, stop
+		}
+
 		return "", false
+	}
+}
+
+// BalancedFractionalTotalInvariant checks that the sum of fractional balances
+// and the remainder amount is divisible by the conversion factor without any
+// leftover amount.
+func ValidFractionalAmountsInvariant(k Keeper) sdk.Invariant {
+	return func(ctx sdk.Context) (string, bool) {
+		var (
+			msg   string
+			count int
+		)
+
+		k.IterateFractionalBalances(ctx, func(addr sdk.AccAddress, amount sdkmath.Int) bool {
+			if err := types.NewFractionalAmountFromInt(amount).Validate(); err != nil {
+				count++
+				msg += fmt.Sprintf("\t%s has an invalid fractional amount of %s\n", addr, amount)
+			}
+
+			return false
+		})
+
+		broken := count != 0
+
+		return sdk.FormatInvariant(
+			types.ModuleName, "valid-fractional-balances",
+			fmt.Sprintf("amount of invalid fractional balances found %d\n%s", count, msg),
+		), broken
+	}
+}
+
+// BalancedFractionalTotalInvariant checks that the sum of fractional balances
+// and the remainder amount is divisible by the conversion factor without any
+// leftover amount.
+func ValidRemainderAmountInvariant(k Keeper) sdk.Invariant {
+	return func(ctx sdk.Context) (string, bool) {
+		var (
+			msg    string
+			broken bool
+		)
+
+		remainderAmount := k.GetRemainderAmount(ctx)
+
+		if !remainderAmount.IsZero() {
+			// Only validate if non-zero, as zero is default value
+			if err := types.NewFractionalAmountFromInt(remainderAmount).Validate(); err != nil {
+				broken = true
+				msg = fmt.Sprintf("remainder amount is invalid: %s", err)
+			}
+		}
+
+		return sdk.FormatInvariant(
+			types.ModuleName, "valid-remainder-amount",
+			msg,
+		), broken
 	}
 }
 
@@ -51,7 +119,7 @@ func BalancedFractionalTotalInvariant(k Keeper) sdk.Invariant {
 		}
 
 		return sdk.FormatInvariant(
-			types.ModuleName, "invalid-fractional-total",
+			types.ModuleName, "balance-remainder-total",
 			msg,
 		), broken
 	}
--- x/precisebank/keeper/invariants_test.go
+++ x/precisebank/keeper/invariants_test.go
@@ -4,6 +4,8 @@ import (
 	"testing"
 
 	sdkmath "cosmossdk.io/math"
+	"github.com/cosmos/cosmos-sdk/store/prefix"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/kava-labs/kava/x/precisebank/keeper"
@@ -54,7 +56,7 @@ func TestBalancedFractionalTotalInvariant(t *testing.T) {
 				k.SetFractionalBalance(ctx, sdk.AccAddress{2}, types.ConversionFactor().QuoRaw(2).SubRaw(1))
 			},
 			true,
-			"precisebank: invalid-fractional-total invariant\n(sum(FractionalBalances) + remainder) % conversionFactor should be 0 but got 999999999999\n",
+			"precisebank: balance-remainder-total invariant\n(sum(FractionalBalances) + remainder) % conversionFactor should be 0 but got 999999999999\n",
 		},
 		{
 			"invalid - invalid balances, non-zero (insufficient) remainder",
@@ -64,7 +66,7 @@ func TestBalancedFractionalTotalInvariant(t *testing.T) {
 				k.SetRemainderAmount(ctx, sdkmath.OneInt())
 			},
 			true,
-			"precisebank: invalid-fractional-total invariant\n(sum(FractionalBalances) + remainder) % conversionFactor should be 0 but got 999999999999\n",
+			"precisebank: balance-remainder-total invariant\n(sum(FractionalBalances) + remainder) % conversionFactor should be 0 but got 999999999999\n",
 		},
 		{
 			"invalid - invalid balances, non-zero (excess) remainder",
@@ -74,7 +76,7 @@ func TestBalancedFractionalTotalInvariant(t *testing.T) {
 				k.SetRemainderAmount(ctx, sdkmath.NewInt(5))
 			},
 			true,
-			"precisebank: invalid-fractional-total invariant\n(sum(FractionalBalances) + remainder) % conversionFactor should be 0 but got 3\n",
+			"precisebank: balance-remainder-total invariant\n(sum(FractionalBalances) + remainder) % conversionFactor should be 0 but got 3\n",
 		},
 	}
 
@@ -87,6 +89,74 @@ func TestBalancedFractionalTotalInvariant(t *testing.T) {
 			tt.setupFn()
 
 			invariantFn := keeper.BalancedFractionalTotalInvariant(k)
+			msg, broken := invariantFn(ctx)
+
+			if tt.wantBroken {
+				require.True(t, broken, "invariant should be broken but is not")
+				require.Equal(t, tt.wantMsg, msg)
+			} else {
+				require.False(t, broken, "invariant should not be broken but is")
+			}
+		})
+	}
+}
+
+func TestValidFractionalAmountsInvariant(t *testing.T) {
+	var ctx sdk.Context
+	var k keeper.Keeper
+	var storeKey storetypes.StoreKey
+
+	tests := []struct {
+		name       string
+		setupFn    func()
+		wantBroken bool
+		wantMsg    string
+	}{
+		{
+			"valid - empty state",
+			func() {},
+			false,
+			"",
+		},
+		{
+			"valid - valid balances",
+			func() {
+				k.SetFractionalBalance(ctx, sdk.AccAddress{1}, types.ConversionFactor().QuoRaw(2))
+				k.SetFractionalBalance(ctx, sdk.AccAddress{2}, types.ConversionFactor().QuoRaw(2))
+			},
+			false,
+			"",
+		},
+		{
+			"invalid - exceeds max balance",
+			func() {
+				// Requires manual store manipulation so it is unlikely to have
+				// invalid state in practice. SetFractionalBalance will validate
+				// before setting.
+				addr := sdk.AccAddress{1}
+				amount := types.ConversionFactor()
+
+				store := prefix.NewStore(ctx.KVStore(storeKey), types.FractionalBalancePrefix)
+
+				amountBytes, err := amount.Marshal()
+				require.NoError(t, err)
+
+				store.Set(types.FractionalBalanceKey(addr), amountBytes)
+			},
+			true,
+			"precisebank: valid-fractional-balances invariant\namount of invalid fractional balances found 1\n\tkava1qy0xn7za has an invalid fractional amount of 1000000000000\n\n",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// Reset each time
+			tk := NewTestKeeper()
+			ctx, k, storeKey = tk.ctx, tk.keeper, tk.storeKey
+
+			tt.setupFn()
+
+			invariantFn := keeper.ValidFractionalAmountsInvariant(k)
 			msg, broken := invariantFn(ctx)
 
 			if tt.wantBroken {
