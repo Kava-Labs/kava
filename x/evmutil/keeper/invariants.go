bfde721b4f459a67229fca2b4b590413a93e0df0
drklee3 <derrick@dlee.dev>
2024-07-02 16:41:13 -0700

    feat: Replace x/evmutil with x/precisebank for x/evm bank, remove x/evmutil bank keeper

--- x/evmutil/keeper/invariants.go
+++ x/evmutil/keeper/invariants.go
@@ -11,8 +11,6 @@ import (
 
 // RegisterInvariants registers the swap module invariants
 func RegisterInvariants(ir sdk.InvariantRegistry, bankK types.BankKeeper, k Keeper) {
-	ir.RegisterRoute(types.ModuleName, "fully-backed", FullyBackedInvariant(bankK, k))
-	ir.RegisterRoute(types.ModuleName, "small-balances", SmallBalancesInvariant(bankK, k))
 	ir.RegisterRoute(types.ModuleName, "cosmos-coins-fully-backed", CosmosCoinsFullyBackedInvariant(bankK, k))
 	// Disable this invariant due to some issues with it requiring some staking params to be set in genesis.
 	// ir.RegisterRoute(types.ModuleName, "backed-conversion-coins", BackedCoinsInvariant(bankK, k))
@@ -21,57 +19,11 @@ func RegisterInvariants(ir sdk.InvariantRegistry, bankK types.BankKeeper, k Keep
 // AllInvariants runs all invariants of the swap module
 func AllInvariants(bankK types.BankKeeper, k Keeper) sdk.Invariant {
 	return func(ctx sdk.Context) (string, bool) {
-		if res, stop := FullyBackedInvariant(bankK, k)(ctx); stop {
-			return res, stop
-		}
 		if res, stop := BackedCoinsInvariant(bankK, k)(ctx); stop {
 			return res, stop
 		}
-		if res, stop := CosmosCoinsFullyBackedInvariant(bankK, k)(ctx); stop {
-			return res, stop
-		}
-		return SmallBalancesInvariant(bankK, k)(ctx)
-	}
-}
 
-// FullyBackedInvariant ensures all minor balances are backed by the coins in the module account.
-//
-// The module balance can be greater than the sum of all minor balances. This can happen in rare cases
-// where the evm module burns tokens.
-func FullyBackedInvariant(bankK types.BankKeeper, k Keeper) sdk.Invariant {
-	broken := false
-	message := sdk.FormatInvariant(types.ModuleName, "fully backed broken", "sum of minor balances greater than module account")
-
-	return func(ctx sdk.Context) (string, bool) {
-		totalMinorBalances := sdk.ZeroInt()
-		k.IterateAllAccounts(ctx, func(acc types.Account) bool {
-			totalMinorBalances = totalMinorBalances.Add(acc.Balance)
-			return false
-		})
-
-		bankAddr := authtypes.NewModuleAddress(types.ModuleName)
-		bankBalance := bankK.GetBalance(ctx, bankAddr, CosmosDenom).Amount.Mul(ConversionMultiplier)
-
-		broken = totalMinorBalances.GT(bankBalance)
-
-		return message, broken
-	}
-}
-
-// SmallBalancesInvariant ensures all minor balances are less than the overflow amount, beyond this they should be converted to the major denom.
-func SmallBalancesInvariant(_ types.BankKeeper, k Keeper) sdk.Invariant {
-	broken := false
-	message := sdk.FormatInvariant(types.ModuleName, "small balances broken", "minor balances not all less than overflow")
-
-	return func(ctx sdk.Context) (string, bool) {
-		k.IterateAllAccounts(ctx, func(account types.Account) bool {
-			if account.Balance.GTE(ConversionMultiplier) {
-				broken = true
-				return true
-			}
-			return false
-		})
-		return message, broken
+		return CosmosCoinsFullyBackedInvariant(bankK, k)(ctx)
 	}
 }
 
