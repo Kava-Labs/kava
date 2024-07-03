2cc73a065ce22da06c4dc3220a013016d0083437
drklee3 <derrick@dlee.dev>
2024-07-03 14:35:01 -0700

    test: Use table test, ensure all balances including ukava behave correctly

--- x/precisebank/keeper/view_integration_test.go
+++ x/precisebank/keeper/view_integration_test.go
@@ -140,53 +140,63 @@ func (suite *viewIntegrationTestSuite) TestKeeper_HiddenReserve() {
 	// Make the reserve hold a non-zero balance
 	// Mint fractional coins to an account, which should cause a mint of 1
 	// integer coin to the reserve to back it.
+	extCoin := sdk.NewCoin(types.ExtendedCoinDenom, types.ConversionFactor().AddRaw(1000))
 	unrelatedCoin := sdk.NewCoin("unrelated", sdk.NewInt(1000))
 	suite.MintToAccount(
 		addr1,
 		sdk.NewCoins(
-			sdk.NewCoin(types.ExtendedCoinDenom, sdk.NewInt(1000)),
+			extCoin,
 			unrelatedCoin,
 		),
 	)
 
 	// Check underlying x/bank balance for reserve
 	reserveIntCoin := suite.BankKeeper.GetBalance(suite.Ctx, moduleAddr, types.IntegerCoinDenom)
-	suite.Require().Equal(sdkmath.NewInt(1), reserveIntCoin.Amount, "reserve should hold 1 integer coin")
+	suite.Require().Equal(
+		sdkmath.NewInt(1),
+		reserveIntCoin.Amount,
+		"reserve should hold 1 integer coin",
+	)
 
-	// x/precisebank queries for reserve show as 0
-	denom := types.ExtendedCoinDenom
+	tests := []struct {
+		name       string
+		giveAddr   sdk.AccAddress
+		giveDenom  string
+		wantAmount sdkmath.Int
+	}{
+		{
+			"reserve account - hidden extended denom",
+			moduleAddr,
+			types.ExtendedCoinDenom,
+			sdkmath.ZeroInt(),
+		},
+		{
+			"reserve account - visible integer denom",
+			moduleAddr,
+			types.IntegerCoinDenom,
+			sdkmath.OneInt(),
+		},
+		{
+			"user account - visible extended denom",
+			addr1,
+			types.ExtendedCoinDenom,
+			extCoin.Amount,
+		},
+		{
+			"user account - visible integer denom",
+			addr1,
+			types.IntegerCoinDenom,
+			extCoin.Amount.Quo(types.ConversionFactor()),
+		},
+	}
 
-	suite.Run("GetBalance()", func() {
-		coin := suite.Keeper.GetBalance(suite.Ctx, moduleAddr, denom)
-		suite.Require().Equal(denom, coin.Denom)
-		suite.Require().Equal(sdkmath.ZeroInt(), coin.Amount)
-	})
+	for _, tt := range tests {
+		suite.Run(tt.name, func() {
+			coin := suite.Keeper.GetBalance(suite.Ctx, tt.giveAddr, tt.giveDenom)
+			suite.Require().Equal(tt.wantAmount.Int64(), coin.Amount.Int64())
 
-	suite.Run("SpendableCoin()", func() {
-		spendableCoin := suite.Keeper.SpendableCoin(suite.Ctx, moduleAddr, denom)
-		suite.Require().Equal(denom, spendableCoin.Denom)
-		suite.Require().Equal(sdkmath.ZeroInt(), spendableCoin.Amount)
-	})
-
-	suite.Run("GetBalance() unrelated denom", func() {
-		// Not affecting module account
-		moduleCoin := suite.Keeper.GetBalance(suite.Ctx, moduleAddr, "unrelated")
-		suite.Require().Equal(unrelatedCoin.Denom, moduleCoin.Denom)
-		suite.Require().Equal(sdkmath.ZeroInt(), moduleCoin.Amount)
-
-		// Still visible in user account balance
-		accCoin := suite.Keeper.GetBalance(suite.Ctx, addr1, "unrelated")
-		suite.Require().Equal(unrelatedCoin, accCoin)
-	})
-
-	suite.Run("SpendableCoin() unrelated denom", func() {
-		// Not affecting module account
-		moduleCoin := suite.Keeper.SpendableCoin(suite.Ctx, moduleAddr, "unrelated")
-		suite.Require().Equal(unrelatedCoin.Denom, moduleCoin.Denom)
-		suite.Require().Equal(sdkmath.ZeroInt(), moduleCoin.Amount)
-
-		// Still visible in user account balance
-		accCoin := suite.Keeper.SpendableCoin(suite.Ctx, addr1, "unrelated")
-		suite.Require().Equal(unrelatedCoin, accCoin)
-	})
+			spendableCoin := suite.Keeper.SpendableCoin(suite.Ctx, tt.giveAddr, tt.giveDenom)
+			suite.Require().Equal(tt.wantAmount.Int64(), spendableCoin.Amount.Int64())
+		})
+	}
 }
