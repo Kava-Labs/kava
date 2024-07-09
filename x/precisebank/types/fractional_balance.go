ce6aac3a72f75218db5eeba46a76173e8436d5b7
drklee3 <derrick@dlee.dev>
2024-07-09 15:33:31 -0700

    refactor(x/precisebank): Replace FractionalAmount wrapper with func (#1961)
    Removal of unnecessary wrapper type, along with using conversionFactor-1 instead of maxFractionalAmount

--- x/precisebank/types/fractional_balance.go
+++ x/precisebank/types/fractional_balance.go
@@ -1,18 +1,15 @@
 package types
 
 import (
+	fmt "fmt"
+
 	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
-var (
-	// conversionFactor is used to convert the fractional balance to integer
-	// balances.
-	conversionFactor = sdkmath.NewInt(1_000_000_000_000)
-	// maxFractionalAmount is the largest valid value in a FractionalBalance amount.
-	// This is for direct internal use so that there are no extra allocations.
-	maxFractionalAmount = conversionFactor.SubRaw(1)
-)
+// conversionFactor is used to convert the fractional balance to integer
+// balances.
+var conversionFactor = sdkmath.NewInt(1_000_000_000_000)
 
 // ConversionFactor returns a copy of the conversionFactor used to convert the
 // fractional balance to integer balances. This is also 1 greater than the max
@@ -39,5 +36,24 @@ func (fb FractionalBalance) Validate() error {
 	}
 
 	// Validate the amount with the FractionalAmount wrapper
-	return NewFractionalAmountFromInt(fb.Amount).Validate()
+	return ValidateFractionalAmount(fb.Amount)
+}
+
+// ValidateFractionalAmount checks if an sdkmath.Int is a valid fractional
+// amount, ensuring it is positive and less than or equal to the maximum
+// fractional amount.
+func ValidateFractionalAmount(amt sdkmath.Int) error {
+	if amt.IsNil() {
+		return fmt.Errorf("nil amount")
+	}
+
+	if !amt.IsPositive() {
+		return fmt.Errorf("non-positive amount %v", amt)
+	}
+
+	if amt.GTE(conversionFactor) {
+		return fmt.Errorf("amount %v exceeds max of %v", amt, conversionFactor.SubRaw(1))
+	}
+
+	return nil
 }
