916ec6d30cc71d2023c2b89217719b2fbc2f59c3
sesheffield <sam@sheffinet.org>
2024-07-29 20:51:08 -0400

    test(insolvency): add tests for Kava lend insolvency check (#1981)
    * add additional tests that attempt to borrow funds from the insolvent market(should fail), and attempt to borrow funds from the not insolvent market (it will fail, but shouldn't). The not insolvent market should continue to processs borrows
    
    * remove unused code
    
    * make tests less specific for string contains
    
    * change the structure for new and old tests and add more verbosity for other tests
    
    * remove print
    
    * remove unneeded code
    
    * add paren
    
    * remove duplicate test case with invalid test name, and update to use error contains in places where it was validating if true for strings contains
    
    ---------
    
    Co-authored-by: Sam Sheffield <sam.sheffield@kavalabs.io>

--- x/hard/keeper/borrow.go
+++ x/hard/keeper/borrow.go
@@ -7,7 +7,6 @@ import (
 	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-
 	"github.com/kava-labs/kava/x/hard/types"
 )
 
