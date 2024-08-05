748b861c004440b7373127e75f90c875f80902a8
Nick DeLuca <nickdeluca08@gmail.com>
2024-08-05 16:55:18 +0000

    fix(lint): Fix lint caching
    This includes a couple fixes - 1) It adds support for full caching of go
    mod and go build, speeding up the lint process quite a bit.  And 2) does
    not mix lint cache with make clean files -- the docker container creates
    root owned files that cause make clean to error and we choose not to
    require make clean to run with higher permissions.  The cache must be
    deleted manually.

--- build/lint.mk
+++ build/lint.mk
@@ -5,8 +5,8 @@ ifndef DOCKER
 $(error DOCKER not set)
 endif
 
-ifndef BUILD_CACHE_DIR
-$(error BUILD_CACHE_DIR not set)
+ifndef BUILD_DIR
+$(error BUILD_DIR not set)
 endif
 
 ################################################################################
@@ -18,21 +18,28 @@ LINT_FROM_REV ?= $(shell git merge-base origin/master HEAD)
 GOLANGCI_VERSION ?= $(shell cat .golangci-version)
 GOLANGCI_IMAGE_TAG ?= golangci/golangci-lint:$(GOLANGCI_VERSION)
 
-GOLANGCI_CACHE_DIR ?= $(CURDIR)/$(BUILD_CACHE_DIR)/golangci-lint/$(GOLANGCI_VERSION)
+GOLANGCI_DIR ?= $(CURDIR)/$(BUILD_DIR)/.golangci-lint
+
+GOLANGCI_CACHE_DIR ?= $(GOLANGCI_DIR)/$(GOLANGCI_VERSION)-cache
+GOLANGCI_MOD_CACHE_DIR ?= $(GOLANGCI_DIR)/go-mod
 
 ################################################################################
 ###                             Lint Target                                  ###
 ################################################################################
 
 .PHONY: lint
-lint: $(GOLANGCI_CACHE_DIR)
+lint: $(GOLANGCI_CACHE_DIR) $(GOLANGCI_MOD_CACHE_DIR)
 	@echo "Running lint from rev $(LINT_FROM_REV), use LINT_FROM_REV var to override."
 	$(DOCKER) run -t --rm \
 		-v $(GOLANGCI_CACHE_DIR):/root/.cache \
+		-v $(GOLANGCI_MOD_CACHE_DIR):/go/pkg/mod \
 		-v $(CURDIR):/app \
 		-w /app \
 		$(GOLANGCI_IMAGE_TAG) \
 		golangci-lint run -v --new-from-rev $(LINT_FROM_REV)
 
 $(GOLANGCI_CACHE_DIR):
+	@mkdir -p $@
+
+$(GOLANGCI_MOD_CACHE_DIR):
 	@mkdir -p $@
