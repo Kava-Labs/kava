1555b8ea5488bd7358a8b5b6fdc3cca945c01e40
Nick DeLuca <nickdeluca08@gmail.com>
2024-08-09 15:35:40 +0000

    chore: Improve noop contract godoc
    Add a more meaningful godoc where the noop contract is constructed.

--- precompile/contracts/noop/contract.go
+++ precompile/contracts/noop/contract.go
@@ -7,6 +7,11 @@ import (
 )
 
 // NewContract returns a new noop stateful precompiled contract.
+//
+//	This contract is used for testing purposes only and should not be used on public chains.
+//	The functions of this contract (once implemented), will be used to exercise and test the various aspects of
+//	the EVM such as gas usage, argument parsing, events, etc. The specific operations tested under this contract are
+//	still to be determined.
 func NewContract() (contract.StatefulPrecompiledContract, error) {
 	precompile, err := contract.NewStatefulPrecompileContract([]*contract.StatefulPrecompileFunction{})
 
