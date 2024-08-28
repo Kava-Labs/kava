323ab11bb1db113293b633048dd8eba1eb4d035f
Nick DeLuca <nickdeluca08@gmail.com>
2024-08-28 20:00:04 +0000

    chore: Add wallet tests for whale and user
    This adds two exported addresses matching the hardhat account
    configuration for the hardhat and kvtool network.  In addition, we
    assert that both these addresses can sign and successfully send
    transactions which ensures the private keys and balances are correctly
    configured.

--- tests/e2e-evm/test/connect.test.ts
+++ tests/e2e-evm/test/connect.test.ts
@@ -1,7 +1,10 @@
 import hre from "hardhat";
+import { extendEnvironment } from "hardhat/config";
 import { expect } from "chai";
+import { Address, defineChain, Chain, PublicClientConfig, WalletClientConfig } from 'viem';
+import { whaleAddress, userAddress } from './addresses';
 
-describe("Viem", function () {
+describe("Viem Setup", function () {
   it("getChainID() matches the configured network", async function() {
     const publicClient = await hre.viem.getPublicClient();
 
@@ -14,7 +17,36 @@ describe("Viem", function () {
       }
     })();
 
-
+    expect(hre.network.config.chainId).to.eq(expectedChainId);
     expect(await publicClient.getChainId()).to.eq(expectedChainId);
+  });
+
+  it("is configured with whale and user accounts", async function() {
+    const walletClients = await hre.viem.getWalletClients();
+
+    expect(walletClients.length).to.equal(2);
+    expect(walletClients[0].account.address).to.equal(whaleAddress)
+    expect(walletClients[1].account.address).to.equal(userAddress)
+  });
+
+  [
+    {name: "whale", from: whaleAddress, to: userAddress, value: 1n},
+    {name: "user", from: userAddress, to: whaleAddress, value: 1n},
+  ].forEach((tc) => {
+    it(`${tc.name} account can sign transactions`, async function() {
+      const publicClient = await hre.viem.getPublicClient();
+      const walletClient = await hre.viem.getWalletClient(tc.from);
+
+      const txHash = await walletClient.sendTransaction({
+        to: tc.to,
+        value: tc.value,
+      });
+      const txReceipt = await publicClient.waitForTransactionReceipt({ hash: txHash });
+      const tx = await publicClient.getTransaction({ hash: txHash });
+
+      expect(txReceipt.status).to.eq('success');
+      expect(txReceipt.gasUsed).to.eq(21000n);
+      expect(tx.value).to.eq(tc.value)
+    });
   });
 });
