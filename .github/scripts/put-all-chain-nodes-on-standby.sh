b78137189dfe2416326a92ee1c038422fc5b2541
Sam Sheffield <sam.sheffield@kavalabs.io>
2024-09-12 13:17:06 -0400

    chore: updateto use new infra

--- .github/scripts/put-all-chain-nodes-on-standby.sh
+++ .github/scripts/put-all-chain-nodes-on-standby.sh
@@ -4,7 +4,7 @@ set -x
 # get all the node's ec2 instance ids for the specified chain id
 # TODO: fix this and the sharded cluster module to add tags
 #chain_node_instance_ids=$(aws ec2 describe-instances --filters "Name=tag:KavaChainId,Values=$CHAIN_ID" | jq -r '[.Reservations | .[] | .Instances | .[] | .InstanceId] | join(" ")')
-chain_node_instance_ids='i-01f39f5bedd1f9182'
+chain_node_instance_ids='i-0823115fc9273dc8c'
 
 for chain_node_instance_id in ${chain_node_instance_ids}
 do
