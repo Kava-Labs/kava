5a486f4a0f40d7e47d85bffd3ebd9ca139bc4f38
drklee3 <derrick@dlee.dev>
2024-10-15 14:15:21 -0700

    test: Add storage function to callcode test contract

--- tests/e2e-evm/contracts/ABI_CallerTest.sol
+++ tests/e2e-evm/contracts/ABI_CallerTest.sol
@@ -2,7 +2,13 @@
 pragma solidity ^0.8.24;
 
 contract TargetContract {
+    uint256 public storageValue;
+
     function getMsgInfo() external payable returns (address, uint256) {
         return (msg.sender, msg.value);
+    }
+
+    function setStorageValue(uint256 value) external {
+        storageValue = value;
     }
 }
