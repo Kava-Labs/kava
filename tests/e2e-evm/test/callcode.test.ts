49fb512d2a91b9ca67fb55cd929bf19d7f8ac1ea
drklee3 <derrick@dlee.dev>
2024-10-15 12:27:54 -0700

    refactor: Move tx params to test cases list
    Build the whole tx params in the test cases themselves to be more
    explicit and more flexible.

--- tests/e2e-evm/test/callcode.test.ts
+++ tests/e2e-evm/test/callcode.test.ts
@@ -2,7 +2,7 @@ import hre from "hardhat";
 import type { ArtifactsMap } from "hardhat/types/artifacts";
 import type { PublicClient, WalletClient, GetContractReturnType } from "@nomicfoundation/hardhat-viem/types";
 import { expect } from "chai";
-import { Address, Hex, decodeFunctionResult, encodeFunctionData, getAddress, isAddressEqual } from "viem";
+import { Address, CallParameters, Chain, decodeFunctionResult, encodeFunctionData, getAddress } from "viem";
 import { whaleAddress } from "./addresses";
 
 const defaultGas = 25000n;
@@ -21,105 +21,112 @@ describe("CallCode", () => {
     lowLevelCaller = await hre.viem.deployContract("Caller");
   });
 
+  interface CallCodeContext {
+    lowLevelCaller: GetContractReturnType<ArtifactsMap["Caller"]["abi"]>;
+    implementationContract: GetContractReturnType<ArtifactsMap["TargetContract"]["abi"]>;
+  }
+
   interface callCodeTestCase {
     name: string;
-    fromAddress: Address;
-    value: bigint;
-    gas: bigint;
-    mutateTxData?: (calledContractAddr: Address, data: Hex) => { to: Address; data: Hex };
-
-    wantSender: () => Address;
+    txParams: (ctx: CallCodeContext) => CallParameters<Chain>;
+    wantSender: (ctx: CallCodeContext, signer: Address) => Address;
     wantValue: bigint;
   }
 
   describe("msg context", () => {
-    let calledContract: GetContractReturnType<ArtifactsMap["TargetContract"]["abi"]>;
+    let ctx: CallCodeContext;
 
     before("deploy called contract", async function () {
       const contract = await hre.viem.deployContract("TargetContract");
-
-      calledContract = contract;
+      ctx = {
+        lowLevelCaller: lowLevelCaller,
+        implementationContract: contract,
+      };
     });
 
     const testCases: callCodeTestCase[] = [
       {
         name: "direct call",
-        fromAddress: whaleAddress,
-        value: 0n,
-        gas: defaultGas,
-        wantSender: () => whaleAddress,
+        txParams: (ctx) => ({
+          to: ctx.implementationContract.address,
+          value: 0n,
+          data: encodeFunctionData({
+            abi: ctx.implementationContract.abi,
+            functionName: "getMsgInfo",
+            args: [],
+          }),
+          gas: defaultGas,
+        }),
+        wantSender: (_, signer) => signer,
         wantValue: 0n,
       },
       {
         name: "direct call with value",
-        fromAddress: whaleAddress,
-        value: 100n,
-        gas: defaultGas,
-        wantSender: () => whaleAddress,
+        txParams: (ctx) => ({
+          to: ctx.implementationContract.address,
+          value: 100n,
+          gas: defaultGas,
+          data: encodeFunctionData({
+            abi: ctx.implementationContract.abi,
+            functionName: "getMsgInfo",
+            args: [],
+          }),
+        }),
+        wantSender: (_, signer) => signer,
         wantValue: 100n,
       },
       {
         name: "callcode",
-        fromAddress: whaleAddress,
-        value: 0n,
-        gas: contractCallerGas,
-        mutateTxData: (contractAddr, data) => ({
-          to: lowLevelCaller.address,
+        txParams: (ctx) => ({
+          to: ctx.lowLevelCaller.address,
+          value: 0n,
+          gas: contractCallerGas,
           data: encodeFunctionData({
             abi: lowLevelCaller.abi,
             functionName: "functionCallCode",
-            args: [contractAddr, data],
+            args: [
+              ctx.implementationContract.address,
+              encodeFunctionData({
+                abi: ctx.implementationContract.abi,
+                functionName: "getMsgInfo",
+                args: [],
+              }),
+            ],
           }),
         }),
         // msg.sender is the caller contract, not the whale
-        wantSender: () => lowLevelCaller.address,
+        wantSender: (ctx) => ctx.lowLevelCaller.address,
         wantValue: 0n,
       },
       {
         name: "callcode with value",
-        fromAddress: whaleAddress,
-        value: 100n,
-        gas: contractCallerGas,
-        mutateTxData: (contractAddr, data) => ({
-          to: lowLevelCaller.address,
+        txParams: (ctx) => ({
+          to: ctx.lowLevelCaller.address,
+          value: 100n,
+          gas: contractCallerGas,
           data: encodeFunctionData({
             abi: lowLevelCaller.abi,
             functionName: "functionCallCode",
-            args: [contractAddr, data],
+            args: [
+              ctx.implementationContract.address,
+              encodeFunctionData({
+                abi: ctx.implementationContract.abi,
+                functionName: "getMsgInfo",
+                args: [],
+              }),
+            ],
           }),
         }),
         // msg.sender is the caller contract, and value is NOT passed
-        wantSender: () => lowLevelCaller.address,
+        wantSender: (ctx) => ctx.lowLevelCaller.address,
         wantValue: 0n,
       },
     ];
 
     for (const tc of testCases) {
       it(tc.name, async function () {
-        let toAddr = calledContract.address;
-
-        let callData = encodeFunctionData({
-          abi: calledContract.abi,
-          functionName: "getMsgInfo",
-          args: [],
-        });
-
-        // Modify the call data if needed, wraps the call in a callcode
-        if (tc.mutateTxData) {
-          const { to, data } = tc.mutateTxData(calledContract.address, callData);
-          callData = data;
-          toAddr = to;
-        }
-
-        const txData = {
-          // Transaction sender
-          account: tc.fromAddress,
-          // Target contract
-          to: toAddr,
-          data: callData,
-          gas: tc.gas,
-          value: tc.value,
-        };
+        const txData = tc.txParams(ctx);
+        txData.account = whaleAddress;
 
         const res = await publicClient.call(txData);
         if (!res.data) {
@@ -127,95 +134,27 @@ describe("CallCode", () => {
           expect.fail("no data returned");
         }
 
+        // Expect all test cases call the getMsgInfo function
         const [returnAddress, returnValue] = decodeFunctionResult({
-          abi: calledContract.abi,
+          abi: ctx.implementationContract.abi,
           functionName: "getMsgInfo",
           data: res.data,
         });
 
+        const expectedSender = tc.wantSender(ctx, whaleAddress);
+
         // getAddress to ensure both are checksum encoded
-        expect(getAddress(returnAddress)).to.equal(getAddress(tc.wantSender()));
+        expect(getAddress(returnAddress)).to.equal(getAddress(expectedSender));
         expect(returnValue).to.equal(tc.wantValue);
 
         const txHash = await walletClient.sendTransaction(txData);
         const txReceipt = await publicClient.waitForTransactionReceipt({ hash: txHash });
         expect(txReceipt.status).to.equal("success");
-        expect(txReceipt.gasUsed < txData.gas, "gas to not be exhausted").to.be.true;
+
+        if (txData.gas) {
+          expect(txReceipt.gasUsed < txData.gas, "gas to not be exhausted").to.be.true;
+        }
       });
     }
-
-    it("should be callable directly", async function () {
-      const callData = encodeFunctionData({
-        abi: calledContract.abi,
-        functionName: "getMsgInfo",
-        args: [],
-      });
-
-      const txData = {
-        account: whaleAddress,
-        to: calledContract.address,
-        data: callData,
-        gas: defaultGas,
-      };
-
-      const res = await publicClient.call(txData);
-      if (!res.data) {
-        // Fail this way as a type guard to ensure res.data is not undefined
-        expect.fail("no data returned");
-      }
-
-      const [returnAddress, returnValue] = decodeFunctionResult({
-        abi: calledContract.abi,
-        functionName: "getMsgInfo",
-        data: res.data,
-      });
-
-      expect(isAddressEqual(returnAddress, whaleAddress)).to.be.true;
-      expect(returnValue).to.equal(0n);
-
-      const txHash = await walletClient.sendTransaction(txData);
-      const txReceipt = await publicClient.waitForTransactionReceipt({ hash: txHash });
-      expect(txReceipt.status).to.equal("success");
-      expect(txReceipt.gasUsed < txData.gas, "gas to not be exhausted").to.be.true;
-    });
-
-    it("should be callable via callcode", async function () {
-      const callData = encodeFunctionData({
-        abi: calledContract.abi,
-        functionName: "getMsgInfo",
-        args: [],
-      });
-
-      const callerData: Hex = encodeFunctionData({
-        abi: lowLevelCaller.abi,
-        functionName: "functionCallCode",
-        args: [calledContract.address, callData],
-      });
-
-      const txData = {
-        to: lowLevelCaller.address,
-        data: callerData,
-        gas: contractCallerGas + 50_000n,
-      };
-
-      const res = await publicClient.call(txData);
-      if (!res.data) {
-        expect.fail("no data returned");
-      }
-
-      const [returnAddress, returnValue] = decodeFunctionResult({
-        abi: calledContract.abi,
-        functionName: "getMsgInfo",
-        data: res.data,
-      });
-
-      expect(isAddressEqual(returnAddress, lowLevelCaller.address)).to.be.true;
-      expect(returnValue).to.equal(0n);
-
-      const txHash = await walletClient.sendTransaction(txData);
-      const txReceipt = await publicClient.waitForTransactionReceipt({ hash: txHash });
-      expect(txReceipt.status).to.equal("success");
-      expect(txReceipt.gasUsed < txData.gas, "gas to not be exhausted").to.be.true;
-    });
   });
 });
