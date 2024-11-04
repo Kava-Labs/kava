4af6545b6a6e9820405773b4f481cd4bea837d78
Robert Pirtle <Astropirtle@gmail.com>
2024-11-04 12:20:17 -0800

    chore: fix misc lints from v0.27 branch (#2049)
    * chore: fix misc lints from v0.27 branch
    
    * ci: update mergify backport rule labels

--- tests/util/addresses.go
+++ tests/util/addresses.go
@@ -7,6 +7,10 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+const (
+	MnemonicEntropyBits = 128
+)
+
 func SdkToEvmAddress(addr sdk.AccAddress) common.Address {
 	return common.BytesToAddress(addr.Bytes())
 }
@@ -17,9 +21,13 @@ func EvmToSdkAddress(addr common.Address) sdk.AccAddress {
 
 // RandomMnemonic generates a random BIP39 mnemonic from 128 bits of entropy
 func RandomMnemonic() (string, error) {
-	entropy, err := bip39.NewEntropy(128)
+	entropy, err := bip39.NewEntropy(MnemonicEntropyBits)
 	if err != nil {
 		return "", errorsmod.Wrap(err, "failed to generate entropy for new mnemonic")
 	}
-	return bip39.NewMnemonic(entropy)
+	mnemonic, err := bip39.NewMnemonic(entropy)
+	if err != nil {
+		return mnemonic, errorsmod.Wrap(err, "failed to create mnemonic")
+	}
+	return mnemonic, nil
 }
